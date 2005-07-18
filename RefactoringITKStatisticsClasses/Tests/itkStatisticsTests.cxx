af5726ddf049cc3e22f7084c435507b9641e6b7f
karthik <karthik@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2005-07-18 04:00:50 +0000

    ENH: Add ImageToListAdaptorTest

--- RefactoringITKStatisticsClasses/Tests/itkStatisticsTests.cxx
+++ RefactoringITKStatisticsClasses/Tests/itkStatisticsTests.cxx
@@ -33,7 +33,7 @@ void RegisterTests()
 //  REGISTER_TEST(itkGoodnessOfFitMixtureModelCostFunctionTest);
 //  REGISTER_TEST(itkGreyLevelCooccurrenceMatrixTextureCoefficientsCalculatorTest);
   REGISTER_TEST(itkHistogramTest);
-//  REGISTER_TEST(itkImageToListAdaptorTest);
+  REGISTER_TEST(itkImageToListAdaptorTest);
 //  REGISTER_TEST(itkImageToCooccurrenceListAdaptorTest);
 //  REGISTER_TEST(itkImageToHistogramGeneratorTest);
   REGISTER_TEST(itkKdTreeBasedKmeansEstimatorTest);
