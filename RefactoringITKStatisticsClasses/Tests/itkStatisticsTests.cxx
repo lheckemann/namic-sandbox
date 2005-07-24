f0c320f21e5da979f74ebc43a8621ea0a6a4572c
karthik <karthik@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2005-07-24 22:55:45 +0000

    ENH: After KdTreeBasedKMeansEstimatorTest works

--- RefactoringITKStatisticsClasses/Tests/itkStatisticsTests.cxx
+++ RefactoringITKStatisticsClasses/Tests/itkStatisticsTests.cxx
@@ -36,7 +36,7 @@ void RegisterTests()
   REGISTER_TEST(itkImageToListAdaptorTest);
   REGISTER_TEST(itkImageToCooccurrenceListAdaptorTest);
 //  REGISTER_TEST(itkImageToHistogramGeneratorTest);
-//  REGISTER_TEST(itkKdTreeBasedKmeansEstimatorTest);
+  REGISTER_TEST(itkKdTreeBasedKmeansEstimatorTest);
   REGISTER_TEST(itkKdTreeGeneratorTest);
   REGISTER_TEST(itkListSampleTest);
 //  REGISTER_TEST(itkListSampleToHistogramFilterTest);
