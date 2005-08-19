87e32d9908475b922d475a8edf9754784f1ca7cf
karthik <karthik@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2005-08-19 20:08:08 +0000

    ENH: Classes for spectral clustering

---
+++ SpectralClustering/itkAffinityClusteringTest.cxx
@@ -0,0 +1,22 @@
+#include "itkAffinityClustering.h"
+
+int main()
+{
+  typedef itk::AffinityClustering AffinityClusteringFilterType;
+  AffinityClusteringFilterType::Pointer filter = AffinityClusteringFilterType::New();
+
+  typedef AffinityClusteringFilterType::AffinityMatrixType AffinityMatrixType;
+  AffinityMatrixType * affinityMatrix = new AffinityMatrixType();
+  affinityMatrix->SetSize( 4, 4 );
+  affinityMatrix->Fill( 12.1 );
+  
+  filter->SetInput( affinityMatrix );
+  
+  filter->Print( std::cout );
+  std::cout << *(filter->GetInput()->Get()) << std::endl;
+  std::cout << filter->GetOutputClusters() << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+  
