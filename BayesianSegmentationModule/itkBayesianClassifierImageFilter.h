aea1dfa3e1fb65aeac8939d35f8c1d6e50c65dd5
melonakos <melonakos@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2005-09-06 19:54:49 +0000

    ENH:Added itkBayesianClassifierImageFilter.h and itkBayesianClassifierImageFilter.txx

---
+++ BayesianSegmentationModule/itkBayesianClassifierImageFilter.h
@@ -0,0 +1,111 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBayesianClassifierImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2005/03/14 21:47:51 $
+  Version:   $Revision: 1.4 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBayesianClassifierImageFilter_h
+#define __itkBayesianClassifierImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkMaximumRatioDecisionRule.h"
+#include "itkGaussianDensityFunction.h"
+
+namespace itk
+{
+  
+/** \class BayesianClassifierImageFilter
+ *
+ * \brief This filter will perform Bayesian Classification on an image.
+ *
+ * [add detailed documentation]
+ * 
+ * This filter is templated over the input image type
+ * and the output image type.
+ * 
+ * The filter expect both images to have the same number of dimensions.
+ *
+ * \author John Melonakos, Georgia Tech
+ *
+ * \ingroup IntensityImageFilters  Multithreaded
+ */
+
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT BayesianClassifierImageFilter :
+    public
+ImageToImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef BayesianClassifierImageFilter  Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage
+  >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(BayesianClassifierImageFilter, ImageToImageFilter);
+
+  /** Pixel types. */
+  typedef typename TInputImage::PixelType  InputPixelType;
+  typedef typename TOutputImage::PixelType OutputPixelType;
+  
+  /** Input and Output image types */
+  typedef typename Superclass::InputImageType   InputImageType;
+  typedef typename Superclass::OutputImageType  OutputImageType;
+
+  /** Type of the Measurement */
+  typedef InputPixelType   MeasurementVectorType;
+
+  /** Type of the Gaussian density functions */
+  typedef Statistics::GaussianDensityFunction< MeasurementVectorType > 
+                                                              MembershipFunctionType;
+  typedef typename MembershipFunctionType::ConstPointer       MembershipFunctionConstPointer;
+
+  /** Membership function container */
+  typedef std::vector< MembershipFunctionConstPointer >       MembershipFunctionContainer;
+
+  /** Decision rule to use for defining the label */
+  typedef MaximumRatioDecisionRule                            DecisionRuleType;
+
+  
+  /** Add a membership function to the filter. This is expected to be a Gaussian */
+  void AddMembershipFunction( const MembershipFunctionType * newFunction );
+
+
+protected:
+  BayesianClassifierImageFilter();
+  virtual ~BayesianClassifierImageFilter() {}
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** Here is where the classification is computed.*/
+  virtual void GenerateData();
+
+private:
+  BayesianClassifierImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  MembershipFunctionContainer     m_MembershipFunctions;  
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkBayesianClassifierImageFilter.txx"
+#endif
+
+#endif
---
+++ BayesianSegmentationModule/itkBayesianClassifierImageFilter.txx
@@ -0,0 +1,106 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkBayesianClassifierImageFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2005/03/08 20:29:20 $
+  Version:   $Revision: 1.2 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkBayesianClassifierImageFilter_txx
+#define _itkBayesianClassifierImageFilter_txx
+
+#include "itkBayesianClassifierImageFilter.h"
+#include "itkImageRegionConstIterator.h"
+
+namespace itk
+{
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+BayesianClassifierImageFilter<TInputImage, TOutputImage>
+::BayesianClassifierImageFilter()
+{
+
+}
+
+
+/**
+ * Add a membership function
+ */
+template <class TInputImage, class TOutputImage>
+void
+BayesianClassifierImageFilter<TInputImage, TOutputImage>
+::AddMembershipFunction( const MembershipFunctionType * newFunction )
+{
+   MembershipFunctionConstPointer functionSmartPointer = newFunction;
+   m_MembershipFunctions.push_back( functionSmartPointer );
+}
+
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+void 
+BayesianClassifierImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "Number of membership functions = " << m_MembershipFunctions.size() << std::endl;
+  for( unsigned int i=0; i< m_MembershipFunctions.size(); i++)
+    {
+    os << indent << m_MembershipFunctions[i].GetPointer() << std::endl;
+    }
+
+}
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+void 
+BayesianClassifierImageFilter<TInputImage, TOutputImage>
+::GenerateData()
+{
+  DecisionRuleType::Pointer decisionRule = DecisionRuleType::New();  
+
+  if( m_MembershipFunctions.size() == 0 )
+    {
+    itkExceptionMacro("No membership functions have been set up. Please call AddMembershipFunction() first");
+    return;
+    }
+
+  typedef ImageRegionConstIterator< InputImageType >  ItertatorType;
+
+  ItertatorType it( this->GetInput(), 
+                    this->GetInput()->GetRequestedRegion() );
+
+  it.GoToBegin();
+
+  while( !it.IsAtEnd() )
+    {
+    it.Get();
+    ++it;
+    }
+
+
+}
+
+
+} // end namespace itk
+
+#endif
