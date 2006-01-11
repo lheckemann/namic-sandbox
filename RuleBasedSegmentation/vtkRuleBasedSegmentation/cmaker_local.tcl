02e98247c10e10737f251d641dd950a917bb9727
karthik <karthik@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-01-11 22:46:25 +0000

    ENH: To link against vtkITK

---
+++ RuleBasedSegmentation/vtkRuleBasedSegmentation/cmaker_local.tcl
@@ -0,0 +1,18 @@
+# Initialize values
+
+set SLICER_MODULE_ARG "-DVTKITK_SOURCE_DIR:PATH=$SLICER_HOME/Modules/vtkITK"
+lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_DIR:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)"
+
+if {[file exists $SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/libvtkITK.so] == 1} {
+    lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/libvtkITK.so"
+    lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/libvtkITKTCL.so" 
+} else {
+    if {$tcl_platform(os) == "Darwin"} {
+        lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/vtkITK.dylib"
+        lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/vtkITKTCL.dylib"  
+    } else {
+        lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/debug/vtkITK.lib"
+        lappend SLICER_MODULE_ARG "-DVTKITK_BUILD_TCL_LIB:PATH=$SLICER_HOME/Modules/vtkITK/builds/$env(BUILD)/bin/debug/vtkITKTCL.lib" 
+    }
+}
+
