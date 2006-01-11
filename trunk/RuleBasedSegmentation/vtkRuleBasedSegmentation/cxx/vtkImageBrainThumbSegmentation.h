9ff0b39b56094fcad51b97127a039e64bd40d176
melonakos <melonakos@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-01-11 22:37:00 +0000

    ENH:Added README for ThumbExtractor

--- trunk/RuleBasedSegmentation/vtkRuleBasedSegmentation/cxx/vtkImageBrainThumbSegmentation.h
+++ trunk/RuleBasedSegmentation/vtkRuleBasedSegmentation/cxx/vtkImageBrainThumbSegmentation.h
@@ -31,89 +31,39 @@
 
 class VTK_SLICER_BASE_EXPORT vtkImageBrainThumbSegmentation : public vtkImageToImageFilter
 {
-  public:
+public:
   static vtkImageBrainThumbSegmentation *New();
   vtkTypeMacro(vtkImageBrainThumbSegmentation,vtkImageToImageFilter);
   void PrintSelf(ostream& os, vtkIndent indent);
   
   // Seed points and input data do not need anything here???
 
-  // what is this?
-  vtkGetObjectMacro(Parent,vtkIntArray);
-
-  // TODO: add Set/Get Macro for SeedPointList
-
-  // is this the input data??
-  void init(vtkImageData *inData);
-  
-
-  // ALGORITHM
-  
-  void InitSingleSource(int startv);
-
-  // Calculate shortest path from vertex startv to vertex endv
-  void RunBrainThumbSegmentation(vtkDataArray *scalars,int source, int sink);
-
-  // edge cost
-  float EdgeCost(vtkDataArray *scalars, int u, int v);
-  void BuildShortestPath(int start,int end);
-  
-  // VARIABLES
-  
-  // The vertex at the start of the shortest path
-  int SourceID;
-  
-  // The vertex at the end of the shortest path
-  int SinkID;  
-  
-  // boundary scalars
-  vtkDataArray *BoundaryScalars;
-
-  // the number of points if the graph
-  int NumberOfInputPoints;
-
-  int UseInverseDistance;
-  int UseInverseSquaredDistance;
-  int UseInverseExponentialDistance;
-  int UseSquaredDistance;
-
-  // list of all the graph nodes
-  //vtkIntArray *GraphNodes;
-  // Adjacency representation
-  //vtkIdList **Neighbors;
-
-  // the pq
-  vtkPriorityQueue *PQ;
-
-  // The vertex ids on the shortest path
-  vtkIdList *ShortestPathIdList;
-  // ShortestPathWeight current summed weight for path to vertex v
-  //vtkFloatArray *CumulativeWeightFromSource;
-  // pre(v) predecessor of v
-  vtkIntArray *Parent;
-  // Visited is the set of vertices with allready determined shortest path
-  // Visited(v) == 1 means that vertex v is in s
-  vtkIntArray *Visited;  
-  // f is the set of vertices wich has not a shortest path yet but has a path
-  // ie. the front set (f(v) == 1 means that vertex v is in f)
-  //vtkIntArray *f;
- 
-  int PathPointer;
-  //vtkImageData* CachedImage;
-  int StopWhenEndReached;
+  vtkSetObjectMacro(SeedPointList,vtkIntArray);
+  vtkGetObjectMacro(SeedPointList,vtkIntArray);
 
 protected:
- vtkImageBrainThumbSegmentation();
- ~vtkImageBrainThumbSegmentation();
- vtkImageBrainThumbSegmentation(const vtkImageBrainThumbSegmentation&) {};
- void operator=(const vtkImageBrainThumbSegmentation&) {};
+  // This contains the seed points
+  vtkIntArray *SeedPointList;
+
+  //
+  vtkImageData *inDataSlice;
+
+  //
+  vtkImageData *thumbMaskTemp;
+
+  void InitSlice( vtkImageData* slice , unsigned int value );
+  void FindNeighbors(vtkIdList *list,int id, vtkDataArray *scalars);
+
+  vtkImageBrainThumbSegmentation();
+  ~vtkImageBrainThumbSegmentation();
+  vtkImageBrainThumbSegmentation(const vtkImageBrainThumbSegmentation&) {};
+  void operator=(const vtkImageBrainThumbSegmentation&) {};
  
- //void Execute(vtkImageData *inData, vtkImageData *outData);
- void ExecuteData(vtkDataObject *);
- void Execute() { this->vtkImageToImageFilter::Execute(); };
- void Execute(vtkImageData *outData) { this->vtkImageToImageFilter::Execute(outData); };
-  
-  
+  //void Execute(vtkImageData *inData, vtkImageData *outData);
+  void ExecuteData(vtkDataObject *);
+  void Execute() { this->vtkImageToImageFilter::Execute(); };
+  void Execute(vtkImageData *outData) { this->vtkImageToImageFilter::Execute(outData); };
+
 };
 
 #endif
