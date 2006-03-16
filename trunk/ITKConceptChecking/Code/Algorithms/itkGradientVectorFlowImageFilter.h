6bbb811c9499ebb206b4479580a20fde3aaa1606
amy <amy@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-03-16 19:37:21 +0000

    ENH: concept checking for NumericTraits

--- trunk/ITKConceptChecking/Code/Algorithms/itkGradientVectorFlowImageFilter.h
+++ trunk/ITKConceptChecking/Code/Algorithms/itkGradientVectorFlowImageFilter.h
@@ -89,6 +89,10 @@ public:
   /** Begin concept checking */
   itkConceptMacro(SameDimensionCheck,
     (Concept::SameDimension<ImageDimension, OutputImageDimension>));
+  itkConceptMacro(InputHasNumericTraitsCheck,
+    (Concept::HasNumericTraits<typename PixelType::ValueType>));
+  itkConceptMacro(OutputHasNumericTraitsCheck,
+    (Concept::HasNumericTraits<typename TOutputImage::PixelType::ValueType>));
   /** End concept checking */
 #endif
 
