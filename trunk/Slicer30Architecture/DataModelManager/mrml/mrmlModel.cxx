1f7ce546d9494f1407ebbd829cc14cf79580a680
malaterre <malaterre@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-03-17 20:49:20 +0000

    ENH: move implementation into vtkmrml ns

--- trunk/Slicer30Architecture/DataModelManager/mrml/mrmlModel.cxx
+++ trunk/Slicer30Architecture/DataModelManager/mrml/mrmlModel.cxx
@@ -13,33 +13,15 @@ Version:   $Revision: 1.12 $
 =========================================================================auto=*/
 #include "mrmlModel.h"
 
-#include "vtkPolyData.h"
-
 namespace mrml
 {
-class ModelInternals
-{
-public:
-  ModelInternals()
-    {
-    this->PolyData = vtkPolyData::New();
-    }
-  ~ModelInternals()
-    {
-    this->PolyData->Delete();
-    this->PolyData = NULL;
-    }
-  vtkPolyData *PolyData;
-};
 
 Model::Model()
 {
-  this->Internal = new ModelInternals;
 }
 
 Model::~Model()
 {
-  delete this->Internal;
 }
 
 } // end namespace mrml
