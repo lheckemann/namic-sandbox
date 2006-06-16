5f58366f4dae55535d656c28ea2d3660026fb739
melonakos <melonakos@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-06-16 01:13:19 +0000

    ENH:Minor changes to code.  Addition of insight-journal-paper

--- ConformalFlattening/src/Db/Db.cxx
+++ ConformalFlattening/src/Db/Db.cxx
@@ -87,9 +87,9 @@ MeshType::Pointer vtkPolyDataToITKMesh(vtkPolyData *polyData)
   for(unsigned int p =0; p < numberOfPoints; p++)
     {
 
-      vtkFloatingPointType * apoint = vtkpoints->GetPoint( p );
+    vtkFloatingPointType * apoint = vtkpoints->GetPoint( p );
     
-      mesh->SetPoint( p, MeshType::PointType( apoint ));
+    mesh->SetPoint( p, MeshType::PointType( apoint ));
     
     }
 
@@ -112,7 +112,7 @@ MeshType::Pointer vtkPolyDataToITKMesh(vtkPolyData *polyData)
 
   while( triangleStrips->GetNextCell( numberOfCellPoints, cellPoints ) )
     {
-      numberOfTriangles += numberOfCellPoints-2;
+    numberOfTriangles += numberOfCellPoints-2;
     }
 
 
@@ -122,10 +122,10 @@ MeshType::Pointer vtkPolyDataToITKMesh(vtkPolyData *polyData)
 
   while( polygons->GetNextCell( numberOfCellPoints, cellPoints ) )
     {
-      if( numberOfCellPoints == 3 )
-        {
-          numberOfTriangles ++;
-        }
+    if( numberOfCellPoints == 3 )
+      {
+      numberOfTriangles ++;
+      }
     }
 
 
@@ -151,25 +151,25 @@ MeshType::Pointer vtkPolyDataToITKMesh(vtkPolyData *polyData)
   while( triangleStrips->GetNextCell( numberOfCellPoints, cellPoints ) )
     {
     
-      unsigned int numberOfTrianglesInStrip = numberOfCellPoints - 2;
+    unsigned int numberOfTrianglesInStrip = numberOfCellPoints - 2;
 
-      unsigned long pointIds[3];
-      pointIds[0] = cellPoints[0];
-      pointIds[1] = cellPoints[1];
-      pointIds[2] = cellPoints[2];
+    unsigned long pointIds[3];
+    pointIds[0] = cellPoints[0];
+    pointIds[1] = cellPoints[1];
+    pointIds[2] = cellPoints[2];
 
-      for( unsigned int t=0; t < numberOfTrianglesInStrip; t++ )
-        {
-          MeshType::CellAutoPointer c;
-          TriangleCellType * tcell = new TriangleCellType;
-          tcell->SetPointIds( pointIds );
-          c.TakeOwnership( tcell );
-          mesh->SetCell( cellId, c );
-          cellId++;
-          pointIds[0] = pointIds[1];
-          pointIds[1] = pointIds[2];
-          pointIds[2] = cellPoints[t+3];
-        }
+    for( unsigned int t=0; t < numberOfTrianglesInStrip; t++ )
+      {
+      MeshType::CellAutoPointer c;
+      TriangleCellType * tcell = new TriangleCellType;
+      tcell->SetPointIds( pointIds );
+      c.TakeOwnership( tcell );
+      mesh->SetCell( cellId, c );
+      cellId++;
+      pointIds[0] = pointIds[1];
+      pointIds[1] = pointIds[2];
+      pointIds[2] = cellPoints[t+3];
+      }
     }
 
 
@@ -177,16 +177,16 @@ MeshType::Pointer vtkPolyDataToITKMesh(vtkPolyData *polyData)
   polygons->InitTraversal();
   while( polygons->GetNextCell( numberOfCellPoints, cellPoints ) )
     {
-      if( numberOfCellPoints !=3 ) // skip any non-triangle.
-        {
-          continue;
-        }
-      MeshType::CellAutoPointer c;
-      TriangleCellType * t = new TriangleCellType;
-      t->SetPointIds( (unsigned long*)cellPoints );
-      c.TakeOwnership( t );
-      mesh->SetCell( cellId, c );
-      cellId++;
+    if( numberOfCellPoints !=3 ) // skip any non-triangle.
+      {
+      continue;
+      }
+    MeshType::CellAutoPointer c;
+    TriangleCellType * t = new TriangleCellType;
+    t->SetPointIds( (unsigned long*)cellPoints );
+    c.TakeOwnership( t );
+    mesh->SetCell( cellId, c );
+    cellId++;
     }
 
   return mesh;
@@ -208,12 +208,12 @@ vtkPolyData* ITKMeshToVtkPolyData(MeshType::Pointer mesh)
 
   for (int i = 0; pntIterator != pntItEnd; ++i, ++pntIterator)
     {
-      ItkPoint pnt = pntIterator.Value();
-      points->InsertPoint(i, pnt[0], pnt[1], pnt[2]);
-      //       std::cout<<i<<"-th point:  ";
-      //       std::cout<<pnt[0]<<std::endl;
-      //       std::cout<<"               "<<pntIterator.Value()<<std::endl;
-      //      ++pntIterator;
+    ItkPoint pnt = pntIterator.Value();
+    points->InsertPoint(i, pnt[0], pnt[1], pnt[2]);
+    //       std::cout<<i<<"-th point:  ";
+    //       std::cout<<pnt[0]<<std::endl;
+    //       std::cout<<"               "<<pntIterator.Value()<<std::endl;
+    //      ++pntIterator;
     }
   newPolyData->SetPoints(points);
   points->Delete();
@@ -228,21 +228,21 @@ vtkPolyData* ITKMeshToVtkPolyData(MeshType::Pointer mesh)
 
   for (int it = 0; cellIt != cellItEnd; ++it, ++cellIt)
     {
-      CellType * cellptr = cellIt.Value();
-      //    LineType * line = dynamic_cast<LineType *>( cellptr );
-      //    std::cout << line->GetNumberOfPoints() << std::endl;
-      //      std::cout << cellptr->GetNumberOfPoints() << std::endl;
+    CellType * cellptr = cellIt.Value();
+    //    LineType * line = dynamic_cast<LineType *>( cellptr );
+    //    std::cout << line->GetNumberOfPoints() << std::endl;
+    //      std::cout << cellptr->GetNumberOfPoints() << std::endl;
 
-      PointIdIterator pntIdIter = cellptr->PointIdsBegin();
-      PointIdIterator pntIdEnd = cellptr->PointIdsEnd();
-      vtkIdList* pts = vtkIdList::New();
+    PointIdIterator pntIdIter = cellptr->PointIdsBegin();
+    PointIdIterator pntIdEnd = cellptr->PointIdsEnd();
+    vtkIdList* pts = vtkIdList::New();
 
-      for (int it1 = 0; pntIdIter != pntIdEnd; ++it1, ++pntIdIter)
-        {
-          pts->InsertNextId( *pntIdIter );
-          //          std::cout<<"           "<<tempCell[it1]<<std::endl;
-        }
-      triangle->InsertNextCell(pts);
+    for (int it1 = 0; pntIdIter != pntIdEnd; ++it1, ++pntIdIter)
+      {
+      pts->InsertNextId( *pntIdIter );
+      //          std::cout<<"           "<<tempCell[it1]<<std::endl;
+      }
+    triangle->InsertNextCell(pts);
     }
   newPolyData->SetPolys(triangle);
   triangle->Delete();
@@ -283,9 +283,9 @@ void Display(vtkPolyData* polyData)
 
 
 void getDb(MeshType::Pointer mesh, 
-                vnl_sparse_matrix<vtkFloatingPointType> &D,
-                vnl_vector<vtkFloatingPointType> &bR,
-                vnl_vector<vtkFloatingPointType> &bI) {
+           vnl_sparse_matrix<vtkFloatingPointType> &D,
+           vnl_vector<vtkFloatingPointType> &bR,
+           vnl_vector<vtkFloatingPointType> &bI) {
 
   int numOfPoints = mesh->GetNumberOfPoints();
   int numOfCells = mesh->GetNumberOfCells();
@@ -296,11 +296,11 @@ void getDb(MeshType::Pointer mesh,
   PointIterator pntIterator = mesh->GetPoints()->Begin();
 
   for ( int it = 0; it < numOfPoints; ++it, ++pntIterator) {
-    ItkPoint pnt = pntIterator.Value();
+  ItkPoint pnt = pntIterator.Value();
     
-    pointXYZ[it][0] = pnt[0];
-    pointXYZ[it][1] = pnt[1];
-    pointXYZ[it][2] = pnt[2];
+  pointXYZ[it][0] = pnt[0];
+  pointXYZ[it][1] = pnt[1];
+  pointXYZ[it][2] = pnt[2];
   }
 
   // 2. store the relationship from point to cell, i.e. for each
@@ -320,19 +320,19 @@ void getDb(MeshType::Pointer mesh,
         itCell < numOfCells;
         ++itCell, ++cellIt) {
     
-    CellType * cellptr = cellIt.Value(); 
-    // cellptr will point to each cell in the mesh
-    // std::cout << cellptr->GetNumberOfPoints() << std::endl;
+  CellType * cellptr = cellIt.Value(); 
+  // cellptr will point to each cell in the mesh
+  // std::cout << cellptr->GetNumberOfPoints() << std::endl;
 
-    PointIdIterator pntIdIter = cellptr->PointIdsBegin(); 
-    //pntIdIter will point to each point in the current cell
-    PointIdIterator pntIdEnd = cellptr->PointIdsEnd();
+  PointIdIterator pntIdIter = cellptr->PointIdsBegin(); 
+  //pntIdIter will point to each point in the current cell
+  PointIdIterator pntIdEnd = cellptr->PointIdsEnd();
 
-    for (int itPntInCell = 0; pntIdIter != pntIdEnd; ++pntIdIter, ++itPntInCell)
-      {
-        pointCell[ *pntIdIter ].push_back(itCell);
-        cellPoint[itCell][itPntInCell] = *pntIdIter;
-      }
+  for (int itPntInCell = 0; pntIdIter != pntIdEnd; ++pntIdIter, ++itPntInCell)
+    {
+    pointCell[ *pntIdIter ].push_back(itCell);
+    cellPoint[itCell][itPntInCell] = *pntIdIter;
+    }
   }
 
 
@@ -378,114 +378,114 @@ void getDb(MeshType::Pointer mesh,
   std::vector< std::vector<int> >::iterator itP, itPEnd = pointCell.end();
   int idP = 0;
   for ( itP = pointCell.begin(); itP != itPEnd; ++itP, ++idP) {
-    std::vector<int> neighborOfP;
-    // for each point P, traverse all cells containing it.
-    std::vector<int>::iterator itCell = (*itP).begin();
-    std::vector<int>::iterator itCellEnd = (*itP).end();
+  std::vector<int> neighborOfP;
+  // for each point P, traverse all cells containing it.
+  std::vector<int>::iterator itCell = (*itP).begin();
+  std::vector<int>::iterator itCellEnd = (*itP).end();
 
-    for (; itCell != itCellEnd; ++itCell) {
-      // for each cell containing P, store the point with larger point Id.
-      // only three points, don't use for-loop to save time.
-      if ( cellPoint[*itCell][0] > idP ) 
-        neighborOfP.push_back(cellPoint[*itCell][0]);
-      if ( cellPoint[*itCell][1] > idP ) 
-        neighborOfP.push_back(cellPoint[*itCell][1]);
-      if ( cellPoint[*itCell][2] > idP ) 
-        neighborOfP.push_back(cellPoint[*itCell][2]);
-    }// ok, now all neighbors of P is stored in neighborOfP;
+  for (; itCell != itCellEnd; ++itCell) {
+  // for each cell containing P, store the point with larger point Id.
+  // only three points, don't use for-loop to save time.
+  if ( cellPoint[*itCell][0] > idP ) 
+    neighborOfP.push_back(cellPoint[*itCell][0]);
+  if ( cellPoint[*itCell][1] > idP ) 
+    neighborOfP.push_back(cellPoint[*itCell][1]);
+  if ( cellPoint[*itCell][2] > idP ) 
+    neighborOfP.push_back(cellPoint[*itCell][2]);
+  }// ok, now all neighbors of P is stored in neighborOfP;
     
-    sort(neighborOfP.begin(), neighborOfP.end());
-    std::vector<int>::iterator it;
-    it = unique(neighborOfP.begin(), neighborOfP.end());
-    neighborOfP.erase(it, neighborOfP.end());
+  sort(neighborOfP.begin(), neighborOfP.end());
+  std::vector<int>::iterator it;
+  it = unique(neighborOfP.begin(), neighborOfP.end());
+  neighborOfP.erase(it, neighborOfP.end());
 
-    //-----------------------------------------------
-    // print out the neighbors
-    //     std::vector<int>::iterator itNeighbor = neighborOfP.begin();
-    //     std::vector<int>::iterator itNeighborEnd = neighborOfP.end();
-    //     std::cerr<<"The neighbors of "<<idP<<" are: ";
-    //     for (; itNeighbor != itNeighborEnd; ++itNeighbor) {
-    //       std::cerr<<*itNeighbor<<" , ";
-    //     }
-    //     std::cerr<<std::endl;
-    // ----------------------------------------------------
+  //-----------------------------------------------
+  // print out the neighbors
+  //     std::vector<int>::iterator itNeighbor = neighborOfP.begin();
+  //     std::vector<int>::iterator itNeighborEnd = neighborOfP.end();
+  //     std::cerr<<"The neighbors of "<<idP<<" are: ";
+  //     for (; itNeighbor != itNeighborEnd; ++itNeighbor) {
+  //       std::cerr<<*itNeighbor<<" , ";
+  //     }
+  //     std::cerr<<std::endl;
+  // ----------------------------------------------------
 
-    // next, from P to each neighbor...
-    // note: itP and itQ point at different type of vectors...
-    // *itP is a vector containing a list of points Ids
-    // idP is the point Id of P
-    // *itQ is the point Id of Q (so idP and *itQ are same type)
-    std::vector<int>::iterator itQ, itQEnd = neighborOfP.end();
-    for ( itQ = neighborOfP.begin(); itQ != itQEnd; ++itQ) {
-      // first check whether PQ is a boundary edge:
-      std::vector<int> cellsContainingP(*itP), cellsContainingQ(pointCell[*itQ]);
-      std::vector<int> cells(cellsContainingP.size() + cellsContainingQ.size());
-      std::vector<int>::iterator itv, endIter;
+  // next, from P to each neighbor...
+  // note: itP and itQ point at different type of vectors...
+  // *itP is a vector containing a list of points Ids
+  // idP is the point Id of P
+  // *itQ is the point Id of Q (so idP and *itQ are same type)
+  std::vector<int>::iterator itQ, itQEnd = neighborOfP.end();
+  for ( itQ = neighborOfP.begin(); itQ != itQEnd; ++itQ) {
+  // first check whether PQ is a boundary edge:
+  std::vector<int> cellsContainingP(*itP), cellsContainingQ(pointCell[*itQ]);
+  std::vector<int> cells(cellsContainingP.size() + cellsContainingQ.size());
+  std::vector<int>::iterator itv, endIter;
 
-      sort(cellsContainingP.begin(), cellsContainingP.end());
-      sort(cellsContainingQ.begin(), cellsContainingQ.end());
+  sort(cellsContainingP.begin(), cellsContainingP.end());
+  sort(cellsContainingQ.begin(), cellsContainingQ.end());
 
-      endIter = set_intersection(cellsContainingP.begin(), cellsContainingP.end(),
-                                 cellsContainingQ.begin(), cellsContainingQ.end(),
-                                 cells.begin());
-      cells.erase(endIter, cells.end());
-      if (cells.size() != 2) continue;
-      // If P and Q are not shared by two triangles, i.e. 1: are not
-      // connected by and edge, or, 2: are on the surface boundary
-      // thus only shared by one triangle. then skip.  However, in
-      // this paper the surface is closed thus there is not boundary.
+  endIter = set_intersection(cellsContainingP.begin(), cellsContainingP.end(),
+                             cellsContainingQ.begin(), cellsContainingQ.end(),
+                             cells.begin());
+  cells.erase(endIter, cells.end());
+  if (cells.size() != 2) continue;
+  // If P and Q are not shared by two triangles, i.e. 1: are not
+  // connected by and edge, or, 2: are on the surface boundary
+  // thus only shared by one triangle. then skip.  However, in
+  // this paper the surface is closed thus there is not boundary.
 
-      // If passed test above, then P and Q are two valid points.
-      // i.e. PQ is a valid edge.  i.e. cells now contain two int's,
-      // which are the Id of the triangles containing P and Q
-
-
-      //       //------------------------------------------------------------
-      //       //print out valid edge
-      //       std::cerr<<idP<<" and "<<*itQ<<" are two valid points"<<std::endl;
-      //       std::cerr<<(endIter == cells.end())<<std::endl;
-      //       //-----------------------------------------------------------
+  // If passed test above, then P and Q are two valid points.
+  // i.e. PQ is a valid edge.  i.e. cells now contain two int's,
+  // which are the Id of the triangles containing P and Q
 
 
-      // Next we extract R and S from cells
-      int itS, itR; // the Id of point S and R;
-      for (int it = 0; it < 3; ++it) {
-        if (cellPoint[cells[0]][it] != idP && cellPoint[cells[0]][it] != *itQ) 
-          itS = cellPoint[cells[0]][it];
-        if (cellPoint[cells[1]][it] != idP && cellPoint[cells[1]][it] != *itQ) 
-          itR = cellPoint[cells[1]][it];
-      }
+  //       //------------------------------------------------------------
+  //       //print out valid edge
+  //       std::cerr<<idP<<" and "<<*itQ<<" are two valid points"<<std::endl;
+  //       std::cerr<<(endIter == cells.end())<<std::endl;
+  //       //-----------------------------------------------------------
 
-      std::vector<double> P(pointXYZ[idP]), 
-        Q(pointXYZ[*itQ]), 
-        R(pointXYZ[itR]),
-        S(pointXYZ[itS]);
 
-      std::vector<double> SP(3), SQ(3), RP(3), RQ(3);
-      double SPnorm = 0, SQnorm = 0, RPnorm = 0, RQnorm = 0, SPSQinnerProd = 0, RPRQinnerProd = 0;
-      for (int it = 0; it<3; ++it) {
-        SP[it] = P[it] - S[it]; SPnorm += SP[it]*SP[it];
-        SQ[it] = Q[it] - S[it]; SQnorm += SQ[it]*SQ[it]; SPSQinnerProd += SP[it]*SQ[it];
-        RP[it] = P[it] - R[it]; RPnorm += RP[it]*RP[it];
-        RQ[it] = Q[it] - R[it]; RQnorm += RQ[it]*RQ[it]; RPRQinnerProd += RP[it]*RQ[it];
-      }
-      SPnorm = sqrt(SPnorm);
-      SQnorm = sqrt(SQnorm);
-      RPnorm = sqrt(RPnorm);
-      RQnorm = sqrt(RQnorm);
+  // Next we extract R and S from cells
+  int itS, itR; // the Id of point S and R;
+  for (int it = 0; it < 3; ++it) {
+  if (cellPoint[cells[0]][it] != idP && cellPoint[cells[0]][it] != *itQ) 
+    itS = cellPoint[cells[0]][it];
+  if (cellPoint[cells[1]][it] != idP && cellPoint[cells[1]][it] != *itQ) 
+    itR = cellPoint[cells[1]][it];
+  }
 
-      double cosS = SPSQinnerProd / (SPnorm * SQnorm); 
-      double cosR = RPRQinnerProd / (RPnorm * RQnorm);
-      double ctgS = cosS/sqrt(1-cosS*cosS), ctgR = cosR/sqrt(1-cosR*cosR);
+  std::vector<double> P(pointXYZ[idP]), 
+    Q(pointXYZ[*itQ]), 
+    R(pointXYZ[itR]),
+    S(pointXYZ[itS]);
 
-      D(idP, *itQ) = -0.5*(ctgS + ctgR);
-      D(idP, idP) += 0.5*(ctgS + ctgR); 
-      // add to the diagonal element of this line.
+  std::vector<double> SP(3), SQ(3), RP(3), RQ(3);
+  double SPnorm = 0, SQnorm = 0, RPnorm = 0, RQnorm = 0, SPSQinnerProd = 0, RPRQinnerProd = 0;
+  for (int it = 0; it<3; ++it) {
+  SP[it] = P[it] - S[it]; SPnorm += SP[it]*SP[it];
+  SQ[it] = Q[it] - S[it]; SQnorm += SQ[it]*SQ[it]; SPSQinnerProd += SP[it]*SQ[it];
+  RP[it] = P[it] - R[it]; RPnorm += RP[it]*RP[it];
+  RQ[it] = Q[it] - R[it]; RQnorm += RQ[it]*RQ[it]; RPRQinnerProd += RP[it]*RQ[it];
+  }
+  SPnorm = sqrt(SPnorm);
+  SQnorm = sqrt(SQnorm);
+  RPnorm = sqrt(RPnorm);
+  RQnorm = sqrt(RQnorm);
 
-      D(*itQ, idP) = -0.5*(ctgS + ctgR); // symmetric
-      D(*itQ, *itQ) += 0.5*(ctgS + ctgR); 
-      // add to the diagonal element of this line.
-    }
+  double cosS = SPSQinnerProd / (SPnorm * SQnorm); 
+  double cosR = RPRQinnerProd / (RPnorm * RQnorm);
+  double ctgS = cosS/sqrt(1-cosS*cosS), ctgR = cosR/sqrt(1-cosR*cosR);
+
+  D(idP, *itQ) = -0.5*(ctgS + ctgR);
+  D(idP, idP) += 0.5*(ctgS + ctgR); 
+  // add to the diagonal element of this line.
+
+  D(*itQ, idP) = -0.5*(ctgS + ctgR); // symmetric
+  D(*itQ, *itQ) += 0.5*(ctgS + ctgR); 
+  // add to the diagonal element of this line.
+  }
   }
 
   // compute b = bR + i*bI separately
