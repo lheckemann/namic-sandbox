2afd0644da1ee08e9faed9aa4de018d034c9492e
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-06-29 18:24:28 +0000

    ENH: Renaming variables with more explicit names.

--- trunk/fMRI-MarkovRandomFields/itkIsingMeanFieldApproximationImageFilter.cxx
+++ trunk/fMRI-MarkovRandomFields/itkIsingMeanFieldApproximationImageFilter.cxx
@@ -13,7 +13,7 @@
 =========================================================================auto=*/
 
 #include "vtkObjectFactory.h"
-#include "vtkIsingMeanfieldApproximation.h"
+#include "itkIsingMeanfieldApproximationImageFilter.h"
 #include "vtkCommand.h"
 
 vtkStandardNewMacro(vtkIsingMeanfieldApproximation);
@@ -33,11 +33,14 @@ vtkIsingMeanfieldApproximation::~vtkIsingMeanfieldApproximation()
 
 void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImageData *output)
 {
+
   dims[0] = x;   // X is the size[0] of the image
   dims[1] = y;   // Y is the size[1] of the image
   dims[2] = z;   // Z is the size[2] of the image
+
   size = x*y*z;
   int numberOfInputs;
+
 #if (VTK_MAJOR_VERSION >= 5)
   numberOfInputs = this->GetNumberOfInputConnections(0);
 #else  
@@ -45,34 +48,53 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
 #endif
   
   // in case of anatomical label map input
-  if (numberOfInputs == 3){
-    segMArray = vtkIntArray::New();
-    for (unsigned long int i=0; i<size; i++){
-      segMArray->InsertNextValue(0);
-    }
+  if (numberOfInputs == 3)
+    {
+    m_SegmentationLabelMap = SegmentationLabelsImageType::New();
+    m_SegmentationLabelMap->FillBuffer(0);
+
     register int i, j, k;     
     for (k=0; k<z; k++)
+      {
       for (j=0; j<y; j++)
-        for (i=0; i<x; i++){
+        {
+        for (i=0; i<x; i++)
+          {
           labelValue = (short int *) (GetInput(2)->GetScalarPointer(i,j,k));
-          segMArray->SetValue((k*x*y)+(j*x)+i,(int)(*labelValue));
+          m_SegmentationLabelMap->SetValue((k*x*y)+(j*x)+i,(int)(*labelValue));
+          }
         }
-    for (int j=0; j<segInput; j++){
-      for (unsigned long int i=0; i<size; i++)
-        if (segMArray->GetValue(i) == segLabel->GetValue(j))
-          segMArray->SetValue(i,j);
+      }
+
+
+    /** REPLACE THIS CODE WITH THE ITK RELABEL IMAGE FILTER (AND ALSO TAKE FROM
+       * IT THE ACTUAL NUMBER OF LABELS */
+                        for (int j=0; j<segInput; j++)
+                          {
+                          for (unsigned long int i=0; i<size; i++)
+                            {
+                            if (m_SegmentationLabelMap->GetValue(i) == segLabel->GetValue(j))
+                              {
+                              m_SegmentationLabelMap->SetValue(i,j);
+                              }
+                            }
+                          }
+    /** END OF CODE TO REPLACE WITH THE RELABEL IMAGE FILTER */
+
     }
-  }
-  else {
-    segMArray = vtkIntArray::New();
-    for (unsigned long int i=0; i<size; i++) 
-      segMArray->InsertNextValue(0);
-  }
+  else 
+    {
+    m_SegmentationLabelMap = vtkIntArray::New();
+    m_SegmentationLabelMap->FillBuffer(0);
+    }
   
   vtkIntArray *classArray = (vtkIntArray *)this->GetInput(0)->GetPointData()->GetScalars();
         
+  
+  numberOfPosteriorClasses = NumberOfSegmetationLabels * m_NumberOfActivationStates;
+
   sum = 0;
-  for (int i=0; i<(nType*nType); i++)
+  for (int i=0; i<(numberOfPosteriorClasses*numberOfPosteriorClasses); i++)
     sum += (transitionMatrix->GetValue(i));
         
   if (sum == 0){      
@@ -84,55 +106,55 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
           if (i != 0){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue((k*x*y)+(j*x)+i-1);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));
           }
           if (i != x-1){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue((k*x*y)+(j*x)+i+1);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));
           }
           if (j != 0){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue((k*x*y)+((j-1)*x)+i);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));
           }
           if (j != y-1){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue((k*x*y)+((j+1)*x)+i);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));       
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));       
           }
           if (k != 0){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue(((k-1)*x*y)+(j*x)+i);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));
           }
          if (k != z-1){
             index1 = classArray->GetValue((k*x*y)+(j*x)+i);
             index2 = classArray->GetValue(((k+1)*x*y)+(j*x)+i);
-            transitionMatrix->SetValue((index1*nType)+index2, (transitionMatrix->GetValue((index1*nType)+index2)+1));
+            transitionMatrix->SetValue((index1*numberOfPosteriorClasses)+index2, (transitionMatrix->GetValue((index1*numberOfPosteriorClasses)+index2)+1));
           }
         }
         
     // neighborhoods were counted double
-    for (int i=0; i<nType; i++)
-      if (transitionMatrix->GetValue((i*nType)+i) != 0)
-        transitionMatrix->SetValue((i*nType)+i, (transitionMatrix->GetValue((i*nType)+i))/2);
+    for (int i=0; i<numberOfPosteriorClasses; i++)
+      if (transitionMatrix->GetValue((i*numberOfPosteriorClasses)+i) != 0)
+        transitionMatrix->SetValue((i*numberOfPosteriorClasses)+i, (transitionMatrix->GetValue((i*numberOfPosteriorClasses)+i))/2);
   }  
   
   // in case of existing 0 values in transition matrix, increase all by 1 to prevent log range error   
-  for (int i=0; i<(nType*nType); i++)
+  for (int i=0; i<(numberOfPosteriorClasses*numberOfPosteriorClasses); i++)
     if (transitionMatrix->GetValue(i) == 0){
-      for (int j=0; j<(nType*nType); j++)             
+      for (int j=0; j<(numberOfPosteriorClasses*numberOfPosteriorClasses); j++)             
         transitionMatrix->SetValue(j, (transitionMatrix->GetValue(j))+1);     
       break;
     }
   
   // construction of log transition matrix
-  logTransitionMatrix->SetNumberOfValues(nType*nType);
-  for (int i=0; i<nType; i++)
-    for (int j=0; j<nType; j++){
-      logHelp = (float) log((transitionMatrix->GetValue((i*nType)+j))/sqrt(((activationFrequence->GetValue(i))*size)*((activationFrequence->GetValue(j))*size)));                  
-      logTransitionMatrix->SetValue((i*nType)+j, logHelp);
+  logTransitionMatrix->SetNumberOfValues(numberOfPosteriorClasses*numberOfPosteriorClasses);
+  for (int i=0; i<numberOfPosteriorClasses; i++)
+    for (int j=0; j<numberOfPosteriorClasses; j++){
+      logHelp = (float) log((transitionMatrix->GetValue((i*numberOfPosteriorClasses)+j))/sqrt(((activationFrequence->GetValue(i))*size)*((activationFrequence->GetValue(j))*size)));                  
+      logTransitionMatrix->SetValue((i*numberOfPosteriorClasses)+j, logHelp);
     }
   
   vtkFloatArray *probGivenClassArray = (vtkFloatArray *)this->GetInput(1)->GetPointData()->GetScalars();
@@ -145,12 +167,12 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
   output->SetOrigin(0.0,0.0,0.0);
   output->AllocateScalars();
 
-  helpArray = new float[nType];
+  helpArray = new float[numberOfPosteriorClasses];
 
   // initialization of class probability output volume
-  for (int n=0; n<nType; n++)
+  for (int n=0; n<numberOfPosteriorClasses; n++)
     for (unsigned long int i=0; i<size; i++)
-      outputArray->InsertNextValue((1.0/nType));
+      outputArray->InsertNextValue((1.0/numberOfPosteriorClasses));
   
   // meanfield iteration     
   register int i, j, k, n;     
@@ -159,37 +181,45 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
       for (j=0; j<y; j++)
         for (i=0; i<x; i++){
           sumHelpArray = 0.0;
-          for (int l=0; l<nType; l++){
+          for (int l=0; l<numberOfPosteriorClasses; l++){
             eValue = 0.0;          
             if (i != 0){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+(j*x)+i-1))*(logTransitionMatrix->GetValue((l*nType)+s))); 
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+(j*x)+i-1))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s))); 
             }    
             if (i != x-1){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+(j*x)+i+1))*(logTransitionMatrix->GetValue((l*nType)+s)));                             
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+(j*x)+i+1))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s)));                             
             }           
             if (j != 0){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+((j-1)*x)+i))*(logTransitionMatrix->GetValue((l*nType)+s))); 
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+((j-1)*x)+i))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s))); 
             }           
             if (j != y-1){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+((j+1)*x)+i))*(logTransitionMatrix->GetValue((l*nType)+s)));
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+(k*x*y)+((j+1)*x)+i))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s)));
             }        
             if (k != 0){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+((k-1)*x*y)+(j*x)+i))*(logTransitionMatrix->GetValue((l*nType)+s))); 
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+((k-1)*x*y)+(j*x)+i))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s))); 
             }    
             if (k != z-1){
-              for (int s=0; s<nType; s++)
-                eValue += ((outputArray->GetValue((s*size)+((k+1)*x*y)+(j*x)+i))*(logTransitionMatrix->GetValue((l*nType)+s))); 
+              for (int s=0; s<numberOfPosteriorClasses; s++)
+                eValue += ((outputArray->GetValue((s*size)+((k+1)*x*y)+(j*x)+i))*(logTransitionMatrix->GetValue((l*numberOfPosteriorClasses)+s))); 
             }        
                         
-            helpArray[l] = (activationFrequence->GetValue(l)) * (probGivenSegM->GetValue((segMArray->GetValue((k*x*y)+(j*x)+i))*nType+l)) * (probGivenClassArray->GetValue((l*size)+(k*x*y)+(j*x)+i)) * exp(eValue);                    
+// THIS REQUIRES THE SEGMENTATION ARRAY m_SegmentationLabelMap to be contiguous labeles
+// {0,1,2,....N} so we can use it as index in probGivenSegM.
+
+            helpArray[l] =   activationFrequence->GetValue(l) 
+                           * m_PriorClassProbabilityGivenSegmentation(
+                                     m_SegmentationLabelMap->GetPixel(k,j,i), l )
+                           * probGivenClassArray->GetValue((l*size)+(k*x*y)+(j*x)+i)
+                           * exp(eValue);
+
             sumHelpArray += helpArray[l];
           }
-          for (int l=0; l<nType; l++){
+          for (int l=0; l<numberOfPosteriorClasses; l++){
             outputArray->SetValue((l*size)+(k*x*y)+(j*x)+i, helpArray[l]/sumHelpArray);
           }
         }   
@@ -200,23 +230,23 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
   for (unsigned long int i=0; i<size; i++){
     max = 0.0;
     posMax = 0;
-    for (int n=0; n<nType; n++){
+    for (int n=0; n<numberOfPosteriorClasses; n++){
       if ((outputArray->GetValue((n*size)+i)) > max){
         max = (outputArray->GetValue((n*size)+i)); 
         posMax = n;
       }
     }   
     if (numActivationStates == 2){
-      if (posMax < (nType/2))
+      if (posMax < (numberOfPosteriorClasses/2))
         finalOutput->InsertNextValue(nonactive);
       else
         finalOutput->InsertNextValue(posactive);
     }
     else{
-      if (posMax < (nType/3))
+      if (posMax < (numberOfPosteriorClasses/3))
         finalOutput->InsertNextValue(nonactive);
       else
-        if ((posMax >= (nType/3)) && (posMax < 2*(nType/3)))
+        if ((posMax >= (numberOfPosteriorClasses/3)) && (posMax < 2*(numberOfPosteriorClasses/3)))
           finalOutput->InsertNextValue(posactive);
         else
           finalOutput->InsertNextValue(negactive);
@@ -232,7 +262,7 @@ void vtkIsingMeanfieldApproximation::SimpleExecute(vtkImageData *input, vtkImage
 #endif
   if (numberOfInputs != 3)
   {
-    segMArray->Delete();
+    m_SegmentationLabelMap->Delete();
   }
   delete [] helpArray;
   outputArray->Delete(); 
