b662a37bc3f47c74dceec312de9c1e569bc0d186
ygao <ygao@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-06-29 17:42:26 +0000

    ENH: add Eular number computation to check whether is genus 0

--- trunk/ConformalFlattening/src/raw2mesh/itkConformalFlatteningFilter.h
+++ trunk/ConformalFlattening/src/raw2mesh/itkConformalFlatteningFilter.h
@@ -1,17 +1,17 @@
 /*=========================================================================
 
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkConformalFlatteningFilter.h,v $
-  Language:  C++
-  Date:      $Date: 2003/09/10 14:28:58 $
-  Version:   $Revision: 1.14 $
+Program:   Insight Segmentation & Registration Toolkit
+Module:    $RCSfile: itkConformalFlatteningFilter.h,v $
+Language:  C++
+Date:      $Date: 2003/09/10 14:28:58 $
+Version:   $Revision: 1.14 $
 
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+Copyright (c) Insight Software Consortium. All rights reserved.
+See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
-     PURPOSE.  See the above copyright notices for more information.
+This software is distributed WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 #ifndef __itkConformalFlatteningFilter_h
@@ -30,100 +30,77 @@
 #include <vnl/vnl_sparse_matrix.h>
 #include <vnl/algo/vnl_conjugate_gradient.h>
 
-template <class matrixDataType>
-class theFunc : public vnl_cost_function {
- public:  
-//   theFunc(vnl_matrix<matrixDataType> const& A, 
-//           vnl_vector<matrixDataType> const& b);
-
-  theFunc(vnl_sparse_matrix<matrixDataType> const& A, 
-          vnl_vector<matrixDataType> const& b);
-
-  double f(vnl_vector<matrixDataType> const& x);
-  void gradf(vnl_vector<matrixDataType> const& x, vnl_vector<matrixDataType> & g);
-
-  inline unsigned int dim() {return _dim;}
-
- private:
-  vnl_matrix<matrixDataType> const* _A;
-  vnl_sparse_matrix<matrixDataType> const* _Asparse;
-  vnl_vector<matrixDataType> const* _b;
-  unsigned int _dim;
-  bool _sparse;
-};
-
 
 namespace itk
 {
 
-/** \class ConformalFlatteningFilter
- * \brief 
- *
- * 
- * \ingroup MeshFilters
- */
-template <class TInputMesh, class TOutputMesh>
-class ITK_EXPORT ConformalFlatteningFilter : 
+  /** \class ConformalFlatteningFilter
+   * \brief 
+   *
+   * 
+   * \ingroup MeshFilters
+   */
+  template <class TInputMesh, class TOutputMesh>
+  class ITK_EXPORT ConformalFlatteningFilter : 
     public MeshToMeshFilter<TInputMesh,TOutputMesh>
-{
-public:
-  /** Standard class typedefs. */
-  typedef ConformalFlatteningFilter  Self;
-  typedef MeshToMeshFilter<TInputMesh,TOutputMesh> Superclass;
-  typedef SmartPointer<Self>  Pointer;
-  typedef SmartPointer<const Self>  ConstPointer;
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef ConformalFlatteningFilter  Self;
+    typedef MeshToMeshFilter<TInputMesh,TOutputMesh> Superclass;
+    typedef SmartPointer<Self>  Pointer;
+    typedef SmartPointer<const Self>  ConstPointer;
   
-  typedef TInputMesh InputMeshType;
-  typedef TOutputMesh OutputMeshType;
-  typedef typename InputMeshType::Pointer InputMeshPointer;
-  typedef typename OutputMeshType::Pointer OutputMeshPointer;
+    typedef TInputMesh InputMeshType;
+    typedef TOutputMesh OutputMeshType;
+    typedef typename InputMeshType::Pointer InputMeshPointer;
+    typedef typename OutputMeshType::Pointer OutputMeshPointer;
 
-  /** Type for representing coordinates. */
-  typedef typename TInputMesh::CoordRepType  CoordRepType;
+    /** Type for representing coordinates. */
+    typedef typename TInputMesh::CoordRepType  CoordRepType;
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
   
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(ConformalFlatteningFilter, MeshToMeshFilter);
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(ConformalFlatteningFilter, MeshToMeshFilter);
 
-  ///////////////////////
-  typedef typename InputMeshType::PointsContainer::ConstIterator PointIterator;
-  typedef typename InputMeshType::CellsContainer::ConstIterator CellIterator;  
-  typedef typename InputMeshType::CellType CellType;
-  typedef typename InputMeshType::CellType::PointIdIterator PointIdIterator;
-  typedef typename InputMeshType::PointType PointType;
+    ///////////////////////
+    typedef typename InputMeshType::PointsContainer::ConstIterator PointIterator;
+    typedef typename InputMeshType::CellsContainer::ConstIterator CellIterator;  
+    typedef typename InputMeshType::CellType CellType;
+    typedef typename InputMeshType::CellType::PointIdIterator PointIdIterator;
+    typedef typename InputMeshType::PointType PointType;
 
-  typedef vnl_vector<CoordRepType> Tvnl_vector;
+    typedef vnl_vector<CoordRepType> Tvnl_vector;
 
-protected:
-  ConformalFlatteningFilter();
-  ~ConformalFlatteningFilter() {};
-  void PrintSelf(std::ostream& os, Indent indent) const;
+  protected:
+    ConformalFlatteningFilter();
+    ~ConformalFlatteningFilter() {};
+    void PrintSelf(std::ostream& os, Indent indent) const;
   
-  /** Generate Requested Data */
-  virtual void GenerateData( void );
+    /** Generate Requested Data */
+    virtual void GenerateData( void );
 
-private:
-  ConformalFlatteningFilter(const ConformalFlatteningFilter&); //purposely not implemented
-  void operator=(const ConformalFlatteningFilter&); //purposely not implemented
+  private:
+    ConformalFlatteningFilter(const ConformalFlatteningFilter&); //purposely not implemented
+    void operator=(const ConformalFlatteningFilter&); //purposely not implemented
 
-  //  for conformal flattening mapping  
-  //  theFunc<CoordRepType> _f;
-  void mapping( InputMeshPointer inputMesh, OutputMeshPointer outputMesh);
-  void stereographicProject( vnl_vector<CoordRepType> const& x,
-    vnl_vector<CoordRepType> const& y,
-    OutputMeshPointer outputMesh);
-  void getDb(OutputMeshPointer mesh, 
-             vnl_sparse_matrix<CoordRepType> &D,
-             vnl_vector<CoordRepType> &bR,
-             vnl_vector<CoordRepType> &bI);  
+    void mapping( InputMeshPointer inputMesh, OutputMeshPointer outputMesh);
+    void stereographicProject( vnl_vector<CoordRepType> const& x,
+                               vnl_vector<CoordRepType> const& y,
+                               OutputMeshPointer outputMesh);
+    void getDb(OutputMeshPointer mesh, 
+               vnl_sparse_matrix<CoordRepType> &D,
+               vnl_vector<CoordRepType> &bR,
+               vnl_vector<CoordRepType> &bI);  
 
-//   vnl_vector<CoordRepType> solveLinearEq(vnl_matrix<CoordRepType> const& A, 
-//                                    vnl_vector<CoordRepType> const& b);
-  vnl_vector<CoordRepType> solveLinearEq(vnl_sparse_matrix<CoordRepType> const& A, 
-                                   vnl_vector<CoordRepType> const& b);
-};
+    vnl_vector<CoordRepType> solveLinearEq(vnl_sparse_matrix<CoordRepType> const& A, 
+                                           vnl_vector<CoordRepType> const& b);
+                                           
+    unsigned int _cellHavePntP;
+             
+  };
 
 } // end namespace itk
 
