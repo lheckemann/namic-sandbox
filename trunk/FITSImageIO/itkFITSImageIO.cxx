5f81698edbbe6d6326799b7f80cb45efb3ff458b
nessus <nessus@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-06-29 16:28:06 +0000

    ENH: Modified to recognize FITS file extensions.

--- trunk/FITSImageIO/itkFITSImageIO.cxx
+++ trunk/FITSImageIO/itkFITSImageIO.cxx
@@ -14,213 +14,274 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-#include "itkFITSImageIO.h"
-#include "itkExceptionObject.h"
-#include "itkByteSwapper.h"
-#include "itkMetaDataObject.h"
 #include <iostream>
 #include <list>
 #include <string>
 #include <math.h>
 #include <zlib.h>
 
-#include "fitsio.h"
+#include <itkExceptionObject.h>
+#include <itkByteSwapper.h>
+#include <itkMetaDataObject.h>
 #include <itksys/SystemTools.hxx>
+#include <fitsio.h>
 
-namespace itk
-{
+#include "itkFITSImageIO.h"
 
+using std::string;
+using std::ostream;
+using std::cerr;
 
-/** Constructor */
-FITSImageIO::FITSImageIO()
-{
-}
-
-
-/** Destructor */
-FITSImageIO::~FITSImageIO()
-{
-}
+// Forward declarations:
+static bool checkExtension(const string&);
 
 
-bool FITSImageIO::CanReadFile( const char* filename ) 
+namespace itk {
+
+//-----------------------------------------------------------------------------
+// CanReadFile(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+/*method*/ bool
+FITSImageIO::CanReadFile(const char* filepath) 
 { 
-  // First check the filename extension
-  bool extensionFound = CheckExtension(filename);
-
-  if( !extensionFound )
-    {
-    itkDebugMacro(<<"The filename extension is not recognized");
-    return false;
-    }
-
-  // TODO: add here further verfication if cfits provides it...
-
-  return true;
-}
-  
-
-
-bool FITSImageIO::CanWriteFile( const char * name )
-{
-  std::string filename = name;
-  if ( filename == "" )
-    {
+  if (!filepath || *filepath == 0) {
     itkDebugMacro(<< "No filename specified.");
-    }
-
-  bool extensionFound = CheckExtension(name);
-
-  if( !extensionFound )
-    {
+    return false;
+  } else if (::checkExtension(filepath)) {
+    return true;
+  } else {
     itkDebugMacro(<<"The filename extension is not recognized");
     return false;
-    }
+  }
+}
 
-  if( extensionFound )
-    {
-    return true;
-    }
+
+//-----------------------------------------------------------------------------
+// CanWriteFile(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+/*method*/ bool
+FITSImageIO::CanWriteFile(const char* const name)
+{
   return false;
 
+  // TODO 
+
+  string filepath = name;
+  if (filepath == "") {
+    itkDebugMacro(<< "No filename specified.");
+  }
+
+  bool extensionFound = ::checkExtension(name);
+
+  if(!extensionFound) {
+    itkDebugMacro(<<"The filename extension is not recognized");
+    return false;
+  }
+
+  if (extensionFound) {
+    return true;
+  }
+  return false;
 }
 
 
- 
-void FITSImageIO::Read(void* buffer)
+//-----------------------------------------------------------------------------
+// Read(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+/*method*/ void
+FITSImageIO::Read(void* const buffer)
 {
    // here is the fun part...
    //
    // TODO: get the buffer here...
-   //
-
 }
 
-/** 
- *  Read Information about the Gipl file
- *  and put the cursor of the stream just before the first data pixel
- */
-void FITSImageIO::ReadImageInformation()
+
+//-----------------------------------------------------------------------------
+// ReadImageInformation(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+//! Read information about the FITS file and put the cursor of the
+//! stream just before the first data pixel
+
+/*method*/ void
+FITSImageIO::ReadImageInformation()
 {
-   // Delegate to cfits and populate the MetaDataDictionary.
+   // TODO: Delegate to cfits and populate the MetaDataDictionary.
    // page 4  Quick Start Guide (how to get the list of tags in the header)
    // page 34 Cfits IO User's guide.
    //
    
-   typedef std::string defaultValueType;
+   typedef string defaultValueType;
 
-   // This is the safe but slow way of opening the file...
-   // reconsider this for compressed files...
-   fitsfile * fptr;
+   // TODO: We may want to open the FITS file in the ctor rather than
+   // in each method, since if we end up opening a compressed file
+   // multiple times, in different methods, the file will end up being
+   // uncompressed multiple times.
 
-
+   fitsfile* fptr;
    int status = 0;
-   int numberOfHeaderEntries = 0;
+   int naxis;
+
+   ::fits_get_img_dim(fptr, &naxis, &status);
+   
+   cerr << "NAXIS=" << naxis << endl; //d
 
    // Open the file
-   fits_open_file( &fptr, m_FileName.c_str(), READONLY, &status );
-
-   // Get the header
-   fits_get_hdrspace( fptr, &numberOfHeaderEntries, NULL, &status );
-
-   const unsigned int maxRecordLength = 1024; // check this number by looking at cfits code...
-
-   char charKey[ maxRecordLength ];
-   char charValue[ maxRecordLength ];
-   char * dontNeedTheComment = NULL;
-
-   // Get the records and populate the MetaDataDictionary 
-   MetaDataDictionary & thisDic=this->GetMetaDataDictionary();
-
-   for(unsigned int keyentry=0; keyentry < numberOfHeaderEntries; keyentry++)
-      { 
-      unsigned int keyentryFortran = keyentry+1;
-
-      // Get the record from cfits
-      fits_read_keyn( fptr, keyentry, charKey, charValue, dontNeedTheComment, &status );
-
-     if( status  != 0 )
-        {
-        break;  // TODO: report an error
-        }
- 
-      std::string stringKey   = charKey;
-      std::string stringValue = charValue;
-
-      // Put the record in the MetaDataDictionary
-      EncapsulateMetaData<defaultValueType>( thisDic, stringKey, stringValue );
-
-      }
-
-   fits_close_file( fptr, &status );
+   ::fits_open_file(&fptr, m_FileName.c_str(), READONLY, &status);
 
    // TODO: Gather : Dimension, NumberOfPixels, Spacing and Origin 
 
-   // TODO: Convert status to a meaningful string and use it to throw and exception....
+   // TODO: Convert status to a meaningful string and use it to throw
+   // an exception....
 
+
+   // TODO: Make the commented-out code below work properly:
+
+//    // Get the header
+      int numberOfHeaderEntries = 0;
+//    ::fits_get_hdrspace(fptr, &numberOfHeaderEntries, NULL, &status);
+
+//    // TODO: check this number by looking at cfits code...
+//    const unsigned int maxRecordLength = 1024;
+
+//    char charKey[ maxRecordLength ];
+//    char charValue[ maxRecordLength ];
+//    char* dontNeedTheComment = NULL;
+
+//    // Get the records and populate the MetaDataDictionary 
+//    MetaDataDictionary& thisDic = this->GetMetaDataDictionary();
+
+//    for(unsigned int keyentry=0; keyentry < numberOfHeaderEntries; keyentry++)
+//      { 
+//        unsigned int keyentryFortran = keyentry + 1;
+
+//        // Get the record from cfits
+//        ::fits_read_keyn(fptr, keyentry, charKey, charValue,
+//                      dontNeedTheComment, &status);
+                        
+
+//        if (status != 0) {
+//         break;  // TODO: report an error
+//        }
+ 
+//       string stringKey   = charKey;
+//       string stringValue = charValue;
+
+//       // Put the record in the MetaDataDictionary
+//       itk::EncapsulateMetaData<defaultValueType>(thisDic, stringKey,
+//                                               stringValue);
+//     }
+
+   ::fits_close_file(fptr, &status);
 }
 
 
-void 
-FITSImageIO
-::WriteImageInformation(void)
+//-----------------------------------------------------------------------------
+// WriteImageInformation(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+/*method*/ void 
+FITSImageIO::WriteImageInformation(void)
 {
   // TODO: Implement writing by invoking cfits
 }
 
 
+//-----------------------------------------------------------------------------
+// Write(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
 
-/** The write function is not implemented */
-void 
-FITSImageIO
-::Write( const void* buffer) 
+//! The Write() function is not yet implemented
+
+/*method*/ void 
+FITSImageIO::Write(const void* const buffer) 
 {
+  // TODO
 }
 
-/** Print Self Method */
-void FITSImageIO::PrintSelf(std::ostream& os, Indent indent) const
+
+//-----------------------------------------------------------------------------
+// PrintSelf(): virtual method of FITSImageIO
+//-----------------------------------------------------------------------------
+
+/*method*/ void
+FITSImageIO::PrintSelf(ostream& os, const Indent indent) const
 {
   Superclass::PrintSelf(os, indent);
   os << indent << "PixelType " << m_PixelType << "\n";
 }
 
+} // END namespace itk
 
-bool FITSImageIO::CheckExtension(const char* filename)
+
+//*****************************************************************************
+//***                     Local procedures                                  ***
+//*****************************************************************************
+
+//-----------------------------------------------------------------------------
+// toLower(): local proc
+//-----------------------------------------------------------------------------
+
+// Converts \a s to lowercase.
+
+/*proc*/ void
+toLower(string& s)
 {
+   for (string::iterator p = s.begin(); p != s.end( ); ++p) {
+     *p = tolower(*p);
+   }
+}
 
-  if ( filename == NULL  )
+
+//-----------------------------------------------------------------------------
+// endMatchesP(): local proc
+//-----------------------------------------------------------------------------
+
+// Returns true iff \a extension is on the end of \a filepath.
+
+/*proc*/ static bool
+endMatchesP(const string& filepath, const string& extension)
+{
+  typedef string::size_type StrLen;
+  StrLen extensionLength = extension.length();
+  StrLen filepathLength = filepath.length();
+  string filepathEnd = filepath.substr(filepathLength - extensionLength);
+  if (filepathEnd == extension) {
+    return true;
+  }
+  else return false;
+}
+
+
+//-----------------------------------------------------------------------------
+// checkExtension(): local proc
+//-----------------------------------------------------------------------------
+
+// Returns true iff filepath ends with an extension that indicates
+// that it is a FITS file.
+
+/*proc*/ static bool
+checkExtension(const string& filepath)
+{
+  string loweredFilepath = filepath;
+  toLower(loweredFilepath);
+  if (endMatchesP(loweredFilepath, ".fits") ||
+      endMatchesP(loweredFilepath, ".fits.gz") ||
+      endMatchesP(loweredFilepath, ".fit") ||
+      endMatchesP(loweredFilepath, ".fit.gz") ||
+      endMatchesP(loweredFilepath, ".fits.Z") ||
+      endMatchesP(loweredFilepath, ".fit.Z"))
     {
-    itkDebugMacro(<< "No filename specified.");
-    return false;
+      return true;
     }
+  else return false;
 
-  std::string fname = filename;
-
-  if ( fname.empty()  )
-    {
-    itkDebugMacro(<< "No filename specified.");
-    return false;
-    }
-
-  bool extensionValid = false;
-
-  std::string extension = itksys::SystemTools::GetFilenameExtension( filename );
-
-  if( extension == "fits" )
-    {
-    extensionValid = true;
-    }
-
-  if( extension == "fit" )
-    {
-    extensionValid = true;
-    }
-
-  //  TODO: add gz ... combinations
-
-  return extensionValid;
+  // TODO: Extend to deal with FITS bracket filtering, etc.
 }
 
 
 
-} // end namespace itk
