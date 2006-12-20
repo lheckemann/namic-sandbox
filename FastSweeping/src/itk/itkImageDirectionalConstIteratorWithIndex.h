ec4bfdb7d23b1b474ec19fc8f7b740d806c06f13
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-12-20 14:00:27 +0000

    ENH: First version of the Directional iterator needed      in the FastSweeping algorithm.

---
+++ FastSweeping/src/itk/itkImageDirectionalConstIteratorWithIndex.h
@@ -0,0 +1,168 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkImageDirectionalConstIteratorWithIndex.h,v $
+  Language:  C++
+  Date:      $Date: 2005/08/16 18:04:33 $
+  Version:   $Revision: 1.10 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkImageDirectionalConstIteratorWithIndex_h
+#define __itkImageDirectionalConstIteratorWithIndex_h
+
+#include "itkImageConstIteratorWithIndex.h"
+#include "itkFixedArray.h"
+
+namespace itk
+{
+
+/** \class ImageDirectionalConstIteratorWithIndex
+ * \brief A multi-dimensional
+ *
+ * Iterator intended to be used in the FastSweeping algorithm.
+ * 
+ * \code
+ * 
+ *  IteratorType it( image, image->GetRequestedRegion() );
+ *
+ *  it.GoToBegin();
+ *
+ *  while( ! it.IsAtEnd() ) 
+ *    {
+ *    while( ! it.IsAtEndOfDirection() ) 
+ *      {  
+ *      it.Set( 100.0 + it.Get() );
+ *      ++it;
+ *      }
+ *    it.NextDirection();
+ *    }
+ *
+ * \endcode
+ *
+  *
+ * \endcode
+ *
+ * \par MORE INFORMATION
+ *
+ *
+ * For a complete description of the ITK Image Iterators and their API, please
+ * see the Iterators chapter in the ITK Software Guide.  The ITK Software Guide
+ * is available in print and as a free .pdf download from http://www.itk.org.
+ *
+ * \ingroup ImageIterators
+ *
+ * \sa ImageConstIterator \sa ConditionalConstIterator
+ * \sa ConstNeighborhoodIterator \sa ConstShapedNeighborhoodIterator
+ * \sa ConstSliceIterator  \sa CorrespondenceDataStructureIterator 
+ * \sa FloodFilledFunctionConditionalConstIterator 
+ * \sa FloodFilledImageFunctionConditionalConstIterator 
+ * \sa FloodFilledImageFunctionConditionalIterator 
+ * \sa FloodFilledSpatialFunctionConditionalConstIterator 
+ * \sa FloodFilledSpatialFunctionConditionalIterator 
+ * \sa ImageConstIterator \sa ImageConstIteratorWithIndex 
+ * \sa ImageIterator \sa ImageIteratorWithIndex
+ * \sa ImageLinearConstIteratorWithIndex  \sa ImageLinearIteratorWithIndex 
+ * \sa ImageRandomConstIteratorWithIndex  \sa ImageRandomIteratorWithIndex 
+ * \sa ImageDirectionalConstIterator \sa ImageDirectionalConstIteratorWithIndex 
+ * \sa ImageRegionExclusionConstIteratorWithIndex 
+ * \sa ImageRegionExclusionIteratorWithIndex 
+ * \sa ImageRegionIterator  \sa ImageRegionIteratorWithIndex 
+ * \sa ImageRegionReverseConstIterator  \sa ImageRegionReverseIterator 
+ * \sa ImageReverseConstIterator  \sa ImageReverseIterator 
+ * \sa ImageSliceConstIteratorWithIndex  \sa ImageSliceIteratorWithIndex 
+ * \sa NeighborhoodIterator \sa PathConstIterator  \sa PathIterator 
+ * \sa ShapedNeighborhoodIterator  \sa SliceIterator 
+ * \sa ImageConstIteratorWithIndex */
+template<typename TImage>
+class ITK_EXPORT ImageDirectionalConstIteratorWithIndex : public ImageConstIteratorWithIndex<TImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ImageDirectionalConstIteratorWithIndex Self;
+  typedef ImageConstIteratorWithIndex<TImage>  Superclass;
+  
+  /** Index typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Index back to itk::Index to that is it not
+   * confused with ImageIterator::Index. */
+  typedef typename TImage::IndexType  IndexType;
+
+  /** Image typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Image back to itk::Image to that is it not
+   * confused with ImageIterator::Image. */
+  typedef TImage ImageType;
+
+  itkStaticConstMacro(ImageDimension, unsigned int, TImage::ImageDimension);
+
+  /** PixelContainer typedef support. Used to refer to the container for
+   * the pixel data. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc. */
+  typedef typename TImage::PixelContainer PixelContainer;
+  typedef typename PixelContainer::Pointer PixelContainerPointer;
+  
+  /** Region typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Region back to itk::ImageRegion so that is
+   * it not confused with ImageIterator::Index. */
+  typedef typename TImage::RegionType RegionType;
+
+  /** Default constructor. Needed since we provide a cast constructor. */
+  ImageDirectionalConstIteratorWithIndex() : ImageConstIteratorWithIndex<TImage>() {};
+  
+  /** Constructor establishes an iterator to walk a particular image and a
+   * particular region of that image. */
+  ImageDirectionalConstIteratorWithIndex( const TImage *ptr,
+                                          const RegionType& region );
+
+  /** Constructor that can be used to cast from an ImageIterator to an
+   * ImageDirectionalConstIteratorWithIndex. Many routines return an ImageIterator but for a
+   * particular task, you may want an ImageDirectionalConstIteratorWithIndex.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageDirectionalConstIteratorWithIndex. */
+  ImageDirectionalConstIteratorWithIndex( const ImageConstIteratorWithIndex<TImage> &it)
+    { this->ImageConstIteratorWithIndex<TImage>::operator=(it); }
+
+  /** Increment (prefix) the fastest moving dimension of the iterator's index.
+   * This operator will constrain the iterator within the region (i.e. the
+   * iterator will automatically wrap from the end of the row of the region
+   * to the beginning of the next row of the region) up until the iterator
+   * tries to moves past the last pixel of the region.  Here, the iterator
+   * will be set to be one pixel past the end of the region.
+   * \sa operator-- */
+  Self & operator++();
+
+
+  void NextDirection();
+  bool IsAtEndOfDirection();
+  bool IsAtEnd();
+  void GoToBegin();
+
+  private:
+
+    FixedArray< int, ImageDimension >  m_Directions;
+
+    IndexType        m_UnchangingBeginIndex;
+    IndexType        m_UnchangingEndIndex;
+
+};
+
+/** \example itkImageIteratorTest.cpp */
+/** \example itkIteratorTests.cpp */
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageDirectionalConstIteratorWithIndex.txx"
+#endif
+
+#endif 
+
---
+++ FastSweeping/src/itk/itkImageDirectionalConstIteratorWithIndex.txx
@@ -0,0 +1,163 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkImageDirectionalConstIteratorWithIndex.txx,v $
+  Language:  C++
+  Date:      $Date: 2004/12/11 20:29:18 $
+  Version:   $Revision: 1.6 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkImageDirectionalConstIteratorWithIndex_txx
+#define _itkImageDirectionalConstIteratorWithIndex_txx
+
+#include "itkImageDirectionalConstIteratorWithIndex.h"
+
+namespace itk
+{
+  
+//----------------------------------------------------------------------
+//  Constructor
+//----------------------------------------------------------------------
+template<class TImage>
+ImageDirectionalConstIteratorWithIndex<TImage>
+::ImageDirectionalConstIteratorWithIndex(const TImage *ptr,
+                               const RegionType & region )
+:ImageConstIteratorWithIndex<TImage>(ptr,region)
+{
+  this->m_Directions.Fill( 1 );
+
+  this->m_UnchangingBeginIndex = this->m_BeginIndex;
+  this->m_UnchangingEndIndex = this->m_EndIndex;
+}
+  
+//----------------------------------------------------------------------
+//  Is At End
+//----------------------------------------------------------------------
+template<class TImage>
+bool
+ImageDirectionalConstIteratorWithIndex<TImage>
+::IsAtEnd()
+{
+  for(unsigned int i; i<ImageDimension; i++)
+    {
+    if( this->m_Directions[i] != -1 )
+      {
+      return false;
+      }
+    }
+   return true;
+}
+
+//----------------------------------------------------------------------
+//  Is At End
+//----------------------------------------------------------------------
+template<class TImage>
+bool
+ImageDirectionalConstIteratorWithIndex<TImage>
+::IsAtEndOfDirection()
+{ 
+  this->Superclass::IsAtEnd();
+}
+
+//----------------------------------------------------------------------
+//  Go to begin of iterations.
+//----------------------------------------------------------------------
+template<class TImage>
+void
+ImageDirectionalConstIteratorWithIndex<TImage>
+::GoToBegin()
+{
+  this->Superclass::GoToBegin();
+  this->m_Directions.Fill( 1 );
+
+}
+ 
+  
+//----------------------------------------------------------------------
+//  Go to next direction
+//----------------------------------------------------------------------
+template<class TImage>
+void
+ImageDirectionalConstIteratorWithIndex<TImage>
+::NextDirection()
+{
+  unsigned long binaryHelper = 0L;
+
+  for(unsigned int i; i<ImageDimension; i++)
+    {
+    unsigned long bit = ( this->m_Directions[i] == 1 ) ? 0 : 1;
+    binaryHelper |= bit;
+    binaryHelper << 1;
+    }
+
+  binaryHelper++;
+
+  for(unsigned int j; j<ImageDimension; j++)
+      {
+      if( binaryHelper & 1 )
+        {
+        this->m_Directions[j] = -1;
+        this->m_BeginIndex[j] = this->m_UnchangingEndIndex[j];
+        this->m_EndIndex[j] = this->m_UnchangingBeginIndex[j];
+        }
+      else
+        {
+        this->m_Directions[j] = 1;
+        this->m_BeginIndex[j] = this->m_UnchangingBeginIndex[j];
+        this->m_EndIndex[j] = this->m_UnchangingEndIndex[j];
+        }
+      binaryHelper >> 1;
+      }
+
+  this->Superclass::GoToBegin();
+}
+ 
+//----------------------------------------------------------------------
+//  Advance along the line
+//----------------------------------------------------------------------
+template<class TImage>
+ImageDirectionalConstIteratorWithIndex<TImage> &
+ImageDirectionalConstIteratorWithIndex<TImage>
+::operator++()
+{
+  
+  this->m_Remaining = false;
+  for( unsigned int in=0; in<TImage::ImageDimension; in++ )
+    {
+    this->m_PositionIndex[ in  ] += this->m_Directions[ in ];
+    if( this->m_PositionIndex[ in ] < this->m_EndIndex[ in ] )
+      {
+      // FIXME: A faster way to do this is to define a duplicate
+      // offset table where we change +/- in NextDirection()
+      this->m_Position += this->m_OffsetTable[in] * this->m_Directions[ in ];
+      this->m_Remaining = true;
+      break;
+      }
+    else 
+      {
+      this->m_Position -= this->m_OffsetTable[ in ] * this->m_Directions[ in ]  
+        * ( static_cast<long>(this->m_Region.GetSize()[in])-1 );
+      this->m_PositionIndex[ in ] = this->m_BeginIndex[ in ]; 
+      }
+    }
+
+  if( !this->m_Remaining ) // It will not advance here otherwise
+    {
+    this->m_Position = this->m_End;
+    }
+
+  return *this;
+}
+
+
+
+} // end namespace itk
+
+#endif
