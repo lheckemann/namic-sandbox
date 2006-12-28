ee0f1e716bd3511fb31caf9bbe0853b4dabcaca8
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2006-12-28 17:44:08 +0000

    ENH: Redefining the region over which the iterator is      going to walk. This ensure that we stay inside the image.

--- FastSweeping/src/itk/Test1.cxx
+++ FastSweeping/src/itk/Test1.cxx
@@ -38,10 +38,26 @@ main()
     ++itr1;
     }
 
+  ImageType::RegionType region2;
+  ImageType::IndexType start2;
+  ImageType::SizeType size2;
+
+  const unsigned int radius = 1;
+
+  size2[0] = size[0] - 2 * radius;
+  size2[1] = size[1] - 2 * radius;
+  size2[2] = size[2] - 2 * radius;
+
+  start2[0] = start[0] + radius;
+  start2[1] = start[1] + radius;
+  start2[2] = start[2] + radius;
+
+  region2.SetSize( size2 );
+  region2.SetIndex( start2 );
 
   IteratorType itr2( image, region );
 
-  itr2.SetRadius( 1 );
+  itr2.SetRadius( radius );
 
   typedef IteratorType::Iterator  NeighborPixel;
 
