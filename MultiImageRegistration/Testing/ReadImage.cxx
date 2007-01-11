4ea2f13897b190a0e0372cdd51d00ed9f1844d69
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-01-11 02:37:21 +0000

    ENH: Example of how to add a test.

---
+++ MultiImageRegistration/Testing/ReadImage.cxx
@@ -0,0 +1,31 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+
+int main( int argc, char * argv [] )
+{
+  if( argc < 2 )
+    {
+    std::cout << "Usage: " << argv[0];
+    std::cout << " inputImageFile " << std::endl;
+    }
+
+  typedef itk::Image< char, 3 > ImageType;
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( argv[1] );
+
+  try
+    {
+    reader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
