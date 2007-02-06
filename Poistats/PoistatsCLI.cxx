9403278019e592446593866a8883be96548e496f
dsjen <dsjen@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-02-06 01:52:10 +0000

    ENH: Using iterators in ConstructOdfList for a very small speed up (~1 second in my test data set) and cleaner code ENH: Removed --header flag, since it's not used anymore ENH: Added nii flag for outputting nifti, thus overriding the default (freesurfer can't read nrrd at this point)

--- Poistats/PoistatsCLI.cxx
+++ Poistats/PoistatsCLI.cxx
@@ -252,129 +252,7 @@ int main (int argc, char * argv[]) {
   TensorImageType::Pointer tensors = tensorReader->GetTensors();
   
   delete tensorReader;
-    
-//  // if the data is stored as 9 components  
-//  if( !isSymmetricTensorData ){
-//    
-//    observer->PostMessage( "not stored symmetrically\n" );
-//    
-//    typedef itk::OrientedImage< float, 4 > FullTensorImageType;
-////    typedef itk::VectorImage< float, 4 > FullTensorImageType;
-//    typedef itk::ImageFileReader< FullTensorImageType > FullTensorReaderType;
-//    FullTensorReaderType::Pointer tensorReader = FullTensorReaderType::New();
-//        
-//    tensorReader->SetFileName( diffusionTensorImage );
-//    observer->PostMessage( "reading tensors...\n" );
-//    try { 
-//      tensorReader->Update();
-//    } catch( itk::ExceptionObject & excp ) {
-//      std::ostringstream output;
-//      output << "Error reading the series." << std::endl << excp << std::endl;
-//      observer->PostErrorMessage( output.str() );
-//      return EXIT_FAILURE;
-//    }
-//    
-//    FullTensorImageType::Pointer fullTensors = tensorReader->GetOutput();
-//        
-//    // TODO:
-//    std::cerr << fullTensors << std::endl;
-//    std::cerr << "\ndirection: \n" << fullTensors->GetDirection();
-//    
-//    // convert the full 9 component tensors to 6 component tensors
-//    //  - create and allocate a new DiffusionTensor3D image
-//    FullTensorImageType::RegionType dtiRegion = 
-//      fullTensors->GetLargestPossibleRegion();
-//    TensorImageType::SizeType size;
-//    double origin[ TensorImageType::RegionType::GetImageDimension() ];
-//    TensorImageType::IndexType start;
-//    TensorImageType::SpacingType spacing;
-//  
-//    for( int cDim=0; cDim<TensorImageType::RegionType::GetImageDimension(); cDim++ ) {    
-//      size[ cDim ] = dtiRegion.GetSize()[ cDim ];
-//      origin[ cDim ] = fullTensors->GetOrigin()[ cDim ];
-//      start[ cDim ] = 0;
-//      spacing[ cDim ] = fullTensors->GetSpacing()[ cDim ];
-//    }
-//        
-//    TensorImageType::RegionType region;
-//    region.SetSize( size );    
-//    region.SetIndex( start );
-//    
-//    tensors = TensorImageType::New();
-//    tensors->SetRegions( region );
-//    tensors->SetOrigin( origin );
-//    tensors->SetSpacing( spacing );      
-//    tensors->Allocate();  
-//    tensors->FillBuffer( 0.0 );
-//
-//    //  - copy the data into the right areas
-//    const int nTensorRows = 3;
-//    const int nTensorCols = 3;
-//
-//    observer->PostMessage( "converting tensors to symmetric format\n" );
-//    for( int cImageRow=0; cImageRow<size[ 0 ]; cImageRow++ ) {
-//      for( int cImageCol=0; cImageCol<size[ 1 ]; cImageCol++ ) {
-//        for( int cImageSlice=0; cImageSlice<size[ 2 ]; cImageSlice++ ) {
-//        
-//          TensorImageType::IndexType symmetricIndex;
-//          symmetricIndex[ 0 ] = cImageRow;
-//          symmetricIndex[ 1 ] = cImageCol;
-//          symmetricIndex[ 2 ] = cImageSlice;
-//
-//          TensorPixelType symmetricTensor;
-//
-//          FullTensorImageType::IndexType fullTensorIndex;
-//          fullTensorIndex[ 0 ] = cImageRow;
-//          fullTensorIndex[ 1 ] = cImageCol;
-//          fullTensorIndex[ 2 ] = cImageSlice;
-//        
-//          int cContinuousIndex = 0;
-//          
-//          // iterate through each component
-//          // TODO: actually we'll only need to iterate through some of them
-//          for( int cTensorRow = 0; cTensorRow<nTensorRows; cTensorRow++ ) {
-//            
-//            for( int cTensorCol = 0; cTensorCol<nTensorCols; cTensorCol++ ) {
-//              
-//              fullTensorIndex[ 3 ] = cContinuousIndex;
-//              
-//              std::cerr << "  pixel: " << fullTensors->GetPixel( fullTensorIndex ) << std::endl;
-//              
-//              symmetricTensor( cTensorRow, cTensorCol ) = 
-//                fullTensors->GetPixel( fullTensorIndex );
-//
-////              if( fullTensors->GetPixel( fullTensorIndex ) != 0.0 )
-////                std::cerr << fullTensors->GetPixel( fullTensorIndex ) << "  ";
-//              
-//              cContinuousIndex++;
-//            }
-//            
-//          }
-//          
-//          tensors->SetPixel( symmetricIndex, symmetricTensor );
-//
-//        }
-//      }
-//    }
-//    
-//    
-//  } else {
-//    // data stored in 6 components
-//    typedef itk::ImageFileReader< TensorImageType > TensorReaderType;
-//    TensorReaderType::Pointer tensorReader = TensorReaderType::New();
-//    tensorReader->SetFileName( diffusionTensorImage );
-//    observer->PostMessage( "reading symmetric tensors...\n" );
-//    try { 
-//      tensorReader->Update();
-//    } catch( itk::ExceptionObject & excp ) {
-//      std::ostringstream output;
-//      output << "Error reading the series." << std::endl << excp << std::endl;
-//      observer->PostErrorMessage( output.str() );
-//      return EXIT_FAILURE;
-//    }
-//    tensors = tensorReader->GetOutput();
-//  }
-  
+      
   typedef itk::Image< float, 3 > OutputImageType;
   typedef itk::PoistatsFilter< TensorImageType, OutputImageType > 
     PoistatsFilterType;
@@ -509,6 +387,11 @@ int main (int argc, char * argv[]) {
     output << "Error thrown in poistats filter." << std::endl << excp << std::endl;
     observer->PostErrorMessage( output.str() );
     return EXIT_FAILURE;
+  }
+ 
+ // this overrides the default output file format
+  if( isOutputNii ){
+    imageFileExtension = ".nii";
   }
   
   typedef itk::ImageFileWriter< PoistatsFilterType::OutputImageType > WriterType;  
