2ab279e1066b8bbfe406d92674a4b9dbf56625bf
tringo <tringo@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-03-20 21:16:20 +0000

    ENH:Removed Unneccessary Files

--- trunk/StochasticTractography/itkImage2VectorImageAdaptor.h
+++
@@ -1,8 +0,0 @@
-  /** Connect a DWI Image **/
-  void AddDWIImage(TDWIImage* image,
-    TGradientDirection direction,
-    TbValue bvalue);
-  
-  /** Connect a DWI Image without specifying a b value **/
-  void AddDWIImageNobValue(TDWIImage* image,
-    TGradientDirection direction);
