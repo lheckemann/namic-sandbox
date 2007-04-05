7e6f3dd027db9df44b43fc7b54f8ebe684ad6e97
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-04-05 21:22:33 +0000

    ENH: Overloading abstract methods of the Sample to have a concrete class.

--- trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleTest.cxx
+++ trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleTest.cxx
@@ -20,10 +20,79 @@
 
 #include <iostream>
 #include "itkSample.h"
+#include "itkObjectFactory.h"
 
+namespace itk {
+namespace Statistics {
+namespace SampleTest {
+
+template <class TMeasurementVector>
+class MySample : public Sample< TMeasurementVector >
+{
+public:
+  /** Standard class typedef. */
+  typedef MySample  Self;
+  typedef Sample< TMeasurementVector > Superclass;
+  typedef SmartPointer< Self > Pointer;
+  typedef SmartPointer<const Self> ConstPointer;
+
+  /** Standard macros */
+  itkTypeMacro(MySample, Sample);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self) ;
+ 
+  typedef typename Superclass::MeasurementVectorType MeasurementVectorType;
+  typedef typename Superclass::TotalFrequencyType TotalFrequencyType;
+  typedef typename Superclass::FrequencyType FrequencyType;
+  typedef typename Superclass::InstanceIdentifier InstanceIdentifier;
+
+  /** Get the size of the sample (number of measurements) */
+  virtual unsigned int Size() const { m_Values.size(); }
+
+  /** Get the measurement associated with a particular
+   * InstanceIdentifier. */
+  virtual const MeasurementVectorType & 
+    GetMeasurementVector(const InstanceIdentifier &id) const
+      { return m_Values[id]; }
+
+  /** Get the frequency of a measurement specified by instance
+   * identifier. */
+  virtual FrequencyType GetFrequency(const InstanceIdentifier &id) const
+      { return m_Frequencies[id]; }
+
+  /** Get the total frequency of the sample. */
+  virtual TotalFrequencyType GetTotalFrequency() const 
+    { 
+    TotalFrequencyType sum = NumericTraits< TotalFrequencyType >::Zero;
+    typedef typename std::vector< FrequencyType >::const_iterator Iterator;
+    Iterator itr = m_Frequencies.begin();
+    while( itr != m_Frequencies.end() )
+      {
+      sum += *itr;
+      ++itr;
+      }
+    return sum;
+    }
+
+private:
+
+  std::vector< TMeasurementVector >  m_Values;
+  std::vector< FrequencyType >       m_Frequencies;
+
+};
+
+}
+}
+}
 int itkSampleTest(int, char* [] )
 {
 
+  typedef itk::FixedArray< float, 10 >  MeasurementVectorType;
+  typedef itk::Statistics::SampleTest::MySample< 
+    MeasurementVectorType >   SampleType;
+
+  SampleType::Pointer sample = SampleType::New();
 
   return (EXIT_SUCCESS);
 }
