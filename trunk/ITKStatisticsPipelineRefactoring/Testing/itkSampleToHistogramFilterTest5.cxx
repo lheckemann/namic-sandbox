8bb62085ac3cb3d58dff4cd5396986b85e1e2af1
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-05-07 17:19:20 +0000

    BUG: Updating the tests to reflect the fact that now the default size of the      histogram is = 1, instead of the previous value = 0.

--- trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleToHistogramFilterTest5.cxx
+++ trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleToHistogramFilterTest5.cxx
@@ -122,9 +122,9 @@ int itkSampleToHistogramFilterTest5(int argc, char *argv[] )
 
   const HistogramType * histogram = filter->GetOutput();
 
-  if( histogram->Size() )
+  if( histogram->Size() != 1 )
     {
-    std::cerr << "Histogram Size should have been zero" << std::endl;
+    std::cerr << "Histogram Size should have been one" << std::endl;
     return EXIT_FAILURE;
     }
 
