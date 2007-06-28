afe271e7a60dffcae8fa7e673acd76486004aad3
nickel <nickel@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-06-28 17:58:33 +0000

    ENH: added README to quickly describe the program's functionality.

--- trunk/UIowaAtlas/Testing/volumeRender/volumeRender.cxx
+++ trunk/UIowaAtlas/Testing/volumeRender/volumeRender.cxx
@@ -26,9 +26,8 @@ int main( int argc, char * argv [] )
 
   if( argc < 2 )
     {
-      std::cerr << "Missing filename command line argument" << std::endl;
-      std::cerr << "Usage: " << argv[0] << " inputFilename [outputFilename]" << std::endl;
-      return EXIT_FAILURE;
+      std::cerr << "USAGE: " << argv[0] << " inputFileName" << std::endl;
+      exit(-1);
     }
 
   try
