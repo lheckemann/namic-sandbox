97ba19475705674cd391fa55ceb8192d147d98da
hjohnson <hjohnson@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-07-07 14:38:09 +0000

    ENH:  Starting the shell of a testing suite for pushing Slicer-3 archetecture.

--- BRAINSToSlicer3Scripting/Scripts/Tcl/CommandLineSlicerScript.tcl
+++ BRAINSToSlicer3Scripting/Scripts/Tcl/CommandLineSlicerScript.tcl
@@ -1,15 +1,17 @@
+##Note: This needs to be sourced first.
 
+## Run with ~/src/Slicer3-build/Slicer3 -f ~/NewScript.tcl
 #### UGLY GLOBAL VARIABLES
 ## If in gui mode
 #if { HaveSlicerGUI then use that } {
-  set Scene $::slicer3::MRMLScene
-  set volumesLogic [ $::slicer3::VolumesGUI GetLogic]
-  set colorLogic [$::slicer3::ColorGUI GetLogic]
+#  set Scene $::slicer3::MRMLScene
+#  set volumesLogic [ $::slicer3::VolumesGUI GetLogic]
+#  set colorLogic [$::slicer3::ColorGUI GetLogic]
 
-## If not in gui mode (--nospash -f )
-#  set Scene [ vtkMRMLScene New ]
-#  set volumesLogic [ vtkSlicerVolumesLogic New ]
-#  ${volumesLogic} SetMRMLScene $Scene
+## If not in gui mode (--no_splash -f )
+  set Scene [ vtkMRMLScene New ]
+  set volumesLogic [ vtkSlicerVolumesLogic New ]
+  ${volumesLogic} SetMRMLScene $Scene
 
 #  puts "-----${volumesLogic}----${volumesLogic}----"
 
@@ -41,21 +43,15 @@ proc b3_load_image { fileName {centered 1} {lableimage 0} {NodeName brain} } {
 proc b3_gaussian_filter { volumeNode {RadiusFactor 2.0} } {
   set GFilter [ vtkImageGaussianSmooth  New ]
   ${GFilter} SetInput [ ${volumeNode} GetImageData ]
-  puts "HERE4"
   ${GFilter} SetRadiusFactor ${RadiusFactor}
   ${GFilter} Update
-  puts "HERE5"
   set GFilterOutput [ ${GFilter} GetOutput ]
   set NewObjName NewObjName
-  puts "HERE6"
 ##Clone the input information
   set outputVolumeNode [ [ GetBatchVolumesLogic ] CloneVolume [ GetBatchMRMLScene ] ${volumeNode} ${NewObjName} ]
-  puts "HERE7"
 ##Attach the image info
   ${outputVolumeNode} SetAndObserveImageData ${GFilterOutput}
-  puts "HERE8"
   ${GFilter} Delete
-  puts "HERE9"
   return ${outputVolumeNode}
 }
 
@@ -66,40 +62,3 @@ proc b3_save_image { ImageToSave outputFileName } {
     $VolumeStorageNode Delete
 }
 
-
-#######
-####### The Real Work
-#######
-#######  Copy Image
-set fileName /Users/hjohnson/Desktop/TRACKHD_3TStudy/HDNI1/2007.05.04_09.22/ANONRAW/AVG_T1.nii.gz
-set inputImage [ b3_load_image ${fileName} ]
-set outputCopyFileName /Users/hjohnson/Desktop/NewCopyFile.nii.gz
-b3_save_image ${inputImage} ${outputCopyFileName}
-######   Create Gaussian Image
-set GaussianImage [b3_gaussian_filter ${inputImage} ]
-set outputGaussianFileName /Users/hjohnson/Desktop/NewGaussianFile.nii.gz
-b3_save_image ${GaussianImage} ${outputGaussianFileName}
-
-${inputImage} Delete
-${GaussianImage} Delete
-
-[ GetBatchMRMLScene ]  Delete
-${volumesLogic} Delete
-exit 0
-
-proc dummy {} {
-    [ vtkMRMLScalarVolumeNode New ]
-### Need to copy the display informaiton.
-  set newmatrix [ vtkMatrix4x4 New ]
-  ${volumeNode} GetIJKToRASMatrix ${newmatrix}
-  ${outputVolumeNode} SetIJKToRASMatrix ${newmatrix}
-#  ${outputVolumeNode} Copy ${volumeNode}; ## This causes a bus error
-## First let the Scene know about the Node
-  [ GetBatchMRMLScene ] AddNode ${outputVolumeNode}
-## Now let the node know about the Scene
-#  ${outputVolumeNode} SetScene [ GetBatchMRMLScene ]
-#  set OutputStorageNode [ vtkMRMLVolumeArchetypeStorageNode New ]
-#  [ GetBatchMRMLScene ]  AddNode ${OutputStorageNode}
-#  set OutputStorageNodeID [ ${OutputStorageNode} GetID ]
-#  ${outputVolumeNode} SetReferenceStorageNodeID ${OutputStorageNodeID}
-}
