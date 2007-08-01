9a9eb79116cf8e6940eade6504735e85c31ef1c1
hawle <hawle@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-08-01 15:07:53 +0000

    ENH: I have fixed an error I had in the reader which was causing the writer to have errors.  Now the reader and writer work.  I also added in some tests into the AnalyzeObjectMapTest.cxx to make sure that the object file I wrote out matches the original object file.
    -Jeffrey Hawley

--- ImageIOStreaming/Testing/AnalyzeObjectMapTest.cxx
+++ ImageIOStreaming/Testing/AnalyzeObjectMapTest.cxx
@@ -32,12 +32,13 @@
 #include <ImageToObjectmap.h>
 int main( int argc, char ** argv )
 {
+  int error_count = 0;
   if ( argc != 3 )
     {
     std::cerr << "USAGE: " << argv[0] << "<inputFileName> <outputFileName>" << std::endl;
     }
-  const std::string InputObjectFileName(argv[1]);
-  const std::string OuptputObjectFileName(argv[2]);
+  const char *InputObjectFileName = argv[1];
+  const char *OuptputObjectFileName = argv[2];
   typedef unsigned char       InputPixelType;
   typedef unsigned char       OutputPixelType;
   const   unsigned int        Dimension = 3;
@@ -83,7 +84,48 @@ int main( int argc, char ** argv )
     return EXIT_FAILURE;
     }
 
-  //reader->SetFileName("objectLabelTest2.obj");
-  //reader->Update();
-  //return EXIT_SUCCESS;
+  std::ifstream ReferenceFile;
+  ReferenceFile.open(InputObjectFileName, std::ios::binary | std::ios::in);
+
+  std::ifstream WrittenFile;
+  WrittenFile.open(OuptputObjectFileName, std::ios::binary | std::ios::in);
+
+  char ReferenceBytes;
+  char WrittenBytes;
+  int count = 0;
+  std::ofstream myfile;
+  myfile.open("testing1.txt");
+  while(!ReferenceFile.eof()  && !WrittenFile.eof())
+  {
+    count++;
+    ReferenceFile.read(reinterpret_cast<char *> (&ReferenceBytes), sizeof(char));
+    WrittenFile.read(reinterpret_cast<char *>(&WrittenBytes), sizeof(char));
+    if(ReferenceBytes != WrittenBytes)
+    {
+        error_count++;
+        myfile<<count<< ":" << (int)ReferenceBytes << ":" << (int)WrittenBytes << std::endl;
+    }
+  }
+
+  if(!ReferenceFile.eof())
+  {
+      error_count++;
+      std::cout<<"ReferenceFile is not at end of file"<<std::endl;
+  }
+  if(!WrittenFile.eof())
+  {
+      error_count++;
+      std::cout<<"WrittenFile is not at end of file"<<std::endl;
+  }
+
+  ReferenceFile.close();
+  WrittenFile.close();
+
+  if( error_count )
+  {
+  return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+
 }
