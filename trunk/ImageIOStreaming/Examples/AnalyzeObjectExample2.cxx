03892e362dbf342cca897f15983335544d88595c
hawle <hawle@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-08-13 17:03:15 +0000

    STYLE: Changed Example 2 so that users do not have to have vtk or itkapplications installed.  However, I left in all of the code to use vtk but I just commented it out.  I then added some code into the latex file.

--- trunk/ImageIOStreaming/Examples/AnalyzeObjectExample2.cxx
+++ trunk/ImageIOStreaming/Examples/AnalyzeObjectExample2.cxx
@@ -1,18 +1,17 @@
 /*This is a simple example for reading in an Analyze object map and
-then use vtk to display the object map.  The user will need ITK,
-Insight Applications (for itkImageToVTKImageFilter), and VTK.
+then showing how to use vtk to display the object map.  
 */
 
 #include "itkImage.h"
 #include "itkRGBPixel.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "vtkRenderer.h"
 
-#include "itkImageToVTKImageFilter.h"
 
-#include "vtkRenderWindowInteractor.h"
-#include <vtkImageViewer2.h>
+//#include "itkImageToVTKImageFilter.h"
+//#include "vtkRenderer.h"
+//#include "vtkRenderWindowInteractor.h"
+//#include <vtkImageViewer2.h>
 
 
 #include <ImageToObjectmap.h>
@@ -28,18 +27,15 @@ typedef itk::Image< RGBPixelType, Dimension > RGBImageType;
 
 typedef itk::ImageFileReader< ImageType > ReaderType;
 
-typedef itk::ImageToVTKImageFilter<RGBImageType> ConnectorType;
+//typedef itk::ImageToVTKImageFilter<RGBImageType> ConnectorType;
 
 int main(int argc, char * argv [] )
 {
-  if(argc != 3)
+  if(argc != 2)
   {
     std::cerr << "USAGE: " << argv[0] << "<inputFileName>" << std::endl;
   }
   const char *DisplayImage = argv[1];
-  const int SliceNumber = argv[2]
-
-  vtkRenderWindowInteractor *windowInteractor = vtkRenderWindowInteractor::New();
 
   ReaderType::Pointer reader  = ReaderType::New();
 
@@ -53,32 +49,38 @@ int main(int argc, char * argv [] )
     reader->Update();
   
     //This will convert the output of the reader into an object map
-    itk::ImageToObjectMap<ImageType>::Pointer ConvertImage = itk::ImageToObjectMap<ImageType>::New();;
+    itk::ImageToObjectMap<ImageType>::Pointer ConvertImage = itk::ImageToObjectMap<ImageType>::New();
     itk::AnalyzeObjectMap::Pointer Objectmap = ConvertImage->TransformImage(reader->GetOutput());
+
+    //If you have vtk and itkApplications installed then you can uncomment this out to display
+    //an object map to the screen.  Otherwise you can see how to display an object map using vtk.
 
     //Set the input, to the connector connecting itk with vtk, with an RGB image of the specific
     //colors that corresponds to each entry in the object map.
-    ConnectorType::Pointer connector= ConnectorType::New();
-    connector->SetInput( Objectmap->ObjectMapToRGBImage() );
 
-    connector->Update();
+    //vtkRenderWindowInteractor *windowInteractor = vtkRenderWindowInteractor::New();
+    //ConnectorType::Pointer connector= ConnectorType::New();
+    //connector->SetInput( Objectmap->ObjectMapToRGBImage() );
+
+    //connector->Update();
      
    //Display a two dimensional view of the object map that was read in
-   vtkImageViewer2 * twodimage = vtkImageViewer2::New();
+   //vtkImageViewer2 * twodimage = vtkImageViewer2::New();
 
-   twodimage->SetInput(connector->GetOutput());
-   twodimage->SetSlice(sliceNumber);
-   twodimage->SetSliceOrientationToXY();
+   //const int SliceNumber = argv[2];
+   //twodimage->SetInput(connector->GetOutput());
+   //twodimage->SetSlice(sliceNumber);
+   //twodimage->SetSliceOrientationToXY();
 
    //Set the background of the renderer to a grayish color so that it is easier to see
    //the outline of the object map since it is usually black
-   twodimage->GetRenderer()->SetBackground(0.4392, 0.5020, 0.5647);
+   //twodimage->GetRenderer()->SetBackground(0.4392, 0.5020, 0.5647);
 
 
-   twodimage->SetupInteractor(windowInteractor);
+   //twodimage->SetupInteractor(windowInteractor);
 
-   twodimage->Render();
-   windowInteractor->Start();
+   //twodimage->Render();
+   //windowInteractor->Start();
 
     }
   catch( itk::ExceptionObject & e )
