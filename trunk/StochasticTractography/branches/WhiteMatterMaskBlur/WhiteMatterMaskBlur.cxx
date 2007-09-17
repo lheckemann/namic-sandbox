48ca50bad0feed02d0b8a63e41f1f3dcb4c22984
tringo <tringo@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2007-09-17 21:36:13 +0000

    ENH:Initial version of whitemattermaskblur

---
+++ trunk/StochasticTractography/branches/WhiteMatterMaskBlur/WhiteMatterMaskBlur.cxx
@@ -0,0 +1,9 @@
+#include "itkSignedDanielssonDistanceMapImageFilter.h"
+#include "WhiteMatterMaskBlurCLP.h"
+
+int main( int argc, char* argv[] ){
+  typedef unsigned char InputPixelType;
+  typedef float OutputPixelType;
+  typedef itk::Image< InputPixelType, 2 > InputImageType;
+  typedef itk::Image< OutputPixelType, 2 > OutputImageType;
+}
