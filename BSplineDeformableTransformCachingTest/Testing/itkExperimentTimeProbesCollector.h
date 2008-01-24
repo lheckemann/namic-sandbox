f03fe3ebb93c71b76b17fb3a9e09179d794d4d02
Matt.Turek <Matt.Turek@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-01-24 21:22:53 +0000

    ENH: Allows for different reporting of timing results

---
+++ BSplineDeformableTransformCachingTest/Testing/itkExperimentTimeProbesCollector.h
@@ -0,0 +1,36 @@
+
+#ifndef ExperimentTimeProbesCollector_h
+#define ExperimentTimeProbesCollector_h
+
+#include "itkTimeProbesCollectorBase.h"
+
+#include <string>
+
+namespace itk
+{
+class ExperimentTimeProbesCollector : public TimeProbesCollectorBase
+{
+public:
+  
+  ExperimentTimeProbesCollector();
+
+  ~ExperimentTimeProbesCollector();
+
+  /** Report the summary of results from the time probes */
+  virtual void Report( std::ostream & os = std::cout ) const;
+
+  void SetExperimentString( std::string& );
+  void SetDelimeterString( std::string& );
+
+protected:
+  void WriteReportHeader( std::ostream & os = std::cout ) const;
+  void WriteReportBody  ( std::ostream & os ) const;
+
+  typedef std::string StringType;
+  StringType m_ExperimentString;
+  StringType m_Delimeter;
+};
+}
+
+#endif // ExperimentTimeProbesCollector_h
+
