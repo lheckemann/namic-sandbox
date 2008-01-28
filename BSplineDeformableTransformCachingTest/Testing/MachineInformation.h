327c2480a337079fa84bb80277746c8db8f682fe
Matt.Turek <Matt.Turek@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-01-28 22:58:00 +0000

    ENH: Collects information about the test machine.

---
+++ BSplineDeformableTransformCachingTest/Testing/MachineInformation.h
@@ -0,0 +1,17 @@
+
+#ifndef __MachineInformation_h__
+#define __MachineInformation_h__
+
+#include <iostream>
+
+class MachineInformation
+{
+public:
+  static void DescribeMachine( std::ostream& );
+  static void DescribeMachineLatex( std::ostream& );
+
+protected:
+
+};
+
+#endif // __MachineInformation_h__
