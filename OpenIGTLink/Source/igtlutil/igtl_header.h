a8ae187e2f2108053428ac13dc65479aa15ebb0a
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-02-01 23:33:18 +0000

    ENH: Added header packing utility (igtlutil)

---
+++ OpenIGTLink/Source/igtlutil/igtl_header.h
@@ -0,0 +1,47 @@
+/*=========================================================================
+
+  Program:   Open ITK Link Library
+  Module:    $RCSfile: $
+  Language:  C
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __IGTL_HEADER_H
+#define __IGTL_HEADER_H
+
+#define IGTL_HEADER_VERSION   1
+#define IGTL_HEADER_SIZE      58
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma pack(1)     /* For 1-byte boundary in memroy */
+
+struct igtl_header {
+  unsigned short version;
+  char           name[8];
+  char           device_name[20];
+  long long      timestamp;
+  long long      body_size;
+  long long      crc;
+};
+
+#pragma pack(0)
+
+void igtl_header_convert_byte_order(struct igtl_header* header);
+
+#ifdef __cplusplus
+}
+#endif
+#endif __IGTL_HEADER_H
+
+
---
+++ OpenIGTLink/Source/igtlutil/igtl_image.h
@@ -0,0 +1,78 @@
+/*=========================================================================
+
+  Program:   Open ITK Link Library
+  Module:    $RCSfile: $
+  Language:  C
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __IGTL_IMAGE_H
+#define __IGTL_IMAGE_H
+
+#include "igtl_util.h"
+
+#define IGTL_IMAGE_HEADER_VERSION  1
+#define IGTL_IMAGE_HEADER_SIZE     72
+
+/* Data type */
+#define IGTL_IMAGE_DTYPE_SCALAR  1
+#define IGTL_IMAGE_DTYPE_VECTOR  2
+
+/* Scalar type */
+#define IGTL_IMAGE_STYPE_TYPE_INT8      2
+#define IGTL_IMAGE_STYPE_TYPE_UINT8     3
+#define IGTL_IMAGE_STYPE_TYPE_INT16     4
+#define IGTL_IMAGE_STYPE_TYPE_UINT16    5
+#define IGTL_IMAGE_STYPE_TYPE_INT32     6
+#define IGTL_IMAGE_STYPE_TYPE_UINT32    7
+
+/* Endian */
+#define IGTL_IMAGE_ENDIAN_BIG       1
+#define IGTL_IMAGE_ENDIAN_LITTLE    2
+
+/* Image coordinate system */
+#define IGTL_IMAGE_COORD_RAS           1
+#define IGTL_IMAGE_COORD_LPS           2
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#pragma pack(1)     /* For 1-byte boundary in memroy */
+
+struct igtl_image_header {
+  unsigned short version;
+  unsigned char  data_type;
+  unsigned char  scalar_type;
+  unsigned char  endian;
+  unsigned char  coord;
+  unsigned short size[3];
+  float          matrix[12];
+  unsigned short subvol_offset[3];
+  unsigned short subvol_size[3];
+};
+
+#pragma pack(0)
+
+
+
+long long igtl_image_get_data_size(struct igtl_image_header* header);
+void igtl_image_get_matrix(float spacing[3], float origin[3],
+                            float norm_i[3], float norm_j[3], float norm_k[3],
+                            struct igtl_image_header* header);
+void igtl_image_convert_byte_order(struct igtl_image_header* header);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __IGTL_IMAGE_H */
