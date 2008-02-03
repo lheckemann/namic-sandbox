107a644d20106fd8e7f01eeac7bcb1c1016aa24d
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-02-03 18:08:19 +0000

    ENH: Fixing the build on Windows.      Replacing #include "vtkWindows.h" with <windows.h>

--- OpenIGTLink/Source/igtlSocket.cxx
+++ OpenIGTLink/Source/igtlSocket.cxx
@@ -31,8 +31,7 @@
 #include "igtlSocket.h"
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
-  #define VTK_WINDOWS_FULL
-  #include "vtkWindows.h"
+  #include <windows.h>
 #else
   #include <sys/types.h>
   #include <sys/socket.h>
