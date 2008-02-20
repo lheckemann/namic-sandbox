ec7d23910ccbf02e4dd5efa9d51af55f83a89709
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-02-20 03:20:32 +0000

    ENH: Missing copyright header.

--- trunk/BSplineDeformableTransformCachingTest/Testing/MachineInformation.cxx
+++ trunk/BSplineDeformableTransformCachingTest/Testing/MachineInformation.cxx
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: DeformableRegistration8.cxx,v $
+  Language:  C++
+  Date:      $Date: 2007/11/22 00:30:16 $
+  Version:   $Revision: 1.16 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 
 #include "MachineInformation.h"
 
