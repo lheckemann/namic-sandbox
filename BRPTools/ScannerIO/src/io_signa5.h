11b302c50727cdbaa8961784c9f29a18dc9c72ae
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-04-08 04:01:22 +0000

    COMP: integer types in signa io library.

--- BRPTools/ScannerIO/src/io_signa5.h
+++ BRPTools/ScannerIO/src/io_signa5.h
@@ -30,9 +30,10 @@
 
 #define _DEBUG_GENRDR
 
-#include <stdint.h>
+/*#include <stdint.h>*/
 
 #include "io_base.h"
+#include "igtl_types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,115 +42,115 @@ extern "C" {
 /*** Control Header data ***/
 
 typedef struct {
-  int32_t   pixOffset;     /* byte displacement to pixel data*/
-  int32_t   width;         /* width */
-  int32_t   height;        /* height */
-  int32_t   depth;         /* depth (bits) */
-  int32_t   compression;   /* compression (0=asis,1=rectangular,2=packed,
+  igtl_int32   pixOffset;     /* byte displacement to pixel data*/
+  igtl_int32   width;         /* width */
+  igtl_int32   height;        /* height */
+  igtl_int32   depth;         /* depth (bits) */
+  igtl_int32   compression;   /* compression (0=asis,1=rectangular,2=packed,
                               3=compressed,4=compressed&packed) */
-  int32_t   bgShade;       /* background shade to use for non-image */
-  uint16_t  eacSum;        /* 16 bit end_around_carry sum of pixels */ 
-  int32_t   pImageId;      /* ptr to unique image identifier */
-  int32_t   lImageId;      /* length of unique image identifier */
-  int32_t   pUnpackHeader; /* ptr to unpack header */
-  int32_t   lUnpackHeader; /* length of unpack header */
-  int32_t   pCompHeader;   /* ptr to compression header */
-  int32_t   lCompHeader;   /* length of compression header */
-  int32_t   pHistHeader;   /* ptr to histogram header */
-  int32_t   lHistHeader;   /* length of histogram header */
-  int32_t   pTextPlane;    /* ptr to text plane */
-  int32_t   lTextPlane;    /* length of text plane */
-  int32_t   pGraphPlane;   /* ptr to graphics plane */
-  int32_t   lGraphPlane;   /* length of graphics plane */
-  int32_t   pDBPlane;      /* ptr to data base header */
-  int32_t   lDBPlane;      /* length of data base header */
-  int32_t   vStoredPixAdd; /* value to add to stored pixels */
-  int32_t   pUserData;     /* ptr to user defined data */
-  int32_t   lUserData;     /* length of user defined data */
-  int32_t   pSuiteHeader;  /* ptr to suite header */
-  int32_t   lSuiteHeader;  /* length of suite header */
-  int32_t   pExamHeader;   /* ptr to exam header */
-  int32_t   lExamHeader;   /* length of exam header */
-  int32_t   pSerHeader;    /* ptr to series header */
-  int32_t   lSerHeader;    /* length of series header */
-  int32_t   pImgHeader;    /* ptr to image header */
-  int32_t   lImgHeader;    /* length of image header */
+  igtl_int32   bgShade;       /* background shade to use for non-image */
+  igtl_uint16  eacSum;        /* 16 bit end_around_carry sum of pixels */ 
+  igtl_int32   pImageId;      /* ptr to unique image identifier */
+  igtl_int32   lImageId;      /* length of unique image identifier */
+  igtl_int32   pUnpackHeader; /* ptr to unpack header */
+  igtl_int32   lUnpackHeader; /* length of unpack header */
+  igtl_int32   pCompHeader;   /* ptr to compression header */
+  igtl_int32   lCompHeader;   /* length of compression header */
+  igtl_int32   pHistHeader;   /* ptr to histogram header */
+  igtl_int32   lHistHeader;   /* length of histogram header */
+  igtl_int32   pTextPlane;    /* ptr to text plane */
+  igtl_int32   lTextPlane;    /* length of text plane */
+  igtl_int32   pGraphPlane;   /* ptr to graphics plane */
+  igtl_int32   lGraphPlane;   /* length of graphics plane */
+  igtl_int32   pDBPlane;      /* ptr to data base header */
+  igtl_int32   lDBPlane;      /* length of data base header */
+  igtl_int32   vStoredPixAdd; /* value to add to stored pixels */
+  igtl_int32   pUserData;     /* ptr to user defined data */
+  igtl_int32   lUserData;     /* length of user defined data */
+  igtl_int32   pSuiteHeader;  /* ptr to suite header */
+  igtl_int32   lSuiteHeader;  /* length of suite header */
+  igtl_int32   pExamHeader;   /* ptr to exam header */
+  igtl_int32   lExamHeader;   /* length of exam header */
+  igtl_int32   pSerHeader;    /* ptr to series header */
+  igtl_int32   lSerHeader;    /* length of series header */
+  igtl_int32   pImgHeader;    /* ptr to image header */
+  igtl_int32   lImgHeader;    /* length of image header */
 } ControlHeader;
 
 
 /*** Exam Header data ***/
 
 typedef struct {
-  char      suiteId[4];     /* suite ID */
-  uint16_t  examNo;         /* exam number */
-  char      patientId[13];  /* patient ID */
-  char      patientName[25];/* patient name */
-  int16_t   patientAge;     /* patient age */
-  int16_t   patientSex;     /* patient sex */
-  char      examType[3];    /* exam type - "MR" or "CT" */
+  igtl_int8    suiteId[4];     /* suite ID */
+  igtl_uint16  examNo;         /* exam number */
+  igtl_int8    patientId[13];  /* patient ID */
+  igtl_int8    patientName[25];/* patient name */
+  igtl_int16   patientAge;     /* patient age */
+  igtl_int16   patientSex;     /* patient sex */
+  igtl_int8    examType[3];    /* exam type - "MR" or "CT" */
 } ExamHeader;
 
 
 /*** Series Header data ***/
 
 typedef struct {
-  int16_t   serNo;          /* series number */
-  char      anatomRef[3];   /* anatomical reference */
-  char      scanProt[25];   /* scan protocol name */
+  igtl_int16   serNo;          /* series number */
+  igtl_int8    anatomRef[3];   /* anatomical reference */
+  igtl_int8    scanProt[25];   /* scan protocol name */
 } SeriesHeader;
 
 
 /*** Image Header data ***/
 typedef struct {
   /** common part for CT and MR **/
-  int16_t   imgNo;          /* image number */
-  float32_t sliceThick;     /* slice thickness mm */
-  int16_t   matrixX;        /* matrix size - X */
-  int16_t   matrixY;        /* matrix size - Y */
-  float32_t dispFovX;       /* display field of view - X (mm) */
-  float32_t dispFovY;       /* display field of view - Y (mm) */
-  float32_t imgDimX;        /* image dimension - X */
-  float32_t imgDimY;        /* image dimension - Y */
-  float32_t pixSizeX;       /* pixel size - X */
-  float32_t pixSizeY;       /* pixel size - Y */
-  char      pixDataId[14];  /* pixel data ID */
-  char      ivContAgent[17];/* iv contrast agent */
-  char      oralContAgent[17]; /* oral contrast agent */
+  igtl_int16   imgNo;          /* image number */
+  igtl_float32 sliceThick;     /* slice thickness mm */
+  igtl_int16   matrixX;        /* matrix size - X */
+  igtl_int16   matrixY;        /* matrix size - Y */
+  igtl_float32 dispFovX;       /* display field of view - X (mm) */
+  igtl_float32 dispFovY;       /* display field of view - Y (mm) */
+  igtl_float32 imgDimX;        /* image dimension - X */
+  igtl_float32 imgDimY;        /* image dimension - Y */
+  igtl_float32 pixSizeX;       /* pixel size - X */
+  igtl_float32 pixSizeY;       /* pixel size - Y */
+  igtl_int8    pixDataId[14];  /* pixel data ID */
+  igtl_int8    ivContAgent[17];/* iv contrast agent */
+  igtl_int8    oralContAgent[17]; /* oral contrast agent */
 
-  float32_t imgLoc;         /* image location */
-  float32_t imgCR;          /* image centre R mm (ie. X +ve to right) */
-  float32_t imgCA;          /* image centre A mm (ie. Y +ve to anterior) */
-  float32_t imgCS;          /* image centre S mm (ie. Z +ve to superior) */
-  float32_t imgTlhcR;       /* image TLHC R mm (ie. X +ve to right) */
-  float32_t imgTlhcA;       /* image TLHC A mm (ie. Y +ve to anterior) */
-  float32_t imgTlhcS;       /* image TLHC S mm (ie. Z +ve to superior) */
-  float32_t imgTrhcR;       /* image TRHC R mm (ie. X +ve to right) */
-  float32_t imgTrhcA;       /* image TRHC A mm (ie. Y +ve to anterior) */
-  float32_t imgTrhcS;       /* image TRHC S mm (ie. Z +ve to superior) */
-  float32_t imgBrhcR;       /* image BRHC R mm (ie. X +ve to right) */
-  float32_t imgBrhcA;       /* image BRHC A mm (ie. Y +ve to anterior) */
-  float32_t imgBrhcS;       /* image BRHC S mm (ie. Z +ve to superior) */
+  igtl_float32 imgLoc;         /* image location */
+  igtl_float32 imgCR;          /* image centre R mm (ie. X +ve to right) */
+  igtl_float32 imgCA;          /* image centre A mm (ie. Y +ve to anterior) */
+  igtl_float32 imgCS;          /* image centre S mm (ie. Z +ve to superior) */
+  igtl_float32 imgTlhcR;       /* image TLHC R mm (ie. X +ve to right) */
+  igtl_float32 imgTlhcA;       /* image TLHC A mm (ie. Y +ve to anterior) */
+  igtl_float32 imgTlhcS;       /* image TLHC S mm (ie. Z +ve to superior) */
+  igtl_float32 imgTrhcR;       /* image TRHC R mm (ie. X +ve to right) */
+  igtl_float32 imgTrhcA;       /* image TRHC A mm (ie. Y +ve to anterior) */
+  igtl_float32 imgTrhcS;       /* image TRHC S mm (ie. Z +ve to superior) */
+  igtl_float32 imgBrhcR;       /* image BRHC R mm (ie. X +ve to right) */
+  igtl_float32 imgBrhcA;       /* image BRHC A mm (ie. Y +ve to anterior) */
+  igtl_float32 imgBrhcS;       /* image BRHC S mm (ie. Z +ve to superior) */
 
   /** part for MR **/
-  int32_t   tr;             /* repetition time(usec) */
-  int32_t   ti;             /* inversion time(usec) */
-  int32_t   te;             /* echo time(usec) */
-  int16_t   nEchoes;        /* number of echoes */
-  int16_t   echoNo;         /* echo number */
-  float32_t nex;            /* NEX */
-  char      seqName[33];    /* pulse sequence name */
-  char      coilName[17];   /* coil name */
-  int16_t   etl;            /* ETL for FSE */
+  igtl_int32   tr;             /* repetition time(usec) */
+  igtl_int32   ti;             /* inversion time(usec) */
+  igtl_int32   te;             /* echo time(usec) */
+  igtl_int16   nEchoes;        /* number of echoes */
+  igtl_int16   echoNo;         /* echo number */
+  igtl_float32 nex;            /* NEX */
+  igtl_int8    seqName[33];    /* pulse sequence name */
+  igtl_int8    coilName[17];   /* coil name */
+  igtl_int16   etl;            /* ETL for FSE */
 } ImageHeader;
 
 typedef struct {
-  int            width;
-  int            height;
-  int            depth;
-  unsigned char* imageData;
-  ExamHeader     eh;
-  SeriesHeader   sh;
-  ImageHeader    ih;
+  igtl_int32   width;
+  igtl_int32   height;
+  igtl_int32   depth;
+  igtl_uint8*  imageData;
+  ExamHeader   eh;
+  SeriesHeader sh;
+  ImageHeader  ih;
 } GenesisImageInfo;
 
 
