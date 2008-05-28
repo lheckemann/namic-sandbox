cbfca8973fca2eea9121b1b62a6fc71c6ef467a5
Koduka <Koduka@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-05-28 11:00:31 +0000

    ENH:a new file

---
+++ trunk/IntelligentSI/Control/slave/robot.h
@@ -0,0 +1,10 @@
+/***************************************************************************
+ * FileName      : robot.h
+ * Created       : 2007/09/14
+ * LastModified  : 2007/09/1
+ * Author        : Hiroaki KOZUKA
+ * Aim           : Main program for a Robot Control
+                   robot class
+ * OS            : VxWorks 5.5.1
+ ***************************************************************************/
+
