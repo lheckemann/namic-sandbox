68e62eb0cff8b9f6e1fd3f060f4eb7c30ec555d2
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-04 00:34:07 +0000

    ENH: Slice control function has been implemented.

--- trunk/IGTLoadableModules/OpenIGTLink/vtkOpenIGTLinkGUI.h
+++ trunk/IGTLoadableModules/OpenIGTLink/vtkOpenIGTLinkGUI.h
@@ -77,11 +77,6 @@ class VTK_OPENIGTLINK_EXPORT vtkOpenIGTLinkGUI : public vtkSlicerModuleGUI
     UPDATE_ALL             = 3,  // Update whole list (incl. changed number of items)
   };
   
-  static const double WorkPhaseColor[vtkOpenIGTLinkLogic::NumPhases][3];
-  static const double WorkPhaseColorActive[vtkOpenIGTLinkLogic::NumPhases][3];
-  static const double WorkPhaseColorDisabled[vtkOpenIGTLinkLogic::NumPhases][3];
-  static const char* WorkPhaseStr[vtkOpenIGTLinkLogic::NumPhases];
-
   static const char* ConnectorTypeStr[vtkIGTLConnector::NUM_TYPE];
   static const char* ConnectorStatusStr[vtkIGTLConnector::NUM_STATE];
   //ETX
@@ -184,6 +179,7 @@ class VTK_OPENIGTLINK_EXPORT vtkOpenIGTLinkGUI : public vtkSlicerModuleGUI
   // Visualization Control Frame
 
   vtkKWCheckButton *FreezeImageCheckButton;
+  vtkKWCheckButton *ObliqueCheckButton;
   vtkKWPushButton  *SetLocatorModeButton;
   vtkKWPushButton  *SetUserModeButton;
 
@@ -197,6 +193,9 @@ class VTK_OPENIGTLINK_EXPORT vtkOpenIGTLinkGUI : public vtkSlicerModuleGUI
   vtkKWPushButton  *StopScanButton;
 
   vtkKWCheckButton *LocatorCheckButton;
+
+  bool              IsSliceOrientationAdded;
+
   
   // Module logic and mrml pointers
 
@@ -211,29 +210,24 @@ class VTK_OPENIGTLINK_EXPORT vtkOpenIGTLinkGUI : public vtkSlicerModuleGUI
   vtkCallbackCommand *DataCallbackCommand;
 
   // Access the slice windows
-  vtkSlicerSliceLogic *Logic0;
-  vtkSlicerSliceLogic *Logic1;
-  vtkSlicerSliceLogic *Logic2;
   vtkMRMLSliceNode *SliceNode0;
   vtkMRMLSliceNode *SliceNode1;
   vtkMRMLSliceNode *SliceNode2;
+  /*
+  vtkSlicerSliceLogic *Logic0;
+  vtkSlicerSliceLogic *Logic1;
+  vtkSlicerSliceLogic *Logic2;
   vtkSlicerSliceControllerWidget *Control0;
   vtkSlicerSliceControllerWidget *Control1;
   vtkSlicerSliceControllerWidget *Control2;
+  */
 
   //BTX
   std::string LocatorModelID;
   std::string LocatorModelID_new;
   //ETX
   
-  int NeedOrientationUpdate0;
-  int NeedOrientationUpdate1;
-  int NeedOrientationUpdate2;
-  
-  //int NeedRealtimeImageUpdate;
-  int FreezeOrientationUpdate;
-
-  int RealtimeImageOrient;
+  //int RealtimeImageOrient;
 
   //----------------------------------------------------------------
   // Locator Model
@@ -250,15 +244,6 @@ class VTK_OPENIGTLINK_EXPORT vtkOpenIGTLinkGUI : public vtkSlicerModuleGUI
   vtkMRMLFiducialListNode *FiducialListNode;
 
   void UpdateAll();
-  /*
-    void UpdateLocator(vtkTransform *, vtkTransform *);
-    void UpdateSliceDisplay(float nx, float ny, float nz, 
-    float tx, float ty, float tz, 
-    float px, float py, float pz);
-
-    void UpdateLocator();
-    void UpdateSliceDisplay();
-  */
 
  private:
 
