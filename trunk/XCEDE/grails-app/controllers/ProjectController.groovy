2508380341a66fedbe1ac7448590e58adcca4541
blezek <blezek@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-07 02:54:14 +0000

    ENH: Added prototype REST support

--- trunk/XCEDE/grails-app/controllers/ProjectController.groovy
+++ trunk/XCEDE/grails-app/controllers/ProjectController.groovy
@@ -1,3 +1,25 @@
 class ProjectController {
   def scaffold = Project
+
+    /*
+    def showXML = {
+      if ( params.id && Project.exists ( params.id ) ) {
+        def p = Project.get(params.id)
+        render ( contentType:'text/xml') {
+          Projects {
+            Project ( name:p.name, description:p.description, id:p.id )
+          }
+        }
+      } else {
+        def all = Project.list()
+        render ( contentType:'text/xml') {
+          Projects {
+            for ( p in all ) {
+              Project ( name:p.name, description:p.description, id:p.id )
+            }
+          }
+        }
+      }
+    }
+    */
 }
