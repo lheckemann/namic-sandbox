7c4d99de83f89a1338e2b8175067e870d53c04c6
hliu <hliu@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-20 20:09:07 +0000

    BUG: updated NeuroNav to be loadable.

--- trunk/IGTLoadableModules/NeuroNav/vtkNeuroNavLogic.cxx
+++ trunk/IGTLoadableModules/NeuroNav/vtkNeuroNavLogic.cxx
@@ -1,67 +1,85 @@
-/*=auto=========================================================================
+/*==========================================================================
 
-  Portions (c) Copyright 2006 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
   See Doc/copyright/copyright.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
 
   Program:   3D Slicer
-  Module:    $RCSfile: $
+  Module:    $HeadURL: $
   Date:      $Date: $
   Version:   $Revision: $
 
-=========================================================================auto=*/
+==========================================================================*/
 
 #include "vtkObjectFactory.h"
 #include "vtkCallbackCommand.h"
 
 #include "vtkNeuroNavLogic.h"
 
-#include "vtkLandmarkTransform.h"
+#include "vtkMRMLModelDisplayNode.h"
+#include "vtkMRMLScalarVolumeNode.h"
+#include "vtkMRMLLinearTransformNode.h"
+#include "vtkSlicerApplication.h"
+#include "vtkSlicerApplicationGUI.h"
+#include "vtkSlicerColorLogic.h"
+
 #include "vtkCylinderSource.h"
+#include "vtkSphereSource.h"
+#include "vtkAppendPolyData.h"
 
-#include "vtkSlicerConfigure.h" /* Slicer3_USE_* */
 
-#ifdef Slicer3_USE_IGSTK
-#include "igstkAuroraTracker.h"
-#endif
+#include "vtkMultiThreader.h"
+
 
 vtkCxxRevisionMacro(vtkNeuroNavLogic, "$Revision: 1.9.12.1 $");
+vtkStandardNewMacro(vtkNeuroNavLogic);
 
-vtkNeuroNavLogic* vtkNeuroNavLogic::New()
-{
-  // First try to create the object from the vtkObjectFactory
-  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkNeuroNavLogic");
-  if(ret)
-    {
-      return (vtkNeuroNavLogic*)ret;
-    }
-  // If the factory was unable to create the object, then create it here.
-  return new vtkNeuroNavLogic;
-}
-
-
+//---------------------------------------------------------------------------
 vtkNeuroNavLogic::vtkNeuroNavLogic()
 {
-#ifdef Slicer3_USE_IGSTK
-  igstk::RealTimeClock::Initialize();
-#endif
+
+  this->SliceDriver[0] = vtkNeuroNavLogic::SLICE_DRIVER_USER;
+  this->SliceDriver[1] = vtkNeuroNavLogic::SLICE_DRIVER_USER;
+  this->SliceDriver[2] = vtkNeuroNavLogic::SLICE_DRIVER_USER;
+
+  // If the following code doesn't work, slice nodes should be obtained from application GUI
+  this->SliceNode[0] = NULL;
+  this->SliceNode[1] = NULL;
+  this->SliceNode[2] = NULL;
+
+  this->NeedRealtimeImageUpdate0 = 0;
+  this->NeedRealtimeImageUpdate1 = 0;
+  this->NeedRealtimeImageUpdate2 = 0;
+  this->ImagingControl = 0;
+
+  // Timer Handling
+
+  this->EnableOblique = false;
+  this->FreezePlane   = false;
+
 }
 
 
-
+//---------------------------------------------------------------------------
 vtkNeuroNavLogic::~vtkNeuroNavLogic()
 {
 
+
 }
 
 
-
+//---------------------------------------------------------------------------
 void vtkNeuroNavLogic::PrintSelf(ostream& os, vtkIndent indent)
 {
-    this->vtkObject::PrintSelf(os, indent);
+  this->vtkObject::PrintSelf(os, indent);
 
-    os << indent << "vtkNeuroNavLogic:             " << this->GetClassName() << "\n";
+  os << indent << "vtkNeuroNavLogic:             " << this->GetClassName() << "\n";
 
 }
+
+
+
+
+
 
