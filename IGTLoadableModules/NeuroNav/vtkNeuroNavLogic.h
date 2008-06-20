8377b0261e43ec1b0f43edee2cdddc2b166baa8c
hliu <hliu@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-20 20:09:00 +0000

    BUG: updated NeuroNav to be loadable.

--- IGTLoadableModules/NeuroNav/vtkNeuroNavLogic.h
+++ IGTLoadableModules/NeuroNav/vtkNeuroNavLogic.h
@@ -1,16 +1,16 @@
-/*=auto=========================================================================
+/*==========================================================================
 
-  Portions (c) Copyright 2006 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
 
   See Doc/copyright/copyright.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
 
   Program:   3D Slicer
-  Module:    $RCSfile: $
+  Module:    $HeadURL: $
   Date:      $Date: $
   Version:   $Revision: $
 
-=========================================================================auto=*/
+==========================================================================*/
 
 // .NAME vtkNeuroNavLogic - slicer logic class for Locator module 
 // .SECTION Description
@@ -21,28 +21,147 @@
 #ifndef __vtkNeuroNavLogic_h
 #define __vtkNeuroNavLogic_h
 
+#include <vector>
+
 #include "vtkNeuroNavWin32Header.h"
+
 #include "vtkSlicerBaseLogic.h"
-#include "vtkSlicerLogic.h"
+#include "vtkSlicerModuleLogic.h"
+#include "vtkSlicerApplication.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkMRMLFiducialListNode.h"
+#include "vtkMRMLSliceNode.h"
 
 
-class VTK_NEURONAV_EXPORT vtkNeuroNavLogic : public vtkSlicerLogic 
+
+class VTK_NEURONAV_EXPORT vtkNeuroNavLogic : public vtkSlicerModuleLogic 
 {
-public:
+ public:
+  //BTX
+  enum WorkPhase {
+    StartUp = 0,
+    Planning,
+    Calibration,
+    Targeting,
+    Manual,
+    Emergency,
+    NumPhases,
+  };
 
-    // The Usual vtk class functions
-    static vtkNeuroNavLogic *New();
-    vtkTypeRevisionMacro(vtkNeuroNavLogic,vtkObject);
-    void PrintSelf(ostream& os, vtkIndent indent);
+  enum {
+    SLICE_DRIVER_USER    = 0,
+    SLICE_DRIVER_LOCATOR = 1,
+    SLICE_DRIVER_RTIMAGE = 2
+  };
+  enum ImageOrient{
+    SLICE_RTIMAGE_NONE      = 0,
+    SLICE_RTIMAGE_PERP      = 1,
+    SLICE_RTIMAGE_INPLANE90 = 2,
+    SLICE_RTIMAGE_INPLANE   = 3
+  };
+  enum {  // Events
+    LocatorUpdateEvent      = 50000,
+    StatusUpdateEvent       = 50001,
+    //SliceUpdateEvent        = 50002,
+  };
+
+  //ETX
+  
+  // Work phase keywords used in NaviTrack (defined in BRPTPRInterface.h)
+
+ public:
+  
+  static vtkNeuroNavLogic *New();
+  
+  vtkTypeRevisionMacro(vtkNeuroNavLogic,vtkObject);
+
+  vtkSetMacro ( NeedRealtimeImageUpdate0, int );
+  vtkGetMacro ( NeedRealtimeImageUpdate0, int );
+  vtkSetMacro ( NeedRealtimeImageUpdate1, int );
+  vtkGetMacro ( NeedRealtimeImageUpdate1, int );
+  vtkSetMacro ( NeedRealtimeImageUpdate2, int );
+  vtkGetMacro ( NeedRealtimeImageUpdate2, int );
+
+  vtkSetMacro ( NeedUpdateLocator,       bool );
+  vtkGetMacro ( NeedUpdateLocator,       bool );
+
+  void SetSliceDriver0(int v) { this->SliceDriver[0] = v; };
+  void SetSliceDriver1(int v) { this->SliceDriver[1] = v; };
+  void SetSliceDriver2(int v) { this->SliceDriver[2] = v; };
+  int  SetSliceDriver0() { return this->SliceDriver[0]; };
+  int  SetSliceDriver1() { return this->SliceDriver[1]; };
+  int  SetSliceDriver2() { return this->SliceDriver[2]; };
 
 
-protected:
+  vtkGetMacro ( Connection,              bool );
 
-    vtkNeuroNavLogic();
-    ~vtkNeuroNavLogic();
-    vtkNeuroNavLogic(const vtkNeuroNavLogic&);
-    void operator=(const vtkNeuroNavLogic&);
+  vtkSetMacro ( EnableOblique,           bool );
+  vtkGetMacro ( EnableOblique,           bool );
+  vtkSetMacro ( FreezePlane,             bool );
+  vtkGetMacro ( FreezePlane,              bool );
 
+  vtkGetObjectMacro ( LocatorTransform, vtkTransform );
+  vtkGetObjectMacro ( LocatorMatrix,    vtkMatrix4x4 );
+
+  void PrintSelf(ostream&, vtkIndent);
+  //void AddRealtimeVolumeNode(const char* name);
+
+
+ protected:
+  
+  vtkNeuroNavLogic();
+  ~vtkNeuroNavLogic();
+  vtkNeuroNavLogic(const vtkNeuroNavLogic&);
+  void operator=(const vtkNeuroNavLogic&);
+
+  
+
+ private:
+
+  //----------------------------------------------------------------
+  // Monitor Timer
+  //----------------------------------------------------------------
+
+  int MonitorFlag;
+  int MonitorInterval;
+  
+
+  //----------------------------------------------------------------
+  // Real-time image
+  //----------------------------------------------------------------
+  
+  vtkMRMLVolumeNode     *RealtimeVolumeNode;
+
+  int   NeedRealtimeImageUpdate0;
+  int   NeedRealtimeImageUpdate1;
+  int   NeedRealtimeImageUpdate2;
+
+  vtkMRMLSliceNode *SliceNode[3];
+
+  int   SliceDriver[3];
+  
+  bool  ImagingControl;
+  bool  NeedUpdateLocator;
+
+  bool  EnableOblique;
+  bool  FreezePlane;
+
+  long  RealtimeImageTimeStamp;
+  //int   RealtimeImageSerial;
+  int   RealtimeImageOrient;
+
+
+  //----------------------------------------------------------------
+  // Locator
+  //----------------------------------------------------------------
+
+  // What's a difference between LocatorMatrix and Locator Transform???
+  vtkMatrix4x4*         LocatorMatrix;
+  vtkTransform*         LocatorTransform;
+
+  bool  Connection;  
+  
 };
 
 #endif
