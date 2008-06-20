3f116743bcd53bf8c60bc5332bd99a98e13ef578
hliu <hliu@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-20 20:08:45 +0000

    BUG: updated NeuroNav to be loadable.

--- IGTLoadableModules/NeuroNav/vtkNeuroNavGUI.h
+++ IGTLoadableModules/NeuroNav/vtkNeuroNavGUI.h
@@ -1,11 +1,19 @@
-// .NAME vtkNeuroNavGUI 
-// .SECTION Description
-// Main Volumes GUI and mediator methods for slicer3. 
+/*==========================================================================
 
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $HeadURL: $
+  Date:      $Date: $
+  Version:   $Revision: $
+
+==========================================================================*/
 
 #ifndef __vtkNeuroNavGUI_h
 #define __vtkNeuroNavGUI_h
-
 
 #ifdef WIN32
 #include "vtkNeuroNavWin32Header.h"
@@ -19,20 +27,11 @@
 #include "vtkCallbackCommand.h"
 #include "vtkSlicerInteractorStyle.h"
 
-
 #include <string>
 
-#include "vtkSlicerConfigure.h" /* Slicer3_USE_* */
-
-//#ifdef Slicer3_USE_NAVITRACK
-//#include "vtkIGTOpenTrackerStream.h"
-//#endif
-#ifdef Slicer3_USE_IGSTK
-#include "vtkIGTIGSTKStream.h"
-#endif
-
-
 class vtkKWPushButton;
+class vtkKWPushButtonSet;
+class vtkKWRadioButtonSet;
 class vtkKWEntryWithLabel;
 class vtkKWMenuButtonWithLabel;
 class vtkKWMenuButton;
@@ -43,205 +42,230 @@ class vtkKWFrame;
 class vtkKWEntryWithLabel;
 class vtkKWLoadSaveButtonWithLabel;
 class vtkKWMultiColumnListWithScrollbars;
+class vtkKWWizardWidget;
 
+class vtkTransform;
 
 // Description:    
-// This class implements Slicer's Volumes GUI.
+// This class implements Slicer's Volumes GUI
 //
 class VTK_NEURONAV_EXPORT vtkNeuroNavGUI : public vtkSlicerModuleGUI
 {
  public:
-    // Description:    
-    // Usual vtk class functions
-    static vtkNeuroNavGUI* New (  );
-    vtkTypeRevisionMacro ( vtkNeuroNavGUI, vtkSlicerModuleGUI );
-    void PrintSelf (ostream& os, vtkIndent indent );
-    
-    // Description: Get/Set MRML node
-    vtkGetObjectMacro (Logic, vtkNeuroNavLogic);
-    vtkSetObjectMacro (Logic, vtkNeuroNavLogic);
+  //BTX
+  enum {
+    SLICE_PLANE_RED    = 0,
+    SLICE_PLANE_YELLOW = 1,
+    SLICE_PLANE_GREEN  = 2
+  };
 
-    // Description: implement setter method for logic class's parent
-    virtual void SetModuleLogic(vtkSlicerLogic*);
+  enum {
+    SLICE_RTIMAGE_NONE      = 0,
+    SLICE_RTIMAGE_PERP      = 1,
+    SLICE_RTIMAGE_INPLANE90 = 2,
+    SLICE_RTIMAGE_INPLANE   = 3
+  };
 
-    // Description:
-    // API for setting VolumeNode, VolumeLogic and
-    // for both setting and observing them.
+  // Connector List update level options
+  enum {
+    UPDATE_SELECTED_ONLY   = 0,  // Update selected item only
+    UPDATE_STATUS_ALL      = 1,  // Update status for all items
+    UPDATE_PROPERTY_ALL    = 2,  // Update all properties for all items
+    UPDATE_ALL             = 3,  // Update whole list (incl. changed number of items)
+  };
+  
+  //ETX
 
-    // virtual void SetModuleLogic ( vtkSlicerLogic *logic )
-    // { this->SetLogic ( vtkObjectPointer (&this->Logic), 
-    //                   dynamic_cast<vtkNeuroNavLogic*> (logic) ); }
-    // virtual void SetAndObserveModuleLogic ( vtkSlicerLogic *logic )
-    // { this->SetAndObserveLogic ( vtkObjectPointer (&this->Logic), 
-    //                          dynamic_cast<vtkNeuroNavLogic*>(logic));}
-    
-    // Description:    
-    // This method builds the IGTDemo module GUI
-    virtual void BuildGUI ( );
+ public:
+  // Description:    
+  // Usual vtk class functions
+  static vtkNeuroNavGUI* New ();
+  vtkTypeRevisionMacro ( vtkNeuroNavGUI, vtkSlicerModuleGUI );
+  void PrintSelf (ostream& os, vtkIndent indent );
+  
+  //SendDATANavitrack
+  // Description:    
+  // Get methods on class members (no Set methods required)
+  vtkGetObjectMacro ( Logic, vtkNeuroNavLogic );
+  
+  // Description:
+  // API for setting VolumeNode, VolumeLogic and
+  // for both setting and observing them.
+  void SetModuleLogic ( vtkSlicerLogic *logic )
+  { 
+    this->SetLogic ( vtkObjectPointer (&this->Logic), logic ); }
 
-    // Description:
-    // Add/Remove observers on widgets in the GUI
-    virtual void AddGUIObservers ( );
-    virtual void RemoveGUIObservers ( );
+  //void SetAndObserveModuleLogic ( vtkNeuroNavLogic *logic )
+  //{ this->SetAndObserveLogic ( vtkObjectPointer (&this->Logic), logic ); }
+  // Description: 
 
+  // Get Target Fiducials (used in the wizard steps)
+  vtkGetStringMacro ( FiducialListNodeID );
+  vtkSetStringMacro ( FiducialListNodeID );
+  vtkGetObjectMacro ( FiducialListNode, vtkMRMLFiducialListNode );
+  vtkSetObjectMacro ( FiducialListNode, vtkMRMLFiducialListNode );
 
-    // Description:
-    // Class's mediator methods for processing events invoked by
-    // either the Logic, MRML or GUI.    
-    virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event, void *callData );
-    virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
-    virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
+  // Description:    
+  // This method builds the IGTDemo module GUI
+  virtual void BuildGUI ( );
+  
+  // Description:
+  // Add/Remove observers on widgets in the GUI
+  virtual void AddGUIObservers ( );
+  virtual void RemoveGUIObservers ( );
 
-    void HandleMouseEvent(vtkSlicerInteractorStyle *style);
+  void AddLogicObservers ( );
+  void RemoveLogicObservers ( );
 
-    
-    // Description:
-    // Describe behavior at module startup and exit.
-    virtual void Enter ( );
-    virtual void Exit ( );
+  // Description:
+  // Class's mediator methods for processing events invoked by
+  // either the Logic, MRML or GUI.    
+  virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
 
-    void Init();
+  void ProcessTimerEvents();
+
+  void HandleMouseEvent(vtkSlicerInteractorStyle *style);
+  
+  // Description:
+  // Describe behavior at module startup and exit.
+  virtual void Enter ( );
+  virtual void Exit ( );
+  
+  void Init();
 
   //BTX
   static void DataCallback(vtkObject *caller, 
-                unsigned long eid, void *clientData, void *callData);
-
+                           unsigned long eid, void *clientData, void *callData);
+  
   //ETX
-
+  
  protected:
-    vtkNeuroNavGUI ( );
-    virtual ~vtkNeuroNavGUI ( );
-    vtkNeuroNavGUI ( const vtkNeuroNavGUI& ); // Not implemented.
-    void operator = ( const vtkNeuroNavGUI& ); //Not implemented.
+  vtkNeuroNavGUI ( );
+  virtual ~vtkNeuroNavGUI ( );
+  
+  //----------------------------------------------------------------
+  // Timer
+  //----------------------------------------------------------------
+  
+  int TimerFlag;
+  int TimerInterval;
 
-    vtkKWEntryWithLabel *NormalOffsetEntry;
-    vtkKWEntryWithLabel *TransOffsetEntry;
-    vtkKWEntryWithLabel *NXTOffsetEntry;
+  //----------------------------------------------------------------
+  // GUI widgets
+  //----------------------------------------------------------------
+  
+  //----------------------------------------------------------------
+  // Connector Browser Frame
 
-    vtkKWEntryWithLabel *NormalSizeEntry;
-    vtkKWEntryWithLabel *TransSizeEntry;
-    vtkKWEntryWithLabel *RadiusEntry;
+  vtkKWMultiColumnListWithScrollbars* ConnectorList;
+  vtkKWPushButton* AddConnectorButton;
+  vtkKWPushButton* DeleteConnectorButton;
 
-    vtkKWEntry *NREntry;    
-    vtkKWEntry *NAEntry;
-    vtkKWEntry *NSEntry;
-    vtkKWEntry *TREntry;
-    vtkKWEntry *TAEntry;
-    vtkKWEntry *TSEntry;
-    vtkKWEntry *PREntry;
-    vtkKWEntry *PAEntry;
-    vtkKWEntry *PSEntry;
+  vtkKWEntry*          ConnectorNameEntry;
+  vtkKWRadioButtonSet* ConnectorTypeButtonSet;
+  vtkKWCheckButton*    ConnectorStatusCheckButton;
+  vtkKWEntry*          ConnectorAddressEntry;
+  vtkKWEntry*          ConnectorPortEntry;
 
-    vtkKWFrame *ExtraFrame;
+  
+  //----------------------------------------------------------------
+  // Visualization Control Frame
 
-    /*
-    vtkKWScaleWithEntry* RedColorScale;
-    vtkKWScaleWithEntry* GreenColorScale;
-    vtkKWScaleWithEntry* BlueColorScale;    
-    */
+  vtkKWCheckButton *FreezeImageCheckButton;
+  vtkKWCheckButton *ObliqueCheckButton;
+  vtkKWPushButton  *SetLocatorModeButton;
+  vtkKWPushButton  *SetUserModeButton;
 
-    vtkKWCheckButton *ConnectCheckButton;
-    vtkKWCheckButton *LocatorCheckButton;
-    vtkKWCheckButton *HandleCheckButton;
-    vtkKWCheckButton *GuideCheckButton;
+  vtkKWMenuButton  *RedSliceMenu;
+  vtkKWMenuButton  *YellowSliceMenu;
+  vtkKWMenuButton  *GreenSliceMenu;
+  vtkKWCheckButton *ImagingControlCheckButton;
+  vtkKWMenuButton  *ImagingMenu;
+  
+  vtkKWPushButton  *StartScanButton;
+  vtkKWPushButton  *StopScanButton;
 
-    vtkKWCheckButton *LocatorModeCheckButton;
-    vtkKWCheckButton *UserModeCheckButton;
+  vtkKWCheckButton *LocatorCheckButton;
 
-    vtkKWMenuButton *RedSliceMenu;
-    vtkKWMenuButton *YellowSliceMenu;
-    vtkKWMenuButton *GreenSliceMenu;
+  bool              IsSliceOrientationAdded;
 
-#ifdef Slicer3_USE_NAVITRACK
-    vtkKWLoadSaveButtonWithLabel *LoadConfigButton;
-    vtkKWEntry *ConfigFileEntry;
-#endif
-#ifdef Slicer3_USE_IGSTK
-    vtkKWMenuButtonWithLabel *DeviceMenuButton;
-    vtkKWMenuButtonWithLabel *PortNumberMenuButton;
-    vtkKWMenuButtonWithLabel *BaudRateMenuButton;
-    vtkKWMenuButtonWithLabel *DataBitsMenuButton;
-    vtkKWMenuButtonWithLabel *ParityTypeMenuButton;
-    vtkKWMenuButtonWithLabel *StopBitsMenuButton;
-    vtkKWMenuButtonWithLabel *HandShakeMenuButton;
-#endif
+  
+  // Module logic and mrml pointers
 
-    vtkKWEntryWithLabel *UpdateRateEntry;
-    vtkKWEntryWithLabel * MultiFactorEntry;
+  //----------------------------------------------------------------
+  // Logic Values
+  //----------------------------------------------------------------
 
-    vtkKWEntryWithLabel *PatCoordinatesEntry;
-    vtkKWEntryWithLabel *SlicerCoordinatesEntry;
-    vtkKWPushButton *GetPatCoordinatesPushButton;
-    vtkKWPushButton *AddPointPairPushButton;
+  vtkNeuroNavLogic *Logic;
 
-    vtkKWMultiColumnListWithScrollbars *PointPairMultiColumnList;
+  vtkIGTDataManager *DataManager;
+  vtkIGTPat2ImgRegistration *Pat2ImgReg;
+  vtkCallbackCommand *DataCallbackCommand;
 
-//    vtkKWPushButton *LoadPointPairPushButton;
-//    vtkKWPushButton *SavePointPairPushButton;
-    vtkKWPushButton *DeletePointPairPushButton;
-    vtkKWPushButton *DeleteAllPointPairPushButton;    
-    vtkKWPushButton *RegisterPushButton;
-    vtkKWPushButton *ResetPushButton;
+  // Access the slice windows
+  vtkMRMLSliceNode *SliceNode0;
+  vtkMRMLSliceNode *SliceNode1;
+  vtkMRMLSliceNode *SliceNode2;
+  /*
+  vtkSlicerSliceLogic *Logic0;
+  vtkSlicerSliceLogic *Logic1;
+  vtkSlicerSliceLogic *Logic2;
+  vtkSlicerSliceControllerWidget *Control0;
+  vtkSlicerSliceControllerWidget *Control1;
+  vtkSlicerSliceControllerWidget *Control2;
+  */
+
+  //BTX
+  std::string LocatorModelID;
+  std::string LocatorModelID_new;
+  //ETX
+  
+  //int RealtimeImageOrient;
+
+  //----------------------------------------------------------------
+  // Locator Model
+  //----------------------------------------------------------------
+
+  //vtkMRMLModelNode           *LocatorModel;
+  int                        CloseScene;
 
 
-    // Module logic and mrml pointers
-    vtkNeuroNavLogic *Logic;
+  //----------------------------------------------------------------
+  // Target Fiducials
+  //----------------------------------------------------------------
 
-    // int StopTimer;
-    vtkMatrix4x4 *LocatorMatrix;
+  char *FiducialListNodeID;
+  vtkMRMLFiducialListNode *FiducialListNode;
 
-    vtkMRMLModelNode *LocatorModelNode;
-    vtkMRMLModelDisplayNode *LocatorModelDisplayNode;
-
-    //BTX
-    std::string LocatorModelID;
-    //ETX
-    vtkIGTDataManager *DataManager;
-    vtkIGTPat2ImgRegistration *Pat2ImgReg;
-
-    vtkCallbackCommand *DataCallbackCommand;
-
-    // Access the slice windows
-    vtkSlicerSliceLogic *Logic0;
-    vtkSlicerSliceLogic *Logic1;
-    vtkSlicerSliceLogic *Logic2;
-    vtkMRMLSliceNode *SliceNode0;
-    vtkMRMLSliceNode *SliceNode1;
-    vtkMRMLSliceNode *SliceNode2;
-    vtkSlicerSliceControllerWidget *Control0;
-    vtkSlicerSliceControllerWidget *Control1;
-    vtkSlicerSliceControllerWidget *Control2;
-
-    int NeedOrientationUpdate0;
-    int NeedOrientationUpdate1;
-    int NeedOrientationUpdate2;
-
-    void UpdateAll();
-    void UpdateLocator();
-    void UpdateSliceDisplay(float nx, float ny, float nz, 
-                            float tx, float ty, float tz, 
-                            float px, float py, float pz);
+  void UpdateAll();
 
  private:
 
-    void BuildGUIForHandPieceFrame ();
-    void BuildGUIForTrackingFrame ();
-    void BuildGUIForDeviceFrame ();
-    void BuildGUIForRegistrationFrame ();
+  vtkNeuroNavGUI ( const vtkNeuroNavGUI& ); // Not implemented.
+  void operator = ( const vtkNeuroNavGUI& ); //Not implemented.
+  
+  void BuildGUIForWizardFrame();
+  void BuildGUIForConnectorBrowserFrame ();
+  void BuildGUIForHelpFrame();
+  void BuildGUIForDeviceFrame();
+  void BuildGUIForVisualizationControlFrame();
+  
+  int  ChangeWorkPhase(int phase, int fChangeWizard=0);
+  void ChangeSlicePlaneDriver(int slice, const char* driver);
 
-    void TrackerLoop();
 
+  //----------------------------------------------------------------
+  // Connector List and Properties control
+  //----------------------------------------------------------------
 
-//#ifdef Slicer3_USE_NAVITRACK
-//    vtkIGTOpenTrackerStream *OpenTrackerStream;
-//    void SetOpenTrackerConnectionParameters();
-//#endif
+  void UpdateConnectorList(int updateLevel);
+  void UpdateConnectorPropertyFrame(int i);
 
-#ifdef Slicer3_USE_IGSTK    
-    vtkIGTIGSTKStream *IGSTKStream;
-    void SetIGSTKConnectionParameters();
-#endif
-
+ 
 };
+
+
 
 #endif
