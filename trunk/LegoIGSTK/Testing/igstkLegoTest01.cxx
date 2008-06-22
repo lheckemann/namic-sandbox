3647d5f7321ba3208014c85e1e77399b2029db68
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-22 00:39:55 +0000

    ENH: First cut at using the LegoTutorial from Danielle Pace along with IGSTK,      as an attempt to provide automatic testing for the IGSTK Trackers.

---
+++ trunk/LegoIGSTK/Testing/igstkLegoTest01.cxx
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkQuadEdgeMeshTest1.cxx,v $
+  Language:  C++
+  Date:      $Date: 2008/01/15 13:43:14 $
+  Version:   $Revision: 1.7 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+
+int main( int , char* [] )
+{
+  std::cout << "Test passed" << std::endl;
+  return EXIT_SUCCESS;
+}
