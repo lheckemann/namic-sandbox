473f4cabe4e9f095c47fcca23aaba342efe0b55a
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-06-23 13:54:43 +0000

    ENH: Added Example directory.

---
+++ OpenIGTLink/Examples/Tracker/Tracker.cxx
@@ -0,0 +1,24 @@
+/*=========================================================================
+
+  Program:   Open IGT Link -- Example for Tracker Client Program
+  Module:    $RCSfile: $
+  Language:  C++
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "igtlMessageBase.h"
+
+
+int main()
+{
+  
+}
