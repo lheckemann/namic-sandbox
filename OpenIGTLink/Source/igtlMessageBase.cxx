56f336ea8c48e7bd32e3eb18424f6e223a71bfa7
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-07-11 16:01:40 +0000

    ENH: Added member functions to support deserialization in the MessageBase class

--- OpenIGTLink/Source/igtlMessageBase.cxx
+++ OpenIGTLink/Source/igtlMessageBase.cxx
@@ -23,10 +23,11 @@ namespace igtl {
 
 MessageBase::MessageBase()
 {
-  m_PackSize   = 0;
-  m_Header     = NULL;
-  m_Body       = NULL;
+  m_PackSize       = 0;
+  m_Header         = NULL;
+  m_Body           = NULL;
 
+  m_BodySizeToRead = 0;
   m_DeviceName = "";
 }
 
@@ -81,7 +82,6 @@ void MessageBase::Pack()
 
 }
 
-
 void MessageBase::Unpack()
 {
   // pack exists?
@@ -91,7 +91,7 @@ void MessageBase::Unpack()
       igtl_header_convert_byte_order(h);
       m_TimeStampSecFraction = h->timestamp & 0xFFFFFFFF;
       m_TimeStampSec = (h->timestamp >> 32 ) & 0xFFFFFFFF;
-      m_PackSize     = h->body_size + IGTL_HEADER_SIZE;
+      m_BodySizeToRead = h->body_size;
 
       char bodyType[13];
       char deviceName[21];
@@ -105,10 +105,14 @@ void MessageBase::Unpack()
     }
 }
 
-
 void* MessageBase::GetPackPointer()
 {
   return (void*) m_Header;
+}
+
+void* MessageBase::GetPackBodyPointer()
+{
+  return (void*) m_Body;
 }
  
 int MessageBase::GetPackSize()
@@ -116,10 +120,41 @@ int MessageBase::GetPackSize()
   return m_PackSize;
 }
 
-
 void MessageBase::AllocatePack()
 {
-  int s = IGTL_HEADER_SIZE + GetBodyPackSize();
+  if (m_BodySizeToRead > 0)
+    {
+      AllocatePack(m_BodySizeToRead);
+    }
+  else
+    {
+      AllocatePack(GetBodyPackSize());
+    }
+
+//  int s = IGTL_HEADER_SIZE + GetBodyPackSize();
+//  if (m_PackSize == 0 || m_Header == NULL)      // for the first time
+//    {
+//      m_Header = new unsigned char [s];
+//    }
+//  else if (m_PackSize != s)  // if the pack area exists but needs to be reallocated
+//    {
+//      unsigned char* old = m_Header;
+//      m_Header = new unsigned char [s];
+//      memcpy(m_Header, old, IGTL_HEADER_SIZE);
+//      delete m_Header;
+//    }
+//  m_Body   = &m_Header[IGTL_HEADER_SIZE];
+//  m_PackSize = s;
+}
+
+void MessageBase::AllocatePack(int bodySize)
+{
+  if (bodySize < 0)
+    {
+      bodySize = 0;
+    }
+
+  int s = IGTL_HEADER_SIZE + bodySize;
   if (m_PackSize == 0 || m_Header == NULL)      // for the first time
     {
       m_Header = new unsigned char [s];
@@ -135,16 +170,40 @@ void MessageBase::AllocatePack()
   m_PackSize = s;
 }
 
-  
+int MessageBase::CopyHeader(const MessageBase &mb)
+{
+
+  this->m_PackSize       = mb.m_PackSize;
+  this->m_BodySizeToRead = mb.m_BodySizeToRead;
+  this->m_BodyType       = mb.m_BodyType;
+  this->m_DeviceName     = mb.m_DeviceName;
+  this->m_TimeStampSec   = mb.m_TimeStampSec;
+  this->m_TimeStampSecFraction = mb.m_TimeStampSecFraction;
+
+  memcpy(this->m_Header, mb.m_Header, IGTL_HEADER_SIZE);
+  this->m_Body           = &this->m_Header[IGTL_HEADER_SIZE];
+
+  return 1;
+}
+
 MessageBase& MessageBase::operator=(const MessageBase &mb)
 {
   if (this == &mb)
     {
       return *this;
     }
-  this->AllocatePack();
-}
 
+  if (mb.m_PackSize > IGTL_HEADER_SIZE)
+    {
+      int bodySize = mb.m_PackSize - IGTL_HEADER_SIZE;
+      this->AllocatePack(bodySize);
+      this->CopyHeader(mb);
+      if (bodySize > 0)
+        {
+          this->CopyBody(mb);
+        }
+    }
+}
 
 }
 
