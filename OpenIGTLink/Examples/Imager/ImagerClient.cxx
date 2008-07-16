2ab3b52d6f762225cc77a2598e51fc405dd751ae
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-07-16 20:45:30 +0000

    ENH: Added example code to send image data.

--- OpenIGTLink/Examples/Imager/ImagerClient.cxx
+++ OpenIGTLink/Examples/Imager/ImagerClient.cxx
@@ -22,7 +22,7 @@
 #include "igtlImageMessage.h"
 #include "igtlClientSocket.h"
 
-void GetTestImage(igtl::ImageMessage::Pointer, int i);
+int GetTestImage(igtl::ImageMessage::Pointer& msg, const char* dir, int i);
 void GetRandomTestMatrix(igtl::Matrix4x4& matrix);
 
 int main(int argc, char* argv[])
@@ -30,13 +30,15 @@ int main(int argc, char* argv[])
   //------------------------------------------------------------
   // Parse Arguments
 
-  if (argc != 4) // check number of arguments
+  if (argc != 5) // check number of arguments
     {
     // If not correct, print usage
-    std::cerr << "Usage: " << argv[0] << " <hostname> <port> <fps>"    << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <hostname> <port> <fps> <imgdir>"    << std::endl;
     std::cerr << "    <hostname> : IP or host name"                    << std::endl;
     std::cerr << "    <port>     : Port # (18944 in Slicer default)"   << std::endl;
     std::cerr << "    <fps>      : Frequency (fps) to send coordinate" << std::endl;
+    std::cerr << "    <imgdir>   : file directory, where files \"igtlTestImage?.raw\" are placed." << std::endl;
+    std::cerr << "                 (usually, in the Examples/Imager/img directory.)" << std::endl;
     exit(0);
     }
 
@@ -44,10 +46,10 @@ int main(int argc, char* argv[])
   int    port     = atoi(argv[2]);
   double fps      = atof(argv[3]);
   int    interval = (int) (1000.0 / fps);
+  char*  filedir  = argv[4];
 
   //------------------------------------------------------------
   // Establish Connection
-
   igtl::ClientSocket::Pointer socket;
   socket = igtl::ClientSocket::New();
   int r = socket->ConnectToServer(hostname, port);
@@ -59,39 +61,89 @@ int main(int argc, char* argv[])
     }
 
   //------------------------------------------------------------
-  // Allocate Transform Message Class
-
-  igtl::TransformMessage::Pointer transMsg;
-  transMsg = igtl::TransformMessage::New();
-  transMsg->SetDeviceName("Tracker");
-
-  //------------------------------------------------------------
   // loop
   for (int i = 0; i < 100; i ++)
     {
+
+    //------------------------------------------------------------
+    // size parameters
+    int   size[] = {256, 256, 1};         // image dimension
+    float spacing[] = {1.0, 1.0, 1.0};    // spacing (mm/pixel)
+    int   svsize[] = {256, 256, 1};       // sub-volume size
+    int   svoffset[] = {0, 0, 0};         // sub-volume offset
+    int   scalarType = igtl::ImageMessage::TYPE_UINT8;// scalar type
+
+    //------------------------------------------------------------
+    // Create a new IMAGE type message
+    igtl::ImageMessage::Pointer imgMsg = igtl::ImageMessage::New();
+    imgMsg->SetDimensions(size);
+    imgMsg->SetSpacing(spacing);
+    imgMsg->SetScalarType(scalarType);
+    imgMsg->SetDeviceName("ImagerClient");
+    imgMsg->SetSubVolume(svsize, svoffset);
+    imgMsg->AllocateScalars();
+
+    //------------------------------------------------------------
+    // Set image data (See GetTestImage() bellow for the details)
+    GetTestImage(imgMsg, filedir, i % 5);
+
+    //------------------------------------------------------------
+    // Get randome orientation matrix and set it.
     igtl::Matrix4x4 matrix;
     GetRandomTestMatrix(matrix);
-    transMsg->SetMatrix(matrix);
-    transMsg->Pack();
-    socket->Send(transMsg->GetPackPointer(), transMsg->GetPackSize());
+    imgMsg->SetMatrix(matrix);
+
+    //------------------------------------------------------------
+    // Pack (serialize) and send
+    imgMsg->Pack();
+    socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+
     igtl::Sleep(interval); // wait
+
     }
 
   //------------------------------------------------------------
   // Close connection
-
-  transMsg->Delete();
   socket->CloseSocket();
-  socket->Delete();
 
 }
 
 
 //------------------------------------------------------------
 // Function to read test image data
-void GetTestImage(igtl::ImageMessage::Pointer, int i)
+int GetTestImage(igtl::ImageMessage::Pointer& msg, const char* dir, int i)
 {
-  
+
+  //------------------------------------------------------------
+  // Check if image index is in the range
+  if (i < 0 && i >= 5) 
+    {
+    std::cerr << "Image index is invalid." << std::endl;
+    return 0;
+    }
+
+  //------------------------------------------------------------
+  // Generate path to the raw image file
+  char filename[128];
+  sprintf(filename, "%s/igtlTestImage%d.raw", dir, i+1);
+  std::cerr << "Reading " << filename << "...";
+
+  //------------------------------------------------------------
+  // Load raw data from the file
+  FILE *fp = fopen(filename, "rb");
+  if (fp == NULL)
+    {
+    std::cerr << "File opeining error: " << filename << std::endl;
+    return 0;
+    }
+  int fsize = msg->GetImageSize();
+  size_t b = fread(msg->GetScalarPointer(), 1, fsize, fp);
+
+  fclose(fp);
+
+  std::cerr << "done." << std::endl;
+
+  return 1;
 }
 
 
