267b77634f02ce8f2e833dd15ab84d7fffcfba4c
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-07-18 14:26:27 +0000

    BUG: image orientation in image transfer examples

--- OpenIGTLink/Examples/Imager/ImagerServer.cxx
+++ OpenIGTLink/Examples/Imager/ImagerServer.cxx
@@ -181,7 +181,7 @@ void GetRandomTestMatrix(igtl::Matrix4x4& matrix)
   */
 
   matrix[0][0] = 1.0;  matrix[1][0] = 0.0;  matrix[2][0] = 0.0; matrix[3][0] = 0.0;
-  matrix[0][1] = 0.0;  matrix[1][1] = 1.0;  matrix[2][1] = 0.0; matrix[3][1] = 0.0;
+  matrix[0][1] = 0.0;  matrix[1][1] = -1.0;  matrix[2][1] = 0.0; matrix[3][1] = 0.0;
   matrix[0][2] = 0.0;  matrix[1][2] = 0.0;  matrix[2][2] = 1.0; matrix[3][2] = 0.0;
   matrix[0][3] = 0.0;  matrix[1][3] = 0.0;  matrix[2][3] = 0.0; matrix[3][3] = 1.0;
   
