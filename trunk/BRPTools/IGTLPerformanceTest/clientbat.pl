221e72588813d4b08281751a18987c4534d229d7
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-08-04 14:39:35 +0000

    ENH: Add tests into clientbat.pl

--- trunk/BRPTools/IGTLPerformanceTest/clientbat.pl
+++ trunk/BRPTools/IGTLPerformanceTest/clientbat.pl
@@ -9,20 +9,81 @@ $LOGFILE = 'client.log';
 $PTPLOG  = 'ptp-XXX.log';
 
 {
-    print "***** Test phase 1 -- Frame rate vs CPU load / delay *****\n";
+    
+    print "***** Test phase 1 -- TRANSFORM -- Frame rate vs CPU load / delay *****\n";
+    $channel = 1;
     $fps = 2;
     for ($i = 0; $i < 10; $i ++) {
         $fps = $fps * 2;
-        $file = sprintf("test-t-%04d", $fps);
+        $file = sprintf("test-t-%04d-%02d", $fps, $channel);
         system("wc -l $PTPLOG >> $LOGFILE");
-        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -f $fps -c 1 >> $LOGFILE";
+        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -f $fps -c $channel >> $LOGFILE";
         print($line."\n");
         system($line);
         sleep(2);
     }
 
-    print "***** Test phase 2 -- Channel vs CPU load / delay @ 128 fps *****\n";
+    print "***** Test phase 2 -- TRANSFORM -- Channel vs CPU load / delay @ 100 fps *****\n";
+    $fps = 128;
+    for ($i = 0; $i < 10; $i ++) {
+        $channel = $i + 1;
+        $file = sprintf("test-t-%04d-%02d", $fps, $channel);
+        system("wc -l $PTPLOG >> $LOGFILE");
+        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -f $fps -c $channel >> $LOGFILE";
+        print($line."\n");
+        system($line);
+        sleep(2);
+    }
 
+    print "***** Test phase 3 -- IMAGE -- Frame raate vs CPU load / delay  *****\n";
+    $fps = 1;
+    $channel = 1;
+    $x = 256;
+    $y = 256;
+    $z = 1;
+    for ($i = 0; $i < 10; $i ++) {
+        $fps = $fps*2;
+        $file = sprintf("test-i-%04d-%02d-%d-%d-%d", $fps, $channel, $x, $y, $z);
+        system("wc -l $PTPLOG >> $LOGFILE");
+        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -F $fps -C $channel -x $x -y $y -z $z >> $LOGFILE";
+        print($line."\n");
+        system($line);
+        sleep(2);
+    }
+
+    print "***** Test phase 4 -- IMAGE -- image size vs CPU load / delay @ 10 fps *****\n";
+    $fps = 10;
+    $channel = 1;
+    $x = 256;
+    $y = 256;
+    $z = 1;
+    for ($i = 0; $i < 10; $i ++) {
+        $z = $z*2;
+        $file = sprintf("test-i-%04d-%02d-%d-%d-%d", $fps, $channel, $x, $y, $z);
+        system("wc -l $PTPLOG >> $LOGFILE");
+        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -F $fps -C $channel -x $x -y $y -z $z >> $LOGFILE";
+        print($line."\n");
+        system($line);
+        sleep(2);
+    }
+
+    print "***** Test phase 5 -- TRANSFORM/IMAGE -- Frame raate vs CPU load / delay @ 100fps (trak) + 10fps (img)*****\n";
+    $trackfps = 100;
+    $trackchannel = 1;
+    $imgfps = 10;
+    $imgchannel = 1;
+    $x = 256;
+    $y = 256;
+    $z = 1;
+    for ($i = 0; $i < 10; $i ++) {
+        $z = $i+1;
+        $file = sprintf("test-t-%04d-%02d-i-%04d-%02d-%d-%d-%d", $trackfps, $trackchannel, $imgfps, $imgchannel, $x, $y, $z);
+        system("wc -l $PTPLOG >> $LOGFILE");
+        $line = "$COMMAND -h $SERVER -p $PORT -t $file -d $DURATION -f $trackfps -c $trackchannel -F $imgfps -C $imgchannel -x $x -y $y -z $z >> $LOGFILE";
+        print($line."\n");
+        system($line);
+        sleep(2);
+    }
         
 }
 
