d2c27dcec88b0bb1237991028723767c6dcd2ee1
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-08-05 00:11:01 +0000

    BUG: The expected size of the emtpy histogram should be zero.

--- trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleToHistogramFilterTest6.cxx
+++ trunk/ITKStatisticsPipelineRefactoring/Testing/itkSampleToHistogramFilterTest6.cxx
@@ -113,9 +113,9 @@ int itkSampleToHistogramFilterTest6(int argc, char *argv[] )
 
   const HistogramType * histogram = filter->GetOutput();
 
-  if( histogram->Size() != 1 )
+  if( histogram->Size() != 0 )
     {
-    std::cerr << "Histogram Size should have been one" << std::endl;
+    std::cerr << "Histogram Size should have been zero" << std::endl;
     return EXIT_FAILURE;
     }
 
