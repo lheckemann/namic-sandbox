88c42a8889d5258827bbd32101fe23723b7f19a8
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-08-13 16:06:44 +0000

    ENH: Converting the ImageToHistogramGenerator into the ImageToHistogramFilter.

---
+++ ITKStatisticsPipelineRefactoring/Source/itkImageToHistogramFilter.h
@@ -0,0 +1,130 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkImageToHistogramFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2005/08/24 15:16:46 $
+  Version:   $Revision: 1.7 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkImageToHistogramFilter_h
+#define __itkImageToHistogramFilter_h
+
+
+#include "itkImageToListAdaptor.h"
+#include "itkListSampleToHistogramGenerator.h"
+#include "itkObject.h"
+
+
+namespace itk {
+namespace Statistics {
+
+template <typename TAdaptor >
+struct GetAdaptorMeasurementVectorLength
+{
+  itkStaticConstMacro( MeasurementVectorLength, unsigned int, TAdaptor::MeasurementVectorSize );
+};
+
+/** \class ImageToHistogramFilter
+ *  \brief This class generates an histogram from an image.
+ *
+ *  The concept of Histogram in ITK is quite generic. It has been designed to
+ *  manage multiple components data. This class facilitates the computation of
+ *  an histogram from an image. Internally it creates a List that is feed into
+ *  the ListSampleToHistogramGenerator.
+ *
+ */
+  
+ 
+template< class TImageType >
+class ImageToHistogramFilter : public ProcessObject
+{
+public:
+  /** Standard typedefs */
+  typedef ImageToHistogramFilter        Self;
+  typedef Object                        Superclass;
+  typedef SmartPointer<Self>            Pointer;
+  typedef SmartPointer<const Self>      ConstPointer;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageToHistogramFilter, Object);
+
+  /** standard New() method support */
+  itkNewMacro(Self);
+
+  typedef TImageType                                      ImageType;
+  typedef ImageToListAdaptor< ImageType >                 AdaptorType;
+  typedef typename AdaptorType::Pointer                   AdaptorPointer;
+  typedef typename ImageType::PixelType                   PixelType;
+  typedef typename PixelType::ValueType                   ValueType;
+  typedef typename NumericTraits< ValueType >::RealType   ValueRealType;
+  typedef DenseFrequencyContainer                         FrequencyContainerType;
+
+  typedef ListSampleToHistogramGenerator< 
+                         AdaptorType, 
+                         ValueRealType,
+                         FrequencyContainerType,
+                         ::itk::Statistics::GetAdaptorMeasurementVectorLength< 
+                                          AdaptorType >::MeasurementVectorLength
+                           > GeneratorType;
+
+  typedef typename GeneratorType::Pointer                   GeneratorPointer;
+
+  typedef typename GeneratorType::HistogramType             HistogramType;
+  typedef typename HistogramType::Pointer                   HistogramPointer;
+  typedef typename HistogramType::ConstPointer              HistogramConstPointer;
+  typedef typename HistogramType::SizeType                  SizeType;
+  typedef typename HistogramType::MeasurementVectorType     MeasurementVectorType;
+
+public:
+
+  /** Triggers the Computation of the histogram */
+  void Compute( void );
+
+  /** Connects the input image for which the histogram is going to be computed */
+  void SetInput( const ImageType * );
+  
+  /** Return the output histogram.
+   \warning This output is only valid after the Compute() method has been invoked 
+   \sa Compute */
+  const HistogramType * GetOutput() const;
+  
+  /** Set number of histogram bins */
+  void SetNumberOfBins( const SizeType & size );
+ 
+  /** Set marginal scale value to be passed to the histogram generator */
+  void SetMarginalScale( double marginalScale );
+  void SetHistogramMin(const MeasurementVectorType & histogramMin);
+  void SetHistogramMax(const MeasurementVectorType & histogramMax);
+  void SetAutoMinMax(bool autoMinMax);
+
+protected:
+  ImageToHistogramFilter();
+  virtual ~ImageToHistogramFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+
+private:
+
+  AdaptorPointer      m_ImageToListAdaptor;
+
+  GeneratorPointer    m_HistogramGenerator;
+
+};
+
+
+} // end of namespace Statistics 
+} // end of namespace itk 
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageToHistogramFilter.txx"
+#endif
+
+#endif
---
+++ ITKStatisticsPipelineRefactoring/Source/itkImageToHistogramFilter.txx
@@ -0,0 +1,136 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkImageToHistogramFilter.txx,v $
+  Language:  C++
+  Date:      $Date: 2007/01/17 15:13:42 $
+  Version:   $Revision: 1.3 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkImageToHistogramFilter_txx
+#define _itkImageToHistogramFilter_txx
+
+#include "itkImageToHistogramFilter.h"
+
+
+namespace itk { 
+namespace Statistics {
+
+
+template < class TImage >
+ImageToHistogramFilter< TImage >
+::ImageToHistogramFilter() 
+{
+  m_ImageToListAdaptor = AdaptorType::New();
+  m_HistogramGenerator = GeneratorType::New();
+  m_HistogramGenerator->SetListSample( m_ImageToListAdaptor );
+
+  // set a usable default value
+  SizeType size;
+  size.Fill( 128 );
+  this->SetNumberOfBins( size );
+
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetInput( const ImageType * image ) 
+{
+  m_ImageToListAdaptor->SetImage( image );
+}
+
+
+template < class TImage >
+const typename ImageToHistogramFilter< TImage >::HistogramType *
+ImageToHistogramFilter< TImage >
+::GetOutput() const
+{
+  return m_HistogramGenerator->GetOutput();
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::Compute() 
+{
+  m_HistogramGenerator->Update();
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetNumberOfBins( const SizeType & size ) 
+{
+  m_HistogramGenerator->SetNumberOfBins( size );
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetMarginalScale( double marginalScale )
+{
+  m_HistogramGenerator->SetMarginalScale( marginalScale );
+}
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetHistogramMin(const MeasurementVectorType & histogramMin)
+{
+  m_HistogramGenerator->SetHistogramMin(histogramMin);
+}
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetHistogramMax(const MeasurementVectorType & histogramMax)
+{
+  m_HistogramGenerator->SetHistogramMax(histogramMax);
+}
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::SetAutoMinMax(bool autoMinMax)
+{
+  m_HistogramGenerator->SetAutoMinMax(autoMinMax);
+}
+
+
+template < class TImage >
+void
+ImageToHistogramFilter< TImage >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+  os << "ImageToListSample adaptor = " << m_ImageToListAdaptor << std::endl;
+  os << "HistogramGenerator = " << m_HistogramGenerator << std::endl;
+}
+
+
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
+
+
