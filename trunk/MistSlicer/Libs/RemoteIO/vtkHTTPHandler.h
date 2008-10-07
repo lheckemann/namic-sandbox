c239552bf5b4219e2ef75fb62c6557117390b2b6
yhayashi <yhayashi@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-10-07 19:54:07 +0000

    ENH: checkin source code

---
+++ trunk/MistSlicer/Libs/RemoteIO/vtkHTTPHandler.h
@@ -0,0 +1,51 @@
+#ifndef __vtkHTTPHandler_h
+#define __vtkHTTPHandler_h
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <slicerlibcurl/slicerlibcurl.h>
+
+#include <vtkRemoteIOConfigure.h>
+#include "vtkRemoteIO.h"
+#include "vtkObject.h"
+#include "vtkObjectFactory.h"
+
+//--- derived from libMRML class
+#include "vtkURIHandler.h"
+
+class VTK_RemoteIO_EXPORT vtkHTTPHandler : public vtkURIHandler 
+{
+  public:
+  
+  // The Usual vtk class functions
+  static vtkHTTPHandler *New();
+  vtkTypeRevisionMacro(vtkHTTPHandler, vtkURIHandler);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // This methods returns 1 if the handler matches the uri's required 
+  // protocol and returns 0 if it's not appropriate for the uri.
+  virtual int CanHandleURI ( const char *uri );
+
+  // Description:
+  // This function wraps curl functionality to download a specified URL to a specified dir
+  void StageFileRead(const char * source, const char * destination);
+  void StageFileWrite(const char * source, const char * destination);
+  CURL* CurlHandle;  
+
+ private:
+  virtual void InitTransfer ( );
+  virtual int CloseTransfer ( );
+
+ protected:
+  vtkHTTPHandler();
+  virtual ~vtkHTTPHandler();
+  vtkHTTPHandler(const vtkHTTPHandler&);
+  void operator=(const vtkHTTPHandler&);
+
+};
+
+#endif
+
