9233fd45476edb22a0ce2f122a946e8f32f097dc
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-10-14 21:04:14 +0000

    ENH: First attempt at reimplementing the itkVotingBinaryIterativeHoleFillingImageFilter.

---
+++ trunk/VotingHoleFillFloodingImageFilter/Source/itkVotingHoleFillFloodingImageFilter.h
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkVotingHoleFillingFloodingImageFilter.h,v $
+  Language:  C++
+  Date:      $Date: 2006-03-24 16:03:17 $
+  Version:   $Revision: 1.17 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkVotingHoleFillingFloodingImageFilter_h
+#define __itkVotingHoleFillingFloodingImageFilter_h
+
+#include "itkImage.h"
+#include "itkImageToImageFilter.h"
+
+namespace itk{
+
+/** /class VotingHoleFillingFloodingImageFilter 
+ *
+ * \brief Perform front-propagation under a quorum sensing (voting) algorithm
+ * for filling holes in a binary mask.
+ * 
+ * This is an alternative implementation of the
+ * VotingBinaryIterativeHoleFillingImageFilter.
+ *
+ * \ingroup RegionGrowingSegmentation 
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT VotingHoleFillingFloodingImageFilter:
+    public VotingBinaryImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef VotingHoleFillingFloodingImageFilter                 Self;
+  typedef VotingBinaryImageFilter<TInputImage,TOutputImage>    Superclass;
+  typedef SmartPointer<Self>                                   Pointer;
+  typedef SmartPointer<const Self>                             ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods).  */
+  itkTypeMacro(VotingHoleFillingFloodingImageFilter, VotingBinaryImageFilter);
+
+  typedef typename Superclass::InputImageType             InputImageType;
+  typedef typename InputImageType::Pointer                InputImagePointer;
+  typedef typename InputImageType::ConstPointer           InputImageConstPointer;
+  typedef typename InputImageType::RegionType             InputImageRegionType; 
+  typedef typename InputImageType::PixelType              InputImagePixelType; 
+  typedef typename InputImageType::IndexType              IndexType;
+  typedef typename InputImageType::SizeType               SizeType;
+  
+  typedef typename Superclass::OutputImageType            OutputImageType;
+  typedef typename OutputImageType::Pointer               OutputImagePointer;
+  typedef typename OutputImageType::RegionType            OutputImageRegionType; 
+  typedef typename OutputImageType::PixelType             OutputImagePixelType; 
+  
+  
+  /** Image dimension constants */
+  itkStaticConstMacro(InputImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+  itkStaticConstMacro(OutputImageDimension, unsigned int,
+                      TOutputImage::ImageDimension);
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+  itkConceptMacro(OutputEqualityComparableCheck,
+    (Concept::EqualityComparable<OutputImagePixelType>));
+  itkConceptMacro(InputEqualityComparableCheck,
+    (Concept::EqualityComparable<InputImagePixelType>));
+  itkConceptMacro(InputConvertibleToOutputCheck,
+    (Concept::Convertible<InputImagePixelType, OutputImagePixelType>));
+  itkConceptMacro(SameDimensionCheck,
+    (Concept::SameDimension<InputImageDimension, OutputImageDimension>));
+  itkConceptMacro(IntConvertibleToInputCheck,
+    (Concept::Convertible<int, InputImagePixelType>));
+  itkConceptMacro(OutputOStreamWritableCheck,
+    (Concept::OStreamWritable<OutputImagePixelType>));
+  /** End concept checking */
+#endif
+
+protected:
+  VotingHoleFillingFloodingImageFilter();
+  ~VotingHoleFillingFloodingImageFilter(){};
+
+  typedef std::vector<IndexType>    SeedListType;
+
+  SeedListType                      m_SeedList;
+
+  
+  // Override since the filter needs all the data for the algorithm
+  void GenerateInputRequestedRegion();
+
+  // Override since the filter produces the entire dataset
+  void EnlargeOutputRequestedRegion(DataObject *output);
+
+  void GenerateData();
+  
+  void PrintSelf ( std::ostream& os, Indent indent ) const;
+
+private:
+  VotingHoleFillingFloodingImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkVotingHoleFillingFloodingImageFilter.txx"
+#endif
+
+#endif
