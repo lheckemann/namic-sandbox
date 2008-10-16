4dc96feffbdde8cbd61fb4cd6a70585761d63a78
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-10-16 17:47:19 +0000

    ENH: Created SkeltonGenerator

---
+++ trunk/IGTLoadableModules/SkeltonGenerator/generate.tcl
@@ -0,0 +1,123 @@
+#!/bin/sh
+# the next line restarts using tclsh \
+    exec tclsh "$0" "$@"
+
+
+## default configurations
+set ::GENERATOR(NAME) "Skelton"
+set ::GENERATOR(TYPE) "default"
+set ::GENERATOR(WDIR)  "."
+
+proc Usage { {msg ""} } {
+    set msg "$msg\nusage: generate \[options\]"
+    set msg "$msg\n  "
+    set msg "$msg\n \[options\] : one of the following:"
+    set msg "$msg\n   --type=<type name>        : type of module (default: \"$::GENERATOR(TYPE)\""
+    set msg "$msg\n   --name=<module name>      : module name (default: \"$::GENERATOR(NAME)\")"
+    set msg "$msg\n   --dir=<working directory> : working directory (default: \"$::GENERATOR(WDIR)\""
+    set msg "$msg\n  "
+    puts stderr $msg
+}
+
+set strippedargs ""
+set argc [llength $argv]
+for {set i 0} {$i < $argc} {incr i} {
+    set a [lindex $argv $i]
+    switch -glob -- $a {
+        "--type=*" {
+            set ::GENERATOR(TYPE) [string range $a 7 end]
+        }
+        "--name=*" {
+            set ::GENERATOR(NAME) [string range $a 7 end]
+        }
+        "--dir=*" {
+            set ::GENERATOR(WDIR) [string range $a 6 end]
+        }
+        "--help" {
+            Usage
+            exit 1
+        }
+        "-*" {
+            Usage "unknown option $a\n"
+            exit 1
+        }
+        default {
+            lappend strippedargs $a
+        }
+    }
+}
+
+puts "Module NAME = $::GENERATOR(NAME)"
+puts "TYPE = $::GENERATOR(TYPE)"
+
+set cwd [file dirname [info script]]
+set template "$cwd/$::GENERATOR(TYPE)"
+
+## check if the template directory exists
+if { [file exists $template] } {
+    puts "Using template data in: $template..."
+} else {
+    puts "ERROR: Could not find a template directory."
+    exit 1
+}
+
+## check if the working directory exists
+if { ![file exists $::GENERATOR(WDIR)] } {
+    puts "ERROR: Could not find the working directory."
+    exit 1
+}
+
+### check if the generator configuration exists
+#if { [file exists "$template/SGConfig.txt"] } {
+#    puts "Found a Skelton Generator configuration in $template..."
+#} else {
+#    puts "ERROR: Could not find a Skelton Generator configuration (SGConfig.txt) in $template."
+#    exit 1
+#}
+
+## glob template files
+puts "Searching template files..."
+set files [glob "$template/*.in"]
+foreach f $files {
+    puts $f
+}
+
+## create target directory
+puts "Creating a skelton directory..."
+set targetdir "$::GENERATOR(WDIR)/$::GENERATOR(NAME)"
+file mkdir $targetdir
+
+## create module configuration file
+set fd [open "$targetdir/$::GENERATOR(NAME).txt" "w"]
+puts $fd "Name: $::GENERATOR(NAME)"
+puts $fd "GUIName: $::GENERATOR(NAME)"
+close $fd
+
+set repModName "s/\\$\\$\{MODULE_NAME\}\\$\\$/$::GENERATOR(NAME)/g"
+
+foreach src $files {
+    regsub -all "$template/*" $src "" srcfilename
+
+    #set srcfilename [string trimright $srcfilename ".in"]
+    set srcfilename [string range $srcfilename 0 [expr [string length $srcfilename] - 4]]
+    puts $srcfilename
+
+    if { $srcfilename == "CMakeLists.txt" } {
+        set dst "$targetdir/$srcfilename"
+    } else {
+        set dstfilename "vtk$::GENERATOR(NAME)$srcfilename"
+        set dst "$targetdir/$dstfilename"
+    }
+
+    exec sed $repModName $src > $dst
+
+}
+
+
+
+
+
+
+
+
+
