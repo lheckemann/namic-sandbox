c0061694c0d32705a0584b0ace8f3cb541c2d03b
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-10-18 02:38:10 +0000

    ENH: implemented transform sending. Arguments for igtlsend() has been changed.

--- BRPTools/MatlabIGTL/igtlsend.cpp
+++ BRPTools/MatlabIGTL/igtlsend.cpp
@@ -15,12 +15,21 @@
 =========================================================================*/
 
 //
-//  r = igtlsend(SD, NAME, DATA, TRANSFORM);
+//  r = igtlsend(SD, DATA);
 //
 //    SD  : (integer)    Socket descriptor (-1 if failed to connect)
-//    NAME: (string)     Data name (DEVICE_NAME in OpenIGTLink)
-//    DATA: (uint16[][]) Image data
-//    TRANS:(real[4][4]) Affine transform matrix (4x4)
+//    DATA: (structure)  Data contents
+//
+//  Data fields for IMAGE data
+//    DATA.Type : (string)     must be 'IMAGE'
+//    DATA.Name : (string)     Data name (DEVICE_NAME in OpenIGTLink)
+//    DATA.Image: (uint16[][]) Image data
+//    DATA.Trans: (real[4][4]) Affine transform matrix (4x4)
+//
+//  Data fields for TRANSFORM data
+//    DATA.Type : (string)     must be 'TRANSFORM'
+//    DATA.NAME : (string)     Data name (DEVICE_NAME in OpenIGTLink)
+//    DATA.Trans: (real[4][4]) Affine transform matrix (4x4)
 //
 
 #include "mex.h"
@@ -28,19 +37,16 @@
 
 #include "igtlOSUtil.h"
 #include "igtlImageMessage.h"
+#include "igtlTransformMessage.h"
 #include "igtlMexClientSocket.h"
 
 using namespace std;
 
 //#define pi (3.141592653589793)
 
-//#define ARG_ID_HOST    0  // host
-//#define ARG_ID_PORT    1  // port
 #define ARG_ID_SD      0
-#define ARG_ID_NAME    1  // name
-#define ARG_ID_DATA    2  // data
-#define ARG_ID_TRANS   3  // transform
-#define ARG_ID_NUM     4  // total number of arguments
+#define ARG_ID_DATA    1  // data
+#define ARG_ID_NUM     2  // total number of arguments
 
 #define MAX_STRING_LEN 256
 
@@ -54,30 +60,190 @@ double& createMatlabScalar (mxArray*& ptr);
 int checkArguments(int nlhs, mxArray *plhs[],
                    int nrhs, const mxArray *prhs[]);
 
+int procTransformData(int sd, const char* name, const mxArray *ptr);
+int procImageData(int sd, const char* name, const mxArray *ptr);
+
+int checkData(const char* type, const mxArray* prhs);
+
+
 // -----------------------------------------------------------------
 // Function definitions.
 void mexFunction (int nlhs, mxArray *plhs[],
-                  int nrhs, const mxArray *prhs[]) {
-  
-  checkArguments(nlhs, plhs, nrhs, prhs);
+                  int nrhs, const mxArray *prhs[])
+{
 
   // ---------------------------------------------------------------
-  // Set variables
-
-  char hostname[MAX_STRING_LEN];
-  char name[MAX_STRING_LEN];
+  // Get reference to return value
+  double& retVal = createMatlabScalar(plhs[0]);
   
-  //mxGetString(prhs[ARG_ID_HOST], hostname, MAX_STRING_LEN);
-  mxGetString(prhs[ARG_ID_NAME], name, MAX_STRING_LEN);
+  // ---------------------------------------------------------------
+  // Check arguments
+  if (checkArguments(nlhs, plhs, nrhs, prhs) == 0)
+    {
+    retVal = 0;
+    return;
+    }
 
-  //int         port     = (int)*mxGetPr(prhs[ARG_ID_PORT]);
-  int         sd       = (int)*mxGetPr(prhs[ARG_ID_SD]);
-  double*     rdata    = mxGetPr(prhs[ARG_ID_DATA]);
-  int ndim             = mxGetNumberOfDimensions(prhs[ARG_ID_DATA]);
-  const int*  s        = mxGetDimensions(prhs[ARG_ID_DATA]);
+  // ---------------------------------------------------------------
+  // Set header variables
+  int  sd;
+  char type[MAX_STRING_LEN];
+  char name[MAX_STRING_LEN];
+
+  sd = (int)*mxGetPr(prhs[ARG_ID_SD]);
+
+  // Get DATA.type
+  mxArray*  typeField = mxGetField(prhs[ARG_ID_DATA], 0, "Type");
+  if (typeField == NULL)
+    {
+    mexErrMsgTxt("No DATA.Type field.");
+    retVal = 0;
+    return;
+    }
+  mxGetString(typeField, type, MAX_STRING_LEN);
+
+  // Get DATA.name
+  mxArray*  nameField = mxGetField(prhs[ARG_ID_DATA], 0, "Name");
+  if (nameField == NULL)
+    {
+    mexErrMsgTxt("No DATA.Name field.");
+    retVal = 0;
+    return;
+    }
+  mxGetString(nameField, name, MAX_STRING_LEN);
+
+  // ---------------------------------------------------------------
+  // Process data field and send through OpenIGTLink connection
+  int r;
+  if (strcmp(type, "TRANSFORM") == 0)
+    {
+    r = procTransformData(sd, name, prhs[ARG_ID_DATA]);
+    }
+  else if (strcmp(type, "IMAGE") == 0)
+    {
+    r = procImageData(sd, name, prhs[ARG_ID_DATA]);
+    }
+
+  // ---------------------------------------------------------------
+  // Return result to Matlab
+
+  retVal = r;
+
+}
+
+int checkArguments(int nlhs, mxArray *plhs[],
+                   int nrhs, const mxArray *prhs[])
+{
+  // ---------------------------------------------------------------
+  // Check numbers of arguments and outputs
+  if (nrhs != ARG_ID_NUM)
+    {
+    mexErrMsgTxt("Incorrect number of input arguments");
+    return 0;
+    }
+
+  if (nlhs != 1)
+    {
+    mexErrMsgTxt("Incorrect number of output arguments");
+    return 0;
+    }
+
+  // ---------------------------------------------------------------
+  // Check types of arguments
+
+  // SD -- socket descriptor
+  if (!mxIsNumeric(prhs[ARG_ID_SD]))
+    {
+    mexErrMsgTxt("SD argument must be integer.");
+    return 0;
+    }
+
+  // DATA
+  if (!mxIsStruct(prhs[ARG_ID_DATA]))
+    {
+    mexErrMsgTxt("DATA argument must be structure.");
+    return 0;
+    }
+
+  return 1;
+
+}
+
+
+int procTransformData(int sd, const char* name, const mxArray *ptr)
+{
+  mxArray*    transField = mxGetField(ptr, 0, "Trans");
+
+  double*    trans       = mxGetPr(transField);
+  const int* s           = mxGetDimensions(transField);
+  
+  igtl::Matrix4x4 mat;
+  mat[0][0] = trans[0];  mat[0][1] = trans[4];  mat[0][2] = trans[8];  mat[0][3] = trans[12];
+  mat[1][0] = trans[1];  mat[1][1] = trans[5];  mat[1][2] = trans[9];  mat[1][3] = trans[13];
+  mat[2][0] = trans[2];  mat[2][1] = trans[6];  mat[2][2] = trans[10]; mat[2][3] = trans[14];
+  mat[3][0] = trans[3];  mat[3][1] = trans[7];  mat[3][2] = trans[11]; mat[3][3] = trans[15];
+  
+  float norm_i[] = {mat[0][0], mat[1][0], mat[2][0]};
+  float norm_j[] = {mat[0][1], mat[1][1], mat[2][1]};
+  float norm_k[] = {mat[0][2], mat[1][2], mat[2][2]};
+  float pos[]    = {mat[0][3], mat[1][3], mat[2][3]};
+
+  float a[3];
+  // calculate absolutes
+  a[0] = sqrt(norm_i[0]*norm_i[0] + norm_i[1]*norm_i[1] + norm_i[2]*norm_i[2]);
+  a[1] = sqrt(norm_j[0]*norm_j[0] + norm_j[1]*norm_j[1] + norm_j[2]*norm_j[2]);
+  a[2] = sqrt(norm_k[0]*norm_k[0] + norm_k[1]*norm_k[1] + norm_k[2]*norm_k[2]);
+
+  // normalize
+  for (int i = 0; i < 3; i ++)
+    {
+    norm_i[i] /= a[i];
+    norm_j[i] /= a[i];
+    norm_k[i] /= a[i];
+    }
+  
+  // print variables
+  mexPrintf("Data Name  : %s\n", name);
+  mexPrintf("Transform  : [%1.6f, %1.6f %1.6f %1.6f]\n", mat[0][0], mat[0][1], mat[0][2], mat[0][3]);
+  mexPrintf("             [%1.6f, %1.6f %1.6f %1.6f]\n", mat[1][0], mat[1][1], mat[1][2], mat[1][3]);
+  mexPrintf("             [%1.6f, %1.6f %1.6f %1.6f]\n", mat[2][0], mat[2][1], mat[2][2], mat[2][3]);
+  mexPrintf("             [%1.6f, %1.6f %1.6f %1.6f]\n", mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
+
+  // ---------------------------------------------------------------
+  // Set up OpenIGTLink Connection
+  igtl::MexClientSocket::Pointer socket;
+  socket = igtl::MexClientSocket::New();
+  int r = socket->SetDescriptor(sd);
+  if (r != 0)
+    {
+    mexErrMsgTxt("Invalid socket descriptor.");
+    }
+
+  // ---------------------------------------------------------------
+  // Prepare Transform message
+  igtl::TransformMessage::Pointer transMsg = igtl::TransformMessage::New();
+  transMsg->SetDeviceName(name);
+  transMsg->SetMatrix(mat);
+  transMsg->Pack();
+  socket->Send(transMsg->GetPackPointer(), transMsg->GetPackSize());
+
+  mexPrintf("The transform has been sent.\n");
+
+  return 1;
+
+}
+
+int procImageData(int sd, const char* name, const mxArray *ptr)
+{
+  mxArray* imageField =  mxGetField(ptr, 0, "Image");
+  mxArray* transField =  mxGetField(ptr, 0, "Trans");
+  
+  double*     rdata    = mxGetPr(imageField);
+  int ndim             = mxGetNumberOfDimensions(imageField);
+  const int*  s        = mxGetDimensions(imageField);
   int size[3];
   size[0] = s[0]; size[1] = s[1]; size[2] = (ndim == 3)? s[2]:1;
-  double*     trans    = mxGetPr(prhs[ARG_ID_TRANS]);
+  double*     trans    = mxGetPr(transField);
 
   igtl::Matrix4x4 mat;
   mat[0][0] = trans[0];  mat[0][1] = trans[4];  mat[0][2] = trans[8];  mat[0][3] = trans[12];
@@ -106,8 +272,6 @@ void mexFunction (int nlhs, mxArray *plhs[],
     }
 
   // print variables
-  //mexPrintf("Hostname   : %s\n", hostname);
-  //mexPrintf("Port #     : %d\n", port);
   mexPrintf("Data Name  : %s\n", name);
   mexPrintf("Size       : (%d, %d, %d)\n", size[0], size[1], size[2]);
   mexPrintf("Spacing    : (%f, %f, %f)\n", spacing[0], spacing[1], spacing[2]);
@@ -116,16 +280,12 @@ void mexFunction (int nlhs, mxArray *plhs[],
   mexPrintf("             [%1.6f, %1.6f %1.6f %1.6f]\n", mat[2][0], mat[2][1], mat[2][2], mat[2][3]);
   mexPrintf("             [%1.6f, %1.6f %1.6f %1.6f]\n", mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
 
-
   // ---------------------------------------------------------------
   // Set up OpenIGTLink Connection
   igtl::MexClientSocket::Pointer socket;
   socket = igtl::MexClientSocket::New();
-
   int r = socket->SetDescriptor(sd);
 
-//  int r = socket->ConnectToServer(hostname, port);
-//
   if (r != 0)
     {
     mexErrMsgTxt("Invalid socket descriptor.");
@@ -156,83 +316,17 @@ void mexFunction (int nlhs, mxArray *plhs[],
         }
       }
     }
-
+  
   // ---------------------------------------------------------------
   // Send image message
-
+  
   imgMsg->Pack();
   socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+  socket->CloseSocket();
 
   mexPrintf("The image has been sent.\n");
 
-  socket->CloseSocket();
-
-  double& p = createMatlabScalar(plhs[0]);
-
-  p = 1;
-}
-
-
-int checkArguments(int nlhs, mxArray *plhs[],
-                   int nrhs, const mxArray *prhs[])
-{
-  // ---------------------------------------------------------------
-  // Check numbers of arguments and outputs
-  if (nrhs != ARG_ID_NUM)
-    {
-    mexErrMsgTxt("Incorrect number of input arguments");
-    }
-
-  if (nlhs != 1)
-    {
-    mexErrMsgTxt("Incorrect number of output arguments");
-    }
-
-  // ---------------------------------------------------------------
-  // Check types of arguments
-
-//  // HOST
-//  if (!mxIsChar(prhs[ARG_ID_HOST]))
-//    {
-//    mexErrMsgTxt("HOST argument must be string.");
-//    }
-//
-//  // PORT
-//  if (!mxIsNumeric(prhs[ARG_ID_PORT]))
-//    {
-//    mexErrMsgTxt("PORT argument must be integer.");
-//    }
-
-  // SD -- socket descriptor
-  if (!mxIsNumeric(prhs[ARG_ID_SD]))
-    {
-    mexErrMsgTxt("SD argument must be integer.");
-    }
-
-  // NAME
-  if (!mxIsChar(prhs[ARG_ID_NAME]))
-    {
-    mexErrMsgTxt("NAME argument must be string.");
-    }
-
-  // DATA
-  if (!(mxIsNumeric(prhs[ARG_ID_DATA]) && 
-        (mxGetNumberOfDimensions(prhs[ARG_ID_DATA]) == 2 ||
-         mxGetNumberOfDimensions(prhs[ARG_ID_DATA]) == 3)))
-    {
-    mexErrMsgTxt("DATA argument must be numeric and 2 or 3 dimensions.");
-    }
-
-  // TRANS
-  if (!(mxIsNumeric(prhs[ARG_ID_TRANS]) && mxGetNumberOfDimensions(prhs[ARG_ID_TRANS]) == 2))
-    {
-    mexErrMsgTxt("TRANS argument must be numeric and 2-dimensional array.");
-    }
-  const int* s = mxGetDimensions(prhs[ARG_ID_TRANS]);
-  if (!(s[0] == 4 && s[1] == 4))
-    {
-    mexErrMsgTxt("TRANS argument must be 4x4 2-dimensional array.");
-    }
+  return 1;
 
 }
 
@@ -245,6 +339,7 @@ double getMatlabScalar (const mxArray* ptr) {
 
   return *mxGetPr(ptr);
 }
+
 
 double& createMatlabScalar (mxArray*& ptr) { 
   ptr = mxCreateDoubleMatrix(1,1,mxREAL);
