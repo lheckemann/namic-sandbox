381a2e3facc8aa81050c689192940a141b420244
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-11-14 19:02:26 +0000

    ENH: MatrixToQuaternion() has been added to igtlMath.

--- OpenIGTLink/Source/igtlMath.h
+++ OpenIGTLink/Source/igtlMath.h
@@ -24,6 +24,7 @@ typedef float  Matrix4x4[4][4];
 
 void PrintMatrix(igtl::Matrix4x4 &matrix);
 void QuaternionToMatrix(float* q, Matrix4x4& m);
+void MatrixToQuaternion(Matrix4x4& m, float* q);
 void Cross(float *a, float *b, float *c);
 void IdentityMatrix(igtl::Matrix4x4 &matrix);
 
