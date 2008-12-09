3c42bbe0ac0ec12753c84cd2ac53b7bb914ab3f1
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-12-09 17:21:02 +0000

    ENH: flow control

--- BRPTools/ScannerIO/src/GEExciteIGTL.cxx
+++ BRPTools/ScannerIO/src/GEExciteIGTL.cxx
@@ -45,20 +45,52 @@
 #include "TransferOpenIGTLink.h"
 
 
+void printUsage(const char* name)
+{
+    std::cerr << "Invalid arguments!" << std::endl;
+    std::cerr << "Usage: " << name << " s <port> <delay> <flowctrl>" << std::endl;
+    std::cerr << "       " << name << " c <address> <port> <delay> <flowctrl>" << std::endl;
+    std::cerr << "  <server>: OpenIGTLink server hostname" << std::endl;
+    std::cerr << "  <delay> : delay of image acuiqisition (ms)" << std::endl;
+    std::cerr << "  <flow>  : flow control -- 0: off / 1: on" << std::endl;
+    std::cerr << std::endl;
+}
+
 int main(int argc, char **argv)
 {
 
-  if (argc < 3) {
-    std::cerr << "Invalid arguments!" << std::endl;
-    std::cerr << "Usage: " << argv[0] << " <server> <delay>"
-              << "  <server>: OpenIGTLink server hostname"
-              << "  <delay> : delay of image acuiqisition (ms)"
-              << std::endl;
+  if (argc < 5) {
+    printUsage(argv[0]);
     exit(-1);
   }
 
-  const char* hostname = argv[1];
+  int fserver = 0;
+  char* hostname = argv[1];
+  int port;
   int delay = atoi(argv[2]);
+  int flow = atoi(argv[3]);
+
+  if (argv[2][0] == 's') // server mode
+    {
+      fserver  = 1;
+      port     = atoi(argv[2]);
+      delay    = atoi(argv[3]);
+      flow     = atoi(argv[4]);
+    }
+  else
+    {
+      if (argc < 6)
+        {
+        printUsage(argv[0]);
+        exit(-1);
+        }
+      fserver  = 0;
+      hostname = argv[2];
+      port     = atoi(argv[3]);
+      delay    = atoi(argv[4]);
+      flow     = atoi(argv[5]);
+    }
+
 
   std::cerr << "Creating new Scanner..." << std::endl;
   //AcquisitionSimulator* acquisition = new AcquisitionSimulator;
@@ -76,14 +108,23 @@ int main(int argc, char **argv)
   acquisition->SetPostProcessThread(dynamic_cast<Thread*>(transfer));
   acquisition->Init();
 
-  //transfer->SetServer("localhost", 18944);
-  //transfer->SetServer("10.121.1.31", 18944);
-  transfer->SetServer(hostname, 18944);
+
+  transfer->FlowControl(flow);
+    
+  if (fserver)  // server mode
+    {
+    transfer->SetServerMode(port);
+    }
+  else // client mode
+    {
+    //transfer->SetServer("localhost", 18944);
+    //transfer->SetServer("10.121.1.31", 18944);
+    transfer->SetClientMode(hostname, port);
+    }
+
   transfer->SetAcquisitionThread(acquisition);
-
-
-  transfer->Connect();
-
+  //transfer->Connect();
+  
   acquisition->StartScan();
   transfer->Run();
 
