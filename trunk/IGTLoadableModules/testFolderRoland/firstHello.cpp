a3651354c40c3b0dbeac73e197d6256f1051e457
goerlitz <goerlitz@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-12-15 21:49:52 +0000

    ENH: more lines of output

--- trunk/IGTLoadableModules/testFolderRoland/firstHello.cpp
+++ trunk/IGTLoadableModules/testFolderRoland/firstHello.cpp
@@ -3,5 +3,6 @@ using namespace std;
   int main()
   {
     cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl;
+    cout << "again this is another line...just because..." << endl;
     return 0; 
   }
