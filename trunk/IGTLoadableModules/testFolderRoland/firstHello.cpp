86059bc4ebd329adf8d94d788c6972647ba54985
goerlitz <goerlitz@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-12-15 22:18:42 +0000

    ENH: and another line of code

--- trunk/IGTLoadableModules/testFolderRoland/firstHello.cpp
+++ trunk/IGTLoadableModules/testFolderRoland/firstHello.cpp
@@ -2,7 +2,9 @@
 using namespace std;
   int main()
   {
-    cout << "Hello World!" << endl;   cout << "Welcome to C++ Programming" << endl;
+    cout << "Hello World!" << endl << "changes made" << endl;   cout << "Welcome to C++ Programming" << endl;
     cout << "again this is another line...just because..." << endl;
+    cout << "but I want this line!" << endl;
+    
     return 0; 
   }
