625a6ae1b81151adcd8bdb9d826e4453f76cc0c3
blezek <blezek@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2008-12-29 04:10:46 +0000

    ENH: libcurl with ssl support

--- ImageNetworkReader/ImageNetworkReaderTest.cxx
+++ ImageNetworkReader/ImageNetworkReaderTest.cxx
@@ -1,8 +1,26 @@
 #include "itkImageNetworkReader.h"
+#ifdef TEST_CURL_SSL
+#include <curl/curl.h>
+#endif
 
 
 int main ( int argc, const char* argv[] )
 {
+
+#ifdef TEST_CURL_SSL
+  curl_global_init ( CURL_GLOBAL_ALL );
+  CURL* handle = curl_easy_init();
+  curl_easy_setopt ( handle, CURLOPT_URL, "https://ssl.fujitsu-general.com/" );
+  // Don't verify the certificate, not recmmonded...
+  curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0);
+  CURLcode retval = curl_easy_perform ( handle );
+  if ( retval != CURLE_OK ) 
+    {
+    std::cout << "Error in curl: " << curl_easy_strerror ( retval ) << std::endl;
+    }
+  curl_easy_cleanup ( handle );
+#endif
+
 
   std::cout << "Starting ImageNetworkReaderTest" << std::endl;
 
