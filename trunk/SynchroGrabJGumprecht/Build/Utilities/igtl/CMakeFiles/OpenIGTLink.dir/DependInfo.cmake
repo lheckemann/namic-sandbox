33898c6745e39599e7eef00c07d5263389168105
jgumprec <jgumprec@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-01-07 20:37:06 +0000

    ENH: Recommit

---
+++ trunk/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlClientSocket.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlClientSocket.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlConditionVariable.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlConditionVariable.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlFastMutexLock.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlFastMutexLock.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlImageMessage.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlImageMessage.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlLightObject.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlLightObject.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlMath.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlMath.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlMessageBase.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlMessageBase.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlMultiThreader.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlMultiThreader.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlMutexLock.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlMutexLock.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlOSUtil.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlOSUtil.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlObject.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlObject.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlObjectFactoryBase.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlObjectFactoryBase.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlServerSocket.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlServerSocket.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlSimpleFastMutexLock.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlSimpleFastMutexLock.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlSocket.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlSocket.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlTimeStamp.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlTimeStamp.o"
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Utilities/igtl/igtlTransformMessage.cxx" "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/CMakeFiles/OpenIGTLink.dir/igtlTransformMessage.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "IGTLSTATIC=1"
+  "PLATFORM_LINUX"
+  "IGTL_USE_PTHREADS"
+  "IGTL_SIZEOF_CHAR=1"
+  "IGTL_SIZEOF_DOUBLE=8"
+  "IGTL_SIZEOF_FLOAT=4"
+  "IGTL_SIZEOF_INT=4"
+  "IGTL_SIZEOF_LONG=8"
+  "IGTL_SIZEOF_SHORT=2"
+  "IGTL_SIZEOF_FLOAT=4"
+  "IGTL_SIZEOF_DOUBLE=8"
+  "IGTL_TYPE_USE_LONG_LONG=1"
+  "IGTL_SIZEOF_LONG_LONG=8"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jgumprec/mrrobot/eclipse/workspace/SynchroGrabJGumprecht/Build/Utilities/igtl/igtlutil/CMakeFiles/igtlutil.dir/DependInfo.cmake"
+  )
---
+++ trunk/SynchroGrabJGumprecht/Build/Utilities/igtl/igtlutil/CMakeFiles/igtlutil.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/igtlutil.dir/igtl_header.o"
+  "CMakeFiles/igtlutil.dir/igtl_image.o"
+  "CMakeFiles/igtlutil.dir/igtl_transform.o"
+  "CMakeFiles/igtlutil.dir/igtl_util.o"
+  "../../../bin/libigtlutil.pdb"
+  "../../../bin/libigtlutil.a"
+  "../../../bin/libigtlutil.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/igtlutil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
