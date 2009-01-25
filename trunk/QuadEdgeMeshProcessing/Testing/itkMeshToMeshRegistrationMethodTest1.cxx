45571ccc74f757499a187252aeaeca2e6cb0736f
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-01-25 20:14:41 +0000

    ENH: Adding initial skeletons for MeshToMeshMetric and MeshToMeshRegistrationMethod.

---
+++ trunk/QuadEdgeMeshProcessing/Testing/itkMeshToMeshRegistrationMethodTest1.cxx
@@ -0,0 +1,51 @@
+#include "itkQuadEdgeMesh.h"
+#include "itkVTKPolyDataReader.h"
+#include "itkVTKPolyDataWriter.h"
+
+#include "VNLIterativeSparseSolverTraits.h"
+#include "itkQuadEdgeMeshParamMatrixCoefficients.h"
+
+#include "itkQuadEdgeMeshToSphereFilter.h"
+#include "itkQuadEdgeMeshSphericalParameterization.h"
+
+
+int main( int argc, char** argv )
+{
+  if( argc != 2 )
+    {
+    std::cout <<"It requires 1 arguments" <<std::endl;
+    std::cout <<"1-Input FileName" <<std::endl;
+    return EXIT_FAILURE;
+    }
+    // ** TYPEDEF **
+  typedef double Coord;
+
+  typedef itk::QuadEdgeMesh< Coord, 3 >                 MeshType;
+  typedef MeshType::Pointer                             MeshPointer;
+  typedef itk::VTKPolyDataReader< MeshType >            ReaderType;
+  typedef itk::VTKPolyDataWriter< MeshType >            WriterType;
+
+  ReaderType::Pointer readerFixed = ReaderType::New();
+  readerFixed->SetFileName( argv[1] );
+
+  ReaderType::Pointer readerMoving = ReaderType::New();
+  readerMoving->SetFileName( argv[1] );
+
+  try
+    {
+    readerFixed->Update( );
+    readerMoving->Update( );
+    }
+  catch( itk::ExceptionObject & exp )
+    {
+    std::cerr << "Exception thrown while reading the input file " << std::endl;
+    std::cerr << exp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  MeshPointer meshFixed  = readerFixed->GetOutput();
+  MeshPointer meshMoving = readerMoving->GetOutput();
+
+
+  return EXIT_SUCCESS;
+}
