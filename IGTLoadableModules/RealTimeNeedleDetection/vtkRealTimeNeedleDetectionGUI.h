df4cc25bfecd4d273b3ffbd802ec8cb8c79f0f20
goerlitz <goerlitz@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-02-18 21:50:06 +0000

    ENH: Initial commit after committing the generated skeleton

--- IGTLoadableModules/RealTimeNeedleDetection/vtkRealTimeNeedleDetectionGUI.h
+++ IGTLoadableModules/RealTimeNeedleDetection/vtkRealTimeNeedleDetectionGUI.h
@@ -1,14 +1,15 @@
 /*==========================================================================
 
-  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Portions (c) Copyright 2009 Brigham and Women's Hospital (BWH) All Rights Reserved.
+  Author: Roland Goerlitz, University of Karlsruhe
 
   See Doc/copyright/copyright.txt
   or http://www.slicer.org/copyright/copyright.txt for details.
 
   Program:   3D Slicer
-  Module:    $HeadURL: $
-  Date:      $Date: $
-  Version:   $Revision: $
+  Module:    $HeadURL: ??$
+  Date:      $Date: 2009/02/12 19:10:09$
+  Version:   $Revision: 1.00$
 
 ==========================================================================*/
 
@@ -23,53 +24,55 @@
 #include "vtkCallbackCommand.h"
 #include "vtkSlicerInteractorStyle.h"
 
+#include "vtkIGTDataManager.h"
+
 #include "vtkRealTimeNeedleDetectionLogic.h"
+#include "ImageProcessor.h"
 
 class vtkKWPushButton;
 
 class VTK_RealTimeNeedleDetection_EXPORT vtkRealTimeNeedleDetectionGUI : public vtkSlicerModuleGUI
 {
  public:
-
-  vtkTypeRevisionMacro ( vtkRealTimeNeedleDetectionGUI, vtkSlicerModuleGUI );
+  vtkTypeRevisionMacro (vtkRealTimeNeedleDetectionGUI, vtkSlicerModuleGUI);
 
   //----------------------------------------------------------------
   // Set/Get Methods
   //----------------------------------------------------------------
-
-  vtkGetObjectMacro ( Logic, vtkRealTimeNeedleDetectionLogic );
-  void SetModuleLogic ( vtkSlicerLogic *logic )
+  vtkGetObjectMacro (Logic, vtkRealTimeNeedleDetectionLogic);
+  void SetModuleLogic (vtkSlicerLogic* logic)
   { 
-    this->SetLogic ( vtkObjectPointer (&this->Logic), logic );
+    this->SetLogic (vtkObjectPointer (&this->Logic), logic);
   }
 
  protected:
   //----------------------------------------------------------------
   // Constructor / Destructor (proctected/private) 
   //----------------------------------------------------------------
-
   vtkRealTimeNeedleDetectionGUI ( );
   virtual ~vtkRealTimeNeedleDetectionGUI ( );
 
  private:
-  vtkRealTimeNeedleDetectionGUI ( const vtkRealTimeNeedleDetectionGUI& ); // Not implemented.
+  vtkRealTimeNeedleDetectionGUI(const vtkRealTimeNeedleDetectionGUI& ); // Not implemented.
+  void ProcessImage(vtkImageData* pImageData, int i); // Function to evoke changes in the image in every itaration | not used anymore
+  void GetImageRegion(vtkImageData* pImageData, unsigned char* pImageRegion);
+  void SetImageRegion(vtkImageData* pImageData, unsigned char* pImageRegion);
   void operator = ( const vtkRealTimeNeedleDetectionGUI& ); //Not implemented.
 
  public:
   //----------------------------------------------------------------
   // New method, Initialization etc.
   //----------------------------------------------------------------
-
   static vtkRealTimeNeedleDetectionGUI* New ();
   void Init();
-  virtual void Enter ( );
-  virtual void Exit ( );
+  virtual void Enter();
+  virtual void Exit();
   void PrintSelf (ostream& os, vtkIndent indent );
+  vtkMRMLNode* CreateNewNode(vtkMRMLScene* scene, const char* name);
 
   //----------------------------------------------------------------
   // Observer Management
   //----------------------------------------------------------------
-
   virtual void AddGUIObservers ( );
   virtual void RemoveGUIObservers ( );
   void AddLogicObservers ( );
@@ -78,7 +81,6 @@ class VTK_RealTimeNeedleDetection_EXPORT vtkRealTimeNeedleDetectionGUI : public 
   //----------------------------------------------------------------
   // Event Handlers
   //----------------------------------------------------------------
-
   virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event, void *callData );
   virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
   virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
@@ -90,45 +92,67 @@ class VTK_RealTimeNeedleDetection_EXPORT vtkRealTimeNeedleDetectionGUI : public 
   //----------------------------------------------------------------
   // Build Frames
   //----------------------------------------------------------------
-
   virtual void BuildGUI ( );
   void BuildGUIForHelpFrame();
-  void BuildGUIForTestFrame1();
-  void BuildGUIForTestFrame2();
+  void BuildGUIForGeneralParameters();
 
   //----------------------------------------------------------------
   // Update routines
   //----------------------------------------------------------------
-
   void UpdateAll();
+  void UpdateGUI();
 
 
- protected:
-  
+ protected:  
   //----------------------------------------------------------------
   // Timer
-  //----------------------------------------------------------------
-  
+  //----------------------------------------------------------------  
   int TimerFlag;
   int TimerInterval;
 
   //----------------------------------------------------------------
   // GUI widgets
   //----------------------------------------------------------------
-
-  vtkKWPushButton* TestButton11;
-  vtkKWPushButton* TestButton12;
-  vtkKWPushButton* TestButton21;
-  vtkKWPushButton* TestButton22;
-
+  vtkKWPushButton* pStartButton;
+  vtkKWPushButton* pStopButton;
+  vtkKWMenuButton* pSourceMenu;
+  vtkKWEntry*      pScannerIDEntry;
+  vtkKWEntry*      pXLowerEntry;
+  vtkKWEntry*      pXUpperEntry;
+  vtkKWEntry*      pYLowerEntry;
+  vtkKWEntry*      pYUpperEntry;
+  
+  //----------------------------------------------------------------
+  // MRML nodes
+  //----------------------------------------------------------------
+  vtkMRMLVolumeNode*  pSourceNode;   // MRML node, which Slicer received via OpenIGTLink from the Scanner
+  vtkMRMLVolumeNode*  pVolumeNode;   // MRML node, which contains the processed image
+    
   //----------------------------------------------------------------
   // Logic Values
   //----------------------------------------------------------------
-
-  vtkRealTimeNeedleDetectionLogic *Logic;
-  vtkCallbackCommand *DataCallbackCommand;
+  vtkIGTDataManager*                 DataManager;
+  vtkRealTimeNeedleDetectionLogic*   Logic;
+  vtkCallbackCommand*               DataCallbackCommand;
   int                        CloseScene;
-
+  int                         started;
+  
+  //----------------------------------------------------------------
+  // Image Values
+  //----------------------------------------------------------------   
+  int             xLowerBound;
+  int             xUpperBound;        
+  int             yLowerBound;     
+  int             yUpperBound;
+  int             xImageRegionSize;
+  int             yImageRegionSize;
+  int              imageDimensions[3]; // the number of cells on x, y and z axis
+  double          imageSpacing[3];
+  double          imageOrigin[3];
+  int             scalarSize;          // 0,1 = 0 | 2,3 (char) = 1 | 4,5 (short) = 2 | 6,7 = 4
+  unsigned long    lastModified;       // saves the time of the last change of the source image
+  void*            pImage;             // pointer to the image in pImageData
+  ImageProcessor* pImageProcessor;
 };
 
 
