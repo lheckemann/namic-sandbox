5901420f6677db11cd937fe2806ee7297314afd8
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-03-04 21:51:42 +0000

    ENH: Calling GetNameOfClass() to increase code coverage.

--- trunk/QuadEdgeMeshRigidRegistration/Testing/itkLinearInterpolateMeshFunctionTest1.cxx
+++ trunk/QuadEdgeMeshRigidRegistration/Testing/itkLinearInterpolateMeshFunctionTest1.cxx
@@ -96,6 +96,8 @@ int main(int argc, char* argv[] )
   std::cout << "Interpolator " << std::endl;
   interpolator->Print( std::cout );
 
+  std::cout << interpolator->GetNameOfClass() << std::endl;
+
   PointType  point;
 
   mesh->GetPoint( 0, &point );
