1673dc51124a90f2ed625a76dc5e63ec08ae5a8d
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-03-09 19:52:25 +0000

    ENH: Adding a test for composition.

--- trunk/QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Testing/itkVersorOperationsTest1.cxx
+++ trunk/QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Testing/itkVersorOperationsTest1.cxx
@@ -34,12 +34,29 @@ int main( int argc, char *argv[] )
   axis1[1] = 0.0;
   axis1[2] = 0.0;
   
-  AngleType angle1 = vnl_math::pi;
+  AngleType angle1 = vnl_math::pi / 2.0;
    
   VersorType versor1;
   versor1.Set( axis1, angle1 );
 
   std::cout << "versor1 = " << versor1 << std::endl;
+
+  AxisType axis2;
+
+  axis2[0] = 0.0;
+  axis2[1] = 0.0;
+  axis2[2] = 1.0;
+  
+  AngleType angle2 = vnl_math::pi / 2.0;
+   
+  VersorType versor2;
+  versor2.Set( axis2, angle2 );
+
+  std::cout << "versor2 = " << versor2 << std::endl;
+
+  VersorType versor12 = versor2 * versor1;
+ 
+  std::cout << "versor12 = " << versor12 << std::endl;
 
   return EXIT_SUCCESS;
 }
