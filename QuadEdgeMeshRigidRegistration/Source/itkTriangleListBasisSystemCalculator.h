e53b598b9801d50d2bc72f5027121a293d5196cf
Michel <Michel@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-03-16 23:35:22 +0000

    ENH: adding triangle list processing for QuadEdgeMesh.

---
+++ QuadEdgeMeshRigidRegistration/Source/itkTriangleListBasisSystemCalculator.h
@@ -0,0 +1,94 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkTriangleListBasisSystemCalculator.h,v $
+  Language:  C++
+  Date:      $Date: 2008-10-17 13:35:26 $
+  Version:   $Revision: 1.47 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkTriangleListBasisSystemCalculator_h
+#define __itkTriangleListBasisSystemCalculator_h
+
+#include "itkMesh.h"
+#include "itkPoint.h"
+#include "itkTriangleBasisSystemCalculator.h"
+
+namespace itk
+{
+
+
+/** \class TriangleListBasisSystemCalculator
+ * \brief  Computes basis coefficients for a list of triangular cells.
+ *
+ * TriangleListBasisSystemCalculator computes basis coefficients
+ * within a list of triangles contained in a mesh. Basis coefficients
+ * can be used thereafter for interpolation and gradient computation
+ * within those triangles. 
+ *
+ * This class is templated over the input vector type and dimension of basis.
+ *
+ * \sa TriangleBasisSystem
+ *
+ */
+template < class TMesh, class TBasisSystem >
+class ITK_EXPORT TriangleListBasisSystemCalculator : TriangleBasisSystemCalculator< TMesh, TBasisSystem >
+{
+public:
+    /** Standard class typedefs. */
+    typedef TriangleListBasisSystemCalculator< TMesh, TBasisSystem >             Self;
+    typedef TriangleBasisSystemCalculator< TMesh, TBasisSystem >                 Superclass;
+    typedef SmartPointer<Self>                        Pointer;
+    typedef SmartPointer<const Self>                  ConstPointer;
+  
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    /** Standard part of every itk Object. */
+    itkTypeMacro(TriangleListBasisSystemCalculator, Object);
+
+    typedef TBasisSystem BasisSystemType;
+    typedef
+       VectorContainer<TMesh, TBasisSystem> BasisSystemListType;
+    typedef typename BasisSystemListType::Iterator  BasisSystemListIterator;    
+    typedef typename BasisSystemListType::Pointer   BasisSystemListPointer;
+
+    typedef TMesh                                   MeshType;
+    typedef typename MeshType::PointType            PointType;
+    typedef typename MeshType::CellType             CellType;
+    typedef typename PointType::VectorType          VectorType;
+    typedef typename MeshType::ConstPointer         MeshConstPointer;
+
+    typedef typename MeshType::CellsContainerIterator CellsContainerIterator;
+    
+
+    itkSetConstObjectMacro( InputMesh, MeshType );
+    itkGetConstObjectMacro( InputMesh, MeshType );
+
+    void Calculate();
+    const BasisSystemListType * GetBasisSystemList() const; 
+
+protected:
+  TriangleListBasisSystemCalculator();
+  virtual ~TriangleListBasisSystemCalculator();
+
+private:
+  MeshConstPointer        m_InputMesh;
+  BasisSystemListPointer   m_List;
+};
+
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkTriangleListBasisSystemCalculator.txx"
+#endif
+
+#endif
---
+++ QuadEdgeMeshRigidRegistration/Testing/itkTriangleListBasisSystemCalculatorTest1.cxx
@@ -0,0 +1,88 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkTriangleBasisSystemCalculatorTest.cxx,v $
+  Language:  C++
+  Date:      $Date: 2005-07-26 15:55:12 $
+  Version:   $Revision: 1.4 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkPoint.h"
+#include "itkVector.h"
+#include "itkQuadEdgeMesh.h"
+#include "itkTriangleBasisSystem.h"
+#include "itkTriangleBasisSystemCalculator.h"
+#include "itkTriangleListBasisSystemCalculator.h"
+#include "itkRegularSphereMeshSource.h"
+#include "itkMeshGeneratorHelper.h"
+#include "itkTestingMacros.h"
+
+int main(int argc, char *argv[])
+{
+
+  const unsigned int SurfaceDimension = 2; 
+  const unsigned int Dimension = 3;
+
+  typedef itk::QuadEdgeMesh< float, Dimension >   MovingMeshType;
+  typedef itk::QuadEdgeMesh< float, Dimension >   MeshType;
+
+  MeshType::Pointer   mesh;
+  MovingMeshType::Pointer  movingMesh;
+  typedef MeshType::PointType       PointType;
+  typedef PointType::VectorType     VectorType;
+
+  typedef MeshGeneratorHelper< MeshType, MovingMeshType >  GeneratorType;
+
+  GeneratorType::GenerateMeshes( mesh, movingMesh );
+
+  typedef itk::TriangleBasisSystem< VectorType, SurfaceDimension>  TriangleBasisSystemType;
+  TriangleBasisSystemType triangleBasisSystem;
+
+  typedef itk::TriangleListBasisSystemCalculator< MeshType, TriangleBasisSystemType >
+     TriangleListBasisSystemCalculatorType;
+
+  TriangleListBasisSystemCalculatorType::Pointer triangleListBasisSystemCalculator =
+     TriangleListBasisSystemCalculatorType::New(); 
+
+  const TriangleListBasisSystemCalculatorType::BasisSystemListType * basisSystemListPtr= NULL; 
+   
+  //std::cout << triangleListBasisSystemCalculator->GetNameOfClass() << std::endl; //Michel FIXME: print is inaccessible
+  //triangleListBasisSystemCalculator->Print( std::cout ); //Michel FIXME: print is inaccessible
+
+  //Mesh not set yet. Exception!
+  TRY_EXPECT_EXCEPTION( triangleListBasisSystemCalculator->Calculate() );
+  
+  TRY_EXPECT_NO_EXCEPTION( triangleListBasisSystemCalculator->SetInputMesh( mesh ) ); 
+
+  //List not computed yet. Exception!
+  TRY_EXPECT_EXCEPTION( triangleListBasisSystemCalculator->GetBasisSystemList() );
+
+  TEST_SET_GET( mesh, triangleListBasisSystemCalculator->GetInputMesh() );
+
+  //This should produce a list
+  TRY_EXPECT_NO_EXCEPTION( triangleListBasisSystemCalculator->Calculate() );
+
+  basisSystemListPtr=  triangleListBasisSystemCalculator->GetBasisSystemList();
+
+#if 0  
+  TriangleListBasisSystemCalculatorType::BasisSystemListIterator basisSystemListIterator;
+
+  basisSystemListIterator= basisSystemListPtr->Begin();  
+  std::cout << " basis list first element " << (*listBasisSystemIterator) << std::endl;
+#endif
+
+  std::cout << "Test passed." << std::endl;
+
+  return EXIT_SUCCESS;
+}
