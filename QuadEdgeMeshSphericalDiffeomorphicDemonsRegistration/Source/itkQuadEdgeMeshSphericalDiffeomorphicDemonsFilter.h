2a8adb8b9267cd844f7f374195d6661043fcc91c
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-03-23 20:01:41 +0000

    ENH: Adding method where the Mn (gradients of the moving image at every node of the fixed image) will be computed. ENH: Adding computation of the Gn() matrix at every node.

--- QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Source/itkQuadEdgeMeshSphericalDiffeomorphicDemonsFilter.h
+++ QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Source/itkQuadEdgeMeshSphericalDiffeomorphicDemonsFilter.h
@@ -102,6 +102,7 @@ private:
   void AllocateInternalArrays();
   void ComputeBasisSystemAtEveryNode();
   void RunIterations();
+  void ComputeGradientsOfMappedMovingValueAtEveryNode();
   void ComputeDeformationFieldUpdate();
   void SmoothDeformationField();
 
