b66e7bb23bd51a1c3aa277bc7a0dcb576106884c
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-03-31 15:01:58 +0000

    ENH: add script to run kety model fitting

---
+++ FourDAnalysis/Python/test_kety.py
@@ -0,0 +1,30 @@
+#! /Users/junichi/igtdev/slicer/Slicer3-lib/python-build/bin/python2.5
+
+import sys
+import scipy, scipy.optimize, scipy.io, numpy
+import re
+import FourDAnalysis as fda
+
+if len(sys.argv) < 4:
+    sys.exit()
+
+Path          = 'CurveFittingKetyModel.py'
+AifCsvFile    = sys.argv[1]
+InputCsvFile  = sys.argv[2]
+OutputCsvFile = sys.argv[3]
+
+aifCurve      = scipy.io.read_array(AifCsvFile, separator=',')
+inputCurve    = scipy.io.read_array(InputCsvFile, separator=',')
+outputCurve   = inputCurve
+
+caexec = fda.CurveAnalysisExecuter(Path)
+caexec.SetInputCurve('AIF', aifCurve)
+result = caexec.Execute(inputCurve, outputCurve)
+
+scipy.io.write_array(OutputCsvFile, outputCurve, separator=',')
+
+for i, v, in result.iteritems():
+    sys.stderr.write(' %5s     : %f\n'  % (i, v) )
+
+
+
