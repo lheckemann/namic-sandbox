42c822ca5662693e5bf99f46eeedc0da976fb96a
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-05-14 17:30:59 +0000

    STYLE: Fixing Style

--- trunk/BSplineDeformableTransformInitializer/Testing/itkBSplineDeformableTransformInitializerTest2.cxx
+++ trunk/BSplineDeformableTransformInitializer/Testing/itkBSplineDeformableTransformInitializerTest2.cxx
@@ -19,7 +19,6 @@
 #endif
 
 
-
 #include "itkImageFileReader.h" 
 #include "itkImageFileWriter.h" 
 
@@ -39,27 +38,27 @@
 class CommandProgressUpdate : public itk::Command 
 {
 public:
-  typedef  CommandProgressUpdate   Self;
+  typedef  CommandProgressUpdate    Self;
   typedef  itk::Command             Superclass;
-  typedef itk::SmartPointer<Self>  Pointer;
+  typedef itk::SmartPointer<Self>   Pointer;
   itkNewMacro( Self );
 protected:
   CommandProgressUpdate() {};
 public:
   void Execute(itk::Object *caller, const itk::EventObject & event)
     {
-      Execute( (const itk::Object *)caller, event);
+    Execute( (const itk::Object *)caller, event);
     }
 
   void Execute(const itk::Object * object, const itk::EventObject & event)
     {
-      const itk::ProcessObject * filter = 
-        dynamic_cast< const itk::ProcessObject * >( object );
-      if( ! itk::ProgressEvent().CheckEvent( &event ) )
-        {
-        return;
-        }
-      std::cout << filter->GetProgress() << std::endl;
+    const itk::ProcessObject * filter = 
+      dynamic_cast< const itk::ProcessObject * >( object );
+    if( ! itk::ProgressEvent().CheckEvent( &event ) )
+      {
+      return;
+      }
+    std::cout << filter->GetProgress() << std::endl;
     }
 };
 
@@ -79,7 +78,7 @@ int main( int argc, char * argv[] )
 
   const     unsigned int   ImageDimension = 2;
 
-  typedef   unsigned char  PixelType;
+  typedef   unsigned char                            PixelType;
   typedef   itk::Image< PixelType, ImageDimension >  FixedImageType;
   typedef   itk::Image< PixelType, ImageDimension >  MovingImageType;
 
@@ -145,10 +144,6 @@ int main( int argc, char * argv[] )
   
   movingWriter->SetInput( resampler->GetOutput() );
 
-
-
-
-
   const unsigned int SpaceDimension = ImageDimension;
   const unsigned int SplineOrder = 3;
   typedef double CoordinateRepType;
@@ -159,7 +154,6 @@ int main( int argc, char * argv[] )
                             SplineOrder >     TransformType;
   
   TransformType::Pointer bsplineTransform = TransformType::New();
-
 
 
   typedef itk::BSplineDeformableTransformInitializer<
@@ -200,20 +194,11 @@ int main( int argc, char * argv[] )
 
   infile.close();
 
-
-
-
-
   bsplineTransform->SetParameters( parameters );
 
+  CommandProgressUpdate::Pointer observer = CommandProgressUpdate::New();
 
-
-
-   CommandProgressUpdate::Pointer observer = CommandProgressUpdate::New();
-
-   resampler->AddObserver( itk::ProgressEvent(), observer );
-  
-
+  resampler->AddObserver( itk::ProgressEvent(), observer );
 
   resampler->SetTransform( bsplineTransform );
   
@@ -229,9 +214,9 @@ int main( int argc, char * argv[] )
     }
 
 
-  typedef itk::Point<  float, ImageDimension >  PointType;
-  typedef itk::Vector< float, ImageDimension >  VectorType;
-  typedef itk::Image< VectorType, ImageDimension >  DeformationFieldType;
+  typedef itk::Point<  float, ImageDimension >        PointType;
+  typedef itk::Vector< float, ImageDimension >        VectorType;
+  typedef itk::Image< VectorType, ImageDimension >    DeformationFieldType;
 
   DeformationFieldType::Pointer field = DeformationFieldType::New();
   field->SetRegions( fixedRegion );
@@ -262,7 +247,6 @@ int main( int argc, char * argv[] )
     }
 
 
-
   typedef itk::ImageFileWriter< DeformationFieldType >  FieldWriterType;
   FieldWriterType::Pointer fieldWriter = FieldWriterType::New();
 
@@ -285,4 +269,3 @@ int main( int argc, char * argv[] )
 
   return EXIT_SUCCESS;
 }
-
