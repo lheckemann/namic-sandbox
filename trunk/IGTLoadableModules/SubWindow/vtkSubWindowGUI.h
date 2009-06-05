f2ad25d7bed59d338a96336b391881ce2a8be528
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-05 13:28:04 +0000

    ENH: Sub-window module (based on Andras Lasso's code)

---
+++ trunk/IGTLoadableModules/SubWindow/vtkSubWindowGUI.h
@@ -0,0 +1,143 @@
+/*==========================================================================
+
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $HeadURL: $
+  Date:      $Date: $
+  Version:   $Revision: $
+
+==========================================================================*/
+
+#ifndef __vtkSubWindowGUI_h
+#define __vtkSubWindowGUI_h
+
+#ifdef WIN32
+#include "vtkSubWindowWin32Header.h"
+#endif
+
+#include "vtkSlicerModuleGUI.h"
+#include "vtkCallbackCommand.h"
+#include "vtkSlicerInteractorStyle.h"
+
+#include "vtkSubWindowLogic.h"
+
+#include "vtkSlicerSecondaryMonitor.h"
+
+class vtkKWPushButton;
+
+class VTK_SubWindow_EXPORT vtkSubWindowGUI : public vtkSlicerModuleGUI
+{
+ public:
+
+  vtkTypeRevisionMacro ( vtkSubWindowGUI, vtkSlicerModuleGUI );
+
+  //----------------------------------------------------------------
+  // Set/Get Methods
+  //----------------------------------------------------------------
+
+  vtkGetObjectMacro ( Logic, vtkSubWindowLogic );
+  void SetModuleLogic ( vtkSlicerLogic *logic )
+  { 
+    this->SetLogic ( vtkObjectPointer (&this->Logic), logic );
+  }
+
+ protected:
+  //----------------------------------------------------------------
+  // Constructor / Destructor (proctected/private) 
+  //----------------------------------------------------------------
+
+  vtkSubWindowGUI ( );
+  virtual ~vtkSubWindowGUI ( );
+
+ private:
+  vtkSubWindowGUI ( const vtkSubWindowGUI& ); // Not implemented.
+  void operator = ( const vtkSubWindowGUI& ); //Not implemented.
+
+ public:
+  //----------------------------------------------------------------
+  // New method, Initialization etc.
+  //----------------------------------------------------------------
+
+  static vtkSubWindowGUI* New ();
+  void Init();
+  virtual void Enter ( );
+  virtual void Exit ( );
+
+  virtual void TearDownGUI();
+
+  void PrintSelf (ostream& os, vtkIndent indent );
+
+  //----------------------------------------------------------------
+  // Observer Management
+  //----------------------------------------------------------------
+
+  virtual void AddGUIObservers ( );
+  virtual void RemoveGUIObservers ( );
+  void AddLogicObservers ( );
+  void RemoveLogicObservers ( );
+
+  //----------------------------------------------------------------
+  // Event Handlers
+  //----------------------------------------------------------------
+
+  virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
+  void ProcessTimerEvents();
+  void HandleMouseEvent(vtkSlicerInteractorStyle *style);
+  static void DataCallback(vtkObject *caller, 
+                           unsigned long eid, void *clientData, void *callData);
+  
+  //----------------------------------------------------------------
+  // Build Frames
+  //----------------------------------------------------------------
+
+  virtual void BuildGUI ( );
+  void BuildGUIForHelpFrame();
+  void BuildGUIForTestFrame1();
+  void BuildGUIForTestFrame2();
+
+  //----------------------------------------------------------------
+  // Update routines
+  //----------------------------------------------------------------
+
+  void UpdateAll();
+
+
+ protected:
+  
+  //----------------------------------------------------------------
+  // Timer
+  //----------------------------------------------------------------
+  
+  int TimerFlag;
+  int TimerInterval;
+
+  //----------------------------------------------------------------
+  // GUI widgets
+  //----------------------------------------------------------------
+
+  vtkKWPushButton* TestButton11;
+  vtkKWPushButton* TestButton12;
+  vtkKWPushButton* TestButton21;
+  vtkKWPushButton* TestButton22;
+
+  vtkSlicerSecondaryMonitor* SecondaryMonitor;
+
+  //----------------------------------------------------------------
+  // Logic Values
+  //----------------------------------------------------------------
+
+  vtkSubWindowLogic *Logic;
+  vtkCallbackCommand *DataCallbackCommand;
+  int                        CloseScene;
+
+};
+
+
+
+#endif
