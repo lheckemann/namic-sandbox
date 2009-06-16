aa7b2e649de528ec7dd86e4ff398f4ba9074e286
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-16 15:47:09 +0000

    ENH: Adding more of the example.

--- CUDA-ITK/Source/Example1.cu
+++ CUDA-ITK/Source/Example1.cu
@@ -6,3 +6,28 @@ __global__ void VectorAddKernel( float * Vector1, float * Vector2, float * Octpu
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   Output[idx] = Vector1[idx] + Vector2[idx];
 }
+
+int main()
+{
+
+  float HostVector1[SIZE];
+  float HostVector2[SIZE];
+  float HostOutputVector[SIZE];
+
+
+  for( int i=0; i<SIZE; i++)
+    {
+    HostVector1[i] = i;
+    HostVector2[i] = i;
+    }
+
+  float * GPUVector1;
+  float * GPUVector2;
+  float * GPUOutputVector;
+
+  cudaError_t err;
+
+  err = cudaMalloc((void**)&GPUVector1, SIZE*sizeof(float));
+  err = cudaMalloc((void**)&GPUVector2, SIZE*sizeof(float));
+
+}
