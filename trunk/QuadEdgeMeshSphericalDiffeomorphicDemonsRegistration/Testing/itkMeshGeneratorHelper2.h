00a54fe87f081012906f0ab73f65661505a576ac
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-17 15:40:29 +0000

    ENH: Adding a new Helpr for generating a Mesh with Vectors as pixel data.

---
+++ trunk/QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Testing/itkMeshGeneratorHelper2.h
@@ -0,0 +1,130 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkMeanSquaresMeshToMeshMetricTest1.cxx,v $
+  Language:  C++
+  Date:      $Date: 2007-09-06 17:44:24 $
+  Version:   $Revision: 1.3 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkMeshGeneratorHelper2_h
+#define __itkMeshGeneratorHelper2_h
+
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkQuadEdgeMesh.h"
+#include "itkRegularSphereMeshSource.h"
+#include "itkQuadEdgeMeshScalarDataVTKPolyDataWriter.h"
+
+
+//
+//  This class expects the Mesh type to use Vectors as
+//  its PixelType (PointData type, to be more specific).
+//
+//
+
+namespace itk
+{
+
+template <class TMesh >
+class MeshGeneratorHelper2 
+{
+public:
+
+  typedef TMesh                    MeshType;
+  typedef typename TMesh::Pointer  MeshPointer;
+
+static void GenerateMesh( MeshPointer & mesh )
+  {
+  typedef itk::RegularSphereMeshSource< MeshType >  SphereMeshSourceType;
+
+  typename SphereMeshSourceType::Pointer sphereMeshSource = SphereMeshSourceType::New();
+
+  typedef typename SphereMeshSourceType::PointType     PointType;
+  typedef typename PointType::VectorType               VectorType;
+
+
+  PointType center; 
+  center.Fill( 0.0 );
+
+  VectorType scale;
+  scale.Fill( 1.0 );
+  
+  sphereMeshSource->SetCenter( center );
+  sphereMeshSource->SetScale( scale );
+  sphereMeshSource->SetResolution( 1 );
+
+  try
+    {
+    sphereMeshSource->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Error during source Update() " << std::endl;
+    std::cerr << excp << std::endl;
+    return;
+    }
+
+  mesh = sphereMeshSource->GetOutput();
+
+  typedef typename MeshType::PointDataContainer::ConstIterator PointDataIterator;
+  PointDataIterator pixelIterator = mesh->GetPointData()->Begin();
+  PointDataIterator pixelEnd      = mesh->GetPointData()->End();
+
+  typedef typename MeshType::PointsContainer::Iterator     PointIterator;
+  PointIterator pointIterator = mesh->GetPoints()->Begin();
+  PointIterator pointEnd      = mesh->GetPoints()->End();
+
+  VectorType vector;
+  vector.Fill( 0.0 );
+
+  while( pixelIterator != pixelEnd  && pointIterator != pointEnd ) 
+    {
+    const PointType & point = pointIterator.Value();
+    pixelIterator.Value() = vector;
+    ++pixelIterator;
+    ++pointIterator;
+    }
+
+  }
+
+};
+
+
+template <class TMesh >
+class MeshWriterHelper 
+{
+public:
+static void WriteMeshToFile( const TMesh * mesh, const char * filename )
+  {
+  typedef itk::QuadEdgeMeshScalarDataVTKPolyDataWriter< TMesh >   WriterType;
+  typename WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( mesh );
+  writer->SetFileName( filename );
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Error during writer Update() " << std::endl;
+    std::cerr << excp << std::endl;
+    return;
+    }
+  }
+};
+
+}
+
+#endif
