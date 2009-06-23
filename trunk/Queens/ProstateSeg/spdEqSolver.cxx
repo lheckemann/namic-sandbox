e6b6f384d8710eedeb287c707527149d388cc75b
ygao <ygao@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-23 15:29:03 +0000

    ENH: use preconditioned CG, faster

--- trunk/Queens/ProstateSeg/spdEqSolver.cxx
+++ trunk/Queens/ProstateSeg/spdEqSolver.cxx
@@ -51,11 +51,11 @@ bool SpdEqSolver::isPosDef(spMtx* A)
 
 int SpdEqSolver::solveSpdEq(mtx* A, vec* b, vec* x, int numIter, elementType tol)
 {
-//   if (!isPosDef(A))
-//     {
-//       std::cerr<<"NOT positive definite! exiting...\n";
-//       exit(-1);
-//     }
+  //   if (!isPosDef(A))
+  //     {
+  //       std::cerr<<"NOT positive definite! exiting...\n";
+  //       exit(-1);
+  //     }
 
   vec r = (*b) - (*A)*(*x);
   vec p = r;
@@ -99,21 +99,12 @@ int SpdEqSolver::solveSpdEq(mtx* A, vec* b, vec* x, int numIter, elementType tol
 }
 
 
-int SpdEqSolver::solveSpdEq(spMtx* A, vec* b, vec* x, int numIter, elementType tol)
+
+
+/* Jacobi Preconditioned CG */
+/*============================================================*/
+int solveSpdEq(spMtx* A, vec* b, vec* x, int numIter, elementType tol)
 {
-//   std::cout<<"in sparse matrix solver!\n"<<std::flush;
-//   std::cout<<"numIter = "<<numIter<<"\n"<<std::flush;
-//   std::cout<<"tol = "<<tol<<"\n"<<std::flush;
-
-//   if (!isPosDef(A))
-//     {
-//       std::cerr<<"NOT positive definite! exiting...\n";
-//       exit(-1);
-//     }
-
-//   std::cout<<"max element of *b = "<<b->max_value()<<std::endl<<std::flush;
-//   std::cout<<"b is zero?"<<b->is_zero()<<std::endl<<std::flush;
-
   reportProgress(0);
 
   vec Ax;
@@ -122,8 +113,28 @@ int SpdEqSolver::solveSpdEq(spMtx* A, vec* b, vec* x, int numIter, elementType t
   //  std::cout<<"Ax is zero? "<<Ax.is_zero()<<std::endl<<std::flush;
 
 
+  unsigned long N = A->cols();
+  if (A->rows() != N)
+    {
+      std::cerr<<"A is not square, abort. \n"<<std::endl;
+      exit(-1);
+    }
+
   vec r = (*b) - Ax;
-  vec p = r;
+
+  vec z( r.size() );
+
+  // Jacobi preconditioner
+  vec Cinv( r.size() );
+  for (unsigned long i = 0; i < N; ++i)
+    {
+      Cinv[i] = 1.0/( (*A)(i, i) + DBL_MIN);
+
+      z[i] = r[i]*Cinv[i];
+    }
+
+  vec d = z;
+
   
   if (numIter == -1)
     {
@@ -133,39 +144,116 @@ int SpdEqSolver::solveSpdEq(spMtx* A, vec* b, vec* x, int numIter, elementType t
 
   for (int i = 0; i <= numIter; ++i)
     {
-      vec Ap;
-      A->pre_mult(p, Ap);
+      vec Ad;
+      A->pre_mult(d, Ad);
 
-      elementType pAp = inner_product(p, Ap);
-      // std::cout<<"pAp = "<<pAp<<std::endl<<std::flush;
+      elementType dAd = inner_product(d, Ad);
 
+      elementType zTr = inner_product(z, r);
 
-      elementType alpha = inner_product(p, r)/(pAp + DBL_MIN);
-      //std::cout<<"alpha = "<<alpha<<std::endl<<std::flush;
+      elementType alpha = zTr/(dAd + DBL_MIN);
 
-      (*x) += alpha*p;
+      (*x) += alpha*d;
 
-      r -= alpha*Ap;
+      r -= alpha*Ad;
 
-      if (inner_product(r, r) < tol)
+      double rTr = inner_product(r, r);
+      if ( rTr < tol)
         {
-          //          std::cout<<"<r, r> = "<<inner_product(r, r)<<std::endl<<std::flush;
+          // std::cout<<"<r, r> = "<<rTr<<std::endl<<std::flush;
           reportProgress(100);
           return i;
         }
 
-      vec Ar;
-      A->pre_mult(r, Ar);
+      for (unsigned long i = 0; i < N; ++i)
+        {
+          z[i] = r[i]*Cinv[i];
+        }
 
-      elementType beta = -inner_product(p, Ar)/(pAp + DBL_MIN);
+      elementType beta \
+        = inner_product(z, r)/(zTr + DBL_MIN);
 
-      p = r + beta*p;
+
+      d = z + beta*d;
+
       reportProgress(i*100/numIter);
     }
 
   reportProgress(100);
   return numIter;
 }
+
+
+// int SpdEqSolver::solveSpdEq(spMtx* A, vec* b, vec* x, int numIter, elementType tol)
+// {
+// //   std::cout<<"in sparse matrix solver!\n"<<std::flush;
+// //   std::cout<<"numIter = "<<numIter<<"\n"<<std::flush;
+// //   std::cout<<"tol = "<<tol<<"\n"<<std::flush;
+
+// //   if (!isPosDef(A))
+// //     {
+// //       std::cerr<<"NOT positive definite! exiting...\n";
+// //       exit(-1);
+// //     }
+
+// //   std::cout<<"max element of *b = "<<b->max_value()<<std::endl<<std::flush;
+// //   std::cout<<"b is zero?"<<b->is_zero()<<std::endl<<std::flush;
+
+//   reportProgress(0);
+
+//   vec Ax;
+//   A->pre_mult(*x, Ax);
+//   //  std::cout<<"*x is zero? "<<x->is_zero()<<std::endl<<std::flush;
+//   //  std::cout<<"Ax is zero? "<<Ax.is_zero()<<std::endl<<std::flush;
+
+
+//   vec r = (*b) - Ax;
+//   vec p = r;
+  
+//   if (numIter == -1)
+//     {
+//       numIter = b->size();
+//       numIter += numIter/10; // let the iteration times a little more than the dimesion
+//     }
+
+//   for (int i = 0; i <= numIter; ++i)
+//     {
+//       vec Ap;
+//       A->pre_mult(p, Ap);
+
+//       elementType pAp = inner_product(p, Ap);
+//       // std::cout<<"pAp = "<<pAp<<std::endl<<std::flush;
+
+
+//       elementType alpha = inner_product(p, r)/(pAp + DBL_MIN);
+//       //std::cout<<"alpha = "<<alpha<<std::endl<<std::flush;
+
+//       (*x) += alpha*p;
+
+//       r -= alpha*Ap;
+
+//       if (inner_product(r, r) < tol)
+//         {
+//           //          std::cout<<"<r, r> = "<<inner_product(r, r)<<std::endl<<std::flush;
+//           reportProgress(100);
+//           return i;
+//         }
+
+//       vec Ar;
+//       A->pre_mult(r, Ar);
+
+//       elementType beta = -inner_product(p, Ar)/(pAp + DBL_MIN);
+
+//       p = r + beta*p;
+//       reportProgress(i*100/numIter);
+//     }
+
+//   reportProgress(100);
+//   return numIter;
+// }
+
+
+
 
 void SpdEqSolver::setProgressCallback(PROGRESS_CALLBACK_TYPE progressCallback)
 {
@@ -175,7 +263,8 @@ void SpdEqSolver::setProgressCallback(PROGRESS_CALLBACK_TYPE progressCallback)
 void SpdEqSolver::reportProgress(int percentComplete)
 {
   if (_progressCallback!=NULL)
-  {
-    (*_progressCallback)(percentComplete);
-  }
+    {
+      (*_progressCallback)(percentComplete);
+    }
 }
+
