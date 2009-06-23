63f08222e8cc21b0ff6b9ef6307fc4faf89c5817
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-23 16:04:39 +0000

    ENH: Adding command line tool for evaluating the metric. Adding tests for applying a metric to registered images.

---
+++ trunk/SIMBIOS/KneeModeling/Registation/ImageToImage/Source/MeanSquaresImageMetric.cxx
@@ -0,0 +1,114 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: MeanSquaresImageMetric1.cxx,v $
+  Language:  C++
+  Date:      $Date: 2006-11-12 22:08:29 $
+  Version:   $Revision: 1.12 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkMeanSquaresImageToImageMetric.h"
+#include "itkIdentityTransform.h"
+#include "itkNearestNeighborInterpolateImageFunction.h"
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  fixedImage  movingImage" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const     unsigned int   Dimension = 3;
+  typedef   signed short   PixelType;
+
+  typedef itk::Image< PixelType, Dimension >   ImageType;
+
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+
+  ReaderType::Pointer fixedReader  = ReaderType::New();
+  ReaderType::Pointer movingReader = ReaderType::New();
+
+  fixedReader->SetFileName(  argv[ 1 ] );
+  movingReader->SetFileName( argv[ 2 ] );
+
+  try 
+    {
+    fixedReader->Update();
+    movingReader->Update();
+    }
+  catch( itk::ExceptionObject & excep )
+    {
+    std::cerr << "Exception catched !" << std::endl;
+    std::cerr << excep << std::endl;
+    }
+
+
+  typedef itk::MeanSquaresImageToImageMetric< 
+                            ImageType, ImageType >  MetricType;
+
+  MetricType::Pointer metric = MetricType::New();
+
+
+  typedef itk::IdentityTransform< double, Dimension >  TransformType;
+
+  TransformType::Pointer transform = TransformType::New();
+
+
+  typedef itk::NearestNeighborInterpolateImageFunction< 
+                                 ImageType, double >  InterpolatorType;
+
+  InterpolatorType::Pointer interpolator = InterpolatorType::New();
+
+
+  ImageType::ConstPointer fixedImage  = fixedReader->GetOutput();
+  ImageType::ConstPointer movingImage = movingReader->GetOutput();
+
+  metric->SetTransform( transform );
+  metric->SetInterpolator( interpolator );
+
+  metric->SetFixedImage(  fixedImage  );
+  metric->SetMovingImage( movingImage );
+
+  metric->SetFixedImageRegion(  fixedImage->GetBufferedRegion()  );
+
+  try 
+    {
+    metric->Initialize();
+    }
+  catch( itk::ExceptionObject & excep )
+    {
+    std::cerr << "Exception catched !" << std::endl;
+    std::cerr << excep << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  MetricType::TransformParametersType displacement( Dimension );
+
+  displacement.Fill(0.0);
+
+  const double value = metric->GetValue( displacement );
+
+  std::cout << "MeanSquaresMetric = " << value << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+
