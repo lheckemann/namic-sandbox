3f9c9b26ddf36566b75d68a571c79336c4e48dd1
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-06-24 16:34:42 +0000

    ENH: Rescaling intensities in output slice.

--- trunk/SIMBIOS/KneeModeling/Registation/ImageToImage/Source/ExtractSlice.cxx
+++ trunk/SIMBIOS/KneeModeling/Registation/ImageToImage/Source/ExtractSlice.cxx
@@ -22,99 +22,71 @@
 #define ITK_LEAN_AND_MEAN
 #endif
 
-
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-
-
-
 #include "itkExtractImageFilter.h"
-
-
+#include "itkRescaleIntensityImageFilter.h"
 #include "itkImage.h"
 
 
 int main( int argc, char ** argv )
 {
   // Verify the number of parameters in the command line
-  if( argc < 3 )
+  if( argc < 4 )
     {
     std::cerr << "Usage: " << std::endl;
     std::cerr << argv[0] << " input3DImageFile  output2DImageFile " << std::endl;
-    std::cerr << " sliceNumber " << std::endl;
+    std::cerr << " orientation(x=0, y=1, z=2)  sliceNumber " << std::endl;
     return EXIT_FAILURE;
     }
 
 
-
-  typedef signed short        InputPixelType;
-  typedef signed short        OutputPixelType;
+  typedef signed short  InputPixelType;
+  typedef unsigned char OutputPixelType;
 
   typedef itk::Image< InputPixelType,  3 >    InputImageType;
+  typedef itk::Image< InputPixelType,  2 >    SliceImageType;
   typedef itk::Image< OutputPixelType, 2 >    OutputImageType;
-
-
 
   typedef itk::ImageFileReader< InputImageType  >  ReaderType;
   typedef itk::ImageFileWriter< OutputImageType >  WriterType;
 
-
-  // Here we recover the file names from the command line arguments
-  //
   const char * inputFilename  = argv[1];
   const char * outputFilename = argv[2];
-
-
 
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
 
-
-
   reader->SetFileName( inputFilename  );
   writer->SetFileName( outputFilename );
 
+  typedef itk::ExtractImageFilter< InputImageType, OutputImageType > ExtractFilterType;
+  ExtractFilterType::Pointer extractor = ExtractFilterType::New();
 
+  typedef itk::RescaleIntensityImageFilter< InputImageType, OutputImageType > RescaleFilterType;
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
 
-  typedef itk::ExtractImageFilter< InputImageType, OutputImageType > FilterType;
-  FilterType::Pointer filter = FilterType::New();
-
-
-
-  
   reader->Update();
-  InputImageType::RegionType inputRegion =
-           reader->GetOutput()->GetLargestPossibleRegion();
+  InputImageType::RegionType inputRegion = reader->GetOutput()->GetLargestPossibleRegion();
 
-
+  unsigned int orientation = atoi( argv[3] );
 
   InputImageType::SizeType size = inputRegion.GetSize();
-  size[2] = 0;
- 
-
-
+  size[orientation] = 0;
 
   InputImageType::IndexType start = inputRegion.GetIndex();
-  const unsigned int sliceNumber = atoi( argv[3] );
-  start[2] = sliceNumber;
-
-
+  const unsigned int sliceNumber = atoi( argv[4] );
+  start[orientation] = sliceNumber;
 
   InputImageType::RegionType desiredRegion;
   desiredRegion.SetSize(  size  );
   desiredRegion.SetIndex( start );
 
+  extractor->SetExtractionRegion( desiredRegion );
 
-
-
-  filter->SetExtractionRegion( desiredRegion );
-
-
-
-  filter->SetInput( reader->GetOutput() );
-  writer->SetInput( filter->GetOutput() );
-
-
+  extractor->SetInput( reader->GetOutput() );
+  rescaler->SetInput( extractor->GetOutput() );
+  writer->SetInput( rescaler->GetOutput() );
 
   try 
     { 
@@ -126,7 +98,6 @@ int main( int argc, char ** argv )
     std::cerr << err << std::endl; 
     return EXIT_FAILURE;
     } 
-
 
   return EXIT_SUCCESS;
 }
