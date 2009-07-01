ad50389cf4ca6c103b8926a24ecbf236f1cfc698
elhawary <elhawary@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-07-01 20:13:04 +0000

    ENH: adding source code to repository

---
+++ IGTLoadableModules/EMNeuro/vtkEMNeuroGUI.h
@@ -0,0 +1,136 @@
+/*==========================================================================
+
+  Portions (c) Copyright 2008 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $HeadURL: $
+  Date:      $Date: $
+  Version:   $Revision: $
+
+==========================================================================*/
+
+#ifndef __vtkEMNeuroGUI_h
+#define __vtkEMNeuroGUI_h
+
+#ifdef WIN32
+#include "vtkEMNeuroWin32Header.h"
+#endif
+
+#include "vtkSlicerModuleGUI.h"
+#include "vtkCallbackCommand.h"
+#include "vtkSlicerInteractorStyle.h"
+
+#include "vtkEMNeuroLogic.h"
+
+class vtkKWPushButton;
+
+class VTK_EMNeuro_EXPORT vtkEMNeuroGUI : public vtkSlicerModuleGUI
+{
+ public:
+
+  vtkTypeRevisionMacro ( vtkEMNeuroGUI, vtkSlicerModuleGUI );
+
+  //----------------------------------------------------------------
+  // Set/Get Methods
+  //----------------------------------------------------------------
+
+  vtkGetObjectMacro ( Logic, vtkEMNeuroLogic );
+  void SetModuleLogic ( vtkSlicerLogic *logic )
+  { 
+    this->SetLogic ( vtkObjectPointer (&this->Logic), logic );
+  }
+
+ protected:
+  //----------------------------------------------------------------
+  // Constructor / Destructor (proctected/private) 
+  //----------------------------------------------------------------
+
+  vtkEMNeuroGUI ( );
+  virtual ~vtkEMNeuroGUI ( );
+
+ private:
+  vtkEMNeuroGUI ( const vtkEMNeuroGUI& ); // Not implemented.
+  void operator = ( const vtkEMNeuroGUI& ); //Not implemented.
+
+ public:
+  //----------------------------------------------------------------
+  // New method, Initialization etc.
+  //----------------------------------------------------------------
+
+  static vtkEMNeuroGUI* New ();
+  void Init();
+  virtual void Enter ( );
+  virtual void Exit ( );
+  void PrintSelf (ostream& os, vtkIndent indent );
+
+  //----------------------------------------------------------------
+  // Observer Management
+  //----------------------------------------------------------------
+
+  virtual void AddGUIObservers ( );
+  virtual void RemoveGUIObservers ( );
+  void AddLogicObservers ( );
+  void RemoveLogicObservers ( );
+
+  //----------------------------------------------------------------
+  // Event Handlers
+  //----------------------------------------------------------------
+
+  virtual void ProcessLogicEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessGUIEvents ( vtkObject *caller, unsigned long event, void *callData );
+  virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
+  void ProcessTimerEvents();
+  void HandleMouseEvent(vtkSlicerInteractorStyle *style);
+  static void DataCallback(vtkObject *caller, 
+                           unsigned long eid, void *clientData, void *callData);
+  
+  //----------------------------------------------------------------
+  // Build Frames
+  //----------------------------------------------------------------
+
+  virtual void BuildGUI ( );
+  void BuildGUIForHelpFrame();
+  void BuildGUIForTestFrame1();
+  void BuildGUIForTestFrame2();
+
+  //----------------------------------------------------------------
+  // Update routines
+  //----------------------------------------------------------------
+
+  void UpdateAll();
+
+
+ protected:
+  
+  //----------------------------------------------------------------
+  // Timer
+  //----------------------------------------------------------------
+  
+  int TimerFlag;
+  int TimerInterval;
+
+  //----------------------------------------------------------------
+  // GUI widgets
+  //----------------------------------------------------------------
+
+  vtkKWPushButton* TestButton11;
+  vtkKWPushButton* TestButton12;
+  vtkKWPushButton* TestButton21;
+  vtkKWPushButton* TestButton22;
+
+  //----------------------------------------------------------------
+  // Logic Values
+  //----------------------------------------------------------------
+
+  vtkEMNeuroLogic *Logic;
+  vtkCallbackCommand *DataCallbackCommand;
+  int                        CloseScene;
+
+};
+
+
+
+#endif
