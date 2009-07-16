7ff7db54d79c571eba5ccb382ff1a1a0a64cc7d4
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-07-16 19:50:39 +0000

    ENH: Simplifying the code to reduce it to a minimal, didactic example.

--- trunk/QuadEdgeMeshFieldSmoothingFilters/Examples/SmoothingMeshWithVectors.cxx
+++ trunk/QuadEdgeMeshFieldSmoothingFilters/Examples/SmoothingMeshWithVectors.cxx
@@ -19,11 +19,9 @@
 #endif
 
 #include "itkQuadEdgeMeshVectorPixelValuesSmoothingFilter.h"
-#include "itkQuadEdgeMesh.h"
-#include "itkMeshWriterHelper2.h"
+#include "itkQuadEdgeMeshVectorDataVTKPolyDataWriter.h"
 #include "itkQuadEdgeMeshVTKPolyDataReader.h"
-#include "itkTestingMacros.h"
-#include "itkFilterWatcher.h"
+#include "itkQuadEdgeMesh.h"
 
 int main( int argc, char *argv[] )
 {
@@ -46,109 +44,41 @@ int main( int argc, char *argv[] )
 
   FilterType::Pointer filter = FilterType::New();
 
-  std::cout << filter->GetNameOfClass() << std::endl;
-  filter->Print( std::cout );
-
   typedef itk::QuadEdgeMeshVTKPolyDataReader< InputMeshType > ReaderType;
 
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
 
-  try
-    {
-    reader->Update( );
-    }
-  catch( itk::ExceptionObject & exp )
-    {
-    std::cerr << "Exception thrown while reading the input file " << std::endl;
-    std::cerr << exp << std::endl;
-    return EXIT_FAILURE;
-    }
+  FilterType::OutputPointType sphereCenter;
+  sphereCenter.Fill( 0.0 );
 
-  filter->SetInput( reader->GetOutput() );
+  const double sphereRadius = 100.0;
 
-  if( filter->GetInput() != reader->GetOutput() )
-    {
-    std::cerr << "Error in SetFixedMesh()/GetFixedMesh() " << std::endl;
-    return EXIT_FAILURE;
-    }
-
-  unsigned int maximumNumberOfIterations = 10;
-  filter->SetMaximumNumberOfIterations( maximumNumberOfIterations );
-  TEST_SET_GET_VALUE( maximumNumberOfIterations, filter->GetMaximumNumberOfIterations() );
-
-  maximumNumberOfIterations = 15;
-  filter->SetMaximumNumberOfIterations( maximumNumberOfIterations );
-  TEST_SET_GET_VALUE( maximumNumberOfIterations, filter->GetMaximumNumberOfIterations() );
-
-  double lambda = 2.0;
-  filter->SetLambda( lambda );
-  TEST_SET_GET_VALUE( lambda, filter->GetLambda() );
-
-  lambda = 3.0;
-  filter->SetLambda( lambda );
-  TEST_SET_GET_VALUE( lambda, filter->GetLambda() );
-
-  FilterType::OutputPointType center1;
-  center1.Fill( 17.0 );
-  filter->SetSphereCenter( center1 );
-  TEST_SET_GET_VALUE( center1, filter->GetSphereCenter() );
-
-  FilterType::OutputPointType center2;
-  center2.Fill( 31.0 );
-  filter->SetSphereCenter( center2 );
-  TEST_SET_GET_VALUE( center2, filter->GetSphereCenter() );
-
-  const double radius1 = 19.0;
-  filter->SetSphereRadius( radius1 );
-  TEST_SET_GET_VALUE( radius1, filter->GetSphereRadius() );
-
-  const double radius2 = 29.0;
-  filter->SetSphereRadius( radius2 );
-  TEST_SET_GET_VALUE( radius2, filter->GetSphereRadius() );
-
-
-  FilterType::OutputPointType center0;
-  center0.Fill( 0.0 );
-
-  filter->SetSphereCenter( center0 );
-  filter->SetSphereRadius( 100.0 );
-
-  FilterWatcher watcher( filter, "Smoothing Filter");
+  filter->SetSphereCenter( sphereCenter );
+  filter->SetSphereRadius( sphereRadius );
 
   filter->SetLambda( atof( argv[3] ) );
   filter->SetMaximumNumberOfIterations( atoi( argv[4] ) );
 
-  TRY_EXPECT_NO_EXCEPTION( filter->Update( ) );
+  typedef itk::QuadEdgeMeshVectorDataVTKPolyDataWriter< OutputMeshType >   WriterType;
 
-  itk::MeshWriterHelper2< OutputMeshType >::WriteMeshToFile( filter->GetOutput(), argv[2] );
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[2] );
 
-#ifdef TEST_EXCEPTIONS
-  InputMeshType::Pointer inputMesh = reader->GetOutput();
-  inputMesh->DisconnectPipeline();
-  
-  reader = NULL; // Destroy the reader
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
 
-  InputMeshType::PointsContainerPointer points = inputMesh->GetPoints();
-  InputMeshType::PointDataContainerPointer pointsData = inputMesh->GetPointData();
+  filter->Update();
 
-  inputMesh->SetPointData( NULL );
-
-  TRY_EXPECT_EXCEPTION( filter->Update( ) );
-  
-  inputMesh->SetPointData( pointsData );
-
-  TRY_EXPECT_NO_EXCEPTION( filter->Update( ) );
-
-  inputMesh->SetPoints( NULL );
-
-  TRY_EXPECT_EXCEPTION( filter->Update( ) );
-  
-  inputMesh->SetPoints( points );
-
-  TRY_EXPECT_NO_EXCEPTION( filter->Update( ) );
-#endif
-
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
 }
