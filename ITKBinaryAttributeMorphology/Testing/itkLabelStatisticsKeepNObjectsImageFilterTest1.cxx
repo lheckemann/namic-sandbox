432f531ed303b946d4b9551eabf457b1ccfa72a3
sophie.chen <sophie.chen@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-07-27 19:50:59 +0000

    ENH: First pass code review of renamed test file.

---
+++ ITKBinaryAttributeMorphology/Testing/itkLabelStatisticsKeepNObjectsImageFilterTest1.cxx
@@ -0,0 +1,64 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkMergeLabelMapFilterTest1.cxx,v $
+  Language:  C++
+  Date:      $Date: 2009-07-23 23:05:00 $
+  Version:   $Revision: 1.2 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkLabelStatisticsKeepNObjectsImageFilter.h"
+
+#include "itkTestingMacros.h"
+
+int itkLabelStatisticsKeepNObjectsImageFilterTest1(int argc, char * argv[])
+{
+
+  if( argc != 8 )
+    {
+    std::cerr << "usage: " << argv[0] << " input input output background nb reverseOrdering attribute" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const unsigned int dim = 3;
+  
+  typedef itk::Image< unsigned char, dim > IType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  
+  ReaderType::Pointer reader2 = ReaderType::New();
+  reader2->SetFileName( argv[2] );
+  
+  typedef itk::LabelStatisticsKeepNObjectsImageFilter< IType, IType > LabelOpeningType;
+  LabelOpeningType::Pointer opening = LabelOpeningType::New();
+  opening->SetInput( reader->GetOutput() );
+  opening->SetFeatureImage( reader2->GetOutput() );
+  opening->SetBackgroundValue( atoi(argv[4]) );
+  opening->SetNumberOfObjects( atoi(argv[5]) );
+  opening->SetReverseOrdering( atoi(argv[6]) );
+  opening->SetAttribute( argv[7] );
+  itk::SimpleFilterWatcher watcher(opening, "filter");
+
+  typedef itk::ImageFileWriter< IType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( opening->GetOutput() );
+  writer->SetFileName( argv[3] );
+  writer->UseCompressionOn();  
+
+  GET_EXPECT_EXCEPTION( writer->Update() );
+  return EXIT_SUCCESS;
+}
