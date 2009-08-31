7e247fb2e94565976bb5003d0561f31d8b30fbdb
ygao <ygao@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-08-31 03:10:25 +0000

    ENH: edit script param

--- MeningiomaSegmentation/SegmentationMethod3/test/toRun.bash
+++ MeningiomaSegmentation/SegmentationMethod3/test/toRun.bash
@@ -5,13 +5,13 @@
 
 
 # Seg image 2, 1500 iteration, smooth factor 0.8, pick a point (59 58 39) in the object, use global statistics 1
-./meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_02_2x.nrrd r2.nrrd 2000 0.8 59 58 39 1
+./r/meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_02_2x.nrrd r2.nrrd 2000 0.8 59 58 39 1
 
 # Seg image 4, 500 iteration, smooth factor 0.8, pick a point (35 34 32) in the object, use global statistics 1
-./meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_04_2x.nrrd r4.nrrd 500 0.8 35 34 32 1
+./r/meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_04_2x.nrrd r4.nrrd 500 0.8 35 34 32 1
 
 # Seg image 8, 500 iteration, smooth factor 1.0, pick a point (36 42 18) in the object, 
 # Use global statistics 0 (Thus, using local statistics )
 # This is slower than the global method, but this is a robust method to handle the difficult roi8 image.
-./meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_04_2x.nrrd r8.nrrd 500 1.0 36 42 18 0
+./r/meningiomaSegmentor ~/usr/work/NAMICSandBox/MeningiomaSegmentation/Data/roi_04_2x.nrrd r8.nrrd 500 1.0 36 42 18 0
 
