1dc766ffbb94ce075d82fd91b42254e1943d9c1c
elhawary <elhawary@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-10-13 21:22:15 +0000

    ENH: added pivot calibration

--- IGTLoadableModules/HybridNav/HybridNav/vtkHybridNavGUI.cxx
+++ IGTLoadableModules/HybridNav/HybridNav/vtkHybridNavGUI.cxx
@@ -33,8 +33,12 @@
 #include "vtkKWEvent.h"
 
 #include "vtkKWPushButton.h"
+#include "vtkKWEntry.h"
+#include "vtkSlicerNodeSelectorWidget.h"
 
 #include "vtkCornerAnnotation.h"
+
+#include "vtkPivotCalibration.h"
 
 
 //---------------------------------------------------------------------------
@@ -53,16 +57,23 @@ vtkHybridNavGUI::vtkHybridNavGUI ( )
   this->DataCallbackCommand = vtkCallbackCommand::New();
   this->DataCallbackCommand->SetClientData( reinterpret_cast<void *> (this) );
   this->DataCallbackCommand->SetCallback(vtkHybridNavGUI::DataCallback);
-  
+
   //----------------------------------------------------------------
   // GUI widgets
-  this->TestButton11 = NULL;
-  this->TestButton12 = NULL;
+  this->ToolNodeSelectorMenu = NULL;
+  this->StartCalibrateButton = NULL;
+  this->numPointsEntry = NULL;
   this->TestButton21 = NULL;
   this->TestButton22 = NULL;
-  
+
   //----------------------------------------------------------------
-  // Locator  (MRML)
+  // Variables
+  this->pivot = vtkPivotCalibration::New();
+
+  //----------------------------------------------------------------
+  // Locator  (MRML)  //----------------------------------------------------------------
+  // GUI widgets
+  //----------------------------------------------------------------
   this->TimerFlag = 0;
 
 }
@@ -87,28 +98,22 @@ vtkHybridNavGUI::~vtkHybridNavGUI ( )
   //----------------------------------------------------------------
   // Remove GUI widgets
 
-  if (this->TestButton11)
+  if (this->ToolNodeSelectorMenu)
     {
-    this->TestButton11->SetParent(NULL);
-    this->TestButton11->Delete();
+    this->ToolNodeSelectorMenu->SetParent(NULL);
+    this->ToolNodeSelectorMenu->Delete();
     }
 
-  if (this->TestButton12)
+  if (this->numPointsEntry)
     {
-    this->TestButton12->SetParent(NULL);
-    this->TestButton12->Delete();
+    this->numPointsEntry->SetParent(NULL);
+    this->numPointsEntry->Delete();
     }
 
-  if (this->TestButton21)
+  if (this->StartCalibrateButton)
     {
-    this->TestButton21->SetParent(NULL);
-    this->TestButton21->Delete();
-    }
-
-  if (this->TestButton22)
-    {
-    this->TestButton22->SetParent(NULL);
-    this->TestButton22->Delete();
+    this->StartCalibrateButton->SetParent(NULL);
+    this->StartCalibrateButton->Delete();
     }
 
   //----------------------------------------------------------------
@@ -130,7 +135,7 @@ void vtkHybridNavGUI::Enter()
 {
   // Fill in
   //vtkSlicerApplicationGUI *appGUI = this->GetApplicationGUI();
-  
+
   if (this->TimerFlag == 0)
     {
     this->TimerFlag = 1;
@@ -163,30 +168,23 @@ void vtkHybridNavGUI::RemoveGUIObservers ( )
 {
   //vtkSlicerApplicationGUI *appGUI = this->GetApplicationGUI();
 
-  if (this->TestButton11)
+  if (this->ToolNodeSelectorMenu)
     {
-    this->TestButton11
+    this->ToolNodeSelectorMenu
+       ->RemoveObserver((vtkCommand *)this->GUICallbackCommand);
+    }
+
+  if (this->numPointsEntry)
+    {
+    this->numPointsEntry
       ->RemoveObserver((vtkCommand *)this->GUICallbackCommand);
     }
 
-  if (this->TestButton12)
+  if (this->StartCalibrateButton)
     {
-    this->TestButton12
+    this->StartCalibrateButton
       ->RemoveObserver((vtkCommand *)this->GUICallbackCommand);
     }
-
-  if (this->TestButton21)
-    {
-    this->TestButton21
-      ->RemoveObserver((vtkCommand *)this->GUICallbackCommand);
-    }
-
-  if (this->TestButton22)
-    {
-    this->TestButton22
-      ->RemoveObserver((vtkCommand *)this->GUICallbackCommand);
-    }
-
 
   this->RemoveLogicObservers();
 
@@ -207,7 +205,7 @@ void vtkHybridNavGUI::AddGUIObservers ( )
   //events->InsertNextValue(vtkMRMLScene::NodeAddedEvent);
   //events->InsertNextValue(vtkMRMLScene::NodeRemovedEvent);
   events->InsertNextValue(vtkMRMLScene::SceneCloseEvent);
-  
+
   if (this->GetMRMLScene() != NULL)
     {
     this->SetAndObserveMRMLSceneEvents(this->GetMRMLScene(), events);
@@ -217,13 +215,11 @@ void vtkHybridNavGUI::AddGUIObservers ( )
   //----------------------------------------------------------------
   // GUI Observers
 
-  this->TestButton11
-    ->AddObserver(vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand);
-  this->TestButton12
-    ->AddObserver(vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand);
-  this->TestButton21
-    ->AddObserver(vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand);
-  this->TestButton22
+  this->ToolNodeSelectorMenu
+    ->AddObserver(vtkSlicerNodeSelectorWidget::NodeSelectedEvent, (vtkCommand *)this->GUICallbackCommand);
+  this->numPointsEntry
+     ->AddObserver(vtkKWEntry::EntryValueChangedEvent, (vtkCommand *)this->GUICallbackCommand);
+  this->StartCalibrateButton
     ->AddObserver(vtkKWPushButton::InvokedEvent, (vtkCommand *)this->GUICallbackCommand);
 
   this->AddLogicObservers();
@@ -247,7 +243,7 @@ void vtkHybridNavGUI::RemoveLogicObservers ( )
 //---------------------------------------------------------------------------
 void vtkHybridNavGUI::AddLogicObservers ( )
 {
-  this->RemoveLogicObservers();  
+  this->RemoveLogicObservers();
 
   if (this->GetLogic())
     {
@@ -276,32 +272,49 @@ void vtkHybridNavGUI::ProcessGUIEvents(vtkObject *caller,
     return;
     }
 
-  
-  if (this->TestButton11 == vtkKWPushButton::SafeDownCast(caller) 
-      && event == vtkKWPushButton::InvokedEvent)
+  //Events related with node selection
+  if (this->ToolNodeSelectorMenu == vtkSlicerNodeSelectorWidget::SafeDownCast(caller)
+      && event == vtkSlicerNodeSelectorWidget::NodeSelectedEvent)
     {
-    std::cerr << "TestButton11 is pressed." << std::endl;
-    }
-  else if (this->TestButton12 == vtkKWPushButton::SafeDownCast(caller)
-      && event == vtkKWPushButton::InvokedEvent)
-    {
-    std::cerr << "TestButton12 is pressed." << std::endl;
-    }
-  else if (this->TestButton21 == vtkKWPushButton::SafeDownCast(caller)
-      && event == vtkKWPushButton::InvokedEvent)
-    {
-    std::cerr << "TestButton21 is pressed." << std::endl;
-    }
-  else if (this->TestButton22 == vtkKWPushButton::SafeDownCast(caller)
-      && event == vtkKWPushButton::InvokedEvent)
-    {
-    std::cerr << "TestButton22 is pressed." << std::endl;
+    std::cerr << "Node Selector is pressed." << std::endl;
     }
 
-} 
+  //Events related with changing numPoints text box
+  else if (this->numPointsEntry == vtkKWEntry::SafeDownCast(caller)
+      && event == vtkKWEntry::EntryValueChangedEvent)
+    {
+    std::cerr << "numPoints has been modified." << std::endl;
+    }
 
+  //Events related with pressing calibration button
+  else if (this->StartCalibrateButton == vtkKWPushButton::SafeDownCast(caller)
+      && event == vtkKWPushButton::InvokedEvent)
+    {
+    //Acknowledge the button has been pressed
+    std::cerr << "StartCalibrateButton is pressed." << std::endl;
 
-void vtkHybridNavGUI::DataCallback(vtkObject *caller, 
+    //Check if a node has been selected
+    if (this->ToolNodeSelectorMenu->GetSelected())
+      {
+      vtkMRMLNode* node = this->ToolNodeSelectorMenu->GetSelected();
+
+      //Remove any current observers on node
+
+      //Add observer on selected node
+      vtkIntArray* nodeEvents = vtkIntArray::New();
+      nodeEvents->InsertNextValue(vtkMRMLTransformableNode::TransformModifiedEvent);
+      vtkMRMLNode* nullNode = NULL;
+      vtkSetAndObserveMRMLNodeEventsMacro(nullNode, node, nodeEvents);
+      nodeEvents->Delete();
+
+      //Initialize pivot calibration object
+      pivot->Initialize(this->numPointsEntry->GetValueAsInt(), node);
+      }
+    }
+}
+
+//----------------------------------------------------------------------------------------
+void vtkHybridNavGUI::DataCallback(vtkObject *caller,
                                      unsigned long eid, void *clientData, void *callData)
 {
   vtkHybridNavGUI *self = reinterpret_cast<vtkHybridNavGUI *>(clientData);
@@ -329,13 +342,28 @@ void vtkHybridNavGUI::ProcessLogicEvents ( vtkObject *caller,
 void vtkHybridNavGUI::ProcessMRMLEvents ( vtkObject *caller,
                                             unsigned long event, void *callData )
 {
-  // Fill in
-
-  if (event == vtkMRMLScene::SceneCloseEvent)
+  // Detect if something has happened in the MRML scene
+  if (caller != NULL)
     {
+    std::cerr<< "MRML event called" << std::endl;
+    vtkMRMLNode* node = vtkMRMLNode::SafeDownCast(caller);
+
+    //Check to see if calibration transform node has been updated
+    if ((node == dynamic_cast<vtkMRMLNode*>(pivot->transformNode) ) && (event == vtkMRMLTransformableNode::TransformModifiedEvent))
+      {
+      //Print node in terminal and send node to calibration vector
+      std::cerr << "Calibration Node has been updated" << std::endl;
+      int i = pivot->AcquireTransform();
+      if (i == 1)
+        {
+        std::cerr << "Calibration matrix: ";
+        pivot->CalibrationTransform->Print(std::cerr);
+        }
+      //node->RemoveObservers(vtkMRMLTransformableNode::TransformModifiedEvent, (vtkCommand *)this->GUICallbackCommand);
+      //node->RemoveAllObservers();
+      }
     }
 }
-
 
 //---------------------------------------------------------------------------
 void vtkHybridNavGUI::ProcessTimerEvents()
@@ -343,9 +371,9 @@ void vtkHybridNavGUI::ProcessTimerEvents()
   if (this->TimerFlag)
     {
     // update timer
-    vtkKWTkUtilities::CreateTimerHandler(vtkKWApplication::GetMainInterp(), 
+    vtkKWTkUtilities::CreateTimerHandler(vtkKWApplication::GetMainInterp(),
                                          this->TimerInterval,
-                                         this, "ProcessTimerEvents");        
+                                         this, "ProcessTimerEvents");
     }
 }
 
@@ -355,13 +383,12 @@ void vtkHybridNavGUI::BuildGUI ( )
 {
 
   // ---
-  // MODULE GUI FRAME 
+  // MODULE GUI FRAME
   // create a page
   this->UIPanel->AddPage ( "HybridNav", "HybridNav", NULL );
 
   BuildGUIForHelpFrame();
-  BuildGUIForTestFrame1();
-  BuildGUIForTestFrame2();
+  BuildGUIForCalibrationFrame();
 
 }
 
@@ -369,7 +396,7 @@ void vtkHybridNavGUI::BuildGUI ( )
 void vtkHybridNavGUI::BuildGUIForHelpFrame ()
 {
   // Define your help text here.
-  const char *help = 
+  const char *help =
     "See "
     "<a>http://www.slicer.org/slicerWiki/index.php/Modules:HybridNav</a> for details.";
   const char *about =
@@ -381,105 +408,111 @@ void vtkHybridNavGUI::BuildGUIForHelpFrame ()
 
 
 //---------------------------------------------------------------------------
-void vtkHybridNavGUI::BuildGUIForTestFrame1()
+void vtkHybridNavGUI::BuildGUIForCalibrationFrame()
 {
 
   vtkSlicerApplication *app = (vtkSlicerApplication *)this->GetApplication();
   vtkKWWidget *page = this->UIPanel->GetPageWidget ("HybridNav");
-  
-  vtkSlicerModuleCollapsibleFrame *conBrowsFrame = vtkSlicerModuleCollapsibleFrame::New();
 
-  conBrowsFrame->SetParent(page);
-  conBrowsFrame->Create();
-  conBrowsFrame->SetLabelText("Test Frame 1");
-  //conBrowsFrame->CollapseFrame();
+  vtkSlicerModuleCollapsibleFrame *calibrationFrame = vtkSlicerModuleCollapsibleFrame::New();
+
+  calibrationFrame->SetParent(page);
+  calibrationFrame->Create();
+  calibrationFrame->SetLabelText("Pivot Calibration");
   app->Script ("pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
-               conBrowsFrame->GetWidgetName(), page->GetWidgetName());
+               calibrationFrame->GetWidgetName(), page->GetWidgetName());
 
   // -----------------------------------------
-  // Test child frame
+  // Calibration Options Frame
 
   vtkKWFrameWithLabel *frame = vtkKWFrameWithLabel::New();
-  frame->SetParent(conBrowsFrame->GetFrame());
+  frame->SetParent(calibrationFrame->GetFrame());
   frame->Create();
-  frame->SetLabelText ("Test child frame");
+  frame->SetLabelText ("Calibration Options");
   this->Script ( "pack %s -side top -fill x -expand y -anchor w -padx 2 -pady 2",
                  frame->GetWidgetName() );
 
-  // -----------------------------------------
-  // Test push button
+  //Node Selector Menu
+  vtkKWFrame *nodeFrame = vtkKWFrame::New();
+  nodeFrame->SetParent(frame->GetFrame());
+  nodeFrame->Create();
+  //nodeFrame->SetWidth(20);
+  app->Script ( "pack %s -fill both -expand true",
+                nodeFrame->GetWidgetName());
 
-  this->TestButton11 = vtkKWPushButton::New ( );
-  this->TestButton11->SetParent ( frame->GetFrame() );
-  this->TestButton11->Create ( );
-  this->TestButton11->SetText ("Test 11");
-  this->TestButton11->SetWidth (12);
+  vtkKWLabel* nodeLabel = vtkKWLabel::New();
+  nodeLabel->SetParent(nodeFrame);
+  nodeLabel->Create();
+  nodeLabel->SetText("Select Transform: ");
+  nodeLabel->SetWidth(15);
 
-  this->TestButton12 = vtkKWPushButton::New ( );
-  this->TestButton12->SetParent ( frame->GetFrame() );
-  this->TestButton12->Create ( );
-  this->TestButton12->SetText ("Tset 12");
-  this->TestButton12->SetWidth (12);
+  this->ToolNodeSelectorMenu = vtkSlicerNodeSelectorWidget::New();
+  this->ToolNodeSelectorMenu->SetParent(nodeFrame);
+  this->ToolNodeSelectorMenu->Create();
+  this->ToolNodeSelectorMenu->SetWidth(15);
+  this->ToolNodeSelectorMenu->SetNewNodeEnabled(0);
+  this->ToolNodeSelectorMenu->SetNodeClass("vtkMRMLLinearTransformNode", NULL, NULL, NULL);
+  this->ToolNodeSelectorMenu->NoneEnabledOn();
+  this->ToolNodeSelectorMenu->SetShowHidden(1);
+  this->ToolNodeSelectorMenu->Create();
+  this->ToolNodeSelectorMenu->SetMRMLScene(this->Logic->GetMRMLScene());
+  this->ToolNodeSelectorMenu->UpdateMenu();
+  this->ToolNodeSelectorMenu->SetBorderWidth(0);
+  //this->NodeSelectorMenu->SetLabelText( "Select Transform");
+  this->ToolNodeSelectorMenu->SetBalloonHelpString("Select a transform from the Scene");
 
-  this->Script("pack %s %s -side left -padx 2 -pady 2", 
-               this->TestButton11->GetWidgetName(),
-               this->TestButton12->GetWidgetName());
+  this->Script("pack %s %s -side left -anchor w -fill x -padx 2 -pady 2",
+               nodeLabel->GetWidgetName(), this->ToolNodeSelectorMenu->GetWidgetName());
 
-  conBrowsFrame->Delete();
+  //Number of points for calibration entry
+  vtkKWFrame* pointsFrame = vtkKWFrame::New();
+  pointsFrame->SetParent(frame->GetFrame());
+  pointsFrame->Create();
+  this->Script ( "pack %s -fill both -expand true",
+                 pointsFrame->GetWidgetName());
+
+  vtkKWLabel* pointsLabel = vtkKWLabel::New();
+  pointsLabel->SetParent(pointsFrame);
+  pointsLabel->Create();
+  pointsLabel->SetWidth(15);
+  pointsLabel->SetText("Number of Points: ");
+
+  this->numPointsEntry = vtkKWEntry::New();
+  this->numPointsEntry->SetParent(pointsFrame);
+  this->numPointsEntry->SetRestrictValueToInteger();
+  this->numPointsEntry->Create();
+  this->numPointsEntry->SetWidth(6);
+  this->numPointsEntry->SetValueAsInt(20);
+
+  this->Script ( "pack %s %s -side left -anchor w -fill x -padx 2 -pady 2",
+                 pointsLabel->GetWidgetName(),
+                 this->numPointsEntry->GetWidgetName());
+
+  //Calibration button
+  vtkKWFrame* startCalibrationFrame = vtkKWFrame::New();
+  startCalibrationFrame->SetParent(calibrationFrame->GetFrame());
+  startCalibrationFrame->Create();
+  this->Script ( "pack %s -fill both -expand true",
+                 startCalibrationFrame->GetWidgetName());
+
+  this->StartCalibrateButton = vtkKWPushButton::New ( );
+  this->StartCalibrateButton->SetParent ( startCalibrationFrame );
+  this->StartCalibrateButton->Create ( );
+  this->StartCalibrateButton->SetText ("Start Calibration");
+  this->StartCalibrateButton->SetWidth (15);
+  this->Script ( "pack %s -side left -expand true",
+                 StartCalibrateButton->GetWidgetName());
+
+  //Delete widget pointers
+  calibrationFrame->Delete();
+  nodeFrame->Delete();
+  pointsFrame->Delete();
+  pointsLabel->Delete();
+  startCalibrationFrame->Delete();
   frame->Delete();
+  nodeLabel->Delete();
 
 }
-
-
-//---------------------------------------------------------------------------
-void vtkHybridNavGUI::BuildGUIForTestFrame2 ()
-{
-  vtkSlicerApplication *app = (vtkSlicerApplication *)this->GetApplication();
-  vtkKWWidget *page = this->UIPanel->GetPageWidget ("HybridNav");
-  
-  vtkSlicerModuleCollapsibleFrame *conBrowsFrame = vtkSlicerModuleCollapsibleFrame::New();
-
-  conBrowsFrame->SetParent(page);
-  conBrowsFrame->Create();
-  conBrowsFrame->SetLabelText("Test Frame 2");
-  //conBrowsFrame->CollapseFrame();
-  app->Script ("pack %s -side top -anchor nw -fill x -padx 2 -pady 2 -in %s",
-               conBrowsFrame->GetWidgetName(), page->GetWidgetName());
-
-  // -----------------------------------------
-  // Test child frame
-
-  vtkKWFrameWithLabel *frame = vtkKWFrameWithLabel::New();
-  frame->SetParent(conBrowsFrame->GetFrame());
-  frame->Create();
-  frame->SetLabelText ("Test child frame");
-  this->Script ( "pack %s -side top -fill x -expand y -anchor w -padx 2 -pady 2",
-                 frame->GetWidgetName() );
-  
-  // -----------------------------------------
-  // Test push button
-
-  this->TestButton21 = vtkKWPushButton::New ( );
-  this->TestButton21->SetParent ( frame->GetFrame() );
-  this->TestButton21->Create ( );
-  this->TestButton21->SetText ("Test 21");
-  this->TestButton21->SetWidth (12);
-
-  this->TestButton22 = vtkKWPushButton::New ( );
-  this->TestButton22->SetParent ( frame->GetFrame() );
-  this->TestButton22->Create ( );
-  this->TestButton22->SetText ("Tset 22");
-  this->TestButton22->SetWidth (12);
-
-  this->Script("pack %s %s -side left -padx 2 -pady 2", 
-               this->TestButton21->GetWidgetName(),
-               this->TestButton22->GetWidgetName());
-
-
-  conBrowsFrame->Delete();
-  frame->Delete();
-}
-
 
 //----------------------------------------------------------------------------
 void vtkHybridNavGUI::UpdateAll()
