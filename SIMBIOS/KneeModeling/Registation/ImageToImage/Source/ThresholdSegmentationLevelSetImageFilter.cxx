1db3a878cb4b7f7225d3bcc5726cbae84c153907
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-10-31 21:56:29 +0000

    BUG: The input mask must be rescaled to be a level set.

--- SIMBIOS/KneeModeling/Registation/ImageToImage/Source/ThresholdSegmentationLevelSetImageFilter.cxx
+++ SIMBIOS/KneeModeling/Registation/ImageToImage/Source/ThresholdSegmentationLevelSetImageFilter.cxx
@@ -49,6 +49,18 @@ int main( int argc, char *argv[] )
   typedef unsigned char                            OutputPixelType;
   typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
 
+  typedef itk::BinaryThresholdImageFilter< 
+    InternalImageType, InternalImageType > InternalThresholdingFilterType;
+
+  InternalThresholdingFilterType::Pointer thresholder2 = InternalThresholdingFilterType::New();
+                        
+  thresholder2->SetLowerThreshold( 0.0 );
+  thresholder2->SetUpperThreshold( 127.5 );
+
+  thresholder2->SetOutsideValue(   4.0  );
+  thresholder2->SetInsideValue(   -4.0 );
+
+
   typedef itk::BinaryThresholdImageFilter<InternalImageType, OutputImageType>
                                                    ThresholdingFilterType;
   
@@ -79,19 +91,21 @@ int main( int argc, char *argv[] )
     ThresholdSegmentationLevelSetImageFilterType::New();
 
 
-  const double curvatureScaling = atof( argv[6] );
+  thresholdSegmentation->SetUpperThreshold( atof( argv[4] ) );
+  thresholdSegmentation->SetLowerThreshold( atof( argv[5] ) );
 
   thresholdSegmentation->SetPropagationScaling( 1.0 );
+
+  const double curvatureScaling = atof( argv[6] );
   thresholdSegmentation->SetCurvatureScaling( curvatureScaling );
   
   thresholdSegmentation->SetMaximumRMSError( 1e-4 );
   thresholdSegmentation->SetNumberOfIterations( 1200 );
 
-  thresholdSegmentation->SetUpperThreshold( atof( argv[4] ) );
-  thresholdSegmentation->SetLowerThreshold( atof( argv[5] ) );
-  thresholdSegmentation->SetIsoSurfaceValue( 127.5 );
+  thresholdSegmentation->SetIsoSurfaceValue( 0.0 );
   
-  thresholdSegmentation->SetInput( maskReader->GetOutput() );
+  thresholder2->SetInput( maskReader->GetOutput() );
+  thresholdSegmentation->SetInput( thresholder2->GetOutput() );
   thresholdSegmentation->SetFeatureImage( imageReader->GetOutput() );
 
   thresholder->SetInput( thresholdSegmentation->GetOutput() );
