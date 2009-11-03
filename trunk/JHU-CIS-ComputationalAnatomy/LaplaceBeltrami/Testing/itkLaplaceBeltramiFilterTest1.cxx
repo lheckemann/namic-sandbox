37b368875a347f011da9e5b4ad91d07b92b2111e
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-11-03 23:27:47 +0000

    ENH: Adding a first test.

---
+++ trunk/JHU-CIS-ComputationalAnatomy/LaplaceBeltrami/Testing/itkLaplaceBeltramiFilterTest1.cxx
@@ -0,0 +1,39 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: ImageRegistration8.cxx,v $
+  Language:  C++
+  Date:      $Date: 2008-03-10 19:46:31 $
+  Version:   $Revision: 1.37 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkLaplaceBeltramiFilter.h"
+
+int main( int argc, char *argv[] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " fixedMeshFile  movingMeshFile ";
+    return EXIT_FAILURE;
+    }
+  
+  typedef float      MeshPixelType;
+  const unsigned int Dimension = 3;
+
+  typedef itk::QuadEdgeMesh< MeshPixelType, Dimension >   MeshType;
+
+  return EXIT_SUCCESS;
+}
