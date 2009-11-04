7df836ff54c077ecb174e966bd9ca397bcffe459
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-11-04 18:29:14 +0000

    ENH: Adding an executable for resampling one image.

---
+++ trunk/SIMBIOS/KneeModeling/Registation/ImageToImage/Source/ResampleImageFilter.cxx
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: ShapeDetectionLevelSetFilter.cxx,v $
+  Language:  C++
+  Date:      $Date: 2009-03-17 21:44:43 $
+  Version:   $Revision: 1.41 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkResampleImageFilter.h"
+#include "itkIdentiyTransform.h"
+
+int main( int argc, char *argv[] )
+{
+  if( argc < 8 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " fixedImageFile  movingImageFile ";
+    std::cerr << " outputImagefile ";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+  
+  const    unsigned int    Dimension = 3;
+  typedef  signed short    PixelType;
+
+  typedef itk::Image< PixelType, Dimension >  FixedImageType;
+  typedef itk::Image< PixelType, Dimension >  MovingImageType;
+
+  typedef itk::IdentityTransform< double >       TransformType;
+
+  typedef itk:: LinearInterpolateImageFunction< 
+                                    MovingImageType,
+                                    double          >    InterpolatorType;
+
+  InterpolatorType::Pointer     interpolator    = InterpolatorType::New();
+  TransformType::Pointer   transform  = TransformType::New();
+  
+  typedef itk::ImageFileReader< FixedImageType  > FixedImageReaderType;
+  typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
+
+  FixedImageReaderType::Pointer  fixedImageReader  = FixedImageReaderType::New();
+  MovingImageReaderType::Pointer movingImageReader = MovingImageReaderType::New();
+
+  fixedImageReader->SetFileName(  argv[1] );
+  movingImageReader->SetFileName( argv[2] );
+
+  try 
+    { 
+    fixedImageReader->Update();
+    movingImageReader->Update();
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+ 
+  const FixedImageType *  fixedImage = fixedImageReader->GetOutput();
+  const MovingImageType * movingImage = movingImageReader->GetOutput();
+
+
+  typedef itk::ResampleImageFilter< 
+                            MovingImageType, 
+                            FixedImageType >    ResampleFilterType;
+
+  ResampleFilterType::Pointer resampler = ResampleFilterType::New();
+
+  resampler->SetTransform( transform );
+  resampler->SetInput( movingImage );
+
+  resampler->SetSize(    fixedImage->GetLargestPossibleRegion().GetSize() );
+  resampler->SetOutputOrigin(  fixedImage->GetOrigin() );
+  resampler->SetOutputSpacing( fixedImage->GetSpacing() );
+  resampler->SetOutputDirection( fixedImage->GetDirection() );
+  resampler->SetDefaultPixelValue( 0 ); // low MRI value
+  
+
+  typedef itk::ImageFileWriter< FixedImageType >  WriterType;
+
+  WriterType::Pointer      writer =  WriterType::New();
+
+  writer->SetFileName( argv[3] );
+  writer->SetInput( resampler->GetOutput()   );
+
+  try 
+    { 
+    writer->Update();
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+ 
+  return EXIT_SUCCESS;
+}
+
