95fc0360f48d4c4724aed7dc4b6d11a9d8fd28b9
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2009-11-05 19:52:09 +0000

    ENH: Preprocessing filter for combining the gradient magnitude of both 3T MRI image channels into a single gradient magnitude to be used as input for the level set.

---
+++ trunk/SIMBIOS/KneeModeling/Registation/ImageToImage/Source/TwoChannelGradientMagnitudeSigmoidFilter.cxx
@@ -0,0 +1,134 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: ShapeDetectionLevelSetFilter.cxx,v $
+  Language:  C++
+  Date:      $Date: 2009-03-17 21:44:43 $
+  Version:   $Revision: 1.41 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
+#include "itkSigmoidImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkTimeProbesCollectorBase.h"
+
+
+int main( int argc, char *argv[] )
+{
+  if( argc < 6 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " inputImage  outputImage";
+    std::cerr << " Sigma SigmoidAlpha SigmoidBeta ";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef   float           InternalPixelType;
+  const     unsigned int    Dimension = 3;
+
+  typedef itk::Image< InternalPixelType, Dimension >  InternalImageType;
+
+
+  typedef unsigned char     OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+  itk::TimeProbesCollectorBase  chronometer;
+
+  typedef  itk::ImageFileReader< InternalImageType > ReaderType;
+  typedef  itk::ImageFileWriter< InternalImageType > WriterType;
+
+  ReaderType::Pointer reader1 = ReaderType::New();
+  ReaderType::Pointer reader2 = ReaderType::New();
+
+  WriterType::Pointer writer = WriterType::New();
+
+  reader1->SetFileName( argv[1] );
+  reader2->SetFileName( argv[2] );
+
+  writer->SetFileName( argv[3] );
+
+  typedef   itk::GradientMagnitudeRecursiveGaussianImageFilter< 
+                               InternalImageType, 
+                               InternalImageType >  GradientFilterType;
+
+  typedef   itk::SigmoidImageFilter<
+                               InternalImageType, 
+                               InternalImageType >  SigmoidFilterType;
+
+  GradientFilterType::Pointer  gradientMagnitude1 = GradientFilterType::New();
+  GradientFilterType::Pointer  gradientMagnitude2 = GradientFilterType::New();
+
+  SigmoidFilterType::Pointer sigmoid = SigmoidFilterType::New();
+
+  sigmoid->SetOutputMinimum(  0.0  );
+  sigmoid->SetOutputMaximum(  1.0  );
+
+  gradientMagnitude1->SetInput( reader1->GetOutput() );
+  gradientMagnitude2->SetInput( reader2->GetOutput() );
+
+  sigmoid->SetInput( gradientMagnitude1->GetOutput() );
+  writer->SetInput( sigmoid->GetOutput() );
+
+  const double sigma = atof( argv[3] );
+
+  gradientMagnitude1->SetSigma(  sigma  );
+  gradientMagnitude2->SetSigma(  sigma  );
+
+  const double alpha =  atof( argv[4] );
+  const double beta  =  atof( argv[5] );
+
+  std::cout << "alpha = " << alpha << std::endl;
+  std::cout << "beta = " << beta << std::endl;
+
+  sigmoid->SetAlpha( alpha );
+  sigmoid->SetBeta(  beta  );
+  
+
+  try
+    {
+    chronometer.Start("reading");
+    reader1->Update();
+    reader2->Update();
+    chronometer.Stop("reading");
+    chronometer.Report( std::cout );
+
+    chronometer.Start("gradient");
+    gradientMagnitude1->Update();
+    gradientMagnitude2->Update();
+    chronometer.Stop("gradient");
+    chronometer.Report( std::cout );
+
+    chronometer.Start("sigmoid");
+    sigmoid->Update();
+    chronometer.Stop("sigmoid");
+    chronometer.Report( std::cout );
+
+    chronometer.Start("writer");
+    writer->Update();
+    chronometer.Stop("writer");
+    }
+  catch( itk::ExceptionObject & excep )
+    {
+    std::cerr << "Exception caught !" << std::endl;
+    std::cerr << excep << std::endl;
+    }
+
+  chronometer.Report( std::cout );
+
+  return EXIT_SUCCESS;
+}
