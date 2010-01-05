7b8e14cdf591e6a2051a2c00a0584ba6f28f81ea
ygao <ygao@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-05 18:06:01 +0000

    BUG flip z dir

--- trunk/Queens/ProstateSeg/ShapeBased/_reg/alignTrainingShapes/toSFLS/learn/learnShapes.bash
+++ trunk/Queens/ProstateSeg/ShapeBased/_reg/alignTrainingShapes/toSFLS/learn/learnShapes.bash
@@ -4,9 +4,24 @@ exeFile=./r/imagePCAAnalysisTest2
 listFile=shapeList.txt
 
 iii=0
-while [ $iii -lt 6 ]; do
+N=6
+while [ $iii -lt $N ]; do
     echo ${iii}
     ${exeFile} ${listFile} ${iii}
+
+    let iii=iii+1
+done
+
+
+
+# flip z-dir
+
+unu flip -a 2 -i meanImage.nrrd -o meanImage.nrrd
+
+iii=0
+while [ $iii -lt $N ]; do
+    eigenName='eigenImg_'$iii'.nrrd'
+    unu flip -a 2 -i ${eigenName} -o ${eigenName}
 
     let iii=iii+1
 done
