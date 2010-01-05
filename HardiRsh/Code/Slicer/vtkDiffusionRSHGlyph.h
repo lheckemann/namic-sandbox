e5c447f0552cc4617fc6a4b1c403626b8b39b880
lbloy <lbloy@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-05 00:49:14 +0000

    BUG:Fixed sbiaVolumes/CMakeLists.txt to link to slicers Volumes modules to enable building on mac

--- HardiRsh/Code/Slicer/vtkDiffusionRSHGlyph.h
+++ HardiRsh/Code/Slicer/vtkDiffusionRSHGlyph.h
@@ -9,13 +9,6 @@
 #include "vtkPointData.h"
 #include "vtkDataArray.h"
 #include "vtkSmartPointer.h"
-#include <vnl/vnl_matrix_fixed.txx>
-
-VNL_MATRIX_FIXED_INSTANTIATE(double,6,6);
-VNL_MATRIX_FIXED_INSTANTIATE(double,15,15);
-VNL_MATRIX_FIXED_INSTANTIATE(double,28,28);
-VNL_MATRIX_FIXED_INSTANTIATE(double,45,45);
-VNL_MATRIX_FIXED_INSTANTIATE(double,66,66);
 
 class vtkImageData;
 class vtkMatrix4x4;
