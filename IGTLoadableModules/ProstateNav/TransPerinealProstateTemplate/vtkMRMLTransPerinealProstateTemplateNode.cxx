d7e10521cb2caa178041c3555afc33dd1b1eb8dc
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-11 16:06:33 +0000

    ENH: apply real template dimensions.

--- IGTLoadableModules/ProstateNav/TransPerinealProstateTemplate/vtkMRMLTransPerinealProstateTemplateNode.cxx
+++ IGTLoadableModules/ProstateNav/TransPerinealProstateTemplate/vtkMRMLTransPerinealProstateTemplateNode.cxx
@@ -31,6 +31,35 @@ Version:   $Revision: 1.2 $
 
 #include "vtkSphereSource.h"
 
+
+//
+// Models and Transforms
+//    + ZFrameTransformNode (position of the center of Z-frame)
+//      + ZFrameModel
+//      + TemplateTransformNode (center of hole (0, 0) on the front surface, in the Z-frame coordinate)
+//          + TemplateModel
+
+// Hole (0, 0) position (surface)
+#define TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_X  -35.0
+#define TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_Y  25.0
+#define TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_Z  55.0
+
+// Template corner position
+#define TEMPLATE_BLOCK_OFFSET_FROM_HOLE_X   -15.0
+#define TEMPLATE_BLOCK_OFFSET_FROM_HOLE_y   15.0
+#define TEMPLATE_BLOCK_OFFSET_FROM_HOLE_Z   0.0
+
+#define TEMPLATE_WIDTH   100.0  // dimension in x direction (mm)
+#define TEMPLATE_HEIGHT  120.0  // dimension in y direction (mm)
+#define TEMPLATE_DEPTH   25.0   // dimension in z direction (mm)
+
+#define TEMPLATE_GRID_PITCH_X 5.0
+#define TEMPLATE_GRID_PITCH_Y 5.0
+
+#define TEMPLATE_NUMBER_OF_GRIDS_X 15
+#define TEMPLATE_NUMBER_OF_GRIDS_y 15
+
+
 //-------------------------------------
 
 static const int STATUS_SCANNER=1;
@@ -78,6 +107,12 @@ vtkMRMLTransPerinealProstateTemplateNode::vtkMRMLTransPerinealProstateTemplateNo
   this->ZFrameTransformNodeID=NULL;
   this->ZFrameTransformNode=NULL;  
 
+  this->TemplateModelNodeID=NULL;
+  this->TemplateModelNode=NULL;
+
+  //this->TemplateTransformNodeID=NULL;
+  //this->TemplateTransformNode=NULL;  
+
   this->WorkspaceModelNodeID=NULL;
   this->WorkspaceModelNode=NULL;
 
@@ -98,13 +133,13 @@ vtkMRMLTransPerinealProstateTemplateNode::vtkMRMLTransPerinealProstateTemplateNo
   this->StatusDescriptors.push_back(s); // STATUS_SCANNER=1
 
   // Needle guiding template information
-  this->TemplateGridPitch[0] = 5.0;
-  this->TemplateGridPitch[1] = 5.0;
-  this->TemplateNumGrids[0]  = 21;
-  this->TemplateNumGrids[1]  = 21;
-  this->TemplateOffset[0]    = -50.0;
-  this->TemplateOffset[1]    = 50.0;
-  this->TemplateOffset[2]    = 40.0;
+  this->TemplateGridPitch[0] = TEMPLATE_GRID_PITCH_X;
+  this->TemplateGridPitch[1] = TEMPLATE_GRID_PITCH_Y;
+  this->TemplateNumGrids[0]  = TEMPLATE_NUMBER_OF_GRIDS_X;
+  this->TemplateNumGrids[1]  = TEMPLATE_NUMBER_OF_GRIDS_y;
+  this->TemplateOffset[0]    = TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_X;
+  this->TemplateOffset[1]    = TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_Y;
+  this->TemplateOffset[2]    = TEMPLATE_HOLE_OFFSET_FROM_ZFRAME_Z;
 
 }
 
@@ -125,6 +160,14 @@ vtkMRMLTransPerinealProstateTemplateNode::~vtkMRMLTransPerinealProstateTemplateN
     {
     SetAndObserveZFrameTransformNodeID(NULL);
     }
+  if (this->TemplateModelNodeID) 
+    {
+    SetAndObserveTemplateModelNodeID(NULL);
+    }
+  //if (this->TemplateTransformNodeID) 
+  //  {
+  //  SetAndObserveTemplateTransformNodeID(NULL);
+  //  }
   if (this->ActiveNeedleModelNodeID)
     {
     SetAndObserveActiveNeedleModelNodeID(NULL);
@@ -209,40 +252,6 @@ void vtkMRMLTransPerinealProstateTemplateNode::ReadXMLAttributes(const char** at
     }
 
 
-/*
-    if (!strcmp(attName, "connectorType"))
-      {
-      if (!strcmp(attValue, "SERVER"))
-        {
-        type = TYPE_SERVER;
-        }
-      else if (!strcmp(attValue, "CLIENT"))
-        {
-        type = TYPE_CLIENT;
-        }
-      else
-        {
-        type = TYPE_NOT_DEFINED;
-        }
-      }
-    if (!strcmp(attName, "serverHostname"))
-      {
-      serverHostname = attValue;
-      }
-    if (!strcmp(attName, "serverPort"))
-      {
-      std::stringstream ss;
-      ss << attValue;
-      ss >> port;
-      }
-    if (!strcmp(attName, "restrictDeviceName"))
-      {
-      std::stringstream ss;
-      ss << attValue;
-      ss >> restrictDeviceName;;
-      }
-*/
-
   /*
   switch(type)
     {
@@ -276,6 +285,8 @@ void vtkMRMLTransPerinealProstateTemplateNode::Copy(vtkMRMLNode *anode)
     this->SetScannerConnectorNodeID(node->ScannerConnectorNodeID);
     this->SetZFrameModelNodeID(node->ZFrameModelNodeID);
     this->SetZFrameTransformNodeID(node->ZFrameTransformNodeID);
+    this->SetTemplateModelNodeID(node->TemplateModelNodeID);
+    //this->SetTemplateTransformNodeID(node->TemplateTransformNodeID);
     this->SetActiveNeedleModelNodeID(node->ActiveNeedleModelNodeID);
     this->SetActiveNeedleTransformNodeID(node->ActiveNeedleTransformNodeID);
   }
@@ -310,6 +321,14 @@ void vtkMRMLTransPerinealProstateTemplateNode::UpdateReferences()
     {
     this->SetAndObserveZFrameTransformNodeID(NULL);
     }
+  if (this->TemplateModelNodeID != NULL && this->Scene->GetNodeByID(this->TemplateModelNodeID) == NULL)
+    {
+    this->SetAndObserveTemplateModelNodeID(NULL);
+    }
+  //if (this->TemplateTransformNodeID != NULL && this->Scene->GetNodeByID(this->TemplateTransformNodeID) == NULL)
+  //  {
+  //  this->SetAndObserveTemplateTransformNodeID(NULL);
+  //  }
   if (this->ActiveNeedleModelNodeID != NULL && this->Scene->GetNodeByID(this->ActiveNeedleModelNodeID) == NULL)
     {
     this->SetAndObserveActiveNeedleModelNodeID(NULL);
@@ -338,6 +357,14 @@ void vtkMRMLTransPerinealProstateTemplateNode::UpdateReferenceID(const char *old
     {
     this->SetAndObserveZFrameTransformNodeID(newID);
     }
+  if (this->TemplateModelNodeID && !strcmp(oldID, this->TemplateModelNodeID))
+    {
+    this->SetAndObserveTemplateModelNodeID(newID);
+    }
+  //if (this->TemplateTransformNodeID && !strcmp(oldID, this->TemplateTransformNodeID))
+  //  {
+  //  this->SetAndObserveTemplateTransformNodeID(newID);
+  //  }
 
   if (this->ActiveNeedleModelNodeID && !strcmp(oldID, this->ActiveNeedleModelNodeID))
     {
@@ -357,6 +384,8 @@ void vtkMRMLTransPerinealProstateTemplateNode::UpdateScene(vtkMRMLScene *scene)
    this->SetAndObserveScannerConnectorNodeID(this->GetScannerConnectorNodeID());
    this->SetAndObserveZFrameModelNodeID(this->GetZFrameModelNodeID());
    this->SetAndObserveZFrameTransformNodeID(this->GetZFrameTransformNodeID());
+   this->SetAndObserveTemplateModelNodeID(this->GetTemplateModelNodeID());
+   //this->SetAndObserveTemplateTransformNodeID(this->GetTemplateTransformNodeID());
    this->SetAndObserveActiveNeedleModelNodeID(this->GetActiveNeedleModelNodeID());
    this->SetAndObserveActiveNeedleTransformNodeID(this->GetActiveNeedleTransformNodeID());
 }
@@ -460,19 +489,62 @@ int vtkMRMLTransPerinealProstateTemplateNode::Init(vtkSlicerApplication* app)
       }
   }
 
+
+  //// Template transform node
+  //if (GetTemplateTransformNode()==NULL)
+  //  {
+  //  vtkMRMLLinearTransformNode* ttnode = vtkMRMLLinearTransformNode::New();
+  //  ttnode->SetName("TemplateTransform");
+  //  vtkMatrix4x4* ztransform = vtkMatrix4x4::New();
+  //  ztransform->Identity();
+  //  ztransform->SetElement(0, 3, this->TemplateOffset[0]);
+  //  ztransform->SetElement(1, 3, this->TemplateOffset[1]);
+  //  ztransform->SetElement(2, 3, this->TemplateOffset[2]);
+  //
+  //  //transformNode->SetAndObserveImageData(transform);
+  //  ttnode->ApplyTransform(ztransform);
+  //  ttnode->SetScene(this->Scene);
+  //  ztransform->Delete();
+  //  this->Scene->AddNode(ttnode);
+  //  SetAndObserveTemplateTransformNodeID(ttnode->GetID());
+  //
+  //  if (GetZFrameTransformNodeID())
+  //    {
+  //    ttnode->SetAndObserveTransformNodeID(GetZFrameTransformNodeID());
+  //    }
+  //}
+
+  // Template model
+  // This part should be moved to Robot Display Node.
+  if (GetTemplateModelNode()==NULL)
+  {
+    const char* nodeID = AddTemplateModel("Template");
+    vtkMRMLModelNode*  modelNode = vtkMRMLModelNode::SafeDownCast(this->Scene->GetNodeByID(nodeID));
+    if (modelNode)
+      {
+      vtkMRMLDisplayNode* displayNode = modelNode->GetDisplayNode();
+      displayNode->SetVisibility(1);
+      modelNode->Modified();
+      this->Scene->Modified();
+      //modelNode->SetAndObserveTransformNodeID(GetTemplateTransformNodeID());
+      modelNode->SetAndObserveTransformNodeID(GetZFrameTransformNodeID());
+      SetAndObserveTemplateModelNodeID(nodeID);
+      }
+  }
+
   // Active needle transform node
   if (GetActiveNeedleTransformNode()==NULL)
   {
-    vtkMRMLLinearTransformNode* ztnode = vtkMRMLLinearTransformNode::New();
-    ztnode->SetName("ActiveNeedleTransform");
+    vtkMRMLLinearTransformNode* atnode = vtkMRMLLinearTransformNode::New();
+    atnode->SetName("ActiveNeedleTransform");
     vtkMatrix4x4* ztransform = vtkMatrix4x4::New();
     ztransform->Identity();
     //transformNode->SetAndObserveImageData(transform);
-    ztnode->ApplyTransform(ztransform);
-    ztnode->SetScene(this->Scene);
+    atnode->ApplyTransform(ztransform);
+    atnode->SetScene(this->Scene);
     ztransform->Delete();
-    this->Scene->AddNode(ztnode);
-    SetAndObserveActiveNeedleTransformNodeID(ztnode->GetID());
+    this->Scene->AddNode(atnode);
+    SetAndObserveActiveNeedleTransformNodeID(atnode->GetID());
   }
 
   // Active Needle model
@@ -787,6 +859,90 @@ const char* vtkMRMLTransPerinealProstateTemplateNode::AddZFrameModel(const char*
 
 
 //----------------------------------------------------------------------------
+const char* vtkMRMLTransPerinealProstateTemplateNode::AddTemplateModel(const char* nodeName)
+{
+
+  vtkMRMLModelNode           *model;
+  vtkMRMLModelDisplayNode    *disp;
+  
+  model = vtkMRMLModelNode::New();
+  disp  = vtkMRMLModelDisplayNode::New();
+
+  this->Scene->SaveStateForUndo();
+  this->Scene->AddNode(disp);
+  this->Scene->AddNode(model);
+
+  disp->SetScene(this->Scene);
+  model->SetName(nodeName);
+  model->SetScene(this->Scene);
+  model->SetAndObserveDisplayNodeID(disp->GetID());
+  model->SetHideFromEditors(0);
+
+  vtkAppendPolyData *apd = vtkAppendPolyData::New();
+
+  for (int i = 0; i < this->TemplateNumGrids[0]; i ++)
+    {
+    for (int j = 0; j < this->TemplateNumGrids[1]; j ++)
+      {
+      double offset[3];
+
+      offset[0] = this->TemplateOffset[0] + i * this->TemplateGridPitch[0];
+      offset[1] = this->TemplateOffset[1] - j * this->TemplateGridPitch[1];
+      offset[2] = this->TemplateOffset[2];
+
+      vtkCylinderSource *cylinder = vtkCylinderSource::New();
+      cylinder->SetResolution(24);
+      cylinder->SetRadius(1.0);
+      cylinder->SetHeight(5);
+      cylinder->SetCenter(0, 0, 0);
+      cylinder->Update();
+      
+      vtkTransform* trans =   vtkTransform::New();
+      trans->Translate(offset);
+      trans->RotateX(90.0);
+      trans->Update();
+      
+      vtkTransformPolyDataFilter *tfilter = vtkTransformPolyDataFilter::New();
+      tfilter->SetInput(cylinder->GetOutput());
+      tfilter->SetTransform(trans);
+      tfilter->Update();
+
+      apd->AddInput(tfilter->GetOutput());
+      apd->Update();
+
+      cylinder->Delete();
+      trans->Delete();
+      tfilter->Delete();
+      }
+    }
+  
+  
+
+
+  model->SetAndObservePolyData(apd->GetOutput());
+
+  double color[3];
+  color[0] = 0.2;
+  color[1] = 0.2;
+  color[2] = 1.0;
+
+  disp->SetPolyData(model->GetPolyData());
+  disp->SetColor(color);
+  disp->SetOpacity(0.5);
+  
+  apd->Delete();
+
+  const char* modelID = model->GetID();
+
+  disp->Delete();
+  model->Delete();
+
+  return modelID;
+
+}
+
+
+//----------------------------------------------------------------------------
 const char* vtkMRMLTransPerinealProstateTemplateNode::AddNeedleModel(const char* nodeName, double length, double diameter)
 {
 
@@ -1029,6 +1185,50 @@ vtkMRMLLinearTransformNode* vtkMRMLTransPerinealProstateTemplateNode::GetZFrameT
   if (this->GetScene() && this->ZFrameTransformNodeID != NULL )
     {    
     return vtkMRMLLinearTransformNode::SafeDownCast(this->GetScene()->GetNodeByID(this->ZFrameTransformNodeID));
+    }
+  return NULL;
+}
+
+////----------------------------------------------------------------------------
+//void vtkMRMLTransPerinealProstateTemplateNode::SetAndObserveTemplateTransformNodeID(const char *nodeId)
+//{
+//  vtkSetAndObserveMRMLObjectMacro(this->TemplateTransformNode, NULL);
+//  this->SetTemplateTransformNodeID(nodeId);
+//  vtkMRMLLinearTransformNode *tnode = this->GetTemplateTransformNode();
+//  vtkSmartPointer<vtkIntArray> events = vtkSmartPointer<vtkIntArray>::New();
+//  events->InsertNextValue(vtkCommand::ModifiedEvent);
+//  vtkSetAndObserveMRMLObjectEventsMacro(this->TemplateTransformNode, tnode, events);
+//}
+
+
+////----------------------------------------------------------------------------
+//vtkMRMLLinearTransformNode* vtkMRMLTransPerinealProstateTemplateNode::GetTemplateTransformNode()
+//{
+//  if (this->GetScene() && this->TemplateTransformNodeID != NULL )
+//    {    
+//    return vtkMRMLLinearTransformNode::SafeDownCast(this->GetScene()->GetNodeByID(this->TemplateTransformNodeID));
+//    }
+//  return NULL;
+//}
+
+
+//----------------------------------------------------------------------------
+void vtkMRMLTransPerinealProstateTemplateNode::SetAndObserveTemplateModelNodeID(const char *nodeId)
+{
+  vtkSetAndObserveMRMLObjectMacro(this->TemplateModelNode, NULL);
+  this->SetTemplateModelNodeID(nodeId);
+  vtkMRMLModelNode *tnode = this->GetTemplateModelNode();
+  vtkSmartPointer<vtkIntArray> events = vtkSmartPointer<vtkIntArray>::New();
+  events->InsertNextValue(vtkCommand::ModifiedEvent);
+  vtkSetAndObserveMRMLObjectEventsMacro(this->TemplateModelNode, tnode, events);
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLModelNode* vtkMRMLTransPerinealProstateTemplateNode::GetTemplateModelNode()
+{
+  if (this->GetScene() && this->TemplateModelNodeID != NULL )
+    {    
+    return vtkMRMLModelNode::SafeDownCast(this->GetScene()->GetNodeByID(this->TemplateModelNodeID));
     }
   return NULL;
 }
