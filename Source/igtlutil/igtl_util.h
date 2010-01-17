1dc12c349c28271d5753f1fd52d65fa577b49a68
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-17 17:45:19 +0000

    BUG: fix compiler warnings: conversion from igtl_uint64 to int.

--- Source/igtlutil/igtl_util.h
+++ Source/igtlutil/igtl_util.h
@@ -40,7 +40,7 @@ extern "C" {
  * Test endian of the host
  */
 int igtl_export igtl_is_little_endian();
-igtl_uint64 igtl_export crc64(unsigned char *data, int len, igtl_uint64 crc);
+igtl_uint64 igtl_export crc64(unsigned char *data, igtl_uint64 len, igtl_uint64 crc);
 
 
 /*
