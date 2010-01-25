1a6e0921de4222e58eda7c88f13ac86084b511f4
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-25 19:25:09 +0000

    ENH: Recofnfiguring the CTest build in order to submit to the Slicer3 Dashboard. This whould be setup (on a machine by machine basis) to be part of the "Modules" group of the Slicer3 dashboard.

--- MultiImageRegistration/CTestConfig.cmake
+++ MultiImageRegistration/CTestConfig.cmake
@@ -1,7 +1,7 @@
-SET (CTEST_PROJECT_NAME "MultiImageRegistration")
-SET (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
-SET (CTEST_DROP_METHOD "http")
-SET (CTEST_DROP_SITE "www.na-mic.org")
-SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
-SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-NAMIC.cgi")
+set(CTEST_PROJECT_NAME "Slicer3")
+set(CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
 
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "www.cdash.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Slicer3")
+set(CTEST_DROP_SITE_CDASH TRUE)
