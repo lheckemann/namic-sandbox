54d475d3d8ebbc48df387876427fb90395b85c02
fedorov <fedorov@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-01-29 17:15:27 +0000

    ENH: another tool to operate on short images for atlas

---
+++ VervetAtlas/Tools/AtlasTools/SubShortFromConst.cxx
@@ -0,0 +1,54 @@
+/*
+ * Add an arbitrary number of float pixel type images
+ *
+ */
+
+
+#include "itkOrientedImage.h"
+#include "itkImageDuplicator.h"
+#include "itkSubtractImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include <iostream>
+
+typedef itk::OrientedImage<short, 3> ImageType;
+typedef itk::SubtractImageFilter<ImageType,ImageType> SubType;
+typedef itk::ImageFileReader<ImageType> ReaderType;
+typedef itk::ImageFileWriter<ImageType> WriterType;
+typedef itk::ImageDuplicator<ImageType> DuplicatorType;
+
+int main(int argc, char** argv){
+  int i;
+
+  if(argc<3){
+    std::cerr << "Usage: " << argv[0] << " input_short_image const output_short_image" << std::endl;
+    return -1;
+  }
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  ImageType::Pointer accumulator;
+
+  reader->SetFileName(argv[1]);
+  reader->Update();
+
+  float max = atoi(argv[2]);
+
+  DuplicatorType::Pointer dup = DuplicatorType::New();
+  dup->SetInputImage(reader->GetOutput());
+  dup->Update();
+
+  ImageType::Pointer ones = dup->GetOutput();
+  ones->FillBuffer(max);
+
+  SubType::Pointer sub = SubType::New();
+  sub->SetInput1(ones);
+  sub->SetInput2(reader->GetOutput());
+  sub->Update();
+
+  writer->SetInput(sub->GetOutput());
+  writer->SetFileName(argv[argc-1]);
+  writer->Update();
+
+  return 0;
+}
