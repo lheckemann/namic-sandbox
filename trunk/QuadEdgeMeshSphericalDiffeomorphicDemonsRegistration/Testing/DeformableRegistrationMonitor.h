4195b4a88ac911619a1434002ffac5a5517cc1d4
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-02-25 23:34:17 +0000

    ENH: Specialization of the RegistrationMonitor to deformable registration.

---
+++ trunk/QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Testing/DeformableRegistrationMonitor.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Surface Registration Program
+  Module:    $RCSfile: DeformableRegistrationMonitor.h,v $
+
+  Copyright (c) Kitware Inc. 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __DeformableRegistrationMonitor_h
+#define __DeformableRegistrationMonitor_h
+
+#include "RegistrationMonitor.h"
+#include "itkProcessObject.h"
+
+/** \class DeformableRegistrationMonitor 
+ *  This class provides a VTK visualization pipeline configured for monitoring
+ *  the progress of a deformable registration process. 
+ */
+template <class TPointSet>
+class DeformableRegistrationMonitor : public RegistrationMonitor
+{
+public:
+  
+  typedef DeformableRegistrationMonitor  Self;
+  typedef RegistrationMonitor        Superclass;
+
+  typedef itk::ProcessObject         ProcessObjectType;
+
+  typedef TPointSet                  PointSetType;
+
+  DeformableRegistrationMonitor();
+  ~DeformableRegistrationMonitor();
+
+  void Observe( const ProcessObjectType * filter, const PointSetType * transform );
+
+protected:
+
+  // These methods will only be called by the Observer
+  virtual void Update();
+  virtual void StartVisualization();
+
+private:
+  
+  typedef itk::SimpleMemberCommand< Self >  ObserverType;
+
+  typename ObserverType::Pointer       IterationObserver;
+  typename ObserverType::Pointer       StartObserver;
+
+  ProcessObjectType::ConstPointer      ObservedFilter;
+};
+
+#include "DeformableRegistrationMonitor.txx"
+
+#endif
