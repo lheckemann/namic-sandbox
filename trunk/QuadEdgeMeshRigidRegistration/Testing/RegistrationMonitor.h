5f67adba82c906d34cbfb6d13f57ffe4d3f68ed0
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-02-26 10:52:59 +0000

    STYLE: Fix copyright headers.

--- trunk/QuadEdgeMeshRigidRegistration/Testing/RegistrationMonitor.h
+++ trunk/QuadEdgeMeshRigidRegistration/Testing/RegistrationMonitor.h
@@ -1,15 +1,17 @@
 /*=========================================================================
 
-  Program:   Surface Registration Program
-  Module:    $RCSfile: RegistrationMonitor.h,v $
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkTestingMacros.h,v $
+  Language:  C++
+  Date:      $Date: 2009-05-09 17:40:20 $
+  Version:   $Revision: 1.1 $
 
-  Copyright (c) Kitware Inc. 
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 
