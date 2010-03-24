5f5edbecdea3079a842eaaa7e3923f5da1579689
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-03-24 00:45:52 +0000

    ENH: Reorganizing the code using for-loops instead of repeated declaration of variables.

--- QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Examples/MultiResolutionRegistration2.cxx
+++ QuadEdgeMeshSphericalDiffeomorphicDemonsRegistration/Examples/MultiResolutionRegistration2.cxx
@@ -63,117 +63,44 @@ int main( int argc, char * argv [] )
 
   MultiResolutionDemonsFilterType::Pointer multiResDemonsFilter = MultiResolutionDemonsFilterType::New();
 
-  multiResDemonsFilter->SetNumberOfResolutionLevels( 4 );
+  typedef MultiResolutionDemonsFilterType::DestinationPointSetType DestinationPointSetType;
+
+  const unsigned int maximumNumberOfResolutions = 4;
+  const unsigned int finestResolution = 1;
+
+  multiResDemonsFilter->SetNumberOfResolutionLevels( finestResolution );
 
   typedef itk::VTKPolyDataReader< MeshType >     ReaderType;
 
-  ReaderType::Pointer fixedMeshReader1 = ReaderType::New();
-  fixedMeshReader1->SetFileName( argv[1] );
+  ReaderType::Pointer fixedMeshReader[maximumNumberOfResolutions];
+  ReaderType::Pointer movingMeshReader[maximumNumberOfResolutions];
 
-  ReaderType::Pointer movingMeshReader1 = ReaderType::New();
-  movingMeshReader1->SetFileName( argv[2] );
-
-  try
+  for(unsigned int i=0; i < maximumNumberOfResolutions; i++ )
     {
-    fixedMeshReader1->Update();
-    movingMeshReader1->Update();
+    fixedMeshReader[i] = ReaderType::New();
+    movingMeshReader[i] = ReaderType::New();
+
+    fixedMeshReader[i]->SetFileName( argv[2*i+1] );
+    movingMeshReader[i]->SetFileName( argv[2*i+2] );
+
+    std::cout << "Fixed   " << argv[2*i+1] << std::endl;
+    std::cout << "Moving  " << argv[2*i+2] << std::endl;
+
+    fixedMeshReader[i]->Update();
+    movingMeshReader[i]->Update();
+
+    multiResDemonsFilter->SetFixedMesh( i, fixedMeshReader[i]->GetOutput() );
+    multiResDemonsFilter->SetMovingMesh( i, movingMeshReader[i]->GetOutput() );
     }
-  catch( itk::ExceptionObject & exp )
-    {
-    std::cerr << exp << std::endl;
-    return EXIT_FAILURE;
-    }
-
-
-  multiResDemonsFilter->SetFixedMesh( 0, fixedMeshReader1->GetOutput() );
-  multiResDemonsFilter->SetMovingMesh( 0, movingMeshReader1->GetOutput() );
-
-
-  ReaderType::Pointer fixedMeshReader2 = ReaderType::New();
-  fixedMeshReader2->SetFileName( argv[3] );
-
-  ReaderType::Pointer movingMeshReader2 = ReaderType::New();
-  movingMeshReader2->SetFileName( argv[4] );
-
-  try
-    {
-    fixedMeshReader2->Update();
-    movingMeshReader2->Update();
-    }
-  catch( itk::ExceptionObject & exp )
-    {
-    std::cerr << exp << std::endl;
-    return EXIT_FAILURE;
-    }
-
-
-  multiResDemonsFilter->SetFixedMesh( 1, fixedMeshReader2->GetOutput() );
-  multiResDemonsFilter->SetMovingMesh( 1, movingMeshReader2->GetOutput() );
-
-
-  ReaderType::Pointer fixedMeshReader3 = ReaderType::New();
-  fixedMeshReader3->SetFileName( argv[5] );
-
-  ReaderType::Pointer movingMeshReader3 = ReaderType::New();
-  movingMeshReader3->SetFileName( argv[6] );
-
-  try
-    {
-    fixedMeshReader3->Update();
-    movingMeshReader3->Update();
-    }
-  catch( itk::ExceptionObject & exp )
-    {
-    std::cerr << exp << std::endl;
-    return EXIT_FAILURE;
-    }
-
-
-  multiResDemonsFilter->SetFixedMesh( 2, fixedMeshReader3->GetOutput() );
-  multiResDemonsFilter->SetMovingMesh( 2, movingMeshReader3->GetOutput() );
-
-
-
-  ReaderType::Pointer fixedMeshReader4 = ReaderType::New();
-  fixedMeshReader4->SetFileName( argv[7] );
-
-  ReaderType::Pointer movingMeshReader4 = ReaderType::New();
-  movingMeshReader4->SetFileName( argv[8] );
-
-  try
-    {
-    fixedMeshReader4->Update();
-    movingMeshReader4->Update();
-    }
-  catch( itk::ExceptionObject & exp )
-    {
-    std::cerr << exp << std::endl;
-    return EXIT_FAILURE;
-    }
-
-
-  multiResDemonsFilter->SetFixedMesh( 3, fixedMeshReader4->GetOutput() );
-  multiResDemonsFilter->SetMovingMesh( 3, movingMeshReader4->GetOutput() );
-
-
-
-  MultiResolutionDemonsFilterType::PointType center;
-  center.Fill( 0.0 );
-
-  const double radius = 100.0;
-
-  multiResDemonsFilter->SetSphereCenter( center );
-  multiResDemonsFilter->SetSphereRadius( radius );
-
-
-  typedef MultiResolutionDemonsFilterType::DestinationPointSetType DestinationPointSetType;
-
 
 #ifdef USE_VTK
   typedef MultiResolutionDeformableAndAffineRegistrationMonitor< 
     DestinationPointSetType > RegistrationMonitorType;
 
   RegistrationMonitorType  visualMonitor;
+
+  multiResDemonsFilter->SetRegistrationMonitor( &visualMonitor );
+
   visualMonitor.SetNumberOfIterationsPerUpdate( 1 );
 
   visualMonitor.SetBaseAnnotationText("Rigid Registration Level 1");
@@ -183,62 +110,36 @@ int main( int argc, char * argv [] )
 
   visualMonitor.SetScalarRange( -0.1, 0.1 );
 
-  visualMonitor.SetNumberOfResolutionLevels( 4 );
-
-  vtkSmartPointer< vtkPolyDataReader > vtkFixedMeshReader1 = vtkSmartPointer< vtkPolyDataReader >::New();
-  vtkSmartPointer< vtkPolyDataReader > vtkMovingMeshReader1 = vtkSmartPointer< vtkPolyDataReader >::New();
-
-  vtkFixedMeshReader1->SetFileName( fixedMeshReader1->GetFileName() );
-  vtkMovingMeshReader1->SetFileName( movingMeshReader1->GetFileName() );
-
-  vtkFixedMeshReader1->Update();
-  vtkMovingMeshReader1->Update();
-
-  visualMonitor.SetFixedSurface( 0, vtkFixedMeshReader1->GetOutput() );
-  visualMonitor.SetMovingSurface( 0, vtkMovingMeshReader1->GetOutput() );
+  visualMonitor.SetNumberOfResolutionLevels( maximumNumberOfResolutions );
 
 
-  vtkSmartPointer< vtkPolyDataReader > vtkFixedMeshReader2 = vtkSmartPointer< vtkPolyDataReader >::New();
-  vtkSmartPointer< vtkPolyDataReader > vtkMovingMeshReader2 = vtkSmartPointer< vtkPolyDataReader >::New();
+  vtkSmartPointer< vtkPolyDataReader > vtkFixedMeshReader[maximumNumberOfResolutions];
+  vtkSmartPointer< vtkPolyDataReader > vtkMovingMeshReader[maximumNumberOfResolutions];
 
-  vtkFixedMeshReader2->SetFileName( fixedMeshReader2->GetFileName() );
-  vtkMovingMeshReader2->SetFileName( movingMeshReader2->GetFileName() );
+  for(unsigned int i=0; i < maximumNumberOfResolutions; i++ )
+    {
+    vtkFixedMeshReader[i] = vtkSmartPointer< vtkPolyDataReader >::New();
+    vtkMovingMeshReader[i] = vtkSmartPointer< vtkPolyDataReader >::New();
 
-  vtkFixedMeshReader2->Update();
-  vtkMovingMeshReader2->Update();
+    vtkFixedMeshReader[i]->SetFileName( fixedMeshReader[i]->GetFileName() );
+    vtkMovingMeshReader[i]->SetFileName( movingMeshReader[i]->GetFileName() );
 
-  visualMonitor.SetFixedSurface( 1, vtkFixedMeshReader2->GetOutput() );
-  visualMonitor.SetMovingSurface( 1, vtkMovingMeshReader2->GetOutput() );
+    vtkFixedMeshReader[i]->Update();
+    vtkMovingMeshReader[i]->Update();
 
-
-  vtkSmartPointer< vtkPolyDataReader > vtkFixedMeshReader3 = vtkSmartPointer< vtkPolyDataReader >::New();
-  vtkSmartPointer< vtkPolyDataReader > vtkMovingMeshReader3 = vtkSmartPointer< vtkPolyDataReader >::New();
-
-  vtkFixedMeshReader3->SetFileName( fixedMeshReader3->GetFileName() );
-  vtkMovingMeshReader3->SetFileName( movingMeshReader3->GetFileName() );
-
-  vtkFixedMeshReader3->Update();
-  vtkMovingMeshReader3->Update();
-
-  visualMonitor.SetFixedSurface( 2, vtkFixedMeshReader3->GetOutput() );
-  visualMonitor.SetMovingSurface( 2, vtkMovingMeshReader3->GetOutput() );
-
-
-  vtkSmartPointer< vtkPolyDataReader > vtkFixedMeshReader4 = vtkSmartPointer< vtkPolyDataReader >::New();
-  vtkSmartPointer< vtkPolyDataReader > vtkMovingMeshReader4 = vtkSmartPointer< vtkPolyDataReader >::New();
-
-  vtkFixedMeshReader4->SetFileName( fixedMeshReader4->GetFileName() );
-  vtkMovingMeshReader4->SetFileName( movingMeshReader4->GetFileName() );
-
-  vtkFixedMeshReader4->Update();
-  vtkMovingMeshReader4->Update();
-
-  visualMonitor.SetFixedSurface( 3, vtkFixedMeshReader4->GetOutput() );
-  visualMonitor.SetMovingSurface( 3, vtkMovingMeshReader4->GetOutput() );
-
-
-  multiResDemonsFilter->SetRegistrationMonitor( &visualMonitor );
+    visualMonitor.SetFixedSurface( i, vtkFixedMeshReader[i]->GetOutput() );
+    visualMonitor.SetMovingSurface( i, vtkMovingMeshReader[i]->GetOutput() );
+    }
 #endif
+
+
+  MultiResolutionDemonsFilterType::PointType center;
+  center.Fill( 0.0 );
+
+  const double radius = 100.0;
+
+  multiResDemonsFilter->SetSphereCenter( center );
+  multiResDemonsFilter->SetSphereRadius( radius );
 
 
   try
@@ -257,8 +158,8 @@ int main( int argc, char * argv [] )
 
   WarpMeshFilterType::Pointer warpFilter = WarpMeshFilterType::New();
 
-  warpFilter->SetInput( fixedMeshReader4->GetOutput() );
-  warpFilter->SetReferenceMesh( fixedMeshReader4->GetOutput() );
+  warpFilter->SetInput( fixedMeshReader[finestResolution-1]->GetOutput() );
+  warpFilter->SetReferenceMesh( fixedMeshReader[finestResolution-1]->GetOutput() );
 
   warpFilter->SetDestinationPoints( multiResDemonsFilter->GetFinalDestinationPoints() );
 
