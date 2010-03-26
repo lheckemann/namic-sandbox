83f49de31e15ab8326e8a68e17106148e909b1bf
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-03-26 20:46:12 +0000

    ENH: Starting to add basic infrastructure.

---
+++ SparseLinearSolverConversion/C++/Source/lsqr.h
@@ -0,0 +1,28 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Language:  C++
+  Date:      $Date: 2009-06-14 11:52:00 $
+  Version:   $Revision: 1.69 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itk_lsqr_h
+#define __itk_lsqr_h
+
+class lsqr 
+{
+public:
+
+  lsqr();
+  ~lsqr();
+
+};
+
+#endif 
