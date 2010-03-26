80c5e55cd7d8c8882a47edeecd6a1f69d468703f
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-03-26 21:14:01 +0000

    ENH: Adding testing infrastructure.

---
+++ trunk/SparseLinearSolverConversion/C++/Testing/lsqrTest1.cxx
@@ -0,0 +1,29 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Language:  C++
+  Date:      $Date: 2009-06-14 11:52:00 $
+  Version:   $Revision: 1.69 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "lsqr.h"
+
+int lsqrTest1( int , char * [] )
+{
+
+  lsqr solver;
+
+  const unsigned int n = 3;
+  double x[n];
+  double z[n];
+
+  solver.HouseholderTransformation(n,z,x);
+}
