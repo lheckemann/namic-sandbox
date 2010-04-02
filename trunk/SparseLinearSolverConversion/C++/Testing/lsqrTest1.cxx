08af6eaf139bb3ca748997ed6aba3c0cb5584386
ibanez <ibanez@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-04-02 16:03:11 +0000

    ENH: Improving testing by including tolerance of relative values when comparing precision of results.

--- trunk/SparseLinearSolverConversion/C++/Testing/lsqrTest1.cxx
+++ trunk/SparseLinearSolverConversion/C++/Testing/lsqrTest1.cxx
@@ -21,10 +21,13 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 int lsqrTest1( int , char * [] )
 {
 
   lsqr solver;
+
+  const double tolerance = 1e-9;
 
   const unsigned int n = 2;
   double x[n];
@@ -51,14 +54,19 @@ int lsqrTest1( int , char * [] )
 
   const double norm =solver.Dnrm2( n1, x1 );
   const double expectedNorm = sqrt(5.0);
-  if( norm != expectedNorm )
+
+  const double ratioOfDifference = 
+    fabs( norm - expectedNorm ) / expectedNorm;
+
+  if( ratioOfDifference > tolerance )
     {
     std::cerr << "Error in Dnrm2() test 1 " << std::endl;
     std::cerr << "Expected = " << expectedNorm << std::endl;
     std::cerr << "Received = " << norm << std::endl;
+    std::cerr << "ratioOfDifference = " << ratioOfDifference << std::endl;
     return EXIT_FAILURE;
     } 
-  std::cout << "Dnrm2 test1 passed " << std::endl;
+  std::cout << "Dnrm2 test 1 passed " << std::endl;
   }
 
   { // Test 2 Dnrm2()
@@ -76,14 +84,60 @@ int lsqrTest1( int , char * [] )
   const double norm =solver.Dnrm2( n2, x2 );
   const double expectedNorm = dominantValue;
 
-  if( norm != expectedNorm )
+  const double ratioOfDifference = 
+    fabs( norm - expectedNorm ) / expectedNorm;
+
+  if( ratioOfDifference > tolerance )
     {
     std::cerr << "Error in Dnrm2() test 2 " << std::endl;
     std::cerr << "Expected = " << expectedNorm << std::endl;
     std::cerr << "Received = " << norm << std::endl;
+    std::cerr << "ratioOfDifference = " << ratioOfDifference << std::endl;
     return EXIT_FAILURE;
     } 
-  std::cout << "Dnrm2 test2 passed " << std::endl;
+  std::cout << "Dnrm2 test 2 passed " << std::endl;
+  }
+
+
+  { // Testing D2Norm
+  const double dominantValue = 1e+300;
+  const double minorValue = 1e-100;
+  const double a = dominantValue;
+  const double b = minorValue;
+  const double d2norm = solver.D2Norm( a, b );
+
+  const double ratioOfDifference = 
+    fabs( d2norm - dominantValue ) / dominantValue;
+
+  if( ratioOfDifference > tolerance )
+    {
+    std::cerr << "Error in D2Norm() test 1 " << std::endl;
+    std::cerr << "Expected = " << dominantValue << std::endl;
+    std::cerr << "Received = " << d2norm << std::endl;
+    return EXIT_FAILURE;
+    } 
+  std::cout << "D2Norm test 1 passed " << std::endl;
+  }
+
+  { // Testing D2Norm
+  const double friendlyValue1 = 1e+3;
+  const double friendlyValue2 = 1e+2;
+  const double a = friendlyValue1;
+  const double b = friendlyValue2;
+  const double d2norm = solver.D2Norm( a, b );
+  const double expectedD2norm = sqrt( a*a + b*b );
+
+  const double ratioOfDifference = 
+    fabs( d2norm - expectedD2norm ) / expectedD2norm;
+
+  if( ratioOfDifference > tolerance )
+    {
+    std::cerr << "Error in D2Norm() test 2 " << std::endl;
+    std::cerr << "Expected = " << expectedD2norm << std::endl;
+    std::cerr << "Received = " << d2norm << std::endl;
+    return EXIT_FAILURE;
+    } 
+  std::cout << "D2Norm test 2 passed " << std::endl;
   }
 
 
@@ -92,6 +146,10 @@ int lsqrTest1( int , char * [] )
   const double eps = 1e-15;
 
   solver.SetEpsilon( eps );
+
+  const double damp = 0.0;
+
+  solver.SetDamp( damp );
 
   solver.SetMaximumNumberOfIterations( 100 );
 
