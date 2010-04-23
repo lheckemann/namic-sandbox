5eaf9b224e8aa7bbd4cc0b023710f2a4146dfd71
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-04-23 03:48:58 +0000

    ENH: adding TRAJECTORY type

--- trunk/OpenIGTLink2_beta/Source/igtlutil/igtl_trajectory.c
+++ trunk/OpenIGTLink2_beta/Source/igtlutil/igtl_trajectory.c
@@ -17,24 +17,26 @@
 #include <string.h>
 #include <math.h>
 
-#include "igtl_point.h"
+#include "igtl_trajectory.h"
 #include "igtl_util.h"
 
-void igtl_export igtl_point_convert_byte_order(igtl_point_element** pointlist, int nitem)
+void igtl_export igtl_trajectory_convert_byte_order(igtl_trajectory_element** trajectorylist, int nitem)
 {
-  igtl_point_element* elem;
+  igtl_trajectory_element* elem;
   int i;
   int j;
   igtl_int32* tmp;
       
   for (i = 0; i < nitem; i ++)
     {
-    elem = pointlist[i];
+    elem = trajectorylist[i];
     if (igtl_is_little_endian()) 
       {
       for (j = 0; j < 3; j ++)
         {
-        tmp = (igtl_int32*)&(elem->position[j]);
+        tmp = (igtl_int32*)&(elem->entry_pos[j]);
+        *tmp = BYTE_SWAP_INT32(*tmp);
+        tmp = (igtl_int32*)&(elem->target_pos[j]);
         *tmp = BYTE_SWAP_INT32(*tmp);
         }
       tmp = (igtl_int32*)&(elem->radius);
@@ -44,17 +46,17 @@ void igtl_export igtl_point_convert_byte_order(igtl_point_element** pointlist, i
 }
 
 
-igtl_uint64 igtl_export igtl_point_get_crc(igtl_point_element** pointlist, int nitem)
+igtl_uint64 igtl_export igtl_trajectory_get_crc(igtl_trajectory_element** trajectorylist, int nitem)
 {
-  igtl_point_element* elem;
+  igtl_trajectory_element* elem;
   int i;
   igtl_uint64  crc;
 
   crc = crc64(0, 0, 0);
   for (i = 0; i < nitem; i ++)
     {
-    elem = pointlist[i];
-    crc = crc64((unsigned char*) elem, IGTL_POINT_ELEMENT_SIZE, crc);
+    elem = trajectorylist[i];
+    crc = crc64((unsigned char*) elem, IGTL_TRAJECTORY_ELEMENT_SIZE, crc);
     }
 
   return crc;
