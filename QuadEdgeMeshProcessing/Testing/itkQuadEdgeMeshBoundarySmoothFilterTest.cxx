a472e680cfb4cf0b3caae0ba88e4e7b43b8b4269
limosquito <limosquito@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-03 20:56:39 +0000

    BUG

--- QuadEdgeMeshProcessing/Testing/itkQuadEdgeMeshBoundarySmoothFilterTest.cxx
+++ QuadEdgeMeshProcessing/Testing/itkQuadEdgeMeshBoundarySmoothFilterTest.cxx
@@ -55,7 +55,7 @@ int main( int argc, char** argv )
 
   smoothFilter->SetIterations (atoi(argv[5]));
 
-  smoothFilter->DebugOn();
+  //smoothFilter->DebugOn();
   smoothFilter->Update();
 
   WriterType::Pointer writer = WriterType::New();
