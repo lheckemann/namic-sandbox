3229f66dd5f51fa2120682369d3c6c53a6758435
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-05 09:10:56 +0000

    ENH: Adding test for TDATA type.

--- OpenIGTLink2_beta/Testing/igtlutil/igtl_tdata_test.c
+++ OpenIGTLink2_beta/Testing/igtlutil/igtl_tdata_test.c
@@ -21,7 +21,7 @@
 #include "igtl_util.h"
 
 /* include test tdata data and serialized tdata message */
-//#include "igtl_test_data_tdata.h"
+#include "igtl_test_data_tdata.h"
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -31,7 +31,7 @@
 #pragma pack(1)
 struct tdata_message {
   igtl_header           header;
-  igtl_tdata_element    metalist[TEST_TDATA_NUM];
+  igtl_tdata_element    tlist[TEST_TDATA_NUM];
 };
 #pragma pack()
 
@@ -50,13 +50,60 @@ int main( int argc, char * argv [] )
     return EXIT_FAILURE;
     }
 
-  //igtl_image_set_matrix(spacing, origin, norm_i, norm_j, norm_k, &(message.iheader));
-
-  /* Copy image data */
-  //memcpy((void*)message.image, (void*)test_image, TEST_IMAGE_MESSAGE_SIZE);
+  /* Tracking data 0 */
+  strncpy((char*)&(message.tlist[0].name), "Tracker0", 20);
+  message.tlist[0].type     = IGTL_TDATA_TYPE_6D;
+  message.tlist[0].reserved = 0;
+  message.tlist[0].transform[0] = -0.954892f;
+  message.tlist[0].transform[1] = 0.196632f;
+  message.tlist[0].transform[2] = -0.222525f;
+  message.tlist[0].transform[3] = -0.196632f;
+  message.tlist[0].transform[4] = 0.142857f;
+  message.tlist[0].transform[5] = 0.970014f;
+  message.tlist[0].transform[6] = 0.222525f;
+  message.tlist[0].transform[7] = 0.970014f;
+  message.tlist[0].transform[8] = -0.0977491f;
+  message.tlist[0].transform[9] = 46.0531f;
+  message.tlist[0].transform[10] = 19.4709f;
+  message.tlist[0].transform[11] = 46.0531f;
   
+  /* Tracking data 1 */
+  strncpy((char*)&(message.tlist[1].name), "Tracker1", 20);
+  message.tlist[1].type     = IGTL_TDATA_TYPE_6D;
+  message.tlist[1].reserved = 0;
+  message.tlist[1].transform[0] = -0.954892f;
+  message.tlist[1].transform[1] = 0.196632f;
+  message.tlist[1].transform[2] = -0.222525f;
+  message.tlist[1].transform[3] = -0.196632f;
+  message.tlist[1].transform[4] = 0.142857f;
+  message.tlist[1].transform[5] = 0.970014f;
+  message.tlist[1].transform[6] = 0.222525f;
+  message.tlist[1].transform[7] = 0.970014f;
+  message.tlist[1].transform[8] = -0.0977491f;
+  message.tlist[1].transform[9] = 46.0531f;
+  message.tlist[1].transform[10] = 19.4709f;
+  message.tlist[1].transform[11] = 46.0531f;
+
+  /* Tracking data 2 */
+  strncpy((char*)&(message.tlist[2].name), "Tracker2", 20);
+  message.tlist[2].type     = IGTL_TDATA_TYPE_6D;
+  message.tlist[2].reserved = 0;
+  message.tlist[2].transform[0] = -0.954892f;
+  message.tlist[2].transform[1] = 0.196632f;
+  message.tlist[2].transform[2] = -0.222525f;
+  message.tlist[2].transform[3] = -0.196632f;
+  message.tlist[2].transform[4] = 0.142857f;
+  message.tlist[2].transform[5] = 0.970014f;
+  message.tlist[2].transform[6] = 0.222525f;
+  message.tlist[2].transform[7] = 0.970014f;
+  message.tlist[2].transform[8] = -0.0977491f;
+  message.tlist[2].transform[9] = 46.0531f;
+  message.tlist[2].transform[10] = 19.4709f;
+  message.tlist[2].transform[11] = 46.0531f;
+
+
   /* Swap byte order if necessary */
-  igtl_tdata_convert_byte_order(message.metalist, TEST_TDATA_NUM);
+  igtl_tdata_convert_byte_order(message.tlist, TEST_TDATA_NUM);
 
   /* Create OpenIGTLink header */
   message.header.version = 1;
@@ -64,21 +111,19 @@ int main( int argc, char * argv [] )
   strncpy( (char*)&(message.header.device_name), "DeviceName", 20 );
   message.header.timestamp = 1234567890;
   message.header.body_size = IGTL_TDATA_ELEMENT_SIZE*TEST_TDATA_NUM;
-  message.header.crc = igtl_tdata_get_crc(message.metalist, TEST_TDATA_NUM);
+  message.header.crc = igtl_tdata_get_crc(message.tlist, TEST_TDATA_NUM);
   igtl_header_convert_byte_order( &(message.header) );
 
   /* Dumping data -- for debugging */
-  /*
   FILE *fp;
-  fp = fopen("image.bin", "w");
-  fwrite(&(message), IGTL_HEADER_SIZE+IGTL_IMAGE_HEADER_SIZE+image_size, 1, fp);
+  fp = fopen("tdata.bin", "w");
+  fwrite(&(message), IGTL_HEADER_SIZE+IGTL_TDATA_ELEMENT_SIZE*TEST_TDATA_NUM, 1, fp);
   fclose(fp);
-  */
+
 
   /* Compare the serialized byte array with the gold standard */ 
-  //r = memcmp((const void*)&message, (const void*)test_tdata_message,
-  //           (size_t)(IGTL_HEADER_SIZE+IGTL_TDATA_HEADER_SIZE));
-  r = 0;
+  r = memcmp((const void*)&message, (const void*)test_tdata_message,
+             (size_t)(IGTL_HEADER_SIZE+IGTL_TDATA_ELEMENT_SIZE*TEST_TDATA_NUM));
 
   if (r == 0)
     {
