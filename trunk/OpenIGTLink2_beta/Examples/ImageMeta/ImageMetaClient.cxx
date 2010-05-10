8d4f2b5c31ae10f7cfd0678c5ba9709edca64544
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-10 21:00:10 +0000

    ENH: implementing client example program for IMGMETA message.

--- trunk/OpenIGTLink2_beta/Examples/ImageMeta/ImageMetaClient.cxx
+++ trunk/OpenIGTLink2_beta/Examples/ImageMeta/ImageMetaClient.cxx
@@ -18,10 +18,15 @@
 #include <math.h>
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
 
 #include "igtlOSUtil.h"
 #include "igtlImageMessage.h"
+#include "igtlImgMetaMessage.h"
 #include "igtlClientSocket.h"
+
+
+int ReceiveImgMetea(igtl::ClientSocket::Pointer& socket, igtl::MessageHeader::Pointer& header);
 
 
 int main(int argc, char* argv[])
@@ -57,16 +62,42 @@ int main(int argc, char* argv[])
   // loop
   for (int i = 0; i < 100; i ++)
     {
-
-    /*
     //------------------------------------------------------------
-    // Pack (serialize) and send
-    imgMsg->Pack();
-    socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+    // Send request data
+    igtl::GetImgMetaMessage::Pointer getImgMetaMsg;
+    getImgMetaMsg = igtl::GetImgMetaMessage::New();
+    getImgMetaMsg->SetDeviceName("Client");
+    getImgMetaMsg->Pack();
+    socket->Send(getImgMetaMsg->GetPackPointer(), getImgMetaMsg->GetPackSize());
+    //igtl::Sleep(1000); // wait
 
-    igtl::Sleep(interval); // wait
-
-    */
+    
+    //------------------------------------------------------------
+    // Wait for a reply
+    igtl::MessageHeader::Pointer headerMsg;
+    headerMsg = igtl::MessageHeader::New();
+    headerMsg->InitPack();
+    int r = socket->Receive(headerMsg->GetPackPointer(), headerMsg->GetPackSize());
+    if (r == 0)
+      {
+      std::cerr << "Connection closed." << std::endl;
+      socket->CloseSocket();
+      exit(0);
+      }
+    if (r != headerMsg->GetPackSize())
+      {
+      std::cerr << "Message size information and actual data size don't match." << std::endl; 
+      exit(0);
+      }
+    if (strcmp(headerMsg->GetDeviceType(), "IMGMETA") == 0)
+      {
+      ReceiveImgMetea(socket, headerMsg);
+      }
+    else
+      {
+      std::cerr << "Invalid response from the server." << std::endl; 
+      exit(0);
+      }
     }
 
   //------------------------------------------------------------
@@ -76,10 +107,37 @@ int main(int argc, char* argv[])
 }
 
 
+int ReceiveImgMetea(igtl::ClientSocket::Pointer& socket, igtl::MessageHeader::Pointer& header)
+{
 
+  std::cerr << "Receiving STATUS data type." << std::endl;
 
+  // Create a message buffer to receive transform data
+  igtl::ImgMetaMessage::Pointer imgMeta;
+  imgMeta = igtl::ImgMetaMessage::New();
+  imgMeta->SetMessageHeader(header);
+  imgMeta->AllocatePack();
+  
+  // Receive transform data from the socket
+  socket->Receive(imgMeta->GetPackBodyPointer(), imgMeta->GetPackBodySize());
+  
+  // Deserialize the transform data
+  // If you want to skip CRC check, call Unpack() without argument.
+  int c = imgMeta->Unpack(1);
+  
+  if (c & igtl::MessageHeader::UNPACK_BODY) // if CRC check is OK
+    {
+    std::cerr << "========== STATUS ==========" << std::endl;
+    //std::cerr << " Code      : " << imgMeta->GetCode() << std::endl;
+    //std::cerr << " SubCode   : " << imgMeta->GetSubCode() << std::endl;
+    //std::cerr << " Error Name: " << imgMeta->GetErrorName() << std::endl;
+    //std::cerr << " Status    : " << imgMeta->GetStatusString() << std::endl;
+    std::cerr << "============================" << std::endl;
+    }
 
+  return 0;
 
+}
 
 
 
