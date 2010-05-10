e8c036b83d24b0be54be79c81c24e69d17185702
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-10 18:31:32 +0000

    ENH: adding example code for IMGMETA message class

---
+++ trunk/OpenIGTLink2_beta/Examples/ImageMeta/ImageMetaClient.cxx
@@ -0,0 +1,85 @@
+/*=========================================================================
+
+  Program:   Open IGT Link -- Example for Image Meta Data Client
+  Module:    $RCSfile: $
+  Language:  C++
+  Date:      $Date: $
+  Version:   $Revision: $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <cstdio>
+
+#include "igtlOSUtil.h"
+#include "igtlImageMessage.h"
+#include "igtlClientSocket.h"
+
+
+int main(int argc, char* argv[])
+{
+  //------------------------------------------------------------
+  // Parse Arguments
+
+  if (argc != 3) // check number of arguments
+    {
+    // If not correct, print usage
+    std::cerr << "Usage: " << argv[0] << " <hostname> <port>"    << std::endl;
+    std::cerr << "    <hostname> : IP or host name"                    << std::endl;
+    std::cerr << "    <port>     : Port # (18944 in Slicer default)"   << std::endl;
+    exit(0);
+    }
+
+  char*  hostname = argv[1];
+  int    port     = atoi(argv[2]);
+
+  //------------------------------------------------------------
+  // Establish Connection
+  igtl::ClientSocket::Pointer socket;
+  socket = igtl::ClientSocket::New();
+  int r = socket->ConnectToServer(hostname, port);
+
+  if (r != 0)
+    {
+    std::cerr << "Cannot connect to the server." << std::endl;
+    exit(0);
+    }
+
+  //------------------------------------------------------------
+  // loop
+  for (int i = 0; i < 100; i ++)
+    {
+
+    /*
+    //------------------------------------------------------------
+    // Pack (serialize) and send
+    imgMsg->Pack();
+    socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+
+    igtl::Sleep(interval); // wait
+
+    */
+    }
+
+  //------------------------------------------------------------
+  // Close connection
+  socket->CloseSocket();
+
+}
+
+
+
+
+
+
+
+
+
