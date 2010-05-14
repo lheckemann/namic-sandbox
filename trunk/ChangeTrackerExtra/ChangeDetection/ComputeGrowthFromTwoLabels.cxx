41313a74204df79430c4b7528f95ce4d9b9e0e81
fedorov <fedorov@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-14 17:50:24 +0000

    ENH: allow to accept transform

--- trunk/ChangeTrackerExtra/ChangeDetection/ComputeGrowthFromTwoLabels.cxx
+++ trunk/ChangeTrackerExtra/ChangeDetection/ComputeGrowthFromTwoLabels.cxx
@@ -10,13 +10,15 @@
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkImageRegionIteratorWithIndex.h"
 #include "itkImageDuplicator.h"
+#include "itkTransformFileReader.h"
+#include "itkAffineTransform.h"
 
 int main(int argc, char * argv[])
 {
-  if( argc != 4 )
+  if( argc < 4 )
     {
     std::cerr << "Usage: " << argv[0];
-    std::cerr << " baseline followup result ";
+    std::cerr << " baseline followup result [transform]";
     std::cerr << std::endl;
     return EXIT_FAILURE;
     }
@@ -31,12 +33,41 @@ int main(int argc, char * argv[])
   typedef itk::BinaryThresholdImageFilter<ImageType,ImageType> ThreshType;
   typedef itk::ImageRegionIteratorWithIndex<ImageType> IterType;
   typedef itk::ImageDuplicator<ImageType> DupType;
+  typedef itk::TransformFileReader TransformReaderType;
+  typedef itk::AffineTransform<double> TransformType;
 
   ReaderType::Pointer r1 = ReaderType::New();
   ReaderType::Pointer r2 = ReaderType::New();
   r1->SetFileName(argv[1]);
   r2->SetFileName(argv[2]);
   
+  TransformType::Pointer affineTfm = NULL;
+  if(argc>4){
+    std::cout << "Will use the supplied transform!" << std::endl;
+    // transform is available
+    affineTfm = TransformType::New();
+
+    TransformReaderType::Pointer tfmReader = TransformReaderType::New();
+    tfmReader->SetFileName(argv[4]);
+    tfmReader->Update();
+  
+    TransformReaderType::TransformType::Pointer rtfm = 
+      *(tfmReader->GetTransformList()->begin());
+    typedef itk::MatrixOffsetTransformBase<double,3,3> OffsetType;
+    OffsetType::Pointer d =
+      dynamic_cast<OffsetType*>(rtfm.GetPointer());
+
+    if(!d){
+      std::cerr << "Cast failed!" << std::endl;
+    return -1;
+    }
+
+    vnl_svd<double> svd(d->GetMatrix().GetVnlMatrix());
+    affineTfm->SetMatrix(svd.U()*vnl_transpose(svd.V()));
+    affineTfm->SetOffset(d->GetOffset());
+  }
+
+
   r1->Update();r2->Update();
   ImageType::Pointer i1 = r1->GetOutput(), i2 = r2->GetOutput();
 
@@ -59,7 +90,11 @@ int main(int argc, char * argv[])
     ImageType::IndexType idx1 = it1.GetIndex(), idx2;
     ImageType::PointType pt;
     i1->TransformIndexToPhysicalPoint(idx1,pt);
-    i2->TransformPhysicalPointToIndex(pt,idx2);
+
+    if(affineTfm)
+      pt = affineTfm->TransformPoint(pt);
+
+    i2->TransformPhysicalPointToIndex(pt,idx2);    
 
     if(it1.Get() && i2->GetPixel(idx2))
       itR.Set(0);
