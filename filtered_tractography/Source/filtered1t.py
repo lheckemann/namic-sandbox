39dac1888d41870f0bd88fad983bb92aafa4a332
malcolm <malcolm@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-17 16:22:52 +0000

    ENH: ironed out type issues

--- filtered_tractography/Source/filtered1t.py
+++ filtered_tractography/Source/filtered1t.py
@@ -219,7 +219,7 @@ def init(S, seeds, u, b, param):
     pp = []
     map(lambda p : map(lambda e : pp.append(p+e), list(E.T)), qq)
 
-    pp = [np.array((27,69,72))] # HACK
+    pp = [np.array((27.0,69.0,72.0))] # HACK
 
     print 'initial seed upper limit: %d' % (2*len(pp))
     # indices -> signals -> tensors
