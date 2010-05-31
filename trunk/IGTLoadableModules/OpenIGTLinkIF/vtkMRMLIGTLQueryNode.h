49c9b5e2d2910d43c0cc65defe9c2c3b63f7bc54
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-05-31 17:53:43 +0000

    ENH: adding query node and image metadata list node

---
+++ trunk/IGTLoadableModules/OpenIGTLinkIF/vtkMRMLIGTLQueryNode.h
@@ -0,0 +1,148 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2009 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See Doc/copyright/copyright.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLCurveAnalysisNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+#ifndef __vtkMRMLIGTLQueryNode_h
+#define __vtkMRMLIGTLQueryNode_h
+
+#include "vtkOpenIGTLinkIFWin32Header.h"
+#include "vtkMRML.h"
+#include "vtkMRMLNode.h"
+#include "vtkMRMLStorageNode.h"
+
+#include "vtkIGTLToMRMLBase.h"
+
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+
+#include "vtkObject.h"
+#include "vtkOpenIGTLinkIFWin32Header.h" 
+
+class VTK_OPENIGTLINKIF_EXPORT vtkMRMLIGTLQueryNode : public vtkMRMLNode
+{
+ public:
+
+  //----------------------------------------------------------------
+  // Constants Definitions
+  //----------------------------------------------------------------
+
+  //BTX
+  // Events
+  enum {
+    NewDeviceEvent        = 118949,
+  };
+
+  enum {
+    TYPE_NOT_DEFINED,
+    TYPE_GET,
+    TYPE_START,
+    TYPE_STOP,
+    NUM_TYPE,
+  };
+
+  enum {
+    STATUS_NOT_DEFINED,
+    STATUS_PREPARED,     // Ready to query
+    STATUS_WAITING,      // Waiting for response from server
+    STATUS_SUCCESS,      // Server accepted query successfuly 
+    STATUS_ERROR,        // Server failed to accept query
+    NUM_STATUS,
+  };
+  //ETX
+
+ public:
+
+  //----------------------------------------------------------------
+  // Access functions
+  //----------------------------------------------------------------
+  vtkGetMacro( QueryStatus, int );
+  vtkSetMacro( QueryStatus, int );
+  vtkGetMacro( QueryType, int );
+  vtkSetMacro( QueryType, int );
+
+  //----------------------------------------------------------------
+  // Standard methods for MRML nodes
+  //----------------------------------------------------------------
+
+  static vtkMRMLIGTLQueryNode *New();
+  vtkTypeMacro(vtkMRMLIGTLQueryNode,vtkMRMLNode);
+  
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  // Description:
+  // Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  // Description:
+  // Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  // Description:
+  // Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  // Description:
+  // Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName()
+  {return "IGTLQuery";};
+
+  // method to propagate events generated in mrml
+  virtual void ProcessMRMLEvents ( vtkObject *caller, unsigned long event, void *callData );
+
+  // Description:
+  // Get OpenIGTLink device name. If the query node is for IMAGE, "IMAGE" is returned.
+  virtual const char* GetIGTLName() { return NULL; };
+
+  // Description:
+  // Return error message after receiving requested message.
+  virtual const char* GetErrorString() { return NULL; };
+  
+ protected:
+  //----------------------------------------------------------------
+  // Constructor and destroctor
+  //----------------------------------------------------------------
+  
+  vtkMRMLIGTLQueryNode();
+  ~vtkMRMLIGTLQueryNode();
+  vtkMRMLIGTLQueryNode(const vtkMRMLIGTLQueryNode&);
+  void operator=(const vtkMRMLIGTLQueryNode&);
+
+ public:
+  //----------------------------------------------------------------
+  // Connector configuration
+  //----------------------------------------------------------------
+
+
+ private:
+  //----------------------------------------------------------------
+  // Data
+  //----------------------------------------------------------------
+
+  int QueryStatus;
+  int QueryType;
+
+  //BTX
+  std::string ConnectorNodeID;
+  //ETX
+
+  // Time when the querry issued. 
+  double TimeStamp;
+  double TimeOut;
+  
+};
+
+#endif
+
