2bf2c27047413eb3ce24667f0d8eee9c50200a6f
lchauvin <lchauvin@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-06-14 21:32:55 +0000

    ENH: Add NeuroEndoscopic Directory

---
+++ trunk/IGTLoadableModules/NeuroEndoscopic/Navigation/CMakeFiles/NeuroEndoscopicNavigation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/NeuroEndoscopicNavigation.dir/NeuroEndoscopicNavigation.o"
+  "bin/NeuroEndoscopicNavigation.pdb"
+  "bin/NeuroEndoscopicNavigation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/NeuroEndoscopicNavigation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
