d35fa705f5279b50c85b53132984446c8543154a
lchauvin <lchauvin@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-06-15 21:22:47 +0000

    ENH: Change name of the directory

---
+++ trunk/IGTLoadableModules/NeuroEndoscopic/CameraCalibration/CMakeFiles/NeuroEndoscopicCameraCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/NeuroEndoscopicCameraCalibration.dir/NeuroEndoscopicCameraCalibration.o"
+  "bin/NeuroEndoscopicCameraCalibration.pdb"
+  "bin/NeuroEndoscopicCameraCalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/NeuroEndoscopicCameraCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
