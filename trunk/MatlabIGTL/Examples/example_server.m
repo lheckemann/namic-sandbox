f0de72ea65656ae69b4af72089622e036a8561cc
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-06-15 22:23:55 +0000

    ENH: image receive in matlab

--- trunk/MatlabIGTL/Examples/example_server.m
+++ trunk/MatlabIGTL/Examples/example_server.m
@@ -1,23 +1,32 @@
+function DATA = example_server(timeout)
 %
 % Example to use igtlwaitconn()
 %
 
 %%% Create server and get server file descriptor
-sfd = igtlopen('', 18945);
+sfd = igtlopen('', 18944);
+disp(sprintf('File descriptor = %d', sfd))
 
-cfd = igtlwaitcon(sfd, 1000);
-if cfd > 0
-  disp ('Client connected.')
-  while cfd
-    [S, DATA] = igtlreceive(cfd);
-    if S==0 % connection lost
-      close(cfd)
-      cfd = 0;
+while sfd > 0
+  cfd = igtlwaitcon(sfd, timeout);
+  if cfd > 0
+    disp ('Client connected.')
+    while cfd
+      [S, DATA] = igtlreceive(cfd);
+      if S==0 % connection lost
+	close(cfd)
+	cfd = 0;
+      else
+        %figure; image(DATA.Image(:,:,60))
+        DATA.Name = 'MatlabImage';
+	r = igtlsend(cfd, DATA);
+      end
     end
+  else
+    igtlclose(sfd);
+    sfd = 0;
   end
 end
 
-
-igtlclose(sfd);
 
 
