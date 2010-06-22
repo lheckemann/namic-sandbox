be74ea1690c19e935dc3c7259e3a2b28ec6c951c
fedorov <fedorov@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-06-22 18:51:11 +0000

    ENH: improvements and bugfixes from Yanling

--- trunk/ProgrammaticVolumeRendering/volume_render.tcl
+++ trunk/ProgrammaticVolumeRendering/volume_render.tcl
@@ -23,13 +23,13 @@ set vp [$propertyNode GetVolumeProperty]
 #set opacity [vtkPiecewiseFunction New]
 #set color [vtkColorTransferFunction New]
 
-
 #$vp SetScalarOpacity $opacity
 #$vp SetRGBTransferFunction $color
 
 #set opacityStr "2 0 0 500 1"
 #[$property GetVolumePropertyNode] GetPiecewiseFunctionFromString $opacityStr 
 
+# need to query input data for scalar range
 
 # this is vtkPiecewiseFunction
 set opacity [$vp GetScalarOpacity]    
@@ -37,15 +37,34 @@ set opacity [$vp GetScalarOpacity]
 # this is vtkColorTransferFunction
 set color [$vp GetRGBTransferFunction] 
 
+set gradient [$vp GetGradientOpacity]
+
 $opacity RemoveAllPoints
-$opacity AddPoint 0 0
-$opacity AddPoint 500 1
-$opacity AddPoint 510 0
+$opacity AddPoint -1000 0
+$opacity AddPoint 198 0
+$opacity AddPoint 300 1
+$opacity AddPoint 430 0
 
 $color RemoveAllPoints
-$color AddRGBPoint 0 0 0 0
-$color AddRGBPoint 500 100 200 50
-$color AddRGBPoint 510 100 0 0
+$color AddRGBPoint -1000 0 0 0
+$color AddRGBPoint 300 0.3 0.9 0.1
+$color AddRGBPoint 430 0.9 0 0
+
+$gradient RemoveAllPoints
+$gradient AddPoint 0 1
+$gradient AddPoint 357 1
+
+$vp SetInterpolationTypeToLinear
+$vp ShadeOn
+$vp SetAmbient 0.30
+$vp SetDiffuse 0.60
+$vp SetSpecular 0.50
+$vp SetSpecularPower 40
+
+$propertyNode SetModifiedSinceRead 1
+
+$roiNode InsideOutOn
+$roiNode VisibilityOff
 
 #set 
 $scene AddNode $propertyNode
@@ -54,6 +73,7 @@ $scene AddNode $roiNode
 $parametersNode SetAndObserveVolumeNodeID "vtkMRMLScalarVolumeNode1"
 $parametersNode SetAndObserveVolumePropertyNodeID [$propertyNode GetID]
 $parametersNode SetAndObserveROINodeID [$roiNode GetID]
+$parametersNode SetCurrentVolumeMapper 3
 
 $scene AddNode $parametersNode
 
