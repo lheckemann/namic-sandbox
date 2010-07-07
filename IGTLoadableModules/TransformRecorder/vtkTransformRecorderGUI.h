81b7a92e1e76903d7208aa02dd20fd4a884bd09c
ungi <ungi@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-07-07 16:02:36 +0000

    ENH: Fifth button.

--- IGTLoadableModules/TransformRecorder/vtkTransformRecorderGUI.h
+++ IGTLoadableModules/TransformRecorder/vtkTransformRecorderGUI.h
@@ -41,23 +41,26 @@ class vtkKWLabel;
 
 // Quick message buttons. -----------------------------------------------------
 
-static const int BUTTON_COUNT = 4;
+static const int BUTTON_COUNT = 5;
 static const char* BUTTON_TEXTS[ BUTTON_COUNT ] =
   {
   "Alignment outside",
   "Pierce skin",
   "Retract inside",
-  "Adjust angle inside"
+  "Adjust angle inside",
+  "Needle placed"
   };
 static const char* BUTTON_MESSAGES[ BUTTON_COUNT ] =
   {
-  "Operator does alignment outside",
-  "Operator does pierce skin",
-  "Operator does retract inside",
-  "Operator does adjust angle inside"
+  "Operator does alignment outside.",
+  "Operator does pierce skin.",
+  "Operator does retract inside.",
+  "Operator does adjust angle inside.",
+  "Operator placed the needle."
   };
 
 // ----------------------------------------------------------------------------
+
 
 
 class VTK_TransformRecorder_EXPORT vtkTransformRecorderGUI : public vtkSlicerModuleGUI
