ba461f76da2299f4979d69c8f2b41377ceb63c6a
stefalie <stefalie@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-10-05 17:29:02 +0000

    ENH: removed debug output

--- trunk/filtered_tractography/Source/filtered.py
+++ trunk/filtered_tractography/Source/filtered.py
@@ -127,7 +127,7 @@ import itertools
 
 def Execute(dwi_node, seeds_node, mask_node, ff_node, \
             record_fa = False, record_state = True, record_cov = False, \
-            model="two-tensor", tensor_model="full", FA_min=.15, GA_min=.10,
+            model="two-tensor", tensor_model="Full", FA_min=.15, GA_min=.10,
             seeds=1, labels=[1],
             Qm=.0030, Ql=100, Rs=.015, theta_max=0):
     for i in xrange(10) : print ''
