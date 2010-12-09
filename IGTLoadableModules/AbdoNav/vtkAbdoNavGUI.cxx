68de306a8679b23c24bd057d173f8412dbf59e45
cammann <cammann@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2010-12-09 05:03:48 +0000

    STYLE: added missing dashed comment line above BuildGUIConnectionFrame()

--- IGTLoadableModules/AbdoNav/vtkAbdoNavGUI.cxx
+++ IGTLoadableModules/AbdoNav/vtkAbdoNavGUI.cxx
@@ -418,6 +418,7 @@ void vtkAbdoNavGUI::BuildGUIHelpFrame()
 }
 
 
+//---------------------------------------------------------------------------
 void vtkAbdoNavGUI::BuildGUIConnectionFrame()
 {
   //----------------------------------------------------------------
