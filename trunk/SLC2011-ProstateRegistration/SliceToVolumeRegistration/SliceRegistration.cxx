5208079a3021c6322f854948bce52094050473db
fedorov <fedorov@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-01-10 17:41:11 +0000

    ENH: prostate registration code for SLC2011 activities

---
+++ trunk/SLC2011-ProstateRegistration/SliceToVolumeRegistration/SliceRegistration.cxx
@@ -0,0 +1,342 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: ImageRegistration8.cxx,v $
+  Language:  C++
+  Date:      $Date: 2009-06-24 12:09:25 $
+  Version:   $Revision: 1.39 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+// This code is based on ITK Examples/Registration/ImageRegistration8.cxx
+
+#include "itkImageRegistrationMethod.h"
+#include "itkMeanSquaresImageToImageMetric.h"
+#include "itkNormalizedCorrelationImageToImageMetric.h"
+#include "itkMattesMutualInformationImageToImageMetric.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkNearestNeighborInterpolateImageFunction.h"
+#include "itkImage.h"
+#include "itkTransformFileWriter.h"
+#include "itkVersorRigid3DTransform.h"
+#include "itkCenteredTransformInitializer.h"
+#include "itkVersorRigid3DTransformOptimizer.h"
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkResampleImageFilter.h"
+#include "itkCastImageFilter.h"
+#include "itkSubtractImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkExtractImageFilter.h"
+#include "itkDiscreteGaussianImageFilter.h"
+
+#include "itkImageRegionConstIteratorWithIndex.h"
+#include "itkImageMaskSpatialObject.h"
+
+#include "SliceRegistrationCLP.h"
+
+//  The following section of code implements a Command observer
+//  that will monitor the evolution of the registration process.
+//
+#include "itkCommand.h"
+class CommandIterationUpdate : public itk::Command 
+{
+public:
+  typedef  CommandIterationUpdate   Self;
+  typedef  itk::Command             Superclass;
+  typedef itk::SmartPointer<Self>   Pointer;
+  itkNewMacro( Self );
+protected:
+  CommandIterationUpdate() {};
+public:
+  typedef itk::VersorRigid3DTransformOptimizer OptimizerType;
+  typedef   const OptimizerType *              OptimizerPointer;
+
+  void Execute(itk::Object *caller, const itk::EventObject & event)
+    {
+    Execute( (const itk::Object *)caller, event);
+    }
+
+  void Execute(const itk::Object * object, const itk::EventObject & event)
+    {
+    OptimizerPointer optimizer = 
+      dynamic_cast< OptimizerPointer >( object );
+    if( ! itk::IterationEvent().CheckEvent( &event ) )
+      {
+      return;
+      }
+    std::cout << optimizer->GetCurrentIteration() << "   ";
+    std::cout << optimizer->GetValue() << "   ";
+    std::cout << optimizer->GetCurrentPosition() << std::endl;
+    }
+};
+
+
+int main( int argc, char *argv[] )
+{
+  PARSE_ARGS;
+  
+  const    unsigned int    Dimension = 3;
+  typedef  float           PixelType;
+
+  typedef itk::Image< PixelType, Dimension >  FixedImageType;
+  typedef itk::Image< PixelType, Dimension >  MovingImageType;
+  typedef itk::Image< unsigned char, Dimension >  MaskImageType;
+
+  typedef itk::VersorRigid3DTransform< double > TransformType;
+
+  typedef itk::VersorRigid3DTransformOptimizer           OptimizerType;
+
+  typedef itk::ImageRegionConstIteratorWithIndex<FixedImageType> IterType;
+
+//  typedef itk::MeanSquaresImageToImageMetric< 
+//                                    FixedImageType, 
+//                                    MovingImageType >    MetricType;
+
+//  typedef itk::NormalizedCorrelationImageToImageMetric<
+//                                    FixedImageType,
+//                                    MovingImageType > MetricType;
+
+  typedef itk::MattesMutualInformationImageToImageMetric<
+                                    FixedImageType,
+                                    MovingImageType > MetricType;
+
+  typedef itk:: LinearInterpolateImageFunction< 
+                                    MovingImageType,
+                                    double          >    InterpolatorType;
+  typedef itk:: NearestNeighborInterpolateImageFunction< 
+                                    MovingImageType,
+                                    double          >    NNInterpolatorType;
+
+  typedef itk::ImageRegistrationMethod< 
+                                    FixedImageType, 
+                                    MovingImageType >    RegistrationType;
+
+  typedef itk::DiscreteGaussianImageFilter<
+                                    MovingImageType, MovingImageType > BlurType;
+
+  MetricType::Pointer         metric        = MetricType::New();
+  OptimizerType::Pointer      optimizer     = OptimizerType::New();
+  InterpolatorType::Pointer   interpolator  = InterpolatorType::New();
+  RegistrationType::Pointer   registration  = RegistrationType::New();
+  
+  metric->SetNumberOfHistogramBins(32);
+
+  std::cout << "Number of spatial samples: " << metric->GetNumberOfSpatialSamples() << std::endl;
+
+  registration->SetMetric(        metric        );
+  registration->SetOptimizer(     optimizer     );
+  registration->SetInterpolator(  interpolator  );
+
+
+  TransformType::Pointer  transform = TransformType::New();
+  registration->SetTransform( transform );
+
+  typedef itk::ImageFileReader< FixedImageType  > FixedImageReaderType;
+  typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
+  typedef itk::ImageFileReader< MaskImageType > MaskReaderType;
+
+  FixedImageReaderType::Pointer  fixedImageReader  = FixedImageReaderType::New();
+  MovingImageReaderType::Pointer movingImageReader = MovingImageReaderType::New();
+  MaskReaderType::Pointer maskImageReader = MaskReaderType::New();
+
+  fixedImageReader->SetFileName( sliceImageName.c_str() );
+  movingImageReader->SetFileName( volumeImageName.c_str() );
+  maskImageReader->SetFileName( volumeMaskImageName.c_str() );
+  maskImageReader->Update();
+
+  typedef itk::ImageMaskSpatialObject<3> MaskObjectType;
+  MaskObjectType::Pointer maskObject = MaskObjectType::New();
+  maskObject->SetImage(maskImageReader->GetOutput());
+  metric->SetFixedImageMask(maskObject);
+
+  registration->SetFixedImage(    fixedImageReader->GetOutput()    );
+
+  BlurType::Pointer blur = BlurType::New();
+  blur->SetInput(movingImageReader->GetOutput());
+  blur->SetVariance(2.0);
+  blur->Update();
+
+  MovingImageType::Pointer movingImage = blur->GetOutput();
+  FixedImageType::Pointer fixedImage = fixedImageReader->GetOutput();
+
+  registration->SetMovingImage(   blur->GetOutput()   );
+  fixedImageReader->Update();
+
+  registration->SetFixedImageRegion( 
+     fixedImageReader->GetOutput()->GetBufferedRegion() );
+ 
+  transform->SetIdentity();
+  registration->SetInitialTransformParameters( transform->GetParameters() );
+
+  typedef OptimizerType::ScalesType       OptimizerScalesType;
+  OptimizerScalesType optimizerScales( transform->GetNumberOfParameters() );
+  double translationScale[3];// = 1.0 / 1000.0;
+  translationScale[0] = 1. / 2000.;//(10.*fixedImage->GetSpacing()[0]*fixedImage->GetBufferedRegion().GetSize()[0]);
+  translationScale[1] = 1. / 2000.;//(10.*fixedImage->GetSpacing()[1]*fixedImage->GetBufferedRegion().GetSize()[1]);
+  translationScale[2] = 1. / 2000.;//(10.*fixedImage->GetSpacing()[2]*fixedImage->GetBufferedRegion().GetSize()[2]);
+
+
+  optimizerScales[0] = 1.0;
+  optimizerScales[1] = 1.0;
+  optimizerScales[2] = 1.0;
+  optimizerScales[3] = translationScale[0];
+//    1./(10.*fixedImage->GetSpacing()[0]*fixedImage->GetBufferedRegion().GetSize()[0]);
+  optimizerScales[4] = translationScale[1];
+//    1./(10.*fixedImage->GetSpacing()[1]*fixedImage->GetBufferedRegion().GetSize()[1]);
+  optimizerScales[5] = translationScale[2];
+//    1./(.1*fixedImage->GetSpacing()[2]*fixedImage->GetBufferedRegion().GetSize()[2]);
+//
+  std::cout << "Optimizer scales: " << optimizerScales << std::endl;    
+  std::cout << "Relaxation factor: " << optimizer->GetRelaxationFactor() << std::endl;
+
+  optimizer->SetScales( optimizerScales );
+
+  optimizer->SetMaximumStepLength( 0.2000  ); 
+  optimizer->SetMinimumStepLength( 0.0001 );
+
+  optimizer->SetNumberOfIterations( 400 );
+
+
+  // Create the Command observer and register it with the optimizer.
+  //
+  CommandIterationUpdate::Pointer observer = CommandIterationUpdate::New();
+  optimizer->AddObserver( itk::IterationEvent(), observer );
+
+
+  try 
+    { 
+    registration->StartRegistration(); 
+    std::cout << "Optimizer stop condition: "
+              << registration->GetOptimizer()->GetStopConditionDescription()
+              << std::endl;
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  
+  OptimizerType::ParametersType finalParameters = 
+                    registration->GetLastTransformParameters();
+
+
+  const double versorX              = finalParameters[0];
+  const double versorY              = finalParameters[1];
+  const double versorZ              = finalParameters[2];
+  const double finalTranslationX    = finalParameters[3];
+  const double finalTranslationY    = finalParameters[4];
+  const double finalTranslationZ    = finalParameters[5];
+
+  const unsigned int numberOfIterations = optimizer->GetCurrentIteration();
+
+  const double bestValue = optimizer->GetValue();
+
+
+  // Print out results
+  //
+  std::cout << std::endl << std::endl;
+  std::cout << "Result = " << std::endl;
+  std::cout << " versor X      = " << versorX  << std::endl;
+  std::cout << " versor Y      = " << versorY  << std::endl;
+  std::cout << " versor Z      = " << versorZ  << std::endl;
+  std::cout << " Translation X = " << finalTranslationX  << std::endl;
+  std::cout << " Translation Y = " << finalTranslationY  << std::endl;
+  std::cout << " Translation Z = " << finalTranslationZ  << std::endl;
+  std::cout << " Iterations    = " << numberOfIterations << std::endl;
+  std::cout << " Metric value  = " << bestValue          << std::endl;
+
+  transform->SetParameters( finalParameters );
+
+  TransformType::MatrixType matrix = transform->GetRotationMatrix();
+  TransformType::OffsetType offset = transform->GetOffset();
+
+  typedef itk::TransformFileWriter TransformWriter;
+  TransformWriter::Pointer tfmWriter = TransformWriter::New();
+  tfmWriter->SetInput(transform);
+  tfmWriter->SetFileName( resultTransformName.c_str() );
+  tfmWriter->Update();
+
+#if 0 // calculation of the error over the mask region
+  typedef itk::ResampleImageFilter< 
+                            MovingImageType, 
+                            FixedImageType >    ResampleFilterType;
+
+  TransformType::Pointer finalTransform = TransformType::New();
+
+  finalTransform->SetCenter( transform->GetCenter() );
+
+  finalTransform->SetParameters( finalParameters );
+  finalTransform->SetFixedParameters( transform->GetFixedParameters() );
+
+  ResampleFilterType::Pointer resampler = ResampleFilterType::New();
+
+  resampler->SetTransform( finalTransform );
+  resampler->SetInput( movingImageReader->GetOutput() );
+
+  fixedImage = fixedImageReader->GetOutput();
+
+  resampler->SetSize(    fixedImage->GetLargestPossibleRegion().GetSize() );
+  resampler->SetOutputOrigin(  fixedImage->GetOrigin() );
+  resampler->SetOutputSpacing( fixedImage->GetSpacing() );
+  resampler->SetOutputDirection( fixedImage->GetDirection() );
+  resampler->SetDefaultPixelValue( 100 );
+  
+  typedef  float  OutputPixelType;
+
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+  
+  typedef itk::CastImageFilter< 
+                        FixedImageType,
+                        OutputImageType > CastFilterType;
+                    
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+
+  CastFilterType::Pointer  caster =  CastFilterType::New();
+
+  NNInterpolatorType::Pointer nnInterp = NNInterpolatorType::New();
+  resampler->SetInput(maskImageReader->GetOutput());
+  resampler->SetInterpolator(nnInterp);
+  resampler->SetDefaultPixelValue( 0 );
+  resampler->Update();
+
+  double avDist = 0, maxDist = 0, cnt = 0;
+  FixedImageType::Pointer resampledMask = resampler->GetOutput();
+  IterType it(resampledMask, resampledMask->GetBufferedRegion());
+  for(it.GoToBegin();!it.IsAtEnd();++it){
+    FixedImageType::PointType pt0, pt1;
+    FixedImageType::IndexType idx;
+    idx = it.GetIndex();
+    if(it.Get()){
+      resampledMask->TransformIndexToPhysicalPoint(idx, pt0);
+      pt1 = finalTransform->TransformPoint(pt0);
+      double dist = sqrt( (pt0[0]-pt1[0])*(pt0[0]-pt1[0])+
+                          (pt0[1]-pt1[1])*(pt0[1]-pt1[1])+
+                          (pt0[2]-pt1[2])*(pt0[2]-pt1[2]) );
+      avDist += dist;
+      if(dist>maxDist)
+        maxDist = dist;
+      cnt++;
+    }
+  }
+
+  std::cout << "Average magnitude: " << avDist/cnt << std::endl;
+  std::cout << "Max magnitude: " << maxDist << std::endl;
+#endif
+
+  return EXIT_SUCCESS;
+}
