4852d17dea0c7230047216ac880ed2bee1233dd4
fedorov <fedorov@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-01-12 21:34:03 +0000

    ENH: added scripts for running registration

---
+++ trunk/SLC2011-ProstateRegistration/Scripts/RunExperiments.sh
@@ -0,0 +1,75 @@
+DATA_ROOT=~/SLC2011/SLC2011-ProstateRegistration/TestData
+EXE_ROOT=~/Slicer/Slicer-3-6/Slicer3-build/lib/Slicer3/Plugins
+
+VOLUME=TR03_series10-T2Ax.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TR03_series16-NeedleAx' 'TR03_series14-NeedleAx'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
+VOLUME=TR02_series10-T2Ax.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TR02_series20-trufiAx' 'TR02_series21-trufiAx2'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
+VOLUME=TR01_series10-T2BC.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TR01_series13-NeedleBC' 'TR01_series19_NeedleBC'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
+VOLUME=TP9_series3.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TP9_series424'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
+VOLUME=TP11_series3.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TP11_series18' 'TP11_series34'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
+VOLUME=TP10_series3.nrrd
+echo
+echo Running case $VOLUME
+for SLICE in 'TP10_series27' 'TP10_series17'
+do
+  for LABEL in 'A' 'AC' 'ACS'
+  do
+    cmd=${EXE_ROOT}'/SliceRegistration --sliceImage '${DATA_ROOT}'/'${SLICE}'.nrrd --volumeImage '${DATA_ROOT}'/'${VOLUME}' --volumeImageMask '${DATA_ROOT}'/'${SLICE}'-label-'${LABEL}'.nrrd --transform '${VOLUME}'-'${SLICE}'-'${LABEL}'-SliceRegistration.tfm'
+    echo $cmd
+  done
+done
+
