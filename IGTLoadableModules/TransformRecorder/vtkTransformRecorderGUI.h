a57ba5dd5fdfe188ee4977e461c454e919a25b5f
ungi <ungi@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-01-14 00:49:20 +0000

    ENH: Unnecessary field deleted.

--- IGTLoadableModules/TransformRecorder/vtkTransformRecorderGUI.h
+++ IGTLoadableModules/TransformRecorder/vtkTransformRecorderGUI.h
@@ -171,9 +171,6 @@ public:
   std::vector< vtkKWEntry* >      AnnotationEntryVector;
   std::vector< vtkKWPushButton* > AnnotationButtonVector;
   //ETX
-  vtkKWEntry*                     CustomEntry;
-  vtkKWPushButton*                CustomButton;
-  
   
   
   //BTX
