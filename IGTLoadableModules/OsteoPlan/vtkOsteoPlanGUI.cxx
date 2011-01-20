2575fe34358a231e53f9bc35642333f1e851b10e
lchauvin <lchauvin@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-01-20 18:44:35 +0000

    ENH: Modify clipping to be able to move clipped part

--- IGTLoadableModules/OsteoPlan/vtkOsteoPlanGUI.cxx
+++ IGTLoadableModules/OsteoPlan/vtkOsteoPlanGUI.cxx
@@ -46,7 +46,9 @@
 #include "vtkPlanes.h"
 #include "vtkClipPolyData.h"
 #include "vtkPolyData.h"
-
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
 
 #include "vtkCornerAnnotation.h"
 
@@ -324,71 +326,109 @@ void vtkOsteoPlanGUI::ProcessGUIEvents(vtkObject *caller,
     {
       // Clipping Slicer Model
       if(this->ModelToCutSelector && this->CuttingPlane)
-     {  
-
-          // Clip Model
-       vtkClipPolyData* clipper = vtkClipPolyData::New();
-          vtkMRMLModelNode* modOut = vtkMRMLModelNode::New();
-          vtkMRMLModelDisplayNode* dnode = vtkMRMLModelDisplayNode::New();
+     {
+          // Get Planes from vtkBoxWidget  
           vtkPlanes* planes = vtkPlanes::New();
           this->CuttingPlane->GetPlanes(planes);
-          
+
+          // Data to clip
           vtkMRMLModelNode* model = reinterpret_cast<vtkMRMLModelNode*>(this->ModelToCutSelector->GetSelected());
-          vtkPolyData* poly = model->GetPolyData();
-
-          vtkPolyData* cpoly = vtkPolyData::New();
-          cpoly->CopyStructure(poly);
- 
-          model->SetAndObservePolyData(cpoly);
-          model->GetDisplayNode()->SetVisibility(0);
-
-          clipper->SetClipFunction(planes);
-          clipper->InsideOutOff();         
+       
+          // Set Clipper 1
+          vtkClipPolyData* clipper = vtkClipPolyData::New();
           clipper->SetInput(model->GetPolyData());
-          clipper->Update();
+          clipper->SetClipFunction(planes);
+          clipper->GenerateClipScalarsOn();
+          clipper->GenerateClippedOutputOn();
+          clipper->InsideOutOn();
+      
+          // Set Clipper 2
+          vtkClipPolyData* clipper2 = vtkClipPolyData::New();
+          clipper2->SetInput(model->GetPolyData());
+          clipper2->SetClipFunction(planes);
+          clipper2->GenerateClipScalarsOn();
+          clipper2->GenerateClippedOutputOn();
+          clipper2->InsideOutOff();
           
-          poly = clipper->GetOutput();
-
-          dnode->SetColor(model->GetDisplayNode()->GetColor());
-          this->GetMRMLScene()->AddNode(dnode);
+          // Set Mapper 1     
+          vtkPolyDataMapper* clipMapper = vtkPolyDataMapper::New();
+          clipMapper->SetInputConnection(clipper->GetOutputPort());
+          clipMapper->ScalarVisibilityOff();
           
+          // Set Mapper 2
+          vtkPolyDataMapper* clipMapper2 = vtkPolyDataMapper::New();
+          clipMapper2->SetInputConnection(clipper2->GetOutputPort());
+          clipMapper2->ScalarVisibilityOff();
+          
+          // Set Actor 1
+          vtkActor* clipActor = vtkActor::New();
+          clipActor->SetMapper(clipMapper);
+          clipActor->GetProperty()->SetColor(1,0,0);
 
-          // Check if a previous clipping has been made
-          vtkCollection* clippedmodel = this->GetMRMLScene()->GetNodesByName("OsteoPlanClippedModel");
-          if(clippedmodel->GetNumberOfItems())
+       // Set Actor 2
+          vtkActor* clipActor2 = vtkActor::New();
+          clipActor2->SetMapper(clipMapper2);
+          clipActor2->GetProperty()->SetColor(0,0,1);
+
+          // Add to MRML Scene
+          vtkMRMLModelNode* part1 = vtkMRMLModelNode::New();
+          vtkMRMLModelNode* part2 = vtkMRMLModelNode::New();
+          vtkMRMLModelDisplayNode* dnode1 = vtkMRMLModelDisplayNode::New();
+          vtkMRMLModelDisplayNode* dnode2 = vtkMRMLModelDisplayNode::New();      
+
+          // - Model 1
+          dnode1->SetColor(clipActor->GetProperty()->GetColor());
+          this->GetMRMLScene()->AddNode(dnode1);
+          part1->SetAndObservePolyData(clipper->GetOutput());
+          part1->SetAndObserveDisplayNodeID(dnode1->GetID());
+          this->GetMRMLScene()->AddNode(part1);
+
+          // - Base
+          // -- Check Base does not exist
+          vtkCollection* listmodel2 = this->GetMRMLScene()->GetNodesByName("Base");
+          if(listmodel2->GetNumberOfItems() > 0)
          {
-           for(int i= 0; i < (clippedmodel->GetNumberOfItems()); i++)
+           // --- Delete All "Base" Model
+           for(int i=0; i < listmodel2->GetNumberOfItems(); i++)
           {
-            vtkMRMLNode* nodetodelete = reinterpret_cast<vtkMRMLNode*>(clippedmodel->GetItemAsObject(i));
-                  this->GetMRMLScene()->RemoveNode(nodetodelete);
-                  //nodetodelete->Delete();
+            vtkMRMLNode* basemodel = reinterpret_cast<vtkMRMLNode*>(listmodel2->GetItemAsObject(i));
+                  this->GetMRMLScene()->RemoveNode(basemodel);
           }
          }
-       // clippedmodel->RemoveAllItems();          
-          clippedmodel->Delete();
+          listmodel2->Delete();
 
-          // Add clipped model to Slicer scene
-          modOut->SetAndObservePolyData(poly);
-          modOut->SetAndObserveDisplayNodeID(dnode->GetID());
-          modOut->SetName("OsteoPlanClippedModel");
-          this->GetMRMLScene()->AddNode(modOut);
+       // -- Draw (or redraw if deleted) "Base" Model
+          dnode2->SetColor(clipActor2->GetProperty()->GetColor());
+          this->GetMRMLScene()->AddNode(dnode2);
+          part2->SetAndObservePolyData(clipper2->GetOutput());         
+          part2->SetAndObserveDisplayNodeID(dnode2->GetID());
+          part2->SetName("Base");
+          this->GetMRMLScene()->AddNode(part2);
 
-          // Update Selector Widget to be ready for a new clipping
-          clippedmodel = this->GetMRMLScene()->GetNodesByName("OsteoPlanClippedModel");
-          if((clippedmodel->GetNumberOfItems() == 1) && this->ModelToCutSelector)
+       // Select base if existing to prepare for next clipping and Hide original model
+          listmodel2 = this->GetMRMLScene()->GetNodesByName("Base");
+          if(this->ModelToCutSelector && listmodel2->GetNumberOfItems() == 1)
          {
-            this->ModelToCutSelector->SetSelected(modOut);
-            this->ModelToCutSelector->UpdateMenu();
-         }
-       //            alreadyclippedmodel->RemoveAllItems();
-          clippedmodel->Delete();
-
-          // Delete
-          cpoly->Delete();
-          dnode->Delete();
-          clipper->Delete();
+              vtkMRMLModelNode* basemodel = reinterpret_cast<vtkMRMLModelNode*>(listmodel2->GetItemAsObject(0));
+              vtkMRMLModelNode* originalmodel = reinterpret_cast<vtkMRMLModelNode*>(this->ModelToCutSelector->GetSelected());
+              originalmodel->GetModelDisplayNode()->SetVisibility(0); 
+              this->ModelToCutSelector->SetSelected(basemodel);
+         } 
+ 
+          // Delete ******************** 
           planes->Delete();
-          modOut->Delete();
+          clipper->Delete();
+          clipper2->Delete();
+          clipMapper->Delete();
+          clipMapper2->Delete();
+          clipActor->Delete();
+          clipActor2->Delete();
+          dnode1->Delete();
+          dnode2->Delete();
+          part1->Delete();
+          part2->Delete();
+          listmodel2->Delete();
+
      }  
      
     }
