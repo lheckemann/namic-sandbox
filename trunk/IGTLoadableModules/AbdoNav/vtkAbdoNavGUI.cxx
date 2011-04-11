1ea6027b3e94468b8e100cd000be048ffa691b42
cammann <cammann@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-04-11 22:59:55 +0000

    ENH: vtkSlicerApplicationGUI is no longer set immediately after registration has been perfomed but when DrawNeedleProjectionCheckButton is checked (since no longer required by the Logic to correctly fit image data to the slice viewers once the user switches back from oblique to non-oblique reslicing but still required to draw the needle projection)

--- trunk/IGTLoadableModules/AbdoNav/vtkAbdoNavGUI.cxx
+++ trunk/IGTLoadableModules/AbdoNav/vtkAbdoNavGUI.cxx
@@ -820,12 +820,6 @@ void vtkAbdoNavGUI::ProcessGUIEvents(vtkObject* caller, unsigned long event, voi
           {
           fnode->SetLocked(1);
           }
-        // set Slicer GUI if not already set (required for
-        // drawing the needle projection and reslicing)
-        if (this->AbdoNavLogic->GetAppGUI() == NULL)
-          {
-          this->AbdoNavLogic->SetAppGUI(this->GetApplicationGUI());
-          }
         }
       else
         {
@@ -874,6 +868,11 @@ void vtkAbdoNavGUI::ProcessGUIEvents(vtkObject* caller, unsigned long event, voi
     {
     int checked = this->DrawNeedleProjectionCheckButton->GetSelectedState();
     this->AbdoNavLogic->SetDrawNeedleProjection(checked);
+    // set Slicer GUI if not already set
+    if (checked && this->AbdoNavLogic->GetAppGUI() == NULL)
+      {
+      this->AbdoNavLogic->SetAppGUI(this->GetApplicationGUI());
+      }
     }
   else if (this->RedSliceMenuButton->GetMenu() == vtkKWMenu::SafeDownCast(caller) && event == vtkKWMenu::MenuItemInvokedEvent)
     {
