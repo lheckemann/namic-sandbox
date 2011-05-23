e317549746652566d25ee35820b6484f3e9c151c
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-05-23 14:42:34 +0000

    ENH: initial GUI implementation done.

--- IGTLoadableModules/OpenIGTLinkIF/vtkIGTLTestWindow.h
+++ IGTLoadableModules/OpenIGTLinkIF/vtkIGTLTestWindow.h
@@ -23,20 +23,14 @@
 #include "vtkOpenIGTLinkIFWin32Header.h"
 
 #include "vtkSmartPointer.h"
-//#include "vtkSlicerFiducialListWidget.h"
-//#include "vtkSlicerROIViewerWidget.h"
-//#include "vtkSlicerSecondaryViewerWidget.h"
-#include "vtkSlicerViewerWidget.h"
-#include "vtkKWMultiColumnList.h"
-#include "vtkKWMultiColumnListWithScrollbars.h"
-#include "vtkKWPushButton.h"
 #include "vtkKWTopLevel.h"
 #include "vtkSmartPointer.h"
-#include "vtkMRMLIGTLConnectorNode.h"
-#include "vtkMRMLIGTLQueryNode.h"
 
 class vtkOpenIGTLinkIFGUI;
 class vtkKWRadioButtonSet;
+class vtkKWPushButton;
+class vtkKWEntry;
+class vtkKWFrame;
 
 class VTK_OPENIGTLINKIF_EXPORT vtkIGTLTestWindow : public vtkKWTopLevel
 {
@@ -49,12 +43,6 @@ public:
   };
   //ETX
 
-  vtkGetObjectMacro(MRMLScene, vtkMRMLScene);
-  vtkSetObjectMacro(MRMLScene, vtkMRMLScene);
-
-  vtkGetObjectMacro(Connector, vtkMRMLIGTLConnectorNode);
-  vtkSetObjectMacro(Connector, vtkMRMLIGTLConnectorNode);
-
   static vtkIGTLTestWindow *New();  
   vtkTypeRevisionMacro(vtkIGTLTestWindow,vtkKWTopLevel);
   void PrintSelf(ostream& os, vtkIndent indent);
@@ -63,44 +51,13 @@ public:
     this->InGUICallbackFlag = flag;
     }
   vtkGetMacro(InGUICallbackFlag, int);
-  void SetInMRMLCallbackFlag (int flag) {
-    this->InMRMLCallbackFlag = flag;
-  }
-  vtkGetMacro(InMRMLCallbackFlag, int);
-
-  void SetAndObserveMRMLScene ( vtkMRMLScene *mrml )
-    {
-    vtkMRMLScene *oldValue = this->MRMLScene;
-    this->MRMLObserverManager->SetAndObserveObject ( vtkObjectPointer( &this->MRMLScene), (vtkObject*)mrml );
-    if ( oldValue != this->MRMLScene )
-      {
-      this->InvokeEvent (vtkCommand::ModifiedEvent);
-      }
-    }
-
-  void SetAndObserveMRMLSceneEvents ( vtkMRMLScene *mrml, vtkIntArray *events )
-    {
-    vtkObject *oldValue = this->MRMLScene;
-    this->MRMLObserverManager->SetAndObserveObjectEvents ( vtkObjectPointer( &this->MRMLScene), mrml, events );
-    if ( oldValue != this->MRMLScene )
-      {
-      this->InvokeEvent (vtkCommand::ModifiedEvent);
-      }
-    }
 
   void DisplayOnWindow();
-  void SetOpenIGTLinkIFGUI(vtkOpenIGTLinkIFGUI* moduleGUI)
-  {
-    this->ModuleGUI = moduleGUI;
-  }
 
 protected:
 
   vtkIGTLTestWindow();
   ~vtkIGTLTestWindow();  
-
-  static void MRMLCallback(vtkObject *caller,
-                           unsigned long eid, void *clientData, void *callData );
 
   static void GUICallback(vtkObject *caller,
                           unsigned long eid, void *clientData, void *callData );
@@ -108,11 +65,13 @@ protected:
   
   virtual void CreateWidget();
   virtual void ProcessGUIEvents(vtkObject *caller, unsigned long event, void *callData);
-  virtual void ProcessMRMLEvents(vtkObject *caller, unsigned long event, void *callData);
   virtual void AddGUIObservers();
   virtual void RemoveGUIObservers();
-
-  void    SwitchMode(int mode);
+  
+  void SwitchMode(int mode);
+  void StartServer(int port, float rate);
+  void StopServer();
+  
   
  protected:
   
@@ -120,42 +79,30 @@ protected:
   // GUI widgets and Callbacks
   //----------------------------------------------------------------
   vtkKWFrame* MainFrame;
-  vtkSlicerViewerWidget* ViewerWidget;
   
   vtkKWRadioButtonSet* ModeButtonSet;
-
-  vtkKWMultiColumnListWithScrollbars* RemoteDataList;
+  vtkKWEntry*          PortEntry;
+  vtkKWEntry*          FrameRateEntry;
 
   vtkKWPushButton* StartTrackingButton;
   vtkKWPushButton* StopTrackingButton;
   vtkKWPushButton* CloseButton;
 
-  vtkCallbackCommand *MRMLCallbackCommand;  
   vtkCallbackCommand *GUICallbackCommand;
-  vtkObserverManager *MRMLObserverManager;
+
   int InGUICallbackFlag;
-  int InMRMLCallbackFlag;
   int IsObserverAddedFlag;
   
   //----------------------------------------------------------------
   // Logic Values
   //----------------------------------------------------------------
-  bool MultipleMonitorsAvailable; 
-  int  WindowPosition[2]; // virtual screen position in pixels
-  int  WindowSize[2]; // virtual screen size in pixels
+  bool   MultipleMonitorsAvailable; 
+  int    WindowPosition[2]; // virtual screen position in pixels
+  int    WindowSize[2]; // virtual screen size in pixels
   
-  int  Mode;
-  
-
-  vtkOpenIGTLinkIFGUI* ModuleGUI;
-  vtkMRMLScene* MRMLScene;
-  
-  vtkMRMLIGTLConnectorNode* Connector;
-  
-  vtkMRMLIGTLQueryNode* TrackingDataQueryNode;
-  //BTX
-  std::list<vtkMRMLIGTLQueryNode*> ImageQueryNodeList;
-  //ETX
+  int    Mode;
+  int    DefaultPort;
+  double DefaultFrameRate;
 
  private:
   vtkIGTLTestWindow(const vtkIGTLTestWindow&);
