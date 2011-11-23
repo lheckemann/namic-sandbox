86ac76dbf3b6cd7b0fd1ce9f7645f546f54463e2
lchauvin <lchauvin@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2011-11-23 23:46:34 +0000

    ENH: Add piston and  cylinder (still issue to fix with cylinder and place anchors)

--- trunk/IGTLoadableModules/DistractorModeling/vtkDistractorModelingLogic.cxx
+++ trunk/IGTLoadableModules/DistractorModeling/vtkDistractorModelingLogic.cxx
@@ -24,6 +24,8 @@
 #include "vtkMRMLLinearTransformNode.h"
 #include "vtkMatrix4x4.h"
 
+#include <math.h>
+
 vtkCxxRevisionMacro(vtkDistractorModelingLogic, "$Revision: 1.9.12.1 $");
 vtkStandardNewMacro(vtkDistractorModelingLogic);
 
@@ -78,40 +80,126 @@ void vtkDistractorModelingLogic::UpdateAll()
 }
 
 //---------------------------------------------------------------------------
-void vtkDistractorModelingLogic::MoveSlider(vtkMRMLModelNode* Slider, double value, vtkMRMLLinearTransformNode* SliderTransformationNode)
+void vtkDistractorModelingLogic::MoveSlider(vtkMRMLModelNode* Slider, vtkMRMLModelNode* Piston, vtkMRMLModelNode* Cylinder, double value, vtkMRMLLinearTransformNode* SliderTransformationNode, vtkMRMLLinearTransformNode* PistonTransformationNode, vtkMRMLLinearTransformNode* CylinderTransformationNode)
 {
+  //--------------------
+  // Slider
+
   double SliderCenter[3];
   Slider->GetPolyData()->GetCenter(SliderCenter);
 
-  vtkTransform* translation = vtkTransform::New();
-  translation->Translate(SliderCenter[0]+47.70,SliderCenter[1],SliderCenter[2]-15);
+  vtkTransform* SliderTranslation = vtkTransform::New();
+  SliderTranslation->Translate(SliderCenter[0]+47.70,SliderCenter[1],SliderCenter[2]-15);
 
-  vtkTransform* rotation = vtkTransform::New();
-  rotation->RotateY(value);
+  vtkTransform* SliderRotation = vtkTransform::New();
+  SliderRotation->RotateY(value);
 
-  vtkMatrix4x4* transformationMatrix = vtkMatrix4x4::New();
-  transformationMatrix->Identity();
+  vtkMatrix4x4* SliderTransformationMatrix = vtkMatrix4x4::New();
+  SliderTransformationMatrix->Identity();
 
-  vtkTransform* invertTranslation = vtkTransform::New();
-  invertTranslation->Translate(-SliderCenter[0]-47.70, -SliderCenter[1], -SliderCenter[2]+15); // 5cm curvature, z = 15, so (x = sqrt(r^2 - z^2) with r in mm => x = -47.70)
-  invertTranslation->PostMultiply();
-  invertTranslation->Concatenate(rotation);
-  invertTranslation->PostMultiply();
-  invertTranslation->Concatenate(translation);
-  invertTranslation->GetMatrix(transformationMatrix);
+  vtkTransform* SliderInvertTranslation = vtkTransform::New();
+  SliderInvertTranslation->Translate(-SliderCenter[0]-47.70, -SliderCenter[1], -SliderCenter[2]+15); // 5cm curvature, z = 15, so (x = sqrt(r^2 - z^2) with r in mm => x = -47.70)
+  SliderInvertTranslation->PostMultiply();
+  SliderInvertTranslation->Concatenate(SliderRotation);
+  SliderInvertTranslation->PostMultiply();
+  SliderInvertTranslation->Concatenate(SliderTranslation);
+  SliderInvertTranslation->GetMatrix(SliderTransformationMatrix);
 
-  SliderTransformationNode->SetAndObserveMatrixTransformToParent(transformationMatrix);
+  SliderTransformationNode->SetAndObserveMatrixTransformToParent(SliderTransformationMatrix);
 
   // Drop transformation node under other transforms (if exists)
   if(Slider->GetParentTransformNode() && Slider->GetParentTransformNode()!=SliderTransformationNode)
     {
-      SliderTransformationNode->SetAndObserveTransformNodeID(Slider->GetParentTransformNode()->GetID());
+    SliderTransformationNode->SetAndObserveTransformNodeID(Slider->GetParentTransformNode()->GetID());
     }
 
-  translation->Delete();
-  rotation->Delete();
-  invertTranslation->Delete();
-  transformationMatrix->Delete();
+  //--------------------
+  // Piston
+
+  double PistonCenter[3] = {-42.7,0,-21};
+
+  double newSliderCenterX = SliderTransformationMatrix->GetElement(0,0)*SliderCenter[0]+SliderTransformationMatrix->GetElement(0,1)*SliderCenter[1]+SliderTransformationMatrix->GetElement(0,2)*SliderCenter[2]+SliderTransformationMatrix->GetElement(0,3)*1;
+  double newSliderCenterY = SliderTransformationMatrix->GetElement(2,0)*SliderCenter[0]+SliderTransformationMatrix->GetElement(2,1)*SliderCenter[1]+SliderTransformationMatrix->GetElement(2,2)*SliderCenter[2]+SliderTransformationMatrix->GetElement(2,3)*1;
+
+  vtkTransform* PistonTranslation = vtkTransform::New();
+  PistonTranslation->Translate(PistonCenter[0],PistonCenter[1],PistonCenter[2]);
+
+  double dx = -newSliderCenterX+PistonCenter[0];
+  double dy = -newSliderCenterY+PistonCenter[2];
+
+  double PistonRotationAngle_rad = atan2(dx,dy);
+  double PistonRotationAngle_deg = PistonRotationAngle_rad*180/M_PI;
+  vtkTransform* PistonRotation = vtkTransform::New();
+  PistonRotation->RotateY(PistonRotationAngle_deg);
+
+  vtkMatrix4x4* PistonTransformationMatrix = vtkMatrix4x4::New();
+  PistonTransformationMatrix->Identity();
+
+  vtkTransform* PistonInvertTranslation = vtkTransform::New();
+  PistonInvertTranslation->Translate(-PistonCenter[0],-PistonCenter[1],-PistonCenter[2]);
+  PistonInvertTranslation->PostMultiply();
+  PistonInvertTranslation->Concatenate(PistonRotation);
+  PistonInvertTranslation->PostMultiply();
+  PistonInvertTranslation->Concatenate(PistonTranslation);
+  PistonInvertTranslation->GetMatrix(PistonTransformationMatrix);
+
+  PistonTransformationNode->SetAndObserveMatrixTransformToParent(PistonTransformationMatrix);
+
+  // Drop transformation node under other transforms (if exists)
+  if(Piston->GetParentTransformNode() && Piston->GetParentTransformNode()!=PistonTransformationNode)
+    {
+    PistonTransformationNode->SetAndObserveTransformNodeID(Piston->GetParentTransformNode()->GetID());
+    }
+
+
+  //--------------------
+  // Cylinder
+
+  double CylinderCenter[3];
+  Cylinder->GetPolyData()->GetCenter(CylinderCenter);
+
+  vtkTransform* CylinderTranslation = vtkTransform::New();
+  CylinderTranslation->Translate(newSliderCenterX,0.5,newSliderCenterY);
+
+  vtkTransform* CylinderRotation = vtkTransform::New();
+  CylinderRotation->RotateY(PistonRotationAngle_deg);
+
+  vtkMatrix4x4* CylinderTransformationMatrix = vtkMatrix4x4::New();
+  CylinderTransformationMatrix->Identity();
+
+  vtkTransform* CylinderInvertTranslation = vtkTransform::New();
+  CylinderInvertTranslation->Translate(-newSliderCenterX,-0.5,-newSliderCenterY);
+  CylinderInvertTranslation->PostMultiply();
+  CylinderInvertTranslation->Concatenate(CylinderRotation);
+  CylinderInvertTranslation->PostMultiply();
+  CylinderInvertTranslation->Concatenate(CylinderTranslation);
+  CylinderInvertTranslation->GetMatrix(CylinderTransformationMatrix);
+
+  CylinderTransformationNode->SetAndObserveMatrixTransformToParent(CylinderTransformationMatrix);
+
+  // Drop transformation node under other transforms (if exists)
+  if(Cylinder->GetParentTransformNode() && Cylinder->GetParentTransformNode()!=CylinderTransformationNode)
+    {
+    CylinderTransformationNode->SetAndObserveTransformNodeID(Cylinder->GetParentTransformNode()->GetID());
+    }
+
+
+
+
+  SliderTranslation->Delete();
+  SliderRotation->Delete();
+  SliderInvertTranslation->Delete();
+  SliderTransformationMatrix->Delete();
+
+  PistonTranslation->Delete();
+  PistonRotation->Delete();
+  PistonInvertTranslation->Delete();
+  PistonTransformationMatrix->Delete();
+
+  CylinderTranslation->Delete();
+  CylinderRotation->Delete();
+  CylinderInvertTranslation->Delete();
+  CylinderTransformationMatrix->Delete();
 }
 
 
