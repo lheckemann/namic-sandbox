f1dffa5f348eeb58ae82671288db1f6bdbe0076c
tokuda <tokuda@5e132c66-7cf4-0310-b4ca-cd4a7079c2d8>
2012-05-04 20:14:46 +0000

    COMP: removing compile warninigs

--- trunk/SNRMRRobotController/newmat/solution.h
+++ trunk/SNRMRRobotController/newmat/solution.h
@@ -23,6 +23,7 @@ class R1_R1
    // constructor to put in additional parameter values (e.g. that will not
    // vary during a solve)
 
+public:
    virtual ~R1_R1();
 protected:
    Real x;                             // Current x value
